8a9add1a7f9a2e7def709e79818ca91e25b71cb5
Eric Schweitz <eschweitz@nvidia.com>
2023-04-05 13:44:57 -0700

    Add regression test for std::pow lowering.

---
+++ test/AST-Quake/pow.cpp
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2023 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+// RUN: cudaq-quake %s | FileCheck %s
+
+#include <cudaq.h>
+
+__qpu__ float magic_func(int i, int j, float f, float g) {
+  int k = std::pow(i, j);
+  float h = std::pow(f, k);
+  return std::pow(g, h);
+}
+
+// CHECK-LABEL:   func.func @__nvqpp__mlirgen__function_magic_func._Z10magic_funciiff(
+// CHECK:           %[[VAL_10:.*]] = math.ipowi %{{.*}}, %{{.*}} : i32
+// CHECK:           %[[VAL_11:.*]] = arith.sitofp %[[VAL_10]] : i32 to f64
+// CHECK:           %[[VAL_16:.*]] = math.fpowi %{{.*}}, %{{.*}} : f32, i32
+// CHECK:           %[[VAL_20:.*]] = math.powf %{{.*}}, %{{.*}} : f32
+// CHECK:         }
+
