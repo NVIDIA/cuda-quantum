349eb968984e066293833f90fcba1e5e1e2771e0
Bettina Heim <heimb@outlook.com>
2023-04-14 17:39:29 +0000

    adding llvm-ar to the list of components

--- scripts/build_llvm.sh
+++ scripts/build_llvm.sh
@@ -84,7 +84,7 @@ if [ -z "${llvm_projects##*lld;*}" ]; then
 fi
 echo "- including general tools and components"
 llvm_components+="cmake-exports;llvm-headers;llvm-libraries;"
-llvm_components+="llvm-config;llc;FileCheck;count;not;"
+llvm_components+="llvm-config;llvm-ar;llc;FileCheck;count;not;"
 
 if [ "$(echo ${projects[*]} | xargs)" != "" ]; then
   echo "- including additional projects "$(echo "${projects[*]}" | xargs | tr ' ' ',')
