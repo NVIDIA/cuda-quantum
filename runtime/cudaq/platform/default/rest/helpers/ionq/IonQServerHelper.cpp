4a7c4b1b2b8368215ffac5cc9d5d86b4fa635e29
Spencer Churchill <churchill@ionq.co>
2023-05-17 10:01:33 -0700

    remove cout

--- runtime/cudaq/platform/default/rest/helpers/ionq/IonQServerHelper.cpp
+++ runtime/cudaq/platform/default/rest/helpers/ionq/IonQServerHelper.cpp
@@ -10,7 +10,6 @@
 #include "common/ServerHelper.h"
 #include "cudaq/utils/cudaq_utils.h"
 #include <fstream>
-#include <iostream>
 #include <thread>
 
 namespace cudaq {
@@ -50,8 +49,6 @@ void IonQServerHelper::initialize(BackendConfig config) {
   backendConfig["token"] = "giveme403";
   backendConfig["job_path"] =
       backendConfig["url"] + '/' + backendConfig["version"] + "/jobs";
-
-  std::cout << "IonQ Initialized" << std::endl;
 }
 
 ServerJobPayload
@@ -66,8 +63,6 @@ IonQServerHelper::createJob(std::vector<KernelExecution> &circuitCodes) {
   job["shots"] = static_cast<int>(shots);
   job["input"] = {{"format", "qir"}, {"data", circuitCodes.front().code}};
 
-  std::cout << "Creating Job" << std::endl;
-
   return std::make_tuple(backendConfig.at("job_path"), getHeaders(),
                          std::vector<ServerMessage>{job});
 }
@@ -76,7 +71,6 @@ std::string IonQServerHelper::extractJobId(ServerMessage &postResponse) {
   if (!postResponse.contains("id"))
     throw std::runtime_error("ServerMessage doesn't contain 'id' key.");
 
-  std::cout << "Extracting Job ID" << std::endl;
   return postResponse.at("id");
 }
 
@@ -88,8 +82,6 @@ std::string IonQServerHelper::constructGetJobPath(ServerMessage &postResponse) {
   if (!keyExists("url"))
     throw std::runtime_error("Key 'url' doesn't exist in backendConfig.");
 
-  std::cout << postResponse << std::endl;
-
   return backendConfig.at("url") +
          postResponse.at("results_url").get<std::string>();
 }
@@ -97,8 +89,6 @@ std::string IonQServerHelper::constructGetJobPath(ServerMessage &postResponse) {
 std::string IonQServerHelper::constructGetJobPath(std::string &jobId) {
   if (!keyExists("job_path"))
     throw std::runtime_error("Key 'job_path' doesn't exist in backendConfig.");
-
-  std::cout << jobId << std::endl;
 
   return backendConfig.at("job_path") + "?id=" + jobId;
 }
@@ -117,8 +107,6 @@ IonQServerHelper::constructGetResultsPath(ServerMessage &resultsGetPath) {
   if (!keyExists("url"))
     throw std::runtime_error("Key 'url' doesn't exist in backendConfig.");
 
-  std::cout << resultsGetPath << std::endl;
-
   return backendConfig.at("url") + jobs[0].at("results_url").get<std::string>();
 }
 
@@ -126,14 +114,11 @@ std::string IonQServerHelper::constructGetResultsPath(std::string &jobId) {
   if (!keyExists("job_path"))
     throw std::runtime_error("Key 'job_path' doesn't exist in backendConfig.");
 
-  std::cout << jobId << std::endl;
-
   return backendConfig.at("job_path") + jobId + "/results";
 }
 
 ServerMessage IonQServerHelper::getResults(std::string &resultsGetPath) {
   auto headers = getHeaders();
-  std::cout << "Getting results" << std::endl;
   return client.get(resultsGetPath, "", headers);
 }
 
@@ -146,8 +131,6 @@ bool IonQServerHelper::jobIsDone(ServerMessage &getJobResponse) {
   if (jobs.empty() || !jobs[0].contains("status"))
     throw std::runtime_error(
         "ServerMessage doesn't contain 'status' key in the first job.");
-
-  std::cout << getJobResponse << std::endl;
 
   return jobs[0].at("status").get<std::string>() == "completed";
 }
@@ -165,8 +148,6 @@ IonQServerHelper::processResults(ServerMessage &postJobResponse) {
     counts[key] = count;
   }
 
-  std::cout << postJobResponse << std::endl;
-
   // construct an ExecutionResult with the counts
   cudaq::ExecutionResult executionResult(counts);
   return cudaq::sample_result(executionResult);
@@ -175,8 +156,6 @@ IonQServerHelper::processResults(ServerMessage &postJobResponse) {
 RestHeaders IonQServerHelper::getHeaders() {
   if (!keyExists("token") || !keyExists("user_agent"))
     throw std::runtime_error("Key doesn't exist in backendConfig.");
-
-  std::cout << "Getting Request Headers" << std::endl;
 
   RestHeaders headers;
   headers["Authorization"] = "apiKey " + backendConfig.at("token");
