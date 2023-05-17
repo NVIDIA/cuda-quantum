6e796fcaf6dfe17126753e0c24eaf24953fe320e
Alex McCaskey <amccaskey@nvidia.com>
2023-05-17 19:50:12 +0000

    address pr comments
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>

--- python/tests/domains/test_qnn.py
+++ python/tests/domains/test_qnn.py
@@ -14,30 +14,32 @@ import cudaq
 
 
 def test_simpleObserveN_QNN():
-    n_qubits = 2
-    n_samples = 5000
+    qubit_count = 2
+    samples_count = 5000
     h = cudaq.spin.z(0)
-    n_parameters = n_qubits * 3
+    parameters_count = qubit_count * 3
     parameters = np.random.default_rng(13).uniform(low=0,
                                                    high=1,
-                                                   size=(n_samples,
-                                                         n_parameters))
+                                                   size=(samples_count,
+                                                         parameters_count))
     np.random.seed(1)
 
+    # Build up a parameterized kernel composed of a layer of
+    # Rx, Ry, Rz, then CZs.
     kernel, params = cudaq.make_kernel(list)
-    qubits = kernel.qalloc(n_qubits)
-    qubits_list = list(range(n_qubits))
-    for i in range(n_qubits):
+    qubits = kernel.qalloc(qubit_count)
+    qubits_list = list(range(qubit_count))
+    for i in range(qubit_count):
         kernel.rx(params[i], qubits[i])
-    for i in range(n_qubits):
-        kernel.ry(params[i + n_qubits], qubits[i])
-    for i in range(n_qubits):
-        kernel.rz(params[i + n_qubits * 2], qubits[i])
+    for i in range(qubit_count):
+        kernel.ry(params[i + qubit_count], qubits[i])
+    for i in range(qubit_count):
+        kernel.rz(params[i + qubit_count * 2], qubits[i])
     for q1, q2 in zip(qubits_list[0::2], qubits_list[1::2]):
         kernel.cz(qubits[q1], qubits[q2])
 
     exp_vals = cudaq.observe_n(kernel, h, parameters)
-    assert len(exp_vals) == n_samples 
+    assert len(exp_vals) == samples_count
     data = np.asarray([e.expectation_z() for e in exp_vals])
     # Test a collection of computed exp vals.
     assert np.isclose(data[0], 0.44686141)
