b63422369fb6193e5d0ed73df60ff572eb9bcfce
Alex McCaskey <amccaskey@nvidia.com>
2023-05-17 19:45:34 +0000

    got tests passing
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>

--- python/tests/backends/test_Quantinuum.py
+++ python/tests/backends/test_Quantinuum.py
@@ -88,41 +88,41 @@ def test_quantinuum_sample():
     assert ('00' in counts)
     assert ('11' in counts)
 
-# def test_quantinuum_observe():
-#     # Create the parameterized ansatz
-#     kernel, theta = cudaq.make_kernel(float)
-#     qreg = kernel.qalloc(2)
-#     kernel.x(qreg[0])
-#     kernel.ry(theta, qreg[1])
-#     kernel.cx(qreg[1], qreg[0])
+def test_quantinuum_observe():
+    # Create the parameterized ansatz
+    kernel, theta = cudaq.make_kernel(float)
+    qreg = kernel.qalloc(2)
+    kernel.x(qreg[0])
+    kernel.ry(theta, qreg[1])
+    kernel.cx(qreg[1], qreg[0])
 
-#     # Define its spin Hamiltonian.
-#     hamiltonian = 5.907 - 2.1433 * spin.x(0) * spin.x(1) - 2.1433 * spin.y(
-#         0) * spin.y(1) + .21829 * spin.z(0) - 6.125 * spin.z(1)
+    # Define its spin Hamiltonian.
+    hamiltonian = 5.907 - 2.1433 * spin.x(0) * spin.x(1) - 2.1433 * spin.y(
+        0) * spin.y(1) + .21829 * spin.z(0) - 6.125 * spin.z(1)
     
-#     # Run the observe task on quantinuum synchronously
-#     res = cudaq.observe(kernel, hamiltonian, .59)
-#     want_expectation_value = -1.71
-#     assert assert_close(want_expectation_value, res.expectation_z(), 1e-2)
+    # Run the observe task on quantinuum synchronously
+    res = cudaq.observe(kernel, hamiltonian, .59)
+    want_expectation_value = -1.71
+    assert assert_close(want_expectation_value, res.expectation_z(), 1e-2)
 
-#     # Launch it asynchronously, enters the job into the queue
-#     future = cudaq.observe_async(kernel, hamiltonian, .59)
-#     # Retrieve the results (since we're on a mock server)
-#     res = future.get()
-#     assert assert_close(want_expectation_value, res.expectation_z(), 1e-2)
+    # Launch it asynchronously, enters the job into the queue
+    future = cudaq.observe_async(kernel, hamiltonian, .59)
+    # Retrieve the results (since we're on a mock server)
+    res = future.get()
+    assert assert_close(want_expectation_value, res.expectation_z(), 1e-2)
 
-#     # Launch the job async, job goes in the queue, and 
-#     # we're free to dump the future to file
-#     future = cudaq.observe_async(kernel, hamiltonian, .59)
-#     print(future)
-#     futureAsString = str(future)
+    # Launch the job async, job goes in the queue, and 
+    # we're free to dump the future to file
+    future = cudaq.observe_async(kernel, hamiltonian, .59)
+    print(future)
+    futureAsString = str(future)
 
-#     # Later you can come back and read it in
-#     # You must provide the spin_op so we can reconstruct 
-#     # the results from the term job ids. 
-#     futureReadIn = cudaq.AsyncObserveResult(futureAsString, hamiltonian)
-#     res = futureReadIn.get()
-#     assert assert_close(want_expectation_value, res.expectation_z(), 1e-2)
+    # Later you can come back and read it in
+    # You must provide the spin_op so we can reconstruct 
+    # the results from the term job ids. 
+    futureReadIn = cudaq.AsyncObserveResult(futureAsString, hamiltonian)
+    res = futureReadIn.get()
+    assert assert_close(want_expectation_value, res.expectation_z(), 1e-2)
 
 # leave for gdb debugging
 if __name__ == "__main__":
