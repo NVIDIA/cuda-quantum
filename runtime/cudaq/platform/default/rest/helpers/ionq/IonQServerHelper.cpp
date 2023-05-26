4498f54c3c8e842cb3d4b696f856fdde7f97e1be
Spencer Churchill <churchill@ionq.co>
2023-05-25 23:39:05 -0700

    add comments & tests

--- runtime/cudaq/platform/default/rest/helpers/ionq/IonQServerHelper.cpp
+++ runtime/cudaq/platform/default/rest/helpers/ionq/IonQServerHelper.cpp
@@ -14,78 +14,132 @@
 
 namespace cudaq {
 
+/// @brief The IonQServerHelper class extends the ServerHelper class to handle
+/// interactions with the IonQ server for submitting and retrieving quantum
+/// computation jobs.
 class IonQServerHelper : public ServerHelper {
 private:
+  /// @brief RestClient used for HTTP requests.
   RestClient client;
+
+  /// @brief Helper method to retrieve the value of an environment variable.
   std::string getEnvVar(const std::string &key) const;
+
+  /// @brief Helper method to check if a key exists in the configuration.
   bool keyExists(const std::string &key) const;
 
 public:
+  /// @brief Returns the name of the server helper.
   const std::string name() const override { return "ionq"; }
+
+  /// @brief Returns the headers for the server requests.
   RestHeaders getHeaders() override;
+
+  /// @brief Initializes the server helper with the provided backend
+  /// configuration.
   void initialize(BackendConfig config) override;
+
+  /// @brief Creates a quantum computation job using the provided kernel
+  /// executions and returns the corresponding payload.
   ServerJobPayload
   createJob(std::vector<KernelExecution> &circuitCodes) override;
+
+  /// @brief Extracts the job ID from the server's response to a job submission.
   std::string extractJobId(ServerMessage &postResponse) override;
+
+  /// @brief Constructs the URL for retrieving a job based on the server's
+  /// response to a job submission.
   std::string constructGetJobPath(ServerMessage &postResponse) override;
+
+  /// @brief Constructs the URL for retrieving a job based on a job ID.
   std::string constructGetJobPath(std::string &jobId) override;
+
+  /// @brief Constructs the URL for retrieving the results of a job based on the
+  /// server's response to a job submission.
   std::string constructGetResultsPath(ServerMessage &postResponse);
+
+  /// @brief Constructs the URL for retrieving the results of a job based on a
+  /// job ID.
   std::string constructGetResultsPath(std::string &jobId);
+
+  /// @brief Retrieves the results of a job using the provided path.
   ServerMessage getResults(std::string &resultsGetPath);
+
+  /// @brief Checks if a job is done based on the server's response to a job
+  /// retrieval request.
   bool jobIsDone(ServerMessage &getJobResponse) override;
+
+  /// @brief Processes the server's response to a job retrieval request and
+  /// maps the results back to sample results.
   cudaq::sample_result processResults(ServerMessage &postJobResponse) override;
 };
 
+// Initialize the IonQ server helper with a given backend configuration
 void IonQServerHelper::initialize(BackendConfig config) {
+  // Move the passed config into the member variable backendConfig
   backendConfig = std::move(config);
+  // Set the necessary configuration variables for the IonQ API
   backendConfig["url"] = "https://api.ionq.co";
   backendConfig["version"] = "v0.3";
   backendConfig["user_agent"] = "cudaq/0.3.0";
   backendConfig["target"] = "simulator";
   backendConfig["qubits"] = 29;
+  // Retrieve the API key from the environment variables
   backendConfig["token"] = getEnvVar("IONQ_API_KEY");
+  // Construct the API job path
   backendConfig["job_path"] =
       backendConfig["url"] + '/' + backendConfig["version"] + "/jobs";
 }
 
+// Retrieve an environment variable
 std::string IonQServerHelper::getEnvVar(const std::string &key) const {
+  // Get the environment variable
   const char *env_var = std::getenv(key.c_str());
+  // If the variable is not set, throw an exception
   if (env_var == nullptr) {
-    // Handle the case where the environment variable is not set
     throw std::runtime_error(key + " environment variable is not set.");
   }
+  // Return the variable as a string
   return std::string(env_var);
 }
 
+// Check if a key exists in the backend configuration
 bool IonQServerHelper::keyExists(const std::string &key) const {
-  // check if a key exists in the map
   return backendConfig.find(key) != backendConfig.end();
 }
 
+// Create a job for the IonQ quantum computer
 ServerJobPayload
 IonQServerHelper::createJob(std::vector<KernelExecution> &circuitCodes) {
-  // Check keys existence before accessing them
+  // Check if the necessary keys exist in the configuration
   if (!keyExists("target") || !keyExists("qubits") || !keyExists("job_path"))
     throw std::runtime_error("Key doesn't exist in backendConfig.");
 
+  // Construct the job message
   ServerMessage job;
   job["target"] = backendConfig.at("target");
   job["qubits"] = backendConfig.at("qubits");
   job["shots"] = static_cast<int>(shots);
   job["input"] = {{"format", "qir"}, {"data", circuitCodes.front().code}};
 
+  // Return a tuple containing the job path, headers, and the job message
   return std::make_tuple(backendConfig.at("job_path"), getHeaders(),
                          std::vector<ServerMessage>{job});
 }
 
+// From a server message, extract the job ID
 std::string IonQServerHelper::extractJobId(ServerMessage &postResponse) {
+  // If the response does not contain the key 'id', throw an exception
   if (!postResponse.contains("id"))
     throw std::runtime_error("ServerMessage doesn't contain 'id' key.");
 
+  // Return the job ID from the response
   return postResponse.at("id");
 }
 
+// Construct the path to get a job
 std::string IonQServerHelper::constructGetJobPath(ServerMessage &postResponse) {
+  // Check if the necessary keys exist in the response and the configuration
   if (!postResponse.contains("results_url"))
     throw std::runtime_error(
         "ServerMessage doesn't contain 'results_url' key.");
@@ -93,23 +147,28 @@ std::string IonQServerHelper::constructGetJobPath(ServerMessage &postResponse) {
   if (!keyExists("url"))
     throw std::runtime_error("Key 'url' doesn't exist in backendConfig.");
 
+  // Return the job path
   return backendConfig.at("url") +
          postResponse.at("results_url").get<std::string>();
 }
 
+// Overloaded version of constructGetJobPath for jobId input
 std::string IonQServerHelper::constructGetJobPath(std::string &jobId) {
   if (!keyExists("job_path"))
     throw std::runtime_error("Key 'job_path' doesn't exist in backendConfig.");
 
+  // Return the job path
   return backendConfig.at("job_path") + "?id=" + jobId;
 }
 
+// Construct the path to get the results of a job
 std::string
-IonQServerHelper::constructGetResultsPath(ServerMessage &resultsGetPath) {
-  if (!resultsGetPath.contains("jobs"))
+IonQServerHelper::constructGetResultsPath(ServerMessage &postResponse) {
+  // Check if the necessary keys exist in the response and the configuration
+  if (!postResponse.contains("jobs"))
     throw std::runtime_error("ServerMessage doesn't contain 'jobs' key.");
 
-  auto &jobs = resultsGetPath.at("jobs");
+  auto &jobs = postResponse.at("jobs");
 
   if (jobs.empty() || !jobs[0].contains("results_url"))
     throw std::runtime_error(
@@ -118,22 +177,28 @@ IonQServerHelper::constructGetResultsPath(ServerMessage &resultsGetPath) {
   if (!keyExists("url"))
     throw std::runtime_error("Key 'url' doesn't exist in backendConfig.");
 
+  // Return the results path
   return backendConfig.at("url") + jobs[0].at("results_url").get<std::string>();
 }
 
+// Overloaded version of constructGetResultsPath for jobId input
 std::string IonQServerHelper::constructGetResultsPath(std::string &jobId) {
   if (!keyExists("job_path"))
     throw std::runtime_error("Key 'job_path' doesn't exist in backendConfig.");
 
+  // Return the results path
   return backendConfig.at("job_path") + jobId + "/results";
 }
 
+// Get the results from a given path
 ServerMessage IonQServerHelper::getResults(std::string &resultsGetPath) {
-  auto headers = getHeaders();
-  return client.get(resultsGetPath, "", headers);
+  // Return the results from the client
+  return client.get(resultsGetPath, "", getHeaders());
 }
 
+// Check if a job is done
 bool IonQServerHelper::jobIsDone(ServerMessage &getJobResponse) {
+  // Check if the necessary keys exist in the response
   if (!getJobResponse.contains("jobs"))
     throw std::runtime_error("ServerMessage doesn't contain 'jobs' key.");
 
@@ -143,15 +208,20 @@ bool IonQServerHelper::jobIsDone(ServerMessage &getJobResponse) {
     throw std::runtime_error(
         "ServerMessage doesn't contain 'status' key in the first job.");
 
+  // Return whether the job is completed
   return jobs[0].at("status").get<std::string>() == "completed";
 }
 
+// Process the results from a job
 cudaq::sample_result
 IonQServerHelper::processResults(ServerMessage &postJobResponse) {
+  // Construct the path to get the results
   auto resultsGetPath = constructGetResultsPath(postJobResponse);
+  // Get the results
   auto results = getResults(resultsGetPath);
   cudaq::CountsDictionary counts;
 
+  // Process the results
   for (const auto &element : results.items()) {
     std::string key = element.key();
     double value = element.value().get<double>();
@@ -159,22 +229,28 @@ IonQServerHelper::processResults(ServerMessage &postJobResponse) {
     counts[key] = count;
   }
 
-  // construct an ExecutionResult with the counts
+  // Create an execution result
   cudaq::ExecutionResult executionResult(counts);
+  // Return a sample result
   return cudaq::sample_result(executionResult);
 }
 
+// Get the headers for the API requests
 RestHeaders IonQServerHelper::getHeaders() {
+  // Check if the necessary keys exist in the configuration
   if (!keyExists("token") || !keyExists("user_agent"))
     throw std::runtime_error("Key doesn't exist in backendConfig.");
 
+  // Construct the headers
   RestHeaders headers;
   headers["Authorization"] = "apiKey " + backendConfig.at("token");
   headers["Content-Type"] = "application/json";
   headers["User-Agent"] = backendConfig.at("user_agent");
+  // Return the headers
   return headers;
 }
 
 } // namespace cudaq
 
+// Register the IonQ server helper in the CUDAQ server helper factory
 CUDAQ_REGISTER_TYPE(cudaq::ServerHelper, cudaq::IonQServerHelper, ionq)
---
+++ utils/mock_qpu/ionq/mock_ionq.py
@@ -0,0 +1,113 @@
+# ============================================================================ #
+# Copyright (c) 2022 - 2023 NVIDIA Corporation & Affiliates.                   #
+# All rights reserved.                                                         #
+#                                                                              #
+# This source code and the accompanying materials are made available under     #
+# the terms of the Apache License 2.0 which accompanies this distribution.     #
+# ============================================================================ #
+
+from fastapi import FastAPI, Request, HTTPException, Header
+from typing import Optional, Union
+import uvicorn, uuid, json, base64
+from pydantic import BaseModel
+from llvmlite import binding as llvm
+
+# Define the REST Server App
+app = FastAPI()
+
+
+# Jobs look like the following type
+class Job(BaseModel):
+    name: str
+    program: str
+    count: int
+
+
+# Keep track of Job Ids to their Names
+createdJobs = {}
+
+# Could how many times the client has requested the Job
+countJobGetRequests = 0
+
+# Global holding the number of shots
+shots = 100
+
+
+# Here we test that the login endpoint works
+@app.post("/login")
+async def login(token: Union[str, None] = Header(alias="Authorization",
+                                                 default=None)):
+    if 'token' == None:
+        raise HTTPException(status_code(401), detail="Credentials not provided")
+    return {"id-token": "hello", "refresh-token": "refreshToken"}
+
+
+# Here we expose a way to post jobs,
+# Must have a Access Token, Job Program must be Adaptive Profile
+# with EntryPoint tag
+@app.post("/job")
+async def postJob(job: Job,
+                  token: Union[str, None] = Header(alias="Authorization",
+                                                   default=None)):
+    global createdJobs, shots
+
+    if 'token' == None:
+        raise HTTPException(status_code(401), detail="Credentials not provided")
+
+    print('Posting job with name = ', job.name)
+    name = job.name
+    newId = str(uuid.uuid4())
+    createdJobs[newId] = name
+    shots = job.count
+    program = job.program
+    decoded = base64.b64decode(program)
+    m = llvm.module.parse_bitcode(decoded)
+    mstr = str(m)
+    assert ('EntryPoint' in mstr)
+
+    if name == "XX":
+        assert ("qis__h__body" in mstr)
+    elif name == "YY":
+        assert ("qis__ry__body" in mstr and "qis__mz__body" in mstr)
+    elif name == "ZI":
+        assert ("qis__mz__body" in mstr)
+    elif name == "IZ":
+        assert ("qis__mz__body" in mstr)
+
+    # Job "created", return the id
+    return {"job": newId}
+
+
+# Retrieve the job, simulate having to wait by counting to 3
+# until we return the job results
+@app.get("/job/{jobId}")
+async def getJob(jobId: str):
+    global countJobGetRequests, createdJobs, shots
+
+    if countJobGetRequests < 3:
+        countJobGetRequests += 1
+        return {"status": "running"}
+
+    countJobGetRequests = 0
+    name = createdJobs[jobId]
+    retData = []
+    if name == "XX":
+        retData = ['11'] * 3887 + ['10'] * 1104 + ['01'] * 1095 + ['00'] * 3914
+    elif name == "YY":
+        retData = ['11'] * 3861 + ['10'] * 1104 + ['01'] * 1095 + ['00'] * 3914
+    elif name == "ZI":
+        retData = ['1'] * 9088 + ['0'] * 912
+    elif name == "IZ":
+        retData = ['1'] * 880 + ['0'] * 9120
+    else:
+        retData = ['00'] * int(shots / 2) + ['11'] * int(shots / 2)
+    res = {"status": "completed", "results": {"mz0": retData}}
+    return res
+
+
+def startServer(port):
+    uvicorn.run(app, port=port, host='0.0.0.0', log_level="info")
+
+
+if __name__ == '__main__':
+    startServer(62454)
