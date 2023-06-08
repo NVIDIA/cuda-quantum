d68ec1639575351aece548e8e12b7f8d66e907e5
Bettina Heim <heimb@outlook.com>
2023-06-08 03:15:42 +0000

    adding llvm build back to ci

--- runtime/cudaq/matrix.h
+++ runtime/cudaq/matrix.h
@@ -8,15 +8,7 @@
 
 #pragma once
 
-// TODO: re-examine this
-#if (defined(__GNUC__) && !defined(__clang__) && !defined(__INTEL_COMPILER))
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wrestrict"
-#endif
 #include <complex>
-#if (defined(__GNUC__) && !defined(__clang__) && !defined(__INTEL_COMPILER))
-#pragma GCC diagnostic pop
-#endif
 #include <memory>
 #include <vector>
 
