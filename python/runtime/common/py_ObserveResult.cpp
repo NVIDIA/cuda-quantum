634014f7a794139fd5cc6ad8b7851073d61bbaac
Pranav Durai <pranavdurai10@gmail.com>
2023-06-11 00:10:51 +0530

    Refactored files: (python/runtime/common) -> py_NoiseModel.cpp -> py_ObserveResult.cpp -> py_SampleResult.cpp

--- python/runtime/common/py_ObserveResult.cpp
+++ python/runtime/common/py_ObserveResult.cpp
@@ -20,64 +20,87 @@ namespace cudaq {
 void bindObserveResult(py::module &mod) {
   py::class_<observe_result>(
       mod, "ObserveResult",
-      "A data-type containing the results of a call to :func:`observe`. "
-      "This includes any measurement counts data, as well as the global "
-      "expectation value of the user-defined `spin_operator`.\n")
+      """
+      A data-type containing the results of a call to :func:`observe`
+      This includes any measurement counts data, as well as the global 
+      expectation value of the user-defined `spin_operator`
+      """
+      )
       /// @brief Bind the member functions of `cudaq.ObserveResult`.
       .def("dump", &observe_result::dump,
-           "Dump the raw data from the :class:`SampleResult` that are stored "
-           "in :class:`ObserveResult` to the terminal.\n")
-      .def("get_spin", &observe_result::get_spin,
-           "Return the `SpinOperator` corresponding to this `ObserveResult`.")
+            """
+            Dump the raw data from the :class:`SampleResult` that are stored
+            in :class:`ObserveResult` to the terminal.
+            """
+           )
       .def(
           "counts", &observe_result::raw_data,
-          "Returns a :class:`SampleResult` dictionary with the measurement "
-          "results from the experiment. The result for each individual term of "
-          "the `spin_operator` is stored in its own measurement register. "
-          "Each register name corresponds to the string representation of the "
-          "spin term (without any coefficients).\n")
+          """
+          Returns:
+          :class:`SampleResult` dictionary with the measurement 
+          results from the experiment. 
+          The result for each individual term of the `spin_operator` is stored 
+          in its own measurement register. 
+          Each register name corresponds to the string representation of the 
+          spin term (without any coefficients).
+          """
+          )
       .def("counts", &observe_result::counts<spin_op>, py::arg("sub_term"),
-           "Given a `sub_term` of the global `spin_operator` that was passed "
-           "to :func:`observe`, return its measurement counts.\n"
-           "\nArgs:\n"
-           "  sub_term (:class:`SpinOperator`): An individual sub-term of the "
-           "`spin_operator`.\n"
-           "\nReturns:\n"
-           "  :class:`SampleResult` : The measurement counts data for the "
-           "individual "
-           "`sub_term`.\n")
+            """
+            Given a `sub_term` of the global `spin_operator` that was passed 
+            to :func:`observe`, return its measurement counts.
+            
+            Args:
+            sub_term (:class:`SpinOperator`): An individual sub-term of the
+            `spin_operator`.
+            
+            Returns:
+            :class:`SampleResult` : The measurement counts data for the 
+            individual `sub_term`.
+            """
+           )
       .def(
           "expectation_z",
           [](observe_result &self) { return self.exp_val_z(); },
-          "Return the expectation value of the "
-          "`spin_operator` "
-          "that was provided in :func:`observe`.\n")
+          """
+          Return the expectation value of the `spin_operator` 
+          that was provided in :func:`observe`.
+          """
+          )
       .def(
           "expectation_z",
           [](observe_result &self, spin_op &spin_term) {
             return self.exp_val_z(spin_term);
           },
           py::arg("sub_term"),
-          "Return the expectation value of an individual `sub_term` of the "
-          "global `spin_operator` that was passed to :func:`observe`.\n"
-          "\nArgs:\n"
-          "  sub_term (:class:`SpinOperator`): An individual sub-term of the "
-          "`spin_operator`.\n"
-          "\nReturns:\n"
-          "  float : The expectation value of the `sub_term` with respect to "
-          "the :class:`Kernel` "
-          "that was passed to :func:`observe`.\n");
+          """
+          Return the expectation value of an individual `sub_term` of the 
+          global `spin_operator` that was passed to :func:`observe`.
+          
+          Args:
+          sub_term (:class:`SpinOperator`): An individual sub-term of the
+          `spin_operator`.
+          
+          Returns:
+          float : The expectation value of the `sub_term` with respect to 
+          the :class:`Kernel` that was passed to :func:`observe`.
+          """
+          );
 
   py::class_<async_observe_result>(
       mod, "AsyncObserveResult",
-      "A data-type containing the results of a call to :func:`observe_async`. "
-      "The `AsyncObserveResult` contains a future, whose "
-      ":class:`ObserveResult` "
-      "may be returned via an invocation of the `get` method. This "
-      "kicks "
-      "off a wait on the current thread until the results are available.\n"
-      "See `future <https://en.cppreference.com/w/cpp/thread/future>`_ "
-      "for more information on this programming pattern.\n")
+      """
+      A data-type containing the results of a call to :func:`observe_async`. 
+      
+      The `AsyncObserveResult` contains a future, whose :class:`ObserveResult` 
+      may be returned via an invocation of the `get` method. 
+      
+      This kicks off a wait on the current thread until the results are available.
+      
+      See `future <https://en.cppreference.com/w/cpp/thread/future>` 
+      for more information on this programming pattern.
+      """
+      )
       .def(py::init([](std::string inJson, spin_op &op) {
         async_observe_result f(&op);
         std::istringstream is(inJson);
@@ -85,8 +108,11 @@ void bindObserveResult(py::module &mod) {
         return f;
       }))
       .def("get", &async_observe_result::get,
-           "Return the :class:`ObserveResult` from the asynchronous observe "
-           "execution.\n")
+      """
+      Returns: 
+      class:`ObserveResult` from the asynchronous observe execution.
+      """
+         )
       .def("__str__", [](async_observe_result &self) {
         std::stringstream ss;
         ss << self;
--- python/runtime/common/py_SampleResult.cpp
+++ python/runtime/common/py_SampleResult.cpp
@@ -23,22 +23,28 @@ void bindMeasureCounts(py::module &mod) {
   // as input.
   py::class_<sample_result>(
       mod, "SampleResult",
-      "A data-type containing the results of a call to :func:`sample`. "
-      "This includes all measurement counts data from both mid-circuit and "
-      "terminal measurements.\n"
-      "\nNote:\n"
-      "  At this time, mid-circuit measurements are not directly supported. "
-      "Mid-circuit measurements may only be used if they are passed through "
-      " to "
-      "`c_if`.\n"
-      "\nAttributes:\n"
-      "  register_names (List[str]): A list of the names of each measurement "
-      "register that are stored in `self`.\n")
+      """
+      A data-type containing the results of a call to :func:`sample`. 
+      This includes all measurement counts data from both mid-circuit and 
+      terminal measurements.
+      
+      Note:
+      At this time, mid-circuit measurements are not directly supported. 
+      Mid-circuit measurements may only be used if they are passed through 
+      to `c_if`.
+      
+      Attributes:
+      register_names (List[str]): A list of the names of each measurement 
+      register that are stored in `self`.
+      """
+      )
       .def_property_readonly("register_names", &sample_result::register_names)
       .def(
           "dump", [](sample_result &self) { self.dump(); },
-          "Print a string of the raw measurement counts data to the "
-          "terminal.\n")
+          """
+          Print a string of the raw measurement counts data to the terminal.
+          """
+          )
       .def(
           "__str__",
           [](sample_result &self) {
@@ -46,8 +52,10 @@ void bindMeasureCounts(py::module &mod) {
             self.dump(ss);
             return ss.str();
           },
-          "Return a string of the raw measurement counts that are stored in "
-          "`self`.\n")
+          """
+          Return a string of the raw measurement counts that are stored in `self`.
+          """
+          )
       .def(
           "__getitem__",
           [](sample_result &self, const std::string &bitstring) {
@@ -60,96 +68,129 @@ void bindMeasureCounts(py::module &mod) {
             return iter->second;
           },
           py::arg("bitstring"),
-          "Return the measurement counts for the given `bitstring`.\n"
-          "\nArgs:\n"
-          "  bitstring (str): The binary string to return the measurement "
-          "data of.\n"
-          "\nReturns:\n"
-          "  float : The number of times the given `bitstring` was measured "
-          "during the `shots_count` number of executions on the QPU.\n")
+          """
+          Return the measurement counts for the given `bitstring`.
+          
+          Args:
+          bitstring (str): The binary string to return the measurement data of.
+          
+          Returns:
+          float : The number of times the given `bitstring` was measured
+          during the `shots_count` number of executions on the QPU.
+          """
+          )
       .def(
           "__len__", [](sample_result &self) { return self.to_map().size(); },
-          "Return the number of elements in `self`. Equivalent to "
-          "the number of uniquely measured bitstrings.\n")
+          """
+          Returns: 
+          The number of elements in `self`. Equivalent to the 
+          number of uniquely measured bitstrings.
+          """
+          )
       .def(
           "__iter__",
           [](sample_result &self) {
             return py::make_key_iterator(self.begin(), self.end());
           },
           py::keep_alive<0, 1>(),
-          "Iterate through the :class:`SampleResult` dictionary.\n")
+          """
+          Iterate through the :class:`SampleResult` dictionary.
+          """
+          )
       .def("expectation_z", &sample_result::exp_val_z,
            py::arg("register_name") = GlobalRegisterName,
-           "Return the expectation value in the Z-basis of the :class:`Kernel` "
-           "that was sampled.\n")
+           """
+           Return the expectation value in the Z-basis of the :class:`Kernel`
+           that was sampled.
+           """
+           )
       .def("probability", &sample_result::probability,
-           "Return the probability of observing the given bit string.\n",
+          """
+          Return the probability of observing the given bit string.
+          """,
            py::arg("bitstring"), py::arg("register_name") = GlobalRegisterName,
-           "Return the probability of measuring the given `bitstring`.\n"
-           "\nArgs:\n"
-           "  bitstring (str): The binary string to return the measurement "
-           "probability of.\n"
-           "  register_name (Optional[str]): The optional measurement register "
-           "name to "
-           "extract the probability from. Defaults to the '__global__' "
-           "register.\n"
-           "\nReturns:\n"
-           "  float : The probability of measuring the given `bitstring`. "
-           "Equivalent "
-           "to the proportion of the total times the bitstring was measured "
-           "vs. the "
-           "number of experiments (`shots_count`).\n")
+           """
+           Return the probability of measuring the given `bitstring`.
+
+           Args:
+           bitstring (str): The binary string to return the measurement 
+           probability of.
+           
+           register_name (Optional[str]): The optional measurement register 
+           name to extract the probability from. Defaults to the '__global__' 
+           register.
+           
+           Returns:
+           float : The probability of measuring the given `bitstring`. 
+           Equivalent to the proportion of the total times the bitstring 
+           was measured vs. the number of experiments (`shots_count`).
+           """
+           )
       .def("most_probable", &sample_result::most_probable,
            py::arg("register_name") = GlobalRegisterName,
-           "Return the bitstring that was measured most frequently in the "
-           "experiment.\n"
-           "\nArgs:\n"
-           "  register_name (Optional[str]): The optional measurement register "
-           "name to "
-           "extract the most probable bitstring from. Defaults to the "
-           "'__global__' "
-           "register.\n"
-           "\nReturns:\n"
-           "  str : The most frequently measured binary string during the "
-           "experiment.\n")
+           """
+           Return the bitstring that was measured most frequently in the 
+           experiment.
+           
+           Args:
+           register_name (Optional[str]): The optional measurement register 
+           name to extract the most probable bitstring from. Defaults to the 
+           '__global__'register.
+           
+           Returns:
+           str : The most frequently measured binary string during the 
+           experiment.
+           """
+           )
       .def("count", &sample_result::count, py::arg("bitstring"),
            py::arg("register_name") = GlobalRegisterName,
-           "Return the number of times the given bitstring was observed.\n"
-           "\nArgs:\n"
-           "  bitstring (str): The binary string to return the measurement "
-           "counts for.\n"
-           "  register_name (Optional[str]): The optional measurement register "
-           "name to "
-           "extract the probability from. Defaults to the '__global__' "
-           "register.\n"
-           "\nReturns:\n"
-           "  int : The number of times the given bitstring was measured "
-           "during "
-           "the experiment.\n")
+           """
+           Return the number of times the given bitstring was observed.
+
+           Args:
+           bitstring (str): The binary string to return the measurement 
+           counts for.
+           
+           register_name (Optional[str]): The optional measurement register 
+           name to extract the probability from. Defaults to the '__global__' 
+           register.
+
+           Returns:
+           int : The number of times the given bitstring 
+           was measured during the experiment.
+           """
+           )
       .def("get_marginal_counts",
            static_cast<sample_result (sample_result::*)(
                const std::vector<std::size_t> &, const std::string_view)>(
                &sample_result::get_marginal),
            py::arg("marginal_indices"), py::kw_only(),
            py::arg("register_name") = GlobalRegisterName,
-           "Extract the measurement counts data for the provided subset of "
-           "qubits (`marginal_indices`).\n"
-           "\nArgs:\n"
-           "  marginal_indices (list[int]): A list of the qubit indices to "
-           "extract the measurement data from.\n"
-           "  register_name (Optional[str]): The optional measurement register "
-           "name to "
-           "extract the counts data from. Defaults to the '__global__' "
-           "register.\n"
-           "\nReturns:\n"
-           "  :class:`SampleResult` : A new `SampleResult` dictionary "
-           "containing the "
-           "extracted measurement data.\n")
+           """
+           Extract the measurement counts data for the provided subset of 
+           qubits (`marginal_indices`).
+
+           Args:
+           marginal_indices (list[int]): A list of the qubit indices to 
+           extract the measurement data from.
+           
+           register_name (Optional[str]): The optional measurement register 
+           name to extract the counts data from. Defaults to the '__global__' 
+           register.
+           
+           Returns:
+           class:`SampleResult` : A new `SampleResult` dictionary 
+           containing the extracted measurement data.
+           """
+           )
       .def("get_sequential_data", &sample_result::sequential_data,
            py::arg("register_name") = GlobalRegisterName,
-           "Return the data from the given register (`register_name`) as it "
-           "was collected sequentially. A list of measurement results, not "
-           "collated into a map.\n")
+           """
+           Return the data from the given register (`register_name`) as it 
+           was collected sequentially. A list of measurement results, not 
+           collated into a map.
+           """
+           )
       .def(
           "get_register_counts",
           [&](sample_result &self, const std::string &registerName) {
@@ -158,26 +199,36 @@ void bindMeasureCounts(py::module &mod) {
             return sample_result(res);
           },
           py::arg("register_name"),
-          "Extract the provided sub-register (`register_name`) as a new "
-          ":class:`SampleResult`.\n")
+          """
+          Extract the provided sub-register (`register_name`) as a new
+          :class:`SampleResult`.
+          """
+          )
       .def(
           "items",
           [](sample_result &self) {
             return py::make_iterator(self.begin(), self.end());
           },
           py::keep_alive<0, 1>(),
-          "Return the key/value pairs in this :class:`SampleResult` "
-          "dictionary.\n")
+          """
+          Return the key/value pairs in this :class:`SampleResult` dictionary.
+          """
+          )
       .def(
           "values",
           [](sample_result &self) {
             return py::make_value_iterator(self.begin(), self.end());
           },
           py::keep_alive<0, 1>(),
-          "Return all values (the counts) in this :class:`SampleResult` "
-          "dictionary.\n")
+          """
+          Return all values (the counts) in this :class:`SampleResult` dictionary.
+          """
+          )
       .def("clear", &sample_result::clear,
-           "Clear out all metadata from `self`.\n");
+          """
+          Clear out all metadata from `self`.
+          """
+      );
 }
 
 } // namespace cudaq
