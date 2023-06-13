bfbf7809f68481ff38e23dd7c227d78e428ea11c
Bettina Heim <heimb@outlook.com>
2023-06-13 20:56:05 +0200

    an attempt to add the github workflows needed for automatic docs publishing...

---
+++ docker/build/cudaqdev.Dockerfile
@@ -0,0 +1,58 @@
+# ============================================================================ #
+# Copyright (c) 2022 - 2023 NVIDIA Corporation & Affiliates.                   #
+# All rights reserved.                                                         #
+#                                                                              #
+# This source code and the accompanying materials are made available under     #
+# the terms of the Apache License 2.0 which accompanies this distribution.     #
+# ============================================================================ #
+
+# Usage:
+# Build from the repo root with
+#   docker build -t nvidia/cuda-quantum-dev:latest -f docker/build/cudaqdev.Dockerfile .
+#
+# If a custom build_environment is used, then the build environment must 
+# 1) have all the necessary build dependendencies installed
+# 2) define the LLVM_INSTALL_PREFIX environment variable indicating where the 
+#    the LLVM binaries that CUDA Quantum depends on are installed
+# 3) set the CC and CXX environment variable to use the same compiler toolchain
+#    as the LLVM dependencies have been built with.
+
+# To keep the default build environment image to a reasonable size, it does not 
+# contain the necessary dependencies to develop GPU-based components. You may hence
+# see a message along the lines of "no GPU detected" during the CUDA Quantum build.
+# Please install the necessary prerequisites listed in the CUDA Quantum build script,
+# or use a suitable build_environment, to enable developing these components.
+ARG build_environment=ghcr.io/nvidia/cuda-quantum-devdeps
+ARG env_tag=llvm-main
+
+FROM $build_environment:$env_tag
+
+ENV CUDAQ_REPO_ROOT=/workspaces/cuda-quantum
+ENV CUDAQ_INSTALL_PREFIX=/usr/local/cudaq
+ENV PATH="$CUDAQ_INSTALL_PREFIX/bin:${PATH}"
+ENV PYTHONPATH="$CUDAQ_INSTALL_PREFIX:${PYTHONPATH}"
+
+ARG workspace=.
+ARG destination="$CUDAQ_REPO_ROOT"
+ADD "$workspace" "$destination"
+WORKDIR "$destination"
+
+# Configuring build_environment that contains the necessary dependencies for GPU
+# accelerated components and passing a build argument 
+#   install="CMAKE_BUILD_TYPE=Release FORCE_COMPILE_GPU_COMPONENTS=true"
+# creates a dev image that can be used as argument to docker/release/cudaq.Dockerfile
+# to create the released cuda-quantum image.
+ARG install=
+RUN if [ -n "$install" ]; \
+    then \
+        expected_prefix=$CUDAQ_INSTALL_PREFIX; \
+        export $install; \
+        bash scripts/build_cudaq.sh -v; \
+        if [ ! "$?" -eq "0" ]; then \
+            exit 1; \
+        elif [ "$CUDAQ_INSTALL_PREFIX" != "$expected_prefix" ]; then \
+            mkdir -p "$expected_prefix"; \
+            mv "$CUDAQ_INSTALL_PREFIX"/* "$expected_prefix"; \
+            rmdir "$CUDAQ_INSTALL_PREFIX"; \
+        fi \
+    fi
--- scripts/build_cudaq.sh
+++ scripts/build_cudaq.sh
@@ -20,10 +20,18 @@
 # CUQUANTUM_INSTALL_PREFIX=/path/to/dir bash scripts/build_cudaq.sh
 #
 # Prerequisites:
-# - git, ninja-build, cmake, python3, libpython3-dev, libstdc++-11-dev, libblas-dev (all available via apt install)
+# - git, ninja-build, cmake, python3, libpython3-dev, libstdc++-12-dev, libblas-dev (all available via apt install)
 # - LLVM binaries, libraries, and headers as built by scripts/build_llvm.sh.
 # - To include simulator backends that use cuQuantum the packages cuquantum and cuquantum-dev are needed. 
 # - Additional python dependencies for running and testing: lit pytest numpy (available via pip install)
+# - Additional dependencies for GPU-accelerated components: cuquantum, cutensor, cuda-11-8
+#
+# Note:
+# The CUDA Quantum build automatically detects whether GPUs are available and will 
+# only include any GPU based components if they are. It is possible to override this 
+# behavior and force building GPU components even if no GPU is detected by setting the
+# FORCE_COMPILE_GPU_COMPONENTS environment variable to true. This is useful primarily
+# when building docker images since GPUs may not be accessible during build.
 
 LLVM_INSTALL_PREFIX=${LLVM_INSTALL_PREFIX:-/opt/llvm}
 CUQUANTUM_INSTALL_PREFIX=${CUQUANTUM_INSTALL_PREFIX:-/opt/nvidia/cuquantum}
@@ -31,13 +39,16 @@ CUDAQ_INSTALL_PREFIX=${CUDAQ_INSTALL_PREFIX:-"$HOME/.cudaq"}
 
 # Process command line arguments
 (return 0 2>/dev/null) && is_sourced=true || is_sourced=false
-build_configuration=Release
+build_configuration=${CMAKE_BUILD_TYPE:-Release}
+verbose=false
 
 __optind__=$OPTIND
 OPTIND=1
-while getopts ":c:" opt; do
+while getopts ":c:v" opt; do
   case $opt in
     c) build_configuration="$OPTARG"
+    ;;
+    v) verbose=true
     ;;
     \?) echo "Invalid command line option -$OPTARG" >&2
     if $is_sourced; then return 1; else exit 1; fi
@@ -80,7 +91,7 @@ fi
 cuda_version=`nvcc --version 2>/dev/null | grep -o 'release [0-9]*\.[0-9]*' | cut -d ' ' -f 2`
 cuda_major=`echo $cuda_version | cut -d '.' -f 1`
 cuda_minor=`echo $cuda_version | cut -d '.' -f 2`
-if [ ! -x "$(command -v nvidia-smi)" ] && [ "$COMPILE_GPU_BACKENDS" != "true" ] ; then # the second check here is to avoid having to use https://discuss.huggingface.co/t/how-to-deal-with-no-gpu-during-docker-build-time/28544 
+if [ ! -x "$(command -v nvidia-smi)" ] && [ "$FORCE_COMPILE_GPU_COMPONENTS" != "true" ] ; then # the second check here is to avoid having to use https://discuss.huggingface.co/t/how-to-deal-with-no-gpu-during-docker-build-time/28544 
   echo "No GPU detected - GPU backends will be omitted from the build."
   custatevec_flag=""
 elif [ "$cuda_version" = "" ] || [ "$cuda_major" -lt "11" ] || ([ "$cuda_minor" -lt "8" ] && [ "$cuda_major" -eq "11" ]); then
@@ -104,14 +115,20 @@ mkdir -p "$CUDAQ_INSTALL_PREFIX/bin"
 mkdir -p "$working_dir/build" && cd "$working_dir/build" && rm -rf * 
 mkdir -p logs && rm -rf logs/* 
 
+# Determine linker and linker flags
+cmake_common_linker_flags_init=""
+if [ -x "$(command -v "$LLVM_INSTALL_PREFIX/bin/ld.lld")" ]; then
+  echo "Configuring nvq++ to use the lld linker by default."
+  NVQPP_LD_PATH="$LLVM_INSTALL_PREFIX/bin/ld.lld"
+fi
+
 # Generate CMake files 
 # (utils are needed for custom testing tools, e.g. CircuitCheck)
-cmake_common_linker_flags_init=""
-llvm_dir="$llvm_lib_dir/cmake/llvm"
-echo "Preparing CUDA Quantum build with LLVM_DIR=$llvm_dir..."
-cmake -G Ninja "$repo_root" \
+echo "Preparing CUDA Quantum build with LLVM installation in $LLVM_INSTALL_PREFIX..."
+cmake_args="-G Ninja "$repo_root" \
+  -DCMAKE_COMPILE_WARNING_AS_ERROR=OFF \
   -DCMAKE_INSTALL_PREFIX="$CUDAQ_INSTALL_PREFIX" \
-  -DLLVM_DIR="$llvm_dir" \
+  -DLLVM_DIR="$llvm_lib_dir/cmake/llvm" \
   -DNVQPP_LD_PATH="$NVQPP_LD_PATH" \
   -DCMAKE_BUILD_TYPE=$build_configuration \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
@@ -120,18 +137,37 @@ cmake -G Ninja "$repo_root" \
   -DCMAKE_EXE_LINKER_FLAGS_INIT="$cmake_common_linker_flags_init" \
   -DCMAKE_MODULE_LINKER_FLAGS_INIT="$cmake_common_linker_flags_init" \
   -DCMAKE_SHARED_LINKER_FLAGS_INIT="$cmake_common_linker_flags_init" \
-  $custatevec_flag 2> logs/cmake_error.txt 1> logs/cmake_output.txt
+  $custatevec_flag"
+if $verbose; then 
+  cmake $cmake_args
+else
+  cmake $cmake_args 2> logs/cmake_error.txt 1> logs/cmake_output.txt
+fi
 
 # Build and install CUDAQ
 echo "Building CUDA Quantum with configuration $build_configuration..."
 logs_dir=`pwd`/logs
-echo "The progress of the build is being logged to $logs_dir/ninja_output.txt."
-ninja install 2> "$logs_dir/ninja_error.txt" 1> "$logs_dir/ninja_output.txt"
+if $verbose; then 
+  ninja install
+else
+  echo "The progress of the build is being logged to $logs_dir/ninja_output.txt."
+  ninja install 2> "$logs_dir/ninja_error.txt" 1> "$logs_dir/ninja_output.txt"
+fi
+
 if [ ! "$?" -eq "0" ]; then
-  echo "Build failed. Please check the files in the $logs_dir directory."
+  echo "Build failed. Please check the console output or the files in the $logs_dir directory."
   cd "$working_dir" && if $is_sourced; then return 1; else exit 1; fi
 else
   cp "$repo_root/LICENSE" "$CUDAQ_INSTALL_PREFIX/LICENSE"
   cp "$repo_root/NOTICE" "$CUDAQ_INSTALL_PREFIX/NOTICE"
+
+  # The CUDA Quantum installation as built above is not fully self-container;
+  # It will, in particular, break if the LLVM tools are not in the expected location.
+  # We save any system configurations that are assumed by the installation with the installation.
+  echo "<build_config>" > "$CUDAQ_INSTALL_PREFIX/build_config.xml"
+  echo "<LLVM_INSTALL_PREFIX>$LLVM_INSTALL_PREFIX</LLVM_INSTALL_PREFIX>" >> "$CUDAQ_INSTALL_PREFIX/build_config.xml"
+  echo "<CUQUANTUM_INSTALL_PREFIX>$CUQUANTUM_INSTALL_PREFIX</CUQUANTUM_INSTALL_PREFIX>" >> "$CUDAQ_INSTALL_PREFIX/build_config.xml"
+  echo "</build_config>" >> "$CUDAQ_INSTALL_PREFIX/build_config.xml"
+
   cd "$working_dir" && echo "Installed CUDA Quantum in directory: $CUDAQ_INSTALL_PREFIX"
 fi
