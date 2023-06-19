bd9b67d2a167e43aff31ef155394bb163635a8b9
Alex McCaskey <amccaskey@nvidia.com>
2023-06-19 10:01:42 -0400

    Local emulation of remote qpu execution (#262)
    * Start on local emulation of remote qpu execution
    
    * setup asynchronicity for emulator
    ---------
    
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>
    Co-authored-by: Bruno Schmitt <bschmitt@nvidia.com>

--- runtime/cudaq/platform/default/rest/RemoteRESTQPU.cpp
+++ runtime/cudaq/platform/default/rest/RemoteRESTQPU.cpp
@@ -39,6 +39,7 @@
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Dialect/Math/IR/Math.h"
+#include "mlir/ExecutionEngine/ExecutionEngine.h"
 #include "mlir/ExecutionEngine/OptUtils.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/Diagnostics.h"
@@ -98,6 +99,27 @@ protected:
   /// @brief Mapping of general key-values for backend
   /// configuration.
   std::map<std::string, std::string> backendConfig;
+
+  /// @brief Flag indicating whether we should emulate
+  /// execution locally.
+  bool emulate = false;
+
+  /// @brief If we are emulating locally, keep track
+  /// of JIT engines for invoking the kernels.
+  std::vector<ExecutionEngine *> jitEngines;
+
+  /// @brief Invoke the kernel in the JIT engine and then delete the JIT engine.
+  void invokeJITKernelAndRelease(ExecutionEngine *jit,
+                                 const std::string &kernelName) {
+    auto funcPtr = jit->lookup(std::string("__nvqpp__mlirgen__") + kernelName);
+    if (!funcPtr) {
+      throw std::runtime_error(
+          "cudaq::builder failed to get kernelReg function.");
+    }
+    reinterpret_cast<void (*)()>(*funcPtr)();
+    // We're done, delete the pointer.
+    delete jit;
+  }
 
 public:
   /// @brief The constructor
@@ -172,6 +194,10 @@ public:
       for (std::size_t i = 1; i < split.size(); i += 2)
         backendConfig.insert({split[i], split[i + 1]});
     }
+
+    // Turn on emulation mode if requested
+    auto iter = backendConfig.find("emulate");
+    emulate = iter != backendConfig.end() && iter->second == "true";
 
     /// Once we know the backend, we should search for the config file
     /// from there we can get the URL/PORT and the required MLIR pass
@@ -293,9 +319,17 @@ public:
         runPassPipeline("canonicalize", tmpModuleOp);
         modules.emplace_back(term.to_string(false), tmpModuleOp);
       }
-
     } else
       modules.emplace_back(kernelName, moduleOp);
+
+    if (emulate) {
+      // If we are in emulation mode, we need to first get a
+      // full QIR representation of the code. Then we'll map to
+      // an LLVM Module, create a JIT ExecutionEngine pointer
+      // and use that for execution
+      for (auto &[name, module] : modules)
+        jitEngines.emplace_back(cudaq::createQIRJITEngine(module));
+    }
 
     // Get the code gen translation
     auto translation = cudaq::getTranslation(codegenTranslation);
@@ -332,8 +366,38 @@ public:
     // Get the Quake code, lowered according to config file.
     auto codes = lowerQuakeCode(kernelName, args);
 
-    // Execute the codes produced in quake lowering
-    auto future = executor->execute(codes);
+    // If emulation requested, then just grab the function
+    // and invoke it with the simulator
+    cudaq::details::future future;
+    if (emulate) {
+      // Get the current execution context and number of shots
+      std::size_t localShots = 1000;
+      if (executionContext->shots != std::numeric_limits<std::size_t>::max() &&
+          executionContext->shots != 0)
+        localShots = executionContext->shots;
+
+      // Launch the execution of the simulated jobs asynchronously
+      future = cudaq::details::future(std::async(
+          std::launch::async,
+          [&, codes, localShots, kernelName,
+           localJIT = std::move(jitEngines)]() mutable -> cudaq::sample_result {
+            std::vector<cudaq::ExecutionResult> results;
+            for (std::size_t i = 0; i < codes.size(); i++) {
+              cudaq::ExecutionContext context("sample", localShots);
+              cudaq::getExecutionManager()->setExecutionContext(&context);
+              invokeJITKernelAndRelease(localJIT[i], kernelName);
+              cudaq::getExecutionManager()->resetExecutionContext();
+              results.emplace_back(context.result.to_map(),
+                                   codes.size() == 1 ? cudaq::GlobalRegisterName
+                                                     : codes[i].name);
+            }
+            localJIT.clear();
+            return cudaq::sample_result(results);
+          }));
+
+    } else
+      // Execute the codes produced in quake lowering
+      future = executor->execute(codes);
 
     // Keep this asynchronous if requested
     if (executionContext->asyncExec) {
