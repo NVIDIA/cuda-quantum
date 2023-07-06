1bb840275d772b893a6910f9800d185cd86d2f3e
Alex McCaskey <amccaskey@nvidia.com>
2023-07-06 13:36:57 -0400

    Callable Kernel Arguments Passed QuakeSynthesizer. (#355)
    * Fix #336, where callable argument caused quake synth to fail.
    
    ---------
    
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>

--- test/NVQPP/test-0.cpp
+++ test/NVQPP/test-0.cpp
@@ -7,7 +7,6 @@
  ******************************************************************************/
 
 // RUN: nvq++ %s -o %basename_t.x --target quantinuum --emulate && ./%basename_t.x | FileCheck %s
-// XFAIL: *
 
 #include <cudaq.h>
 #include <iostream>
@@ -24,10 +23,10 @@ struct baz {
 
 struct foo {
   template <typename CallableKernel>
-  __qpu__ void operator()(CallableKernel &&func) {
-    cudaq::qubit q;
-    func(q);
-    mz(q);
+  __qpu__ void operator()(CallableKernel &&func, int size) {
+    cudaq::qreg q(size);
+    func(q[0]);
+    mz(q[0]);
   }
 };
 
@@ -36,7 +35,7 @@ int main() {
   // auto result = cudaq::sample(1000, foo{}, &bar);
 
   // QuakeSynth don't support:
-  auto result = cudaq::sample(1000, foo{}, baz{});
+  auto result = cudaq::sample(1000, foo{}, baz{}, /*qreg size*/ 1);
   for (auto &&[bits, counts] : result) {
     std::cout << bits << " : " << counts << '\n';
   }
