ced382d81540dbf3b63ab51f118cd06efb5acd25
Bettina Heim <heimb@outlook.com>
2023-07-19 09:24:41 +0000

    disabling openmp in the blas build

--- scripts/install_prerequisites.sh
+++ scripts/install_prerequisites.sh
@@ -89,7 +89,13 @@ if [ ! -f "$OPENBLAS_INSTALL_PREFIX/lib/libopenblas.a" ]; then
 
   wget https://github.com/xianyi/OpenBLAS/releases/download/v0.3.23/OpenBLAS-0.3.23.tar.gz
   tar -xf OpenBLAS-0.3.23.tar.gz && cd OpenBLAS-0.3.23
-  make USE_OPENMP=1 && make install PREFIX="$OPENBLAS_INSTALL_PREFIX"
+
+  # TODO: gcc and clang work with different OpenMP libraries (libgomp vs libomp).
+  # To enable OpenMP support here we need to build it with the same compiler toolchain
+  # as we build CUDA Quantum with to ensure we link against the OpenMP library supported
+  # by that compiler. OpenMP support is disabled until we add the tools to build this 
+  # with each toolchains in the dev images.
+  make USE_OPENMP=0 && make install PREFIX="$OPENBLAS_INSTALL_PREFIX"
   cd .. && rm -rf OpenBLAS-0.3.23*
 fi
 
