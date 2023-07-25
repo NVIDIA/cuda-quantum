dbccd8bd7e2d6459016e9489ceeaed13628c067b
Alex McCaskey <amccaskey@nvidia.com>
2023-07-25 00:18:58 +0000

    add docs
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>

--- runtime/cudaq.h
+++ runtime/cudaq.h
@@ -190,6 +190,8 @@ void unset_noise();
 /// @brief Utility function for clearing the shots
 void clear_shots(const std::size_t nShots);
 
+/// @brief Set a seed for any random number
+/// generators used in backend simulations.
 void set_random_seed(std::size_t seed);
 
 namespace mpi {
