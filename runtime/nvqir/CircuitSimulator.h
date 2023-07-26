a37af77b61ae5bb7e1c9bda70d00cfa65d4d915c
Alex McCaskey <amccaskey@nvidia.com>
2023-07-26 07:53:27 -0400

    Improve MPI Finalize handling for CircuitSimulator subclasses (#492)
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>
    Co-authored-by: Bettina Heim <heimb@outlook.com>

--- runtime/nvqir/CircuitSimulator.h
+++ runtime/nvqir/CircuitSimulator.h
@@ -46,6 +46,13 @@ public:
   /// apply them to the state.
   void flushGateQueue() { flushGateQueueImpl(); }
 
+  /// @brief Provide an opportunity for any tear-down
+  /// tasks before MPI Finalize is invoked. Here we leave
+  /// this unimplemented, it is meant for subclasses.
+  virtual void tearDownBeforeMPIFinalize() {
+    // do nothing
+  }
+
   /// @brief Set the current noise model to consider when
   /// simulating the state. This should be overridden by
   /// simulation strategies that support noise modeling.
