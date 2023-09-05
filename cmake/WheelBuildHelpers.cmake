e14faed59400d341bc61addade2a337a70bae6a7
Thien Nguyen <58006629+1tnguyen@users.noreply.github.com>
2023-09-06 00:11:28 +1000

    Working on workflows to build cross-platform cudaq_hpc Docker and Python wheel (#584)
    * Added info log when .so load failed
    
    To assist debugging (for dev and potentially during deployment)
    
    Also, fix a build race condition in CustomPassPlugin: it needs generated h.inc files which may not be generated yet depending on the build parallelism.
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    * Try adding a hook to package additional backends to the wheel build
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    * Copy and paste error
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    * Added cutensornet to Python preload lib
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    * Allow env as input to be compatible with pyproject.toml
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    * Try enabling Packages workflow for testing
    
    - Disable/skip `create_release` step.
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    * Revert "Try enabling Packages workflow for testing"
    
    This reverts commit 504deebc590c27dd8c12dc2b1577aa6a131f3441.
    
    ---------
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>

--- cmake/WheelBuildHelpers.cmake
+++ cmake/WheelBuildHelpers.cmake
@@ -6,6 +6,14 @@
 # the terms of the Apache License 2.0 which accompanies this distribution.     #
 # ============================================================================ #
 
-add_llvm_pass_plugin(CustomPassPlugin CustomPassPlugin.cpp)
-# Depends on QuakeDialect TableGen to use the generated `.h.inc` files.
-add_dependencies(CustomPassPlugin QuakeDialect)
+include_guard()
+
+# Making a NVQIR backend lib available inside wheel 
+function(add_target_libs_to_wheel nvqir_backend_lib nvqir_backend_config)
+    message(STATUS "Installing NVQIR backend lib '${nvqir_backend_lib}' with config '${nvqir_backend_config}'")
+    if (NOT EXISTS "${nvqir_backend_lib}" OR NOT EXISTS "${nvqir_backend_config}")
+        message(FATAL_ERROR "Invalid file paths to NVQIR backend lib.")
+    endif()
+    install(FILES ${nvqir_backend_lib} DESTINATION lib)
+    install(FILES ${nvqir_backend_config} DESTINATION targets)
+endfunction()
