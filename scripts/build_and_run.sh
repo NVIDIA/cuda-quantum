a1d4b39bd4196b5c4de7983e18c66635acdc9d6b
owen arnold <oarnold@oxfordquantumcircuits.com>
2023-09-11 12:23:30 +0100

    Fix RPATH and seperate install out for test runs

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -3,12 +3,11 @@ set -ex
 cmake -S .. -G "Ninja" \
          -DCMAKE_CXX_COMPILER=$CONDA_PREFIX/bin/clang++ \
          -DCMAKE_C_COMPILER=$CONDA_PREFIX/bin/clang \
-         -DCMAKE_INSTALL_PREFIX=./ \
-         -DCMAKE_INSTALL_RPATH=./lib \
+         -DCMAKE_INSTALL_PREFIX=./install \
          -DLLVM_DIR=$CONDA_PREFIX/lib/cmake/llvm \
          -DCUDAQ_ENABLE_PYTHON=TRUE \
          -DCMAKE_COMPILE_WARNING_AS_ERROR=FALSE \
          -DCMAKE_CXX_STANDARD=20 
 ninja install 
-export PYTHONPATH=$PYTHONPATH:$PWD
+export PYTHONPATH=$PYTHONPATH:$PWD/install
 python -m pytest ../python/tests/unittests
