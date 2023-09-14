7bd2e23c42ac3b5e9c4dc67663b35325d99d60e1
Alex McCaskey <amccaskey@nvidia.com>
2023-09-14 16:04:26 +0000

    add a nvqpp test
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>

---
+++ docs/sphinx/examples/cpp/photonics/test_photonics.cpp
@@ -0,0 +1,26 @@
+// Compile and run with:
+// ```
+// nvq++ test_photonics.cpp --target photonics 
+// ./a.out
+// ```
+
+#include "cudaq.h"
+#include "cudaq/photonics.h"
+
+struct photonicsKernel {
+  void operator()() __qpu__ {
+    cudaq::qreg<cudaq::dyn, 3> qutrits(2);
+    plus(qutrits[0]);
+    plus(qutrits[1]);
+    plus(qutrits[1]);
+    mz(qutrits);
+  }
+};
+
+int main() {
+
+  auto counts = cudaq::sample(photonicsKernel{});
+  for (auto &[k, v] : counts) {
+    printf("Result / Count = %s : %lu\n", k.c_str(), v);
+  }
+}
