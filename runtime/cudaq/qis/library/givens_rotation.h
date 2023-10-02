32b9c8bda3685ca9d5c777a3261d7ee957757629
Thien Nguyen <thiennguyen@nvidia.com>
2023-10-02 19:13:34 +1100

    Refactor givens rotation impl to use native exp_pauli ops

--- runtime/cudaq/qis/library/givens_rotation.h
+++ runtime/cudaq/qis/library/givens_rotation.h
@@ -10,66 +10,46 @@
 #include <cudaq.h>
 
 namespace cudaq {
-__qpu__ void iswap_pow(double exponent, cudaq::qubit &a, cudaq::qubit &b) {
-  x<cudaq::ctrl>(a, b);
-  h(a);
-  x<cudaq::ctrl>(b, a);
-  r1(M_PI * exponent / 2.0, a);
-  x<cudaq::ctrl>(b, a);
-  r1(-M_PI * exponent / 2.0, a);
-  h(a);
-  x<cudaq::ctrl>(a, b);
+/// @brief Implement Givens rotation at a specific angle
+///
+/// This kernel is equivalent to matrix exp(-i theta (YX - XY) / 2)
+/// | 1, 0,  0, 0 |
+/// | 0, c, -s, 0 |
+/// | 0, s,  c, 0 |
+/// | 0, 0,  0, 1 |
+/// c = cos(theta); s = sin(theta)
+/// @param theta Rotation angle (in rads)
+/// @param q0 First qubit operand
+/// @param q1 Second qubit operand
+__qpu__ void givens_rotation(double theta, cudaq::qubit &q0, cudaq::qubit &q1) {
+  exp_pauli(theta, "YX", q0, q1);
+  exp_pauli(-theta, "XY", q0, q1);
 }
 
+/// @brief Add Givens rotation kernel (theta angle as a QuakeValue) to the
+/// kernel builder object
+/// @tparam KernelBuilder
+/// @param kernel
+/// @param theta
+/// @param q0
+/// @param q1
 template <typename KernelBuilder>
-void iswap_pow(KernelBuilder &kernel, cudaq::QuakeValue exponent,
-               cudaq::QuakeValue a, cudaq::QuakeValue b) {
-  kernel.template x<cudaq::ctrl>(a, b);
-  kernel.h(a);
-  kernel.template x<cudaq::ctrl>(b, a);
-  kernel.r1(M_PI * exponent / 2.0, a);
-  kernel.template x<cudaq::ctrl>(b, a);
-  kernel.r1(-M_PI * exponent / 2.0, a);
-  kernel.h(a);
-  kernel.template x<cudaq::ctrl>(a, b);
+void givens_rotation(KernelBuilder &kernel, cudaq::QuakeValue theta,
+                     cudaq::QuakeValue q0, cudaq::QuakeValue q1) {
+  kernel.exp_pauli(theta, "YX", q0, q1);
+  kernel.exp_pauli(-theta, "XY", q0, q1);
 }
 
-__qpu__ void phased_iswap_pow(double phase_exponent, double exponent,
-                              cudaq::qubit &a, cudaq::qubit &b) {
-  r1(M_PI * phase_exponent, a);
-  r1(-M_PI * phase_exponent, b);
-  iswap_pow(exponent, a, b);
-  r1(-M_PI * phase_exponent, a);
-  r1(M_PI * phase_exponent, b);
-}
-
+/// @brief Add Givens rotation kernel (fixed theta angle) to the kernel builder
+/// object
+/// @tparam KernelBuilder
+/// @param kernel
+/// @param theta
+/// @param q0
+/// @param q1
 template <typename KernelBuilder>
-void phased_iswap_pow(KernelBuilder &kernel, cudaq::QuakeValue phase_exponent,
-                      cudaq::QuakeValue exponent, cudaq::QuakeValue a,
-                      const cudaq::QuakeValue b) {
-  kernel.r1(M_PI * phase_exponent, a);
-  kernel.r1(-M_PI * phase_exponent, b);
-  iswap_pow(kernel, exponent, a, b);
-  kernel.r1(-M_PI * phase_exponent, a);
-  kernel.r1(M_PI * phase_exponent, b);
-}
-
-__qpu__ void givens_rotation(double angle_rads, cudaq::qubit &a,
-                             cudaq::qubit &b) {
-  const double exponent = 2.0 * angle_rads / M_PI;
-  phased_iswap_pow(0.25, exponent, a, b);
-}
-
-template <typename KernelBuilder>
-void givens_rotation(KernelBuilder &kernel, cudaq::QuakeValue angle_rads,
-                     cudaq::QuakeValue a, cudaq::QuakeValue b) {
-  cudaq::QuakeValue exponent = (2.0 * angle_rads) / M_PI;
-  phased_iswap_pow(kernel, kernel.constantVal(0.25), exponent, a, b);
-}
-
-template <typename KernelBuilder>
-void givens_rotation(KernelBuilder &kernel, double angle_rads,
-                     cudaq::QuakeValue a, cudaq::QuakeValue b) {
-  givens_rotation(kernel, kernel.constantVal(angle_rads), a, b);
+void givens_rotation(KernelBuilder &kernel, double theta, cudaq::QuakeValue q0,
+                     cudaq::QuakeValue q1) {
+  givens_rotation(kernel, kernel.constantVal(theta), q0, q1);
 }
 } // namespace cudaq
