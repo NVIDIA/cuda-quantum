23fda3f286c256c0a9bd4159a6488427edd6bc1e
A.M. Santana <anthonys@nvidia.com>
2023-10-03 15:39:24 +0000

    check dags
    Signed-off-by: A.M. Santana <anthonys@nvidia.com>

--- test/AST-Quake/vector_front_back.cpp
+++ test/AST-Quake/vector_front_back.cpp
@@ -19,22 +19,22 @@ __qpu__ void testFrontFloat() {
 
 
 // CHECK-LABEL:   func.func @__nvqpp__mlirgen__function_testFrontFloat._Z14testFrontFloatv() attributes {"cudaq-entrypoint", "cudaq-kernel", no_this} {
-// CHECK:           %[[VAL_0:.*]] = arith.constant 3.000000e+00 : f32
-// CHECK:           %[[VAL_1:.*]] = arith.constant 2.000000e+00 : f32
-// CHECK:           %[[VAL_2:.*]] = arith.constant 1.000000e+00 : f32
-// CHECK:           %[[VAL_3:.*]] = arith.constant 0.000000e+00 : f32
-// CHECK:           %[[VAL_4:.*]] = cc.alloca !cc.array<f32 x 4>
-// CHECK:           %[[VAL_5:.*]] = cc.compute_ptr %[[VAL_4]][0] : (!cc.ptr<!cc.array<f32 x 4>>) -> !cc.ptr<f32>
+// CHECK-DAG:       %[[VAL_0:.*]] = arith.constant 3.000000e+00 : f32
+// CHECK-DAG:       %[[VAL_1:.*]] = arith.constant 2.000000e+00 : f32
+// CHECK-DAG:       %[[VAL_2:.*]] = arith.constant 1.000000e+00 : f32
+// CHECK-DAG:       %[[VAL_3:.*]] = arith.constant 0.000000e+00 : f32
+// CHECK-DAG:       %[[VAL_4:.*]] = cc.alloca !cc.array<f32 x 4>
+// CHECK-DAG:       %[[VAL_5:.*]] = cc.compute_ptr %[[VAL_4]][0] : (!cc.ptr<!cc.array<f32 x 4>>) -> !cc.ptr<f32>
 // CHECK:           cc.store %[[VAL_3]], %[[VAL_5]] : !cc.ptr<f32>
-// CHECK:           %[[VAL_6:.*]] = cc.compute_ptr %[[VAL_4]][1] : (!cc.ptr<!cc.array<f32 x 4>>) -> !cc.ptr<f32>
+// CHECK-DAG:       %[[VAL_6:.*]] = cc.compute_ptr %[[VAL_4]][1] : (!cc.ptr<!cc.array<f32 x 4>>) -> !cc.ptr<f32>
 // CHECK:           cc.store %[[VAL_2]], %[[VAL_6]] : !cc.ptr<f32>
-// CHECK:           %[[VAL_7:.*]] = cc.compute_ptr %[[VAL_4]][2] : (!cc.ptr<!cc.array<f32 x 4>>) -> !cc.ptr<f32>
+// CHECK-DAG:       %[[VAL_7:.*]] = cc.compute_ptr %[[VAL_4]][2] : (!cc.ptr<!cc.array<f32 x 4>>) -> !cc.ptr<f32>
 // CHECK:           cc.store %[[VAL_1]], %[[VAL_7]] : !cc.ptr<f32>
-// CHECK:           %[[VAL_8:.*]] = cc.compute_ptr %[[VAL_4]][3] : (!cc.ptr<!cc.array<f32 x 4>>) -> !cc.ptr<f32>
+// CHECK-DAG:       %[[VAL_8:.*]] = cc.compute_ptr %[[VAL_4]][3] : (!cc.ptr<!cc.array<f32 x 4>>) -> !cc.ptr<f32>
 // CHECK:           cc.store %[[VAL_0]], %[[VAL_8]] : !cc.ptr<f32>
-// CHECK:           %[[VAL_9:.*]] = cc.cast %[[VAL_4]] : (!cc.ptr<!cc.array<f32 x 4>>) -> !cc.ptr<f32>
-// CHECK:           %[[VAL_10:.*]] = cc.load %[[VAL_9]] : !cc.ptr<f32>
-// CHECK:           %[[VAL_11:.*]] = cc.alloca f32
+// CHECK-DAG:       %[[VAL_9:.*]] = cc.cast %[[VAL_4]] : (!cc.ptr<!cc.array<f32 x 4>>) -> !cc.ptr<f32>
+// CHECK-DAG:       %[[VAL_10:.*]] = cc.load %[[VAL_9]] : !cc.ptr<f32>
+// CHECK-DAG:       %[[VAL_11:.*]] = cc.alloca f32
 // CHECK:           cc.store %[[VAL_10]], %[[VAL_11]] : !cc.ptr<f32>
 // CHECK:           return
 // CHECK:         }
@@ -47,20 +47,20 @@ __qpu__ void testFrontBool() {
 
 
 // CHECK-LABEL:   func.func @__nvqpp__mlirgen__function_testFrontBool._Z13testFrontBoolv() attributes {"cudaq-entrypoint", "cudaq-kernel", no_this} {
-// CHECK:           %[[VAL_0:.*]] = arith.constant true
-// CHECK:           %[[VAL_1:.*]] = arith.constant false
-// CHECK:           %[[VAL_2:.*]] = cc.alloca !cc.array<i1 x 4>
-// CHECK:           %[[VAL_3:.*]] = cc.compute_ptr %[[VAL_2]][0] : (!cc.ptr<!cc.array<i1 x 4>>) -> !cc.ptr<i1>
+// CHECK-DAG:       %[[VAL_0:.*]] = arith.constant true
+// CHECK-DAG:       %[[VAL_1:.*]] = arith.constant false
+// CHECK-DAG:       %[[VAL_2:.*]] = cc.alloca !cc.array<i1 x 4>
+// CHECK-DAG:       %[[VAL_3:.*]] = cc.compute_ptr %[[VAL_2]][0] : (!cc.ptr<!cc.array<i1 x 4>>) -> !cc.ptr<i1>
 // CHECK:           cc.store %[[VAL_1]], %[[VAL_3]] : !cc.ptr<i1>
-// CHECK:           %[[VAL_4:.*]] = cc.compute_ptr %[[VAL_2]][1] : (!cc.ptr<!cc.array<i1 x 4>>) -> !cc.ptr<i1>
+// CHECK-DAG:       %[[VAL_4:.*]] = cc.compute_ptr %[[VAL_2]][1] : (!cc.ptr<!cc.array<i1 x 4>>) -> !cc.ptr<i1>
 // CHECK:           cc.store %[[VAL_0]], %[[VAL_4]] : !cc.ptr<i1>
-// CHECK:           %[[VAL_5:.*]] = cc.compute_ptr %[[VAL_2]][2] : (!cc.ptr<!cc.array<i1 x 4>>) -> !cc.ptr<i1>
+// CHECK-DAG:       %[[VAL_5:.*]] = cc.compute_ptr %[[VAL_2]][2] : (!cc.ptr<!cc.array<i1 x 4>>) -> !cc.ptr<i1>
 // CHECK:           cc.store %[[VAL_1]], %[[VAL_5]] : !cc.ptr<i1>
-// CHECK:           %[[VAL_6:.*]] = cc.compute_ptr %[[VAL_2]][3] : (!cc.ptr<!cc.array<i1 x 4>>) -> !cc.ptr<i1>
+// CHECK-DAG:       %[[VAL_6:.*]] = cc.compute_ptr %[[VAL_2]][3] : (!cc.ptr<!cc.array<i1 x 4>>) -> !cc.ptr<i1>
 // CHECK:           cc.store %[[VAL_0]], %[[VAL_6]] : !cc.ptr<i1>
-// CHECK:           %[[VAL_7:.*]] = cc.cast %[[VAL_2]] : (!cc.ptr<!cc.array<i1 x 4>>) -> !cc.ptr<i1>
-// CHECK:           %[[VAL_8:.*]] = cc.load %[[VAL_7]] : !cc.ptr<i1>
-// CHECK:           %[[VAL_9:.*]] = cc.alloca i1
+// CHECK-DAG:       %[[VAL_7:.*]] = cc.cast %[[VAL_2]] : (!cc.ptr<!cc.array<i1 x 4>>) -> !cc.ptr<i1>
+// CHECK-DAG:       %[[VAL_8:.*]] = cc.load %[[VAL_7]] : !cc.ptr<i1>
+// CHECK-DAG:       %[[VAL_9:.*]] = cc.alloca i1
 // CHECK:           cc.store %[[VAL_8]], %[[VAL_9]] : !cc.ptr<i1>
 // CHECK:           return
 // CHECK:         }
