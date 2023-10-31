40209ccf5e891a03a1703de4cb7ac0a9193e9282
Eric Schweitz <eschweitz@nvidia.com>
2023-10-31 11:20:43 -0700

    Update test for new canonicalization patterns.

--- test/AST-Quake/vector_front_back.cpp
+++ test/AST-Quake/vector_front_back.cpp
@@ -76,8 +76,6 @@ __qpu__ void testBackFloat() {
 // CHECK-DAG:       %[[VAL_1:.*]] = arith.constant 2.000000e+00 : f32
 // CHECK-DAG:       %[[VAL_2:.*]] = arith.constant 1.000000e+00 : f32
 // CHECK-DAG:       %[[VAL_3:.*]] = arith.constant 0.000000e+00 : f32
-// CHECK-DAG:       %[[VAL_4:.*]] = arith.constant -1 : i64
-// CHECK-DAG:       %[[VAL_5:.*]] = arith.constant 4 : i64
 // CHECK-DAG:       %[[VAL_6:.*]] = cc.alloca !cc.array<f32 x 4>
 // CHECK:           %[[VAL_7:.*]] = cc.compute_ptr %[[VAL_6]][0] : (!cc.ptr<!cc.array<f32 x 4>>) -> !cc.ptr<f32>
 // CHECK:           cc.store %[[VAL_3]], %[[VAL_7]] : !cc.ptr<f32>
@@ -87,11 +85,8 @@ __qpu__ void testBackFloat() {
 // CHECK:           cc.store %[[VAL_1]], %[[VAL_9]] : !cc.ptr<f32>
 // CHECK:           %[[VAL_10:.*]] = cc.compute_ptr %[[VAL_6]][3] : (!cc.ptr<!cc.array<f32 x 4>>) -> !cc.ptr<f32>
 // CHECK:           cc.store %[[VAL_0]], %[[VAL_10]] : !cc.ptr<f32>
-// CHECK:           %[[VAL_11:.*]] = cc.stdvec_init %[[VAL_6]], %[[VAL_5]] : (!cc.ptr<!cc.array<f32 x 4>>, i64) -> !cc.stdvec<f32>
 // CHECK:           %[[VAL_12:.*]] = cc.cast %[[VAL_6]] : (!cc.ptr<!cc.array<f32 x 4>>) -> !cc.ptr<f32>
-// CHECK:           %[[VAL_13:.*]] = cc.stdvec_size %[[VAL_11]] : (!cc.stdvec<f32>) -> i64
-// CHECK:           %[[VAL_14:.*]] = arith.addi %[[VAL_13]], %[[VAL_4]] : i64
-// CHECK:           %[[VAL_15:.*]] = cc.compute_ptr %[[VAL_12]]{{\[}}%[[VAL_14]]] : (!cc.ptr<f32>, i64) -> !cc.ptr<f32>
+// CHECK:           %[[VAL_15:.*]] = cc.compute_ptr %[[VAL_12]][3] : (!cc.ptr<f32>) -> !cc.ptr<f32>
 // CHECK:           %[[VAL_16:.*]] = cc.load %[[VAL_15]] : !cc.ptr<f32>
 // CHECK:           %[[VAL_17:.*]] = cc.alloca f32
 // CHECK:           cc.store %[[VAL_16]], %[[VAL_17]] : !cc.ptr<f32>
@@ -107,8 +102,6 @@ __qpu__ void testBackBool() {
 // CHECK-LABEL:   func.func @__nvqpp__mlirgen__function_testBackBool
 // CHECK-DAG:       %[[VAL_0:.*]] = arith.constant true
 // CHECK-DAG:       %[[VAL_1:.*]] = arith.constant false
-// CHECK-DAG:       %[[VAL_2:.*]] = arith.constant -1 : i64
-// CHECK-DAG:       %[[VAL_3:.*]] = arith.constant 4 : i64
 // CHECK-DAG:       %[[VAL_4:.*]] = cc.alloca !cc.array<i1 x 4>
 // CHECK:           %[[VAL_5:.*]] = cc.compute_ptr %[[VAL_4]][0] : (!cc.ptr<!cc.array<i1 x 4>>) -> !cc.ptr<i1>
 // CHECK:           cc.store %[[VAL_1]], %[[VAL_5]] : !cc.ptr<i1>
@@ -118,11 +111,8 @@ __qpu__ void testBackBool() {
 // CHECK:           cc.store %[[VAL_1]], %[[VAL_7]] : !cc.ptr<i1>
 // CHECK:           %[[VAL_8:.*]] = cc.compute_ptr %[[VAL_4]][3] : (!cc.ptr<!cc.array<i1 x 4>>) -> !cc.ptr<i1>
 // CHECK:           cc.store %[[VAL_0]], %[[VAL_8]] : !cc.ptr<i1>
-// CHECK:           %[[VAL_9:.*]] = cc.stdvec_init %[[VAL_4]], %[[VAL_3]] : (!cc.ptr<!cc.array<i1 x 4>>, i64) -> !cc.stdvec<i1>
 // CHECK:           %[[VAL_10:.*]] = cc.cast %[[VAL_4]] : (!cc.ptr<!cc.array<i1 x 4>>) -> !cc.ptr<i1>
-// CHECK:           %[[VAL_11:.*]] = cc.stdvec_size %[[VAL_9]] : (!cc.stdvec<i1>) -> i64
-// CHECK:           %[[VAL_12:.*]] = arith.addi %[[VAL_11]], %[[VAL_2]] : i64
-// CHECK:           %[[VAL_13:.*]] = cc.compute_ptr %[[VAL_10]]{{\[}}%[[VAL_12]]] : (!cc.ptr<i1>, i64) -> !cc.ptr<i1>
+// CHECK:           %[[VAL_13:.*]] = cc.compute_ptr %[[VAL_10]][3] : (!cc.ptr<i1>) -> !cc.ptr<i1>
 // CHECK:           %[[VAL_14:.*]] = cc.load %[[VAL_13]] : !cc.ptr<i1>
 // CHECK:           %[[VAL_15:.*]] = cc.alloca i1
 // CHECK:           cc.store %[[VAL_14]], %[[VAL_15]] : !cc.ptr<i1>
