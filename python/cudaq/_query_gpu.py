4f31aa81bbfbd91951d03d4b9f0d1fd38808f02f
Pradnya Khalate <pkhalate@nvidia.com>
2023-11-01 14:19:20 -0700

    :art: Applied code formatting for newly added Python code.

--- python/cudaq/_query_gpu.py
+++ python/cudaq/_query_gpu.py
@@ -11,13 +11,16 @@ from pathlib import Path
 import subprocess
 
 # Install directory is wherever this script is and up one directory
-install_dir=Path(__file__).parent.parent.resolve()
+install_dir = Path(__file__).parent.parent.resolve()
+
 
 def is_gpu_available():
     try:
-        gpu_list = subprocess.check_output(["nvidia-smi", "-L"], encoding="utf-8")
+        gpu_list = subprocess.check_output(["nvidia-smi", "-L"],
+                                           encoding="utf-8")
         ngpus = len(gpu_list.splitlines())
-        lib_path = Path(os.path.join(install_dir, "lib/libnvqir-custatevec-fp32.so"))
+        lib_path = Path(
+            os.path.join(install_dir, "lib/libnvqir-custatevec-fp32.so"))
         if ngpus > 0 and lib_path.is_file():
             return True
     except Exception:
