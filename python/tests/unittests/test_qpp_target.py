70f4204a70b1d882b1877e07568d08a5be7cfcaa
Pradnya Khalate <pkhalate@nvidia.com>
2023-11-01 22:04:40 -0700

    * Added basic tests to check setting of the newly defined 'qpp-cpu'   target in C++ and Python.

---
+++ python/tests/unittests/test_qpp_target.py
@@ -0,0 +1,28 @@
+# ============================================================================ #
+# Copyright (c) 2022 - 2023 NVIDIA Corporation & Affiliates.                   #
+# All rights reserved.                                                         #
+#                                                                              #
+# This source code and the accompanying materials are made available under     #
+# the terms of the Apache License 2.0 which accompanies this distribution.     #
+# ============================================================================ #
+
+import pytest
+
+import cudaq
+
+def test_cpu_only_target():
+    """Tests the QPP-based CPU-only target"""
+
+    cudaq.set_target("qpp-cpu")
+    print(cudaq.get_target().name)
+
+    kernel = cudaq.make_kernel()
+    qubits = kernel.qalloc(2)
+    kernel.h(qubits[0])
+    kernel.cx(qubits[0], qubits[1])
+    kernel.mz(qubits)
+
+    result = cudaq.sample(kernel)
+    result.dump()
+    assert '00' in result
+    assert '11' in result
