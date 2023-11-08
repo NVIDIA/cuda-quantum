3023bff67426237e75afebd2ce10fdaa777dd7a1
Pradnya Khalate <pkhalate@nvidia.com>
2023-11-08 10:41:56 -0800

    * Move the environment variable tests out of unittest set up since the   environment variable isn't retained.

--- python/tests/compiler/target_env_var.py
+++ python/tests/compiler/target_env_var.py
@@ -6,6 +6,8 @@
 # the terms of the Apache License 2.0 which accompanies this distribution.     #
 # ============================================================================ #
 
+# RUN: PYTHONPATH=../../ pytest -rP  %s
+
 import os
 os.environ["CUDAQ_DEFAULT_SIMULATOR"] = "density-matrix-cpu"
 
@@ -15,24 +17,8 @@ import cudaq
 
 def test_default_target():
     """Tests the default target set by environment variable"""
+
     assert ("density-matrix-cpu" == cudaq.get_target().name)
-
-    kernel = cudaq.make_kernel()
-    qubits = kernel.qalloc(2)
-    kernel.h(qubits[0])
-    kernel.cx(qubits[0], qubits[1])
-    kernel.mz(qubits)
-
-    result = cudaq.sample(kernel)
-    result.dump()
-    assert '00' in result
-    assert '11' in result
-
-def test_target_override():
-    """Tests the target set by environment variable is overridden by user setting"""
-
-    cudaq.set_target("qpp-cpu")
-    assert("qpp-cpu" == cudaq.get_target().name)
 
     kernel = cudaq.make_kernel()
     qubits = kernel.qalloc(2)
@@ -63,6 +49,23 @@ def test_env_var_with_emulate():
     assert '00' in result
     assert '11' in result
 
+def test_target_override():
+    """Tests the target set by environment variable is overridden by user setting"""
+
+    cudaq.set_target("qpp-cpu")
+    assert("qpp-cpu" == cudaq.get_target().name)
+
+    kernel = cudaq.make_kernel()
+    qubits = kernel.qalloc(2)
+    kernel.h(qubits[0])
+    kernel.cx(qubits[0], qubits[1])
+    kernel.mz(qubits)
+
+    result = cudaq.sample(kernel)
+    result.dump()
+    assert '00' in result
+    assert '11' in result
+
 def test_ignore_invalid_target():
     """Tests that if the environment variable is not a valid target, it is ignored"""
 
@@ -81,3 +84,8 @@ def test_ignore_invalid_target():
     assert '11' in result
 
 os.environ.pop("CUDAQ_DEFAULT_SIMULATOR")
+
+# leave for gdb debugging
+if __name__ == "__main__":
+    loc = os.path.abspath(__file__)
+    pytest.main([loc, "-rP"])
