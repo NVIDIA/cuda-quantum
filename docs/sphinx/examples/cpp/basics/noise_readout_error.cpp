7216a0de5c53f89cb2cb64fdaa3fe60c3d346820
Justin Lietz <jlietz@nvidia.com>
2023-11-20 22:01:43 +0000

    clang-format

--- docs/sphinx/examples/cpp/basics/noise_readout_error.cpp
+++ docs/sphinx/examples/cpp/basics/noise_readout_error.cpp
@@ -18,16 +18,15 @@ int main() {
   // our depolarization channel to.
   cudaq::noise_model noise;
 
-
-  // Readout error model using the simplest constructor. This noise model is that
-  // each qubit is treated independently, and with the same probabilities. Here, the
-  // two arguments are
-  // p0 = p(1|0) = the probability that |0> is measured as |1> and
-  // p1 = p(0|1) = the probability that |1> is measure as |0>.
+  // Readout error model using the simplest constructor. This noise model is
+  // that each qubit is treated independently, and with the same probabilities.
+  // Here, the two arguments are p0 = p(1|0) = the probability that |0> is
+  // measured as |1> and p1 = p(0|1) = the probability that |1> is measure as
+  // |0>.
   cudaq::readout_error_model error_model(0.1, 0.2);
   // Two other constructors worth including could be:
-  // 1) Independent (but different) qubit readout errors. So a p0,p1 pair for each qubit. and
-  // 2) Loading in the full 2^N by 2^N confusion matrix.
+  // 1) Independent (but different) qubit readout errors. So a p0,p1 pair for
+  // each qubit. and 2) Loading in the full 2^N by 2^N confusion matrix.
 
   // We will apply the readout error to the noise model
   noise.add_readout_error(error_model);
@@ -43,8 +42,8 @@ int main() {
   // Now let's set the noise and we're ready to run the simulation!
   cudaq::set_noise(noise);
 
-  // With noise, we should find qubit 0 as been measured in the |1> state about 10% of the time,
-  // and qubit 1 in the |0> state about 80% of the time.
+  // With noise, we should find qubit 0 as been measured in the |1> state about
+  // 10% of the time, and qubit 1 in the |0> state about 80% of the time.
   auto noisy_counts = cudaq::sample(kernel);
   noisy_counts.dump();
 
