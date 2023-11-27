10d6f32b76e1102c746b84374f6d18ffd14c4ba3
Thien Nguyen <58006629+1tnguyen@users.noreply.github.com>
2023-11-28 09:38:34 +1100

    Update runtime/cudaq/distributed/builtin/mpi_comm_impl.cpp
    Co-authored-by: Ben Howe <141149032+bmhowe23@users.noreply.github.com>

--- runtime/cudaq/distributed/builtin/mpi_comm_impl.cpp
+++ runtime/cudaq/distributed/builtin/mpi_comm_impl.cpp
@@ -6,7 +6,7 @@
  * the terms of the Apache License 2.0 which accompanies this distribution.    *
  ******************************************************************************/
 
-/*! \file mpi_comm_impl.h
+/*! \file mpi_comm_impl.cpp
     \brief Reference implementation of CUDAQ MPI interface wrapper
 
     This is an implementation of the MPI shim interface defined in
