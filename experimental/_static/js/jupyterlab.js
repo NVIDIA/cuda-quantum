592953d69a6502101adccb34e636da419324f4e8
Bettina Heim <heimb@outlook.com>
2023-12-04 16:40:38 +0100

    Update jupyterlab.js

--- experimental/_static/js/jupyterlab.js
+++ experimental/_static/js/jupyterlab.js
@@ -1,6 +1,6 @@
 var launchJupyterLab = (frame_id, notebook_path = null) => {
     let token = prompt("Enter Token:");
-    let url="http://localhost:8888/doc/"
+    let url="http://cuda-quantum-1.development.dli-infra.nvidia.com/"; // "http://localhost:8888/doc/"
     var frame = document.getElementById(frame_id);
     var path = notebook_path == null ? "" : "tree/" + notebook_path;
     frame.src = url + path + "?token=" + token;
