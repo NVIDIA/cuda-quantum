4de1441c54c122c632304035e24be5c49d35492d
A.M. Santana <39563805+anthony-santana@users.noreply.github.com>
2023-12-18 15:32:01 -0700

    Support controlled-SWAP's in kernel builder (#924)
    Does the glue work for #918 to perform controlled SWAP's from the python and C++ builder.
    
    Adds new cudaq::ctrl overloads to the SWAP operation in the C++ builder
    New overloads support the following as controls: vector of individual qubits, qubit register, variadic arguments containing a mix of qubits/vecs
    Writes new C++ builder tests for these SWAP functions
    Binds the controlled SWAP gates to python as cswap
    New compiler and unit tests for the python controlled SWAP's
    Adds the python cswap gate to API documentation
    Note: Ignore the random formatting changes to unrelated tests
    
    * upper half of implementation (broken lowering ot qir)
    
    Signed-off-by: A.M. Santana <anthonys@nvidia.com>
    
    * clean up
    
    Signed-off-by: A.M. Santana <anthonys@nvidia.com>
    
    * revert bridge changes
    
    Signed-off-by: A.M. Santana <anthonys@nvidia.com>
    
    * finish implementation and tests
    
    Signed-off-by: A.M. Santana <anthonys@nvidia.com>
    
    * clean up docstring
    
    Signed-off-by: A.M. Santana <anthonys@nvidia.com>
    
    * fix bugs in tests
    
    Signed-off-by: A.M. Santana <anthonys@nvidia.com>
    
    * pr comments
    
    Signed-off-by: A.M. Santana <anthonys@nvidia.com>
    
    * fixes to python docstring
    
    Signed-off-by: A.M. Santana <anthonys@nvidia.com>
    
    ---------
    
    Signed-off-by: A.M. Santana <anthonys@nvidia.com>

--- runtime/cudaq/builder/kernel_builder.h
+++ runtime/cudaq/builder/kernel_builder.h
@@ -586,6 +586,44 @@ public:
     details::swap(*opBuilder, empty, qubits);
   }
 
+  /// @brief SWAP operation for performing a Fredkin gate between two qubits,
+  /// based on the state of input `control` qubit/s.
+  template <typename mod, typename = typename std::enable_if_t<
+                              std::is_same_v<mod, cudaq::ctrl>>>
+  void swap(const QuakeValue &control, const QuakeValue &first,
+            const QuakeValue &second) {
+    const std::vector<QuakeValue> ctrl{control};
+    const std::vector<QuakeValue> targets{first, second};
+    details::swap(*opBuilder, ctrl, targets);
+  }
+
+  /// @brief SWAP operation for performing a Fredkin gate between two qubits,
+  /// based on the state of an input vector of `controls`.
+  template <typename mod, typename = typename std::enable_if_t<
+                              std::is_same_v<mod, cudaq::ctrl>>>
+  void swap(const std::vector<QuakeValue> &controls, const QuakeValue &first,
+            const QuakeValue &second) {
+    const std::vector<QuakeValue> targets{first, second};
+    details::swap(*opBuilder, controls, targets);
+  }
+
+  /// @brief SWAP operation for performing a Fredkin gate between two qubits,
+  /// based on the state of a variadic input of control qubits and registers.
+  /// Note: the final two qubits in the variadic list will always be the qubits
+  /// that undergo a SWAP. This requires >=3 qubits in the arguments.
+  template <
+      typename mod, typename... QubitValues,
+      typename = typename std::enable_if_t<sizeof...(QubitValues) >= 3>,
+      typename = typename std::enable_if_t<std::is_same_v<mod, cudaq::ctrl>>>
+  void swap(QubitValues... args) {
+    std::vector<QuakeValue> values{args...};
+    // Up until the last two arguments will be our controls.
+    const std::vector<QuakeValue> controls(values.begin(), values.end() - 2);
+    // The last two args will be the two qubits to swap.
+    const std::vector<QuakeValue> targets(values.end() - 2, values.end());
+    details::swap(*opBuilder, controls, targets);
+  }
+
   /// @brief Reset the given qubit or qubits.
   void reset(const QuakeValue &qubit) { details::reset(*opBuilder, qubit); }
 
