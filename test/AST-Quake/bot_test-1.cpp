b5bea2ec0cb7d9d8d396344a376c3d1505bc3b6d
Eric Schweitz <eschweitz@nvidia.com>
2024-01-24 07:05:55 -0800

    Test the tester.
    Add tests to reproduce Github task failures.

---
+++ test/AST-Quake/bot_test-1.cpp
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2023 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+// RUN: cudaq-quake %s | FileCheck %s
+
+#include <cudaq.h>
+
+struct Mate {
+  void operator()() __qpu__ {
+    cudaq::qubit q;
+    h(q);
+  }
+};
+
+// CHECK: Mate
