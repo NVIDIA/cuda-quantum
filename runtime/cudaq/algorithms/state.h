0cc76821def27752af75ef7fb9f13c9bebf62e42
Alex McCaskey <amccaskey@nvidia.com>
2024-01-26 16:50:48 +0000

    address PR comments - bring back state.h for deprecation warning, fix spin_op matrix elements ordering
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>

---
+++ runtime/cudaq/algorithms/state.h
@@ -0,0 +1,11 @@
+/****************************************************************-*- C++ -*-****
+ * Copyright (c) 2022 - 2023 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+#pragma once
+#pragma message("cudaq/algorithms/state.h is deprecated, use cudaq/algorithms/get_state.h")
+#include "cudaq/algorithms/get_state.h"
