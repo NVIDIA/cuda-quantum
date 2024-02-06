3ece616fbc7fce126342759bc8b94fc2dc85fac7
Pradnya Khalate <pkhalate@nvidia.com>
2024-02-06 12:06:54 -0800

    * Missing header file that got deleted while resolving merge conflict.

--- runtime/cudaq/platform/default/rest_server/helpers/RestRemoteServer.cpp
+++ runtime/cudaq/platform/default/rest_server/helpers/RestRemoteServer.cpp
@@ -6,6 +6,7 @@
  * the terms of the Apache License 2.0 which accompanies this distribution.    *
  ******************************************************************************/
 
+#include "common/JIT.h"
 #include "common/JsonConvert.h"
 #include "common/Logger.h"
 #include "common/PluginUtils.h"
