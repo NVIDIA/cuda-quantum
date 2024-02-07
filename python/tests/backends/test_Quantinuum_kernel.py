3bccc5d226aa43c2d7e573b4439780a0daa6ea6a
Pradnya Khalate <pkhalate@nvidia.com>
2024-02-06 16:35:53 -0800

    * Additional backend tests on Quantinuum backend for new Python mode   (i.e. 'ast' mode; 'eager' mode is not supported) * Rename existing test with 'builder' suffix

---
+++ python/tests/backends/test_Quantinuum_kernel.py
@@ -0,0 +1,155 @@
+# ============================================================================ #
+# Copyright (c) 2022 - 2024 NVIDIA Corporation & Affiliates.                   #
+# All rights reserved.                                                         #
+#                                                                              #
+# This source code and the accompanying materials are made available under     #
+# the terms of the Apache License 2.0 which accompanies this distribution.     #
+# ============================================================================ #
+
+import cudaq, pytest, os, time
+from cudaq import spin
+from multiprocessing import Process
+try:
+    from utils.mock_qpu.quantinuum import startServer
+except:
+    print("Mock qpu not available, skipping Quantinuum tests.")
+    pytest.skip("Mock qpu not available.", allow_module_level=True)
+
+# Define the port for the mock server
+port = 62440
+
+
+def assert_close(got) -> bool:
+    return got < -1.5 and got > -1.9
+
+
+@pytest.fixture(scope="session", autouse=True)
+def startUpMockServer():
+    # We need a Fake Credentials Config file
+    credsName = '{}/QuantinuumFakeConfig.config'.format(os.environ["HOME"])
+    f = open(credsName, 'w')
+    f.write('key: {}\nrefresh: {}\ntime: 0'.format("hello", "rtoken"))
+    f.close()
+
+    cudaq.set_random_seed(13)
+
+    # Set the targeted QPU
+    cudaq.set_target('quantinuum', url='http://localhost:{}'.format(port))
+
+    # Launch the Mock Server
+    p = Process(target=startServer, args=(port,))
+    p.start()
+    time.sleep(1)
+
+    yield credsName
+
+    # Kill the server, remove the file
+    p.terminate()
+    os.remove(credsName)
+
+
+@pytest.fixture(scope="function", autouse=True)
+def configureTarget(startUpMockServer):
+
+    # Set the targeted QPU with credentials
+    cudaq.set_target('quantinuum',
+                     url='http://localhost:{}'.format(port),
+                     credentials=startUpMockServer)
+
+    yield "Running the test."
+    cudaq.reset_target()
+
+
+def test_quantinuum_sample():
+    # Create the kernel we'd like to execute on Quantinuum
+    @cudaq.kernel(jit=True)
+    def simple():
+        qubits = cudaq.qvector(2)
+        h(qubits[0])
+        x.ctrl(qubits[0], qubits[1])
+        mz(qubits)
+
+    print(simple)
+
+    # Run sample synchronously, this is fine
+    # here in testing since we are targeting a mock
+    # server. In reality you'd probably not want to
+    # do this with the remote job queue.
+    counts = cudaq.sample(simple)
+    counts.dump()
+    assert (len(counts) == 2)
+    assert ('00' in counts)
+    assert ('11' in counts)
+
+    # Run sample, but do so asynchronously. This enters
+    # the execution job into the remote Quantinuum job queue.
+    future = cudaq.sample_async(simple)
+    # We could go do other work, but since this
+    # is a mock server, get the result
+    counts = future.get()
+    assert (len(counts) == 2)
+    assert ('00' in counts)
+    assert ('11' in counts)
+
+    # Ok now this is the most likely scenario, launch the
+    # job asynchronously, this puts it in the queue, now
+    # you can take the future and persist it to file for later.
+    future = cudaq.sample_async(simple)
+    print(future)
+
+    # Persist the future to a file (or here a string,
+    # could write this string to file for later)
+    futureAsString = str(future)
+
+    # Later you can come back and read it in and get
+    # the results, which are now present because the job
+    # made it through the queue
+    futureReadIn = cudaq.AsyncSampleResult(futureAsString)
+    counts = futureReadIn.get()
+    assert (len(counts) == 2)
+    assert ('00' in counts)
+    assert ('11' in counts)
+
+
+def test_quantinuum_observe():
+    # Create the parameterized ansatz
+    @cudaq.kernel(jit=True)
+    def ansatz(theta: float):
+        qreg = cudaq.qvector(2)
+        x(qreg[0])
+        ry(theta, qreg[1])
+        x.ctrl(qreg[1], qreg[0])
+
+    # Define its spin Hamiltonian.
+    hamiltonian = 5.907 - 2.1433 * spin.x(0) * spin.x(1) - 2.1433 * spin.y(
+        0) * spin.y(1) + .21829 * spin.z(0) - 6.125 * spin.z(1)
+
+    # Run the observe task on quantinuum synchronously
+    res = cudaq.observe(ansatz, hamiltonian, .59)
+    assert assert_close(res.expectation())
+
+    ## [SKIP_TEST] : Reason - 'cudaq.observe_async' fails with "AttributeError: 'PyKernelDecorator' object has no attribute 'arguments'"
+    # # Launch it asynchronously, enters the job into the queue
+    # future = cudaq.observe_async(ansatz, hamiltonian, .59)
+    # # Retrieve the results (since we're on a mock server)
+    # res = future.get()
+    # assert assert_close(res.expectation())
+
+    # # Launch the job async, job goes in the queue, and
+    # # we're free to dump the future to file
+    # future = cudaq.observe_async(ansatz, hamiltonian, .59)
+    # print(future)
+    # futureAsString = str(future)
+
+    # # Later you can come back and read it in
+    # # You must provide the spin_op so we can reconstruct
+    # # the results from the term job ids.
+    # futureReadIn = cudaq.AsyncObserveResult(futureAsString, hamiltonian)
+    # res = futureReadIn.get()
+    # assert assert_close(res.expectation())
+
+
+# leave for gdb debugging
+if __name__ == "__main__":
+    loc = os.path.abspath(__file__)
+    pytest.main([loc, "-s"])
