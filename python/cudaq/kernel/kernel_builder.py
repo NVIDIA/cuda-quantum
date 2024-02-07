d304af9fc6398d03f68b7d8eb6c9126339ae7f95
Alex McCaskey <amccaskey@nvidia.com>
2024-02-07 17:46:25 +0000

    rename test files, remove print stmt
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>

--- python/cudaq/kernel/kernel_builder.py
+++ python/cudaq/kernel/kernel_builder.py
@@ -986,7 +986,6 @@ class PyKernel(object):
             else:
                 processedArgs.append(arg)
 
-        print("HELLO: ", processedArgs)
         cudaq_runtime.pyAltLaunchKernel(self.name, self.module, *processedArgs)
 
 
