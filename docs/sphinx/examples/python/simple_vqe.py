2c8bf913cdfc816947f35140c0e6140af07cd665
Bettina Heim <heimb@outlook.com>
2024-02-15 20:48:16 +0100

    Update simple_vqe.py

--- docs/sphinx/examples/python/simple_vqe.py
+++ docs/sphinx/examples/python/simple_vqe.py
@@ -10,15 +10,20 @@ hamiltonian = 5.907 - 2.1433 * spin.x(0) * spin.x(1) - 2.1433 * spin.y(
 # Next, using the `cudaq.Kernel`, we define the variational quantum circuit
 # that we'd like to use as an ansatz.
 # Create a kernel that takes a list of floats as a function argument.
-kernel, thetas = cudaq.make_kernel(list)
-# Allocate 2 qubits.
-qubits = kernel.qalloc(2)
-kernel.x(qubits[0])
-# Apply an `ry` gate that is parameterized by the first
-# `QuakeValue` entry of our list, `thetas`.
-kernel.ry(thetas[0], qubits[1])
-kernel.cx(qubits[1], qubits[0])
-# Note: the kernel must not contain measurement instructions.
+@cudaq.kernel(jit=True)
+def kernel(thetas: list):
+
+
+    # Allocate 2 qubits.
+    qubits = cudaq.qvector(2)
+    x(qubits[0])
+
+    # Apply an `ry` gate that is parameterized by the first
+    # `QuakeValue` entry of our list, `thetas`.
+    ry(thetas[0], qubits[1])
+    x.ctrl(qubits[1], qubits[0])
+
+    # Note: the kernel must not contain measurement instructions.
 
 # The last thing we need is to pick an optimizer from the suite of `cudaq.optimizers`.
 # We can optionally tune this optimizer through its initial parameters, iterations,
@@ -30,7 +35,6 @@ optimizer = cudaq.optimizers.COBYLA()
 # Finally, we pass all of that into `cudaq.vqe`, and it will automatically run our
 # optimization loop, returning a tuple of the minimized eigenvalue of our `spin_operator`
 # and the list of optimal variational parameters.
-
 energy, parameter = cudaq.vqe(
     kernel=kernel,
     spin_operator=hamiltonian,
