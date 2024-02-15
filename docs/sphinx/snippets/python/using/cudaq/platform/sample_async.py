76868ca2404a0f7d76d865656bb66451b20a2ce9
Bettina Heim <heimb@outlook.com>
2024-02-15 20:53:18 +0100

    Update sample_async.py

--- docs/sphinx/snippets/python/using/cudaq/platform/sample_async.py
+++ docs/sphinx/snippets/python/using/cudaq/platform/sample_async.py
@@ -13,12 +13,15 @@ target = cudaq.get_target()
 num_qpus = target.num_qpus()
 print("Number of QPUs:", num_qpus)
 
-kernel, runtime_param = cudaq.make_kernel(int)
-qubits = kernel.qalloc(runtime_param)
-# Place qubits in superposition state.
-kernel.h(qubits)
-# Measure.
-kernel.mz(qubits)
+@cudaq.kernel(jit=True)
+def kernel(nr_qubits: int):
+
+    qubits = cudaq.qvector(nr_qubits)
+    # Place qubits in superposition state.
+    kernel.h(qubits)
+    # Measure.
+    kernel.mz(qubits)
+
 
 count_futures = []
 for qpu in range(num_qpus):
