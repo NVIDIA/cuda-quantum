6df1df38bbdda6388f8a8718dad046348dcfd4b8
Thien Nguyen <thiennguyen@nvidia.com>
2024-02-23 06:46:20 +0000

    Fix test

--- python/tests/remote/test_error_recovery.py
+++ python/tests/remote/test_error_recovery.py
@@ -21,41 +21,43 @@ import cudaq
 # Define the random port for the mock server
 port = 62433
 
+
 @pytest.fixture(scope="session", autouse=True)
 def startUpMockServer():
     cudaq_path = os.path.dirname(os.path.realpath(cudaq.__file__))
     os.environ["PATH"] += os.pathsep + os.path.join(cudaq_path, "../bin")
     # Launch the QPU daemon Server
     proc = subprocess.Popen(['cudaq-qpud', '--port', str(port)], shell=False)
-    time.sleep(3) 
+    time.sleep(3)
     yield "Running the tests."
     # Kill the server
     proc.kill()
 
 
 def test_recovery():
-    url_str = "localhost:"+str(port)
-    cudaq.set_target("remote-mqpu", url=url_str, backend="tensornet")
+    url_str = "localhost:" + str(port)
+    cudaq.set_target("remote-mqpu", url=url_str)
     poison_kernel = cudaq.make_kernel()
     # 50 qubits => should cause an OOM
     qubits = poison_kernel.qalloc(50)
     poison_kernel.h(qubits[0])
     poison_kernel.cx(qubits[0], qubits[1])
     poison_kernel.mz(qubits)
-    # with pytest.raises(RuntimeError) as error:
-    counts = cudaq.sample(poison_kernel, shots_count=10)
-    
+    with pytest.raises(RuntimeError) as error:
+        counts = cudaq.sample(poison_kernel)
+
     # Now run another kernel afterward
     kernel = cudaq.make_kernel()
     qubits = kernel.qalloc(2)
     kernel.h(qubits[0])
     kernel.cx(qubits[0], qubits[1])
     kernel.mz(qubits)
-    counts = cudaq.sample(kernel)    
+    counts = cudaq.sample(kernel)
     print(counts)
     assert '11' in counts
     assert '00' in counts
-        
+
+
 # leave for gdb debugging
 if __name__ == "__main__":
     loc = os.path.abspath(__file__)
