eb7bc2fc8e37301d9f90bc7d5f7372bf8b164744
Alex McCaskey <amccaskey@nvidia.com>
2024-02-27 10:55:49 -0500

    Finish up circuit drawing, expose to python (#1299)
    * [rt] Unitary circuit drawing
    
    * Enable circuit drawing for any simulator target, expose to python
    
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>
    
    * clang format
    
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>
    
    * Fix bug in LowerToQIR for S/T adjoint, add the circuit print test
    
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>
    
    ---------
    
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>
    Co-authored-by: Bruno Schmitt <bschmitt@nvidia.com>

---
+++ python/runtime/cudaq/algorithms/py_draw.cpp
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2024 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+#include "cudaq/algorithms/draw.h"
+#include "utils/OpaqueArguments.h"
+#include "mlir/Bindings/Python/PybindAdaptors.h"
+
+#include <iostream>
+#include <pybind11/complex.h>
+#include <pybind11/stl.h>
+
+namespace cudaq {
+
+void pyAltLaunchKernel(const std::string &, MlirModule, OpaqueArguments &,
+                       const std::vector<std::string> &);
+
+/// @brief Run `cudaq::get_state` on the provided kernel and spin operator.
+std::string pyDraw(py::object &kernel, py::args args) {
+
+  if (py::hasattr(kernel, "compile"))
+    kernel.attr("compile")();
+
+  auto kernelName = kernel.attr("name").cast<std::string>();
+  auto kernelMod = kernel.attr("module").cast<MlirModule>();
+  args = simplifiedValidateInputArguments(args);
+  auto *argData = toOpaqueArgs(args);
+
+  return details::extractTrace([&]() mutable {
+    pyAltLaunchKernel(kernelName, kernelMod, *argData, {});
+    delete argData;
+  });
+}
+
+/// @brief Bind the draw cudaq function
+void bindPyDraw(py::module &mod) { mod.def("draw", &pyDraw, ""); }
+
+} // namespace cudaq
