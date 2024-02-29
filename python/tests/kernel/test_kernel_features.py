54eb4318d231f42f60d0ae457c6e4af1e21b901c
Alex McCaskey <amccaskey@nvidia.com>
2024-02-29 14:50:54 -0500

    Implement proper Pythonic capture of variables from parent scope (#1314)
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>

--- python/tests/kernel/test_kernel_features.py
+++ python/tests/kernel/test_kernel_features.py
@@ -14,6 +14,7 @@ from typing import Callable, List
 import sys
 
 import cudaq
+from cudaq import spin
 
 ## [PYTHON_VERSION_FIX]
 skipIfPythonLessThan39 = pytest.mark.skipif(
@@ -419,6 +420,12 @@ def test_simple_return_types():
         def kernel(a: int, b: int):  # No return type
             return a * b
 
+    @cudaq.kernel
+    def boolKernel() -> bool:
+        return True
+
+    assert boolKernel()
+
 
 def test_list_creation():
 
@@ -559,16 +566,113 @@ def test_sample_async_issue_args_processed():
     assert len(counts) == 2 and '01' in counts and '10' in counts
 
 
-def test_capture_vars_disallowed():
+def test_capture_vars():
 
     n = 5
+    f = 0.0
+    m = 5
+    hello = str()
 
     @cudaq.kernel
-    def test():
+    def kernel():
         q = cudaq.qvector(n)
+        x(q)
+        cudaq.dbg.ast.print_f64(f)
+        for qb in q:
+            rx(f, qb)
+
+    counts = cudaq.sample(kernel)
+    counts.dump()
+    assert '1' * n in counts
+
+    f = np.pi
+    counts = cudaq.sample(kernel)
+    counts.dump()
+    assert '0' * n in counts
+
+    n = 7
+    f = 0.0
+    counts = cudaq.sample(kernel)
+    counts.dump()
+    assert '1' * n in counts
+
+    counts = cudaq.sample(kernel)
+    counts.dump()
+    assert '1' * n in counts
+
+    n = 3
+    counts = cudaq.sample(kernel)
+    counts.dump()
+    assert '1' * n in counts
+
+    @cudaq.kernel
+    def testCanOnlyCaptureIntAndFloat():
+        i = hello
 
     with pytest.raises(RuntimeError) as e:
-        test()
+        testCanOnlyCaptureIntAndFloat()
+
+    b = True
+
+    @cudaq.kernel
+    def canCaptureBool():
+        q = cudaq.qubit()
+        if b:
+            x(q)
+
+    counts = cudaq.sample(canCaptureBool)
+    counts.dump()
+    assert len(counts) == 1 and '1' in counts
+
+    b = False
+    counts = cudaq.sample(canCaptureBool)
+    counts.dump()
+    assert len(counts) == 1 and '0' in counts
+
+    l = [.59]
+    li = [0, 1]
+
+    @cudaq.kernel
+    def canCaptureList():
+        q = cudaq.qvector(2)
+        firstIdx = li[0]
+        secondIdx = li[1]
+        x(q[firstIdx])
+        ry(l[firstIdx], q[secondIdx])
+        x.ctrl(q[secondIdx], q[firstIdx])
+
+    hamiltonian = 5.907 - 2.1433 * spin.x(0) * spin.x(1) - 2.1433 * spin.y(
+        0) * spin.y(1) + .21829 * spin.z(0) - 6.125 * spin.z(1)
+    assert np.isclose(-1.748,
+                      cudaq.observe(canCaptureList, hamiltonian).expectation(),
+                      atol=1e-3)
+
+
+def test_inner_function_capture():
+
+    n = 3
+    m = 5
+
+    def innerClassical():
+
+        @cudaq.kernel()
+        def foo():
+            q = cudaq.qvector(n)
+
+        def innerInnerClassical():
+
+            @cudaq.kernel()
+            def bar():
+                q = cudaq.qvector(m)
+                x(q)
+
+            return cudaq.sample(bar)
+
+        return cudaq.sample(foo), innerInnerClassical()
+
+    fooCounts, barCounts = innerClassical()
+    assert len(fooCounts) == 1 and '0' * n in fooCounts
+    assert len(barCounts) == 1 and '1' * m in barCounts
 
 
 def test_error_qubit_constructor():
