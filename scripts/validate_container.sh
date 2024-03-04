86ddeb7b9c4d44c8588dae25d05935d611729e14
Ben Howe <bhowe@nvidia.com>
2024-03-04 14:35:14 +0000

    Revert "Trim trailing whitespace"
    This reverts commit 34bb9782c2fbd4d87b85a1850f558ded26cdd995.

--- scripts/validate_container.sh
+++ scripts/validate_container.sh
@@ -9,7 +9,7 @@
 # ============================================================================ #
 
 # Usage:
-# Launch the NVIDIA CUDA Quantum Docker container,
+# Launch the NVIDIA CUDA Quantum Docker container, 
 # and run this script from the home directory.
 # Check the logged output.
 
@@ -18,45 +18,45 @@ failed=0
 skipped=0
 samples=0
 
-if [ -x "$(command -v nvidia-smi)" ] && [ "$(nvidia-smi | egrep -o "CUDA Version: ([0-9]{1,}\.)+[0-9]{1,}")" != "" ];
+if [ -x "$(command -v nvidia-smi)" ] && [ "$(nvidia-smi | egrep -o "CUDA Version: ([0-9]{1,}\.)+[0-9]{1,}")" != "" ]; 
 then gpu_available=true
 else gpu_available=false
 fi
 
-if $gpu_available;
+if $gpu_available; 
 then echo "GPU detected." && nvidia-smi
 else echo "No GPU detected."
-fi
+fi 
 
-if [ -x "$(command -v ssh -V)" ];
+if [ -x "$(command -v ssh -V)" ]; 
 then ssh_available=true
 else ssh_available=false
 fi
 
-if $ssh_available;
-then echo "SSH Client detected."
+if $ssh_available; 
+then echo "SSH Client detected." 
 else echo "No SSH Client detected."
-fi
+fi 
 
-if [ -x "$(command -v mpiexec --version)" ];
+if [ -x "$(command -v mpiexec --version)" ]; 
 then mpi_available=true
 else mpi_available=false
 fi
-if $mpi_available;
+if $mpi_available; 
 then echo "MPI detected."
 else echo "No MPI detected."
-fi
+fi 
 
 # We shouldn't run python validation on images that don't contain Python (i.e.
 # the Installer validation images).
-if [ -x "$(command -v python3)" ];
+if [ -x "$(command -v python3)" ]; 
 then python_available=true
 else python_available=false
 fi
-if $python_available;
+if $python_available; 
 then echo "Python detected."
 else echo "No Python detected."
-fi
+fi 
 
 export UCX_LOG_LEVEL=warning
 requested_backends=`\
@@ -70,15 +70,15 @@ installed_backends=`\
     do basename $file | cut -d "." -f 1; \
     done`
 
-# remote_rest targets are automatically filtered,
+# remote_rest targets are automatically filtered, 
 # so is execution on the photonics backend
 available_backends=`\
     echo "default"
     for file in $(ls $CUDA_QUANTUM_PATH/targets/*.config); \
     do
-        if grep -q "LIBRARY_MODE_EXECUTION_MANAGER=photonics" $file ; then
+        if grep -q "LIBRARY_MODE_EXECUTION_MANAGER=photonics" $file ; then 
           continue
-        fi
+        fi 
         platform=$(cat $file | grep "PLATFORM_QPU=")
         qpu=${platform#PLATFORM_QPU=}
         requirements=$(cat $file | grep "GPU_REQUIREMENTS=")
@@ -102,7 +102,7 @@ else
             echo "No backend configuration found for $t."
             missing_backend=true
         fi
-    done
+    done    
 fi
 
 echo
@@ -116,10 +116,10 @@ echo "Testing backends:"
 echo "$requested_backends"
 echo
 
-if $missing_backend || [ "$available_backends" == "" ];
+if $missing_backend || [ "$available_backends" == "" ]; 
 then
     echo "Abort due to missing backend configuration."
-    exit 1
+    exit 1 
 fi
 
 # Skip some tests (multi-controlled gates) for the MPS backend;
@@ -135,7 +135,7 @@ echo "==      Python Tests       =="
 echo "============================="
 
 for ex in `find examples/ -name '*.py'`;
-do
+do 
     filename=$(basename -- "$ex")
     filename="${filename%.*}"
     echo "Testing $filename:"
@@ -150,11 +150,11 @@ do
         python3 $ex 1> /dev/null
         status=$?
         echo "Exited with code $status"
-        if [ "$status" -eq "0" ]; then
+        if [ "$status" -eq "0" ]; then 
             let "passed+=1"
         else
             let "failed+=1"
-        fi
+        fi 
     fi
     echo "============================="
 done
@@ -172,7 +172,7 @@ do
     echo "Source: $ex"
     let "samples+=1"
 
-    # Look for a --target flag to nvq++ in the
+    # Look for a --target flag to nvq++ in the 
     # comment block at the beginning of the file.
     intended_target=`sed -e '/^$/,$d' $ex | grep -oP '^//\s*nvq++.+--target\s+\K\S+'`
     if [ -n "$intended_target" ]; then
@@ -184,7 +184,7 @@ do
         if [ "$t" == "default" ]; then target_flag=""
         else target_flag="--target $t"
         fi
-
+    
         if [ -n "$intended_target" ] && [ "$intended_target" != "$t" ];
         then
             let "skipped+=1"
@@ -208,7 +208,7 @@ do
         elif [ "$t" == "remote-mqpu" ]; then
 
             # Skipped long-running tests (variational optimization loops) for the "remote-mqpu" target to keep CI runtime managable.
-            # A simplified test for these use cases is included in the 'test/Remote-Sim/' test suite.
+            # A simplified test for these use cases is included in the 'test/Remote-Sim/' test suite. 
             # Skipped tests that require passing kernel callables to entry-point kernels for the "remote-mqpu" target.
             if [[ "$ex" == *"vqe_h2"* || "$ex" == *"qaoa_maxcut"* || "$ex" == *"gradients"* || "$ex" == *"grover"* || "$ex" == *"multi_controlled_operations"* || "$ex" == *"phase_estimation"* ]];
             then
@@ -217,7 +217,7 @@ do
                 echo ":white_flag: $filename: Not executed for performance reasons. Test skipped." >> "${tmpFile}_$(echo $t | tr - _)"
                 continue
 
-            # Don't run remote-mqpu if the MPI installation is incomplete (e.g., missing an ssh-client).
+            # Don't run remote-mqpu if the MPI installation is incomplete (e.g., missing an ssh-client).            
             elif [[ "$mpi_available" == true && "$ssh_available" == false ]];
             then
                 let "skipped+=1"
@@ -233,7 +233,7 @@ do
         fi
 
         echo "Testing on $t target..."
-        nvq++ $ex $target_flag
+        nvq++ $ex $target_flag 
         if [ ! $? -eq 0 ]; then
             let "failed+=1"
             echo ":x: Compilation failed for $filename." >> "${tmpFile}_$(echo $t | tr - _)"
@@ -243,23 +243,23 @@ do
         ./a.out &> /tmp/cudaq_validation.out
         status=$?
         echo "Exited with code $status"
-        if [ "$status" -eq "0" ]; then
+        if [ "$status" -eq "0" ]; then 
             let "passed+=1"
             echo ":white_check_mark: Successfully ran $filename." >> "${tmpFile}_$(echo $t | tr - _)"
         else
             cat /tmp/cudaq_validation.out
             let "failed+=1"
             echo ":x: Failed to execute $filename." >> "${tmpFile}_$(echo $t | tr - _)"
-        fi
+        fi 
         rm a.out /tmp/cudaq_validation.out &> /dev/null
     done
     echo "============================="
 done
 
-if [ -f "$GITHUB_STEP_SUMMARY" ];
+if [ -f "$GITHUB_STEP_SUMMARY" ]; 
 then
     for t in $requested_backends
-    do
+    do  
         file="${tmpFile}_$(echo $t | tr - _)"
         if [ -f "$file" ]; then
             echo "## Execution on $t target" >> $GITHUB_STEP_SUMMARY
