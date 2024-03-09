3384ce5b655357487ace37bb69db86d422478cb9
Ben Howe <bhowe@nvidia.com>
2024-03-09 03:00:06 +0000

    Follow-up to #1363 - Fix test for Python 3.8 compatibility

--- python/tests/builder/test_SpinOperator.py
+++ python/tests/builder/test_SpinOperator.py
@@ -377,7 +377,7 @@ def test_spin_op_from_word():
 # 30 qubits
 def test_spin_op_serdes():
     for nq in range(1, 31):
-        for nt in range(1, nq):
+        for nt in range(1, nq + 1):
             h1 = cudaq.SpinOperator.random(qubit_count=nq, term_count=nt)
             h2 = h1.serialize()
             h3 = cudaq.SpinOperator(h2, nq)
