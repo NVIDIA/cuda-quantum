09710388f890dd6ca2699fe8be4c15dd2080f355
Pradnya Khalate <148914294+khalatepradnya@users.noreply.github.com>
2024-03-11 14:53:46 -0700

    Restore registry.h

--- runtime/cudaq/utils/registry.h
+++ runtime/cudaq/utils/registry.h
@@ -1,5 +1,5 @@
 /****************************************************************-*- C++ -*-****
- * Copyright (c) 2022 - 2024 NVIDIA Corporation & Affiliates.                  *
+ * Copyright (c) 2022 - 2023 NVIDIA Corporation & Affiliates.                  *
  * All rights reserved.                                                        *
  *                                                                             *
  * This source code and the accompanying materials are made available under    *
@@ -11,7 +11,6 @@
 
 namespace cudaq {
 namespace registry {
-
 extern "C" {
 void deviceCodeHolderAdd(const char *, const char *);
 void cudaqRegisterKernelName(const char *);
