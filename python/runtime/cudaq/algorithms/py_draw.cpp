9590638093c6dbfa63db43c6f7b16e916bd812bb
Pradnya Khalate <148914294+khalatepradnya@users.noreply.github.com>
2024-03-13 11:51:11 -0700

    * Add docstrings for 'draw' (#1377)

--- python/runtime/cudaq/algorithms/py_draw.cpp
+++ python/runtime/cudaq/algorithms/py_draw.cpp
@@ -18,7 +18,7 @@ namespace cudaq {
 void pyAltLaunchKernel(const std::string &, MlirModule, OpaqueArguments &,
                        const std::vector<std::string> &);
 
-/// @brief Run `cudaq::get_state` on the provided kernel and spin operator.
+/// @brief Run `cudaq::draw` on the provided kernel.
 std::string pyDraw(py::object &kernel, py::args args) {
 
   if (py::len(kernel.attr("arguments")) != args.size())
@@ -39,6 +39,50 @@ std::string pyDraw(py::object &kernel, py::args args) {
 }
 
 /// @brief Bind the draw cudaq function
-void bindPyDraw(py::module &mod) { mod.def("draw", &pyDraw, ""); }
+void bindPyDraw(py::module &mod) {
+  mod.def(
+      "draw", &pyDraw,
+      R"#(Return a UTF-8 encoded string representing drawing of the execution 
+path, i.e., the trace, of the provided `kernel`.
+      
+Args:
+  kernel (:class:`Kernel`): The :class:`Kernel` to draw.
+  *arguments (Optional[Any]): The concrete values to evaluate the kernel 
+    function at. Leave empty if the kernel doesn't accept any arguments.
+
+Returns:
+  The UTF-8 encoded string of the circuit, without measurement operations.
+
+.. code-block:: python
+
+  # Example
+  import cudaq
+  @cudaq.kernel
+  def bell_pair():
+      q = cudaq.qvector(2)
+      h(q[0])
+      cx(q[0], q[1])
+      mz(q)
+  print(cudaq.draw(bell_pair))
+  # Output
+  #      ╭───╮     
+  # q0 : ┤ h ├──●──
+  #      ╰───╯╭─┴─╮
+  # q1 : ─────┤ x ├
+  #           ╰───╯
+  
+  # Example with arguments
+  import cudaq
+  @cudaq.kernel
+  def kernel(angle:float):
+      q = cudaq.qubit()
+      h(q)
+      ry(angle, q)
+  print(cudaq.draw(kernel, 0.59))
+  # Output
+  #      ╭───╮╭──────────╮
+  # q0 : ┤ h ├┤ ry(0.59) ├
+  #      ╰───╯╰──────────╯)#");
+}
 
 } // namespace cudaq
