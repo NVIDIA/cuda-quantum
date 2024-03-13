7d5158426f6672ca48a45e5b0aa0e6efd63b557d
Thien Nguyen <thiennguyen@nvidia.com>
2024-03-13 06:00:03 +0000

    Address code review

--- lib/Optimizer/Transforms/GenDeviceCodeLoader.cpp
+++ lib/Optimizer/Transforms/GenDeviceCodeLoader.cpp
@@ -23,11 +23,22 @@
 using namespace mlir;
 
 namespace llvm {
-// FIXME: `GraphTraits<const mlir::CallGraphNode *>` as defined in
-// "mlir/Analysis/CallGraph.h" has a bug: missing `const` in `using NodeRef =
-// mlir::CallGraphNode *`. This makes it unusable (causing compiler errors).
+// FIXME: `GraphTraits` specialization for `const mlir::CallGraphNode *` in
+// "mlir/Analysis/CallGraph.h" has a bug.
+// In particular, `GraphTraits<const mlir::CallGraphNode *>` typedef'ed `NodeRef
+// -> mlir::CallGraphNode *`, (without `const`), causing problems when using
+// `mlir::CallGraphNode` with graph iterator (e.g., `llvm::df_iterator`). The
+// entry node getter has the signature `NodeRef getEntryNode(NodeRef node)`,
+// i.e., `mlir::CallGraphNode * getEntryNode(mlir::CallGraphNode * node)`; but a
+// graph iterator for `const mlir::CallGraphNode *` will pass a `const
+// mlir::CallGraphNode *` to that `getEntryNode` function => compile error.
 // Here, we define a non-const overload, which hasn't been defined, to work
 // around that issue.
+//
+// Note: this isn't an issue for the whole `mlir::CallGraph` graph, i.e.,
+// `GraphTraits<const mlir::CallGraph *>`. `getEntryNode` is defined as
+// `getExternalCallerNode`, which is a const method of `mlir::CallGraph`.
+
 template <>
 struct GraphTraits<mlir::CallGraphNode *> {
   using NodeRef = mlir::CallGraphNode *;
@@ -121,11 +132,11 @@ public:
         mlir::CallGraphNode *node =
             callGraph.lookupNode(funcOp.getCallableRegion());
         // Iterate over all dependent kernels starting at this node.
-        for (auto Itr = llvm::df_begin(node), ItrE = llvm::df_end(node);
-             Itr != ItrE; ++Itr) {
+        for (auto it = llvm::df_begin(node), itEnd = llvm::df_end(node);
+             it != itEnd; ++it) {
           // Only consider those that are defined in this module.
-          if (!Itr->isExternal()) {
-            auto *callableRegion = Itr->getCallableRegion();
+          if (!it->isExternal()) {
+            auto *callableRegion = it->getCallableRegion();
             auto parentFuncOp =
                 callableRegion->getParentOfType<mlir::func::FuncOp>();
             LLVM_DEBUG(llvm::dbgs() << "  Adding dependent function "
