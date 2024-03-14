3594cc53ce3e7f1fe97dc93d69084108d24365d2
Bettina Heim <>
2024-03-14 13:05:32 -0700

    review comment

--- docs/sphinx/snippets/cpp/quick_start.cpp
+++ docs/sphinx/snippets/cpp/quick_start.cpp
@@ -10,18 +10,18 @@
 // [Begin Documentation]
 #include <cudaq.h>
 
-__qpu__ void kernel(int n_qubits) {
-  cudaq::qvector qubits(n_qubits);
+__qpu__ void kernel(int qubit_count) {
+  cudaq::qvector qubits(qubit_count);
   h(qubits[0]);
-  for (auto i = 1; i < n_qubits; ++i) {
+  for (auto i = 1; i < qubit_count; ++i) {
     cx(qubits[0], qubits[i]);
   }
   mz(qubits);
 }
 
 int main(int argc, char *argv[]) {
-  auto n_qubits = 1 < argc ? atoi(argv[1]) : 2;
-  auto result = cudaq::sample(kernel, n_qubits);
+  auto qubit_count = 1 < argc ? atoi(argv[1]) : 2;
+  auto result = cudaq::sample(kernel, qubit_count);
   result.dump(); // Example: { 11:500 00:500 }
 }
 // [End Documentation]
