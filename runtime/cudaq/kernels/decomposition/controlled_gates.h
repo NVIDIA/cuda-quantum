18886a5533fc5f999b40f5c9701bb53f745a350d
Thien Nguyen <thiennguyen@nvidia.com>
2024-03-15 05:47:49 +0000

    Lib mode for all multi-control decomp

--- runtime/cudaq/kernels/decomposition/controlled_gates.h
+++ runtime/cudaq/kernels/decomposition/controlled_gates.h
@@ -98,68 +98,121 @@ void x(const std::vector<std::reference_wrapper<cudaq::qubit>> &ctrls,
   }
 }
 
-template <typename mod>
-void z(const std::vector<std::reference_wrapper<cudaq::qubit>> &ctrls,
-       cudaq::qubit &target) __qpu__ {
-  static_assert(std::is_same_v<mod, cudaq::ctrl>);
-  const std::size_t numCtrls = ctrls.size();
-  if (numCtrls == 0) {
-    z(target);
-  } else if (numCtrls == 1) {
-    z<cudaq::ctrl>(ctrls[0].get(), target);
-  } else {
-    cudaq::qvector aux(numCtrls - 1);
-    cudaq::compute_action(
-        [&]() {
-          CollectControls(ctrls, aux, 0);
-          AdjustForSingleControl(ctrls, aux);
-        },
-        [&]() { z<cudaq::ctrl>(aux[ctrls.size() - 2], target); });
+#define CUDAQ_ONE_TARGET_QUBIT_MULTI_CONTROL_IMPL(NAME)                        \
+  template <typename mod>                                                      \
+  void NAME(const std::vector<std::reference_wrapper<cudaq::qubit>> &ctrls,    \
+            cudaq::qubit &target) __qpu__ {                                    \
+    static_assert(std::is_same_v<mod, cudaq::ctrl>);                           \
+    const std::size_t numCtrls = ctrls.size();                                 \
+    if (numCtrls == 0) {                                                       \
+      NAME(target);                                                            \
+    } else if (numCtrls == 1) {                                                \
+      NAME<cudaq::ctrl>(ctrls[0].get(), target);                               \
+    } else {                                                                   \
+      cudaq::qvector aux(numCtrls - 1);                                        \
+      cudaq::compute_action(                                                   \
+          [&]() {                                                              \
+            CollectControls(ctrls, aux, 0);                                    \
+            AdjustForSingleControl(ctrls, aux);                                \
+          },                                                                   \
+          [&]() { NAME<cudaq::ctrl>(aux[ctrls.size() - 2], target); });        \
+    }                                                                          \
   }
-}
+
+CUDAQ_ONE_TARGET_QUBIT_MULTI_CONTROL_IMPL(h)
+CUDAQ_ONE_TARGET_QUBIT_MULTI_CONTROL_IMPL(y)
+CUDAQ_ONE_TARGET_QUBIT_MULTI_CONTROL_IMPL(z)
+CUDAQ_ONE_TARGET_QUBIT_MULTI_CONTROL_IMPL(t)
+CUDAQ_ONE_TARGET_QUBIT_MULTI_CONTROL_IMPL(s)
+
+#define CUDAQ_ONE_TARGET_QUBIT_ONE_PARAM_MULTI_CONTROL_IMPL(NAME)              \
+  template <typename mod, typename ScalarAngle>                                \
+  void NAME(ScalarAngle angle,                                                 \
+            const std::vector<std::reference_wrapper<cudaq::qubit>> &ctrls,    \
+            cudaq::qubit &target) __qpu__ {                                    \
+    static_assert(std::is_same_v<mod, cudaq::ctrl>);                           \
+    const std::size_t numCtrls = ctrls.size();                                 \
+    if (numCtrls == 0) {                                                       \
+      NAME(angle, target);                                                     \
+    } else if (numCtrls == 1) {                                                \
+      oneQubitSingleParameterApply<qubit_op::NAME##Op, mod>(                   \
+          angle, ctrls[0].get(), target);                                      \
+    } else {                                                                   \
+      cudaq::qvector aux(numCtrls - 1);                                        \
+      cudaq::compute_action(                                                   \
+          [&]() {                                                              \
+            CollectControls(ctrls, aux, 0);                                    \
+            AdjustForSingleControl(ctrls, aux);                                \
+          },                                                                   \
+          [&]() {                                                              \
+            oneQubitSingleParameterApply<qubit_op::NAME##Op, mod>(             \
+                angle, aux[ctrls.size() - 2], target);                         \
+          });                                                                  \
+    }                                                                          \
+  }
+
+CUDAQ_ONE_TARGET_QUBIT_ONE_PARAM_MULTI_CONTROL_IMPL(rx)
+CUDAQ_ONE_TARGET_QUBIT_ONE_PARAM_MULTI_CONTROL_IMPL(ry)
+CUDAQ_ONE_TARGET_QUBIT_ONE_PARAM_MULTI_CONTROL_IMPL(rz)
+CUDAQ_ONE_TARGET_QUBIT_ONE_PARAM_MULTI_CONTROL_IMPL(r1)
 } // namespace internal
 
-template <typename mod, typename... QubitTy>
-void x(cudaq::qubit &c0, cudaq::qubit &c1, QubitTy &...qubits) __qpu__ {
-  static_assert(std::is_same_v<mod, cudaq::ctrl>);
-  std::vector<std::reference_wrapper<cudaq::qubit>> ctls{{qubits...}};
-  // Last qubit is the target
-  ctls.pop_back();
-  // Add the two explicit qubits
-  ctls.emplace_back(c1);
-  ctls.emplace_back(c0);
-  internal::x<mod>(
-      ctls, cudaq::getParameterPackVals<sizeof...(qubits) - 1>(qubits...));
-}
+#define CUDAQ_ONE_TARGET_QUBIT_MULTI_CONTROL(NAME)                             \
+  template <typename mod, typename... QubitTy>                                 \
+  void NAME(cudaq::qubit &c0, cudaq::qubit &c1, QubitTy &...qubits) __qpu__ {  \
+    static_assert(std::is_same_v<mod, cudaq::ctrl>);                           \
+    std::vector<std::reference_wrapper<cudaq::qubit>> ctls{{qubits...}};       \
+    /* Last qubit is the target */                                             \
+    ctls.pop_back();                                                           \
+    /*Add the two explicit qubits */                                           \
+    ctls.emplace_back(c1);                                                     \
+    ctls.emplace_back(c0);                                                     \
+    internal::NAME<mod>(                                                       \
+        ctls, cudaq::getParameterPackVals<sizeof...(qubits) - 1>(qubits...));  \
+  }                                                                            \
+  template <typename mod>                                                      \
+  void NAME(cudaq::qview<> ctrls, cudaq::qubit &target) __qpu__ {              \
+    static_assert(std::is_same_v<mod, cudaq::ctrl>);                           \
+    std::vector<std::reference_wrapper<cudaq::qubit>> castedCtls;              \
+    for (auto &q : ctrls)                                                      \
+      castedCtls.emplace_back(q);                                              \
+    internal::NAME<mod>(castedCtls, target);                                   \
+  }
 
-template <typename mod>
-void x(cudaq::qview<> ctrls, cudaq::qubit &target) __qpu__ {
-  static_assert(std::is_same_v<mod, cudaq::ctrl>);
-  std::vector<std::reference_wrapper<cudaq::qubit>> castedCtls;
-  for (auto &q : ctrls)
-    castedCtls.emplace_back(q);
-  internal::x<mod>(castedCtls, target);
-}
+CUDAQ_ONE_TARGET_QUBIT_MULTI_CONTROL(h)
+CUDAQ_ONE_TARGET_QUBIT_MULTI_CONTROL(x)
+CUDAQ_ONE_TARGET_QUBIT_MULTI_CONTROL(y)
+CUDAQ_ONE_TARGET_QUBIT_MULTI_CONTROL(z)
+CUDAQ_ONE_TARGET_QUBIT_MULTI_CONTROL(t)
+CUDAQ_ONE_TARGET_QUBIT_MULTI_CONTROL(s)
 
-template <typename mod, typename... QubitTy>
-void z(cudaq::qubit &c0, cudaq::qubit &c1, QubitTy &...qubits) __qpu__ {
-  static_assert(std::is_same_v<mod, cudaq::ctrl>);
-  std::vector<std::reference_wrapper<cudaq::qubit>> ctls{{qubits...}};
-  // Last qubit is the target
-  ctls.pop_back();
-  // Add the two explicit qubits
-  ctls.emplace_back(c1);
-  ctls.emplace_back(c0);
-  internal::z<mod>(
-      ctls, cudaq::getParameterPackVals<sizeof...(qubits) - 1>(qubits...));
-}
+#define CUDAQ_ONE_TARGET_ONE_PARAM_QUBIT_MULTI_CONTROL(NAME)                   \
+  template <typename mod, typename ScalarAngle, typename... QubitTy>           \
+  void NAME(ScalarAngle angle, cudaq::qubit &c0, cudaq::qubit &c1,             \
+            QubitTy &...qubits) __qpu__ {                                      \
+    static_assert(std::is_same_v<mod, cudaq::ctrl>);                           \
+    static_assert(sizeof...(qubits) > 0);                                      \
+    std::vector<std::reference_wrapper<cudaq::qubit>> ctls{{qubits...}};       \
+    /* Last qubit is the target */                                             \
+    cudaq::qubit &target = ctls.back();                                        \
+    ctls.pop_back();                                                           \
+    /*Add the two explicit qubits */                                           \
+    ctls.emplace_back(c1);                                                     \
+    ctls.emplace_back(c0);                                                     \
+    internal::NAME<mod>(angle, ctls, target);                                  \
+  }                                                                            \
+  template <typename mod, typename ScalarAngle>                                \
+  void NAME(ScalarAngle angle, cudaq::qview<> ctrls, cudaq::qubit &target)     \
+      __qpu__ {                                                                \
+    static_assert(std::is_same_v<mod, cudaq::ctrl>);                           \
+    std::vector<std::reference_wrapper<cudaq::qubit>> castedCtls;              \
+    for (auto &q : ctrls)                                                      \
+      castedCtls.emplace_back(q);                                              \
+    internal::NAME<mod>(angle, castedCtls, target);                            \
+  }
 
-template <typename mod>
-void z(cudaq::qview<> ctrls, cudaq::qubit &target) __qpu__ {
-  static_assert(std::is_same_v<mod, cudaq::ctrl>);
-  std::vector<std::reference_wrapper<cudaq::qubit>> castedCtls;
-  for (auto &q : ctrls)
-    castedCtls.emplace_back(q);
-  internal::z<mod>(castedCtls, target);
-}
+CUDAQ_ONE_TARGET_ONE_PARAM_QUBIT_MULTI_CONTROL(rx)
+CUDAQ_ONE_TARGET_ONE_PARAM_QUBIT_MULTI_CONTROL(ry)
+CUDAQ_ONE_TARGET_ONE_PARAM_QUBIT_MULTI_CONTROL(rz)
+CUDAQ_ONE_TARGET_ONE_PARAM_QUBIT_MULTI_CONTROL(r1)
 } // namespace cudaq
