fb0a8080c39738a757fe86c8ff4075e9068ab3d6
Eric Schweitz <eschweitz@nvidia.com>
2024-03-27 07:04:33 -0700

    Discard unneeded helper functions. (#1445)
    * Discard unneeded helper functions.
    
    These functions weren't being used much any longer. Go ahead and
    eliminate them.
    
    * clean-format

--- lib/Optimizer/Transforms/DecompositionPatterns.cpp
+++ lib/Optimizer/Transforms/DecompositionPatterns.cpp
@@ -30,7 +30,7 @@ inline Value createConstant(Location loc, double value, Type type,
 
 inline Value createConstant(Location loc, std::size_t value,
                             PatternRewriter &rewriter) {
-  return cudaq::opt::factory::createI64Constant(loc, rewriter, value);
+  return rewriter.create<arith::ConstantIntOp>(loc, value, 64);
 }
 
 inline Value createDivF(Location loc, Value numerator, double denominator,
