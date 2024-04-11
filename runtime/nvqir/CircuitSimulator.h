93d7e7b2a5999a721f42f96c2c09366a6bc3f839
Ben Howe <bhowe@nvidia.com>
2024-04-11 21:47:54 +0000

    Slight correction - not that it matters though

--- runtime/nvqir/CircuitSimulator.h
+++ runtime/nvqir/CircuitSimulator.h
@@ -156,7 +156,7 @@ public:
       else if (type == cudaq::pauli::X)
         basisChange.emplace_back([&, qubitIdx](bool) {
           if (qubitIdx >= qubitIds.size())
-            cudaq::info("qubitIdx out of range: {}");
+            cudaq::info("qubitIdx out of range: {}", qubitIdx);
           h(qubitIds[qubitIdx]);
         });
     });
