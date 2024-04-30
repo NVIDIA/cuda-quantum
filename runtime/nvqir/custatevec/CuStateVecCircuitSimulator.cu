1a8f14f5b65150b2230d8e1f396379fb08cbe7ac
Thien Nguyen <58006629+1tnguyen@users.noreply.github.com>
2024-05-01 09:44:37 +1000

    [StateHandling] State data with value semantics via `state` acting as a thin pointer wrapper    (#1582)
    * Implement a new state passing approach
    
    `state` remains a thin wrapper, easily copiable, pass by value.
    
    State allocation from a `state` requires the simulator to clone the
    state, keeping the input state as-is.
    
    * Add NVQIR tests for allocs
    
    * Code format
    
    * Use `const state &` as the base signature
    
    Because we have test/AST-Quake/qalloc_state.cpp test that requires these
    signatures and having  state/state&/const state& at the same time is
    ambiguous.
    
    * Escape python test using list[...] type annotation for Python < 3.9
    
    To fix wheel validation failures on Python 3.8.
    
    * Fix another python 3.8  CI issue: module 'ast' has no attribute 'unparse'
    
    * Update unittests/qir/NVQIRTester.cpp
    
    Co-authored-by: Eric Schweitz <eschweitz@nvidia.com>
    
    * Update runtime/nvqir/qpp/QppDMCircuitSimulator.cpp
    
    Co-authored-by: Eric Schweitz <eschweitz@nvidia.com>
    
    * Update runtime/nvqir/qpp/QppCircuitSimulator.cpp
    
    Co-authored-by: Eric Schweitz <eschweitz@nvidia.com>
    
    * Update runtime/cudaq/qis/qvector.h
    
    Co-authored-by: Eric Schweitz <eschweitz@nvidia.com>
    
    * Address code review
    
    * Fix CI: delegation cycle [-Wdelegating-ctor-cycles]
    
    ---------
    
    Co-authored-by: Eric Schweitz <eschweitz@nvidia.com>

--- runtime/nvqir/custatevec/CuStateVecCircuitSimulator.cu
+++ runtime/nvqir/custatevec/CuStateVecCircuitSimulator.cu
@@ -342,6 +342,48 @@ protected:
     deviceStateVector = newDeviceStateVector;
   }
 
+  void addQubitsToState(const cudaq::SimulationState &in_state) override {
+    const cudaq::CusvState<ScalarType> *const casted =
+        dynamic_cast<const cudaq::CusvState<ScalarType> *>(&in_state);
+    if (!casted)
+      throw std::invalid_argument(
+          "[CuStateVecCircuitSimulator] Incompatible state input");
+
+    if (!deviceStateVector) {
+      // Create the memory and the handle
+      HANDLE_CUDA_ERROR(cudaMalloc((void **)&deviceStateVector,
+                                   stateDimension * sizeof(CudaDataType)));
+      HANDLE_ERROR(custatevecCreate(&handle));
+      // First allocation, so just copy the user provided data (device mem) here
+      HANDLE_CUDA_ERROR(cudaMemcpy(deviceStateVector, casted->getDevicePointer(),
+                                   stateDimension * sizeof(CudaDataType),
+                                   cudaMemcpyDeviceToDevice));
+      return;
+    }
+
+    // Expanding the state
+    // Allocate new vector to place the kron prod result
+    void *newDeviceStateVector;
+    HANDLE_CUDA_ERROR(cudaMalloc((void **)&newDeviceStateVector,
+                                 stateDimension * sizeof(CudaDataType)));
+    constexpr int32_t threads_per_block = 256;
+    uint32_t n_blocks =
+        (stateDimension + threads_per_block - 1) / threads_per_block;
+    // Compute the kronecker product
+    kronprod<CudaDataType><<<n_blocks, threads_per_block>>>(
+        previousStateDimension,
+        reinterpret_cast<CudaDataType *>(deviceStateVector),
+        (1UL << in_state.getNumQubits()),
+        reinterpret_cast<const CudaDataType *>(casted->getDevicePointer()),
+        reinterpret_cast<CudaDataType *>(newDeviceStateVector));
+    HANDLE_CUDA_ERROR(cudaGetLastError());
+
+    // Free the old state we don't need anymore.
+    // Note: the devicePtr of the input state is owned by the caller.
+    HANDLE_CUDA_ERROR(cudaFree(deviceStateVector));
+    deviceStateVector = newDeviceStateVector;
+  }
+
   /// @brief Increase the state size by one qubit.
   void addQubitToState() override {
     ScopedTraceWithContext("CuStateVecCircuitSimulator::addQubitToState");
