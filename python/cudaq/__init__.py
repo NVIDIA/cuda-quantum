c173c00e58451bec93f08aeee982dab90b22d275
Ben Howe <bhowe@nvidia.com>
2024-05-03 21:37:23 +0000

    Update __init__.py for aarch64

--- python/cudaq/__init__.py
+++ python/cudaq/__init__.py
@@ -6,7 +6,7 @@
 # the terms of the Apache License 2.0 which accompanies this distribution.     #
 # ============================================================================ #
 
-import sys, os
+import sys, os, platform
 from ._packages import *
 
 if not "CUDAQ_DYNLIBS" in os.environ:
@@ -17,12 +17,15 @@ if not "CUDAQ_DYNLIBS" in os.environ:
         cutensornet_libs = get_library_path("cutensornet-cu11")
         cutensornet_path = os.path.join(cutensornet_libs, "libcutensornet.so.2")
 
-        # FIXME - will this work on `aarch64`?
-        cudart_libs = get_library_path("nvidia-cuda_runtime-cu11")
-        cudart_path = os.path.join(cudart_libs, "libcudart.so.11.0")
+        os.environ["CUDAQ_DYNLIBS"] = f"{custatevec_path}:{cutensornet_path}"
 
-        os.environ[
-            "CUDAQ_DYNLIBS"] = f"{custatevec_path}:{cutensornet_path}:{cudart_path}"
+        # The following package is only available on `x86_64` (not `aarch64`). For
+        # `aarch64`, the library must be provided another way (likely with
+        # LD_LIBRARY_PATH).
+        if platform.processor() == "x86_64":
+            cudart_libs = get_library_path("nvidia-cuda_runtime-cu11")
+            cudart_path = os.path.join(cudart_libs, "libcudart.so.11.0")
+            os.environ["CUDAQ_DYNLIBS"] += f":{cudart_path}"
     except:
         import importlib.util
         if not importlib.util.find_spec("cuda-quantum") is None:
