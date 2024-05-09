6757f3c4b0b5c3416ebbc6d944c26fd96a09f6b5
Eric Schweitz <eschweitz@nvidia.com>
2024-05-09 07:42:02 -0700

    Update the terminology used in the names of type testing functions.
    We were using the terminology "value-SSA" and "reference-SSA" from
    classic computing in some of the old code to distinguish between quantum
    values that are referred to by value and by reference. The
    value/reference model is clearly appropriate, but the SSA isn't quite
    correct, pedantically speaking.
    
    Unlike SSA, in quantum computation a value has a linear quantum type. It
    must be used exactly once. This follows from the inability to "make a
    copy" of a quantum state. SSA values, on the other hand, cannot be
    redefined but one can make as many copies of them as one wants.
    
    While tweaking this terminology (hopefully to make it easier on the
    reader), these changes tighten some of the IR checks. One can now test
    if an Operation is purely in linear-value or reference form (or
    neither).

--- lib/Optimizer/Transforms/GenKernelExecution.cpp
+++ lib/Optimizer/Transforms/GenKernelExecution.cpp
@@ -1356,10 +1356,10 @@ public:
   /// must be called via other quantum code.
   bool hasLegalType(FunctionType funTy) {
     for (auto ty : funTy.getInputs())
-      if (quake::isaQuantumType(ty))
+      if (quake::isQuantumType(ty))
         return false;
     for (auto ty : funTy.getResults())
-      if (quake::isaQuantumType(ty))
+      if (quake::isQuantumType(ty))
         return false;
     return true;
   }
