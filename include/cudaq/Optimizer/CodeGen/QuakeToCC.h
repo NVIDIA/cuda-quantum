c8da33091ad0f054ebce8f03ac393cb396ce5df9
Eric Schweitz <eschweitz@nvidia.com>
2024-05-31 09:09:02 -0700

    Streamlined codegen for simulation. (#1727)
    * Streamlined codegen for simulation.
    
    Implement a codegen path from quake to the execution manager. This does
    not implement all CUDA-Q features. It does implement most of the
    "obvious" execution manager interface however.
    
    The strategy is to lower all quake references and veqs to "qubit spans"
    which are stack allocated structs which contain a pointer and length
    pair (a.k.a. a span). A qubit is a long integer index (per the execution
    manager). We use spans to collect ranges of these qubits to pass to the
    execution manager.
    
    A C ABI of adaptors is provided to convert the span structures to
    `std::vector` data types, which again are the execution manager
    interface. This glue code lets the C++ compiler supply a C++ version
    agnostic implementation to support our nvq++ calls.
    
    The scheme is stack space intensive, but I expect LLVM's optimizer will
    be able to fold much of the patterns with lifetime analysis. This may
    need some further tuning.
    
      - Add test for this new codegen lowering.
      - Add registered pipeline. (cudaq-opt -lower-quake)
      - Use canonicalizer to fixup alloca on arrays. This lets us insert a cast
        operation to prevent type checking failures.
      - Add other canonicalization patterns to eliminate unneeded casts.
      - Fix tests to deal with new canonicalization patterns.
    
    * clang-format
    
    * Fix the python test failures.
    
    * Silence warning.
    
    * Attempt to work around bugs in build compiler.

---
+++ include/cudaq/Optimizer/CodeGen/QuakeToCC.h
@@ -0,0 +1,25 @@
+/****************************************************************-*- C++ -*-****
+ * Copyright (c) 2022 - 2024 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+#pragma once
+
+#include "cudaq/Optimizer/Dialect/CC/CCTypes.h"
+
+namespace mlir {
+class RewritePatternSet;
+class TypeConverter;
+} // namespace mlir
+
+namespace cudaq::opt {
+cc::PointerType getCudaqQubitType(mlir::MLIRContext *context);
+cc::StructType getCudaqQubitSpanType(mlir::MLIRContext *context);
+
+void populateQuakeToCCPatterns(mlir::TypeConverter &typeConverter,
+                               mlir::RewritePatternSet &patterns);
+void populateQuakeToCCPrepPatterns(mlir::RewritePatternSet &patterns);
+} // namespace cudaq::opt
