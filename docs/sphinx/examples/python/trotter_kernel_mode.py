96b0a772d7a2d44a4d253ca3f57a6fd795edb6a5
Eric Schweitz <eschweitz@nvidia.com>
2024-06-04 08:19:38 -0700

    Comment out target line.

--- docs/sphinx/examples/python/trotter_kernel_mode.py
+++ docs/sphinx/examples/python/trotter_kernel_mode.py
@@ -20,7 +20,7 @@ import numpy as np
 # https://en.m.wikipedia.org/wiki/Quantum_Heisenberg_model
 
 # Set the target to `nvidia` 32-bit simulator.
-cudaq.set_target('nvidia')
+# cudaq.set_target('nvidia')
 
 
 # Alternating up/down spins
