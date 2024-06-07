ef549a51113610c3f870a2b7f145d55552465fb5
Sachin Pisal <spisal@nvidia.com>
2024-06-06 17:19:25 -0700

    making optimizer picklable

--- runtime/cudaq/algorithms/optimizers/ensmallen/ensmallen.h
+++ runtime/cudaq/algorithms/optimizers/ensmallen/ensmallen.h
@@ -9,15 +9,13 @@
 #pragma once
 
 #include "cudaq/algorithms/optimizer.h"
+#include <optional>
 
 namespace cudaq::optimizers {
 struct max_eval;
 struct initial_parameters;
 
 class BaseEnsmallen : public cudaq::optimizer {
-protected:
-  void validate(optimizable_function &optFunction);
-
 public:
   std::optional<std::size_t> max_eval;
   std::optional<std::vector<double>> initial_parameters;
@@ -26,134 +24,151 @@ public:
   std::optional<double> f_tol;
   std::optional<double> step_size;
 
-  virtual std::string serialize() const = 0;
-  static BaseEnsmallen deserialize(const std::string &serialized_data);
+  std::string __getstate__() const { return serialize(); }
+
+  void __setstate__(const std::string &data) { deserialize(data); }
+
+  std::string serialize() const {
+    std::ostringstream oss;
+    serializeOptional(oss, max_eval);
+    serializeOptional(oss, initial_parameters);
+    serializeOptional(oss, lower_bounds);
+    serializeOptional(oss, upper_bounds);
+    serializeOptional(oss, f_tol);
+    serializeOptional(oss, step_size);
+    return oss.str();
+  }
+
+  void deserialize(const std::string &data) {
+    std::istringstream iss(data);
+    deserializeOptional(iss, max_eval);
+    deserializeOptional(iss, initial_parameters);
+    deserializeOptional(iss, lower_bounds);
+    deserializeOptional(iss, upper_bounds);
+    deserializeOptional(iss, f_tol);
+    deserializeOptional(iss, step_size);
+  }
+
+protected:
+  void validate(optimizable_function &optFunction);
+
+  template <typename T>
+  void serializeOptional(std::ostringstream &oss,
+                         const std::optional<T> &opt) const {
+    if (opt.has_value()) {
+      oss << "1 ";
+      serialize(oss, opt.value());
+    } else {
+      oss << "0 ";
+    }
+  }
+
+  template <typename T>
+  void deserializeOptional(std::istringstream &iss, std::optional<T> &opt) {
+    int has_value;
+    iss >> has_value;
+    if (has_value) {
+      T value;
+      deserialize(iss, value);
+      if constexpr (std::is_fundamental_v<T>) {
+        opt = value;
+      } else {
+        opt.emplace(std::move(value));
+      }
+    } else {
+      opt.reset();
+    }
+  }
+
+  void serialize(std::ostringstream &oss,
+                 const std::vector<double> &vec) const {
+    oss << vec.size() << " ";
+    for (const auto &val : vec) {
+      oss << val << " ";
+    }
+  }
+
+  void deserialize(std::istringstream &iss, std::vector<double> &vec) {
+    size_t size;
+    iss >> size;
+    vec.resize(size);
+    for (auto &val : vec) {
+      iss >> val;
+    }
+  }
+
+  void serialize(std::ostringstream &oss, const std::size_t &value) const {
+    oss << value << " ";
+  }
+
+  void deserialize(std::istringstream &iss, std::size_t &value) {
+    iss >> value;
+  }
+
+  void serialize(std::ostringstream &oss, const double &value) const {
+    oss << value << " ";
+  }
+
+  void deserialize(std::istringstream &iss, double &value) { iss >> value; }
 };
 
-class Adam : public BaseEnsmallen {
-public:
-  Adam() = default;
-  bool requiresGradients() override { return true; }
-  optimization_result optimize(const int dim, optimizable_function &&opt_function) override;
-  std::string serialize() const override;
-};
-
-std::string Adam::serialize() const {
-  std::ostringstream oss;
-
-  if (max_eval.has_value()) {
-    oss << "max_eval:" << max_eval.value() << ";";
-  }
-
-  if (initial_parameters.has_value()) {
-    oss << "initial_parameters";
-    for (double param : initial_parameters.value()) {
-      oss << param << ",";
-    }
-    oss << ";";
-  }
-
-  if (lower_bounds.has_value()) {
-    oss << "lower_bounds";
-    for (double param : lower_bounds.value()) {
-      oss << param << ",";
-    }
-    oss << ";";
-  }
-
-  if (upper_bounds.has_value()) {
-    oss << "upper_bounds";
-    for (double param : upper_bounds.value()) {
-      oss << param << ",";
-    }
-    oss << ";";
-  }
-
-  if (f_tol.has_value()) {
-    oss << "f_tol:" << f_tol.value() << ";";
-  }
-
-  if (step_size.has_value()) {
-    oss << "step_size:" << step_size.value() << ";";
-  }
-
-  return oss.str();
-}
-
-Adam deserialize(const std::string &serialized_data) {
-  Adam obj;
-
-  std::istringstream iss(serialized_data);
-  char delimiter = ';';
-  std::string field;
-
-  while (iss >> field >> delimiter) {
-    if (field == "max_eval:") {
-      iss >> *obj.max_eval;
-    } else if (field == "initial_parameters:") {
-      std::string params_str;
-      iss >> params_str;
-      std::istringstream params_ss(params_str);
-      double param;
-      while (params_ss >> param) {
-        obj.initial_parameters->push_back(param);
-        if (params_ss.peek() == ',')
-          params_ss.ignore();
-      }
-    } else if (field == "step_size:") {
-      iss >> *obj.step_size;
-    } else if (field == "lower_bounds:") {
-      std::string params_str;
-      iss >> params_str;
-      std::istringstream params_ss(params_str);
-      double param;
-      while (params_ss >> param) {
-        obj.lower_bounds->push_back(param);
-        if (params_ss.peek() == ',')
-          params_ss.ignore();
-      }
-    } else if (field == "upper_bounds:") {
-      std::string params_str;
-      iss >> params_str;
-      std::istringstream params_ss(params_str);
-      double param;
-      while (params_ss >> param) {
-        obj.upper_bounds->push_back(param);
-        if (params_ss.peek() == ',')
-          params_ss.ignore();
-      }
-    } else if (field == "f_tol:") {
-      iss >> *obj.f_tol;
-    }
-  } 
-
-  return obj;
-}
-
-#define CUDAQ_ENSMALLEN_ALGORITHM_TYPE(NAME, NEEDS_GRADIENTS, EXTRA_PARAMS)    \
+#define CUDAQ_ENSMALLEN_ALGORITHM_TYPE(                                        \
+    NAME, NEEDS_GRADIENTS, EXTRA_PARAMS_DECL, EXTRA_PARAMS_SERIALIZE,          \
+    EXTRA_PARAMS_DESERIALIZE)                                                  \
   class NAME : public BaseEnsmallen {                                          \
   public:                                                                      \
     NAME() = default;                                                          \
     bool requiresGradients() override { return NEEDS_GRADIENTS; }              \
     optimization_result                                                        \
     optimize(const int dim, optimizable_function &&opt_function) override;     \
-    EXTRA_PARAMS                                                               \
+                                                                               \
+    std::string serialize() const {                                            \
+      std::ostringstream oss;                                                  \
+      oss << BaseEnsmallen::serialize();                                       \
+      EXTRA_PARAMS_SERIALIZE                                                   \
+      return oss.str();                                                        \
+    }                                                                          \
+                                                                               \
+    void deserialize(const std::string &data) {                                \
+      std::istringstream iss(data);                                            \
+      BaseEnsmallen::deserialize(data);                                        \
+      EXTRA_PARAMS_DESERIALIZE                                                 \
+    }                                                                          \
+    EXTRA_PARAMS_DECL                                                          \
   };
 
+#define SERIALIZE_PARAM(oss, param) serializeOptional(oss, param);
+#define DESERIALIZE_PARAM(iss, param) deserializeOptional(iss, param);
+
 CUDAQ_ENSMALLEN_ALGORITHM_TYPE(
-    lbfgs, true, std::optional<std::size_t> max_line_search_trials;)
+    lbfgs, true, std::optional<std::size_t> max_line_search_trials;
+    , SERIALIZE_PARAM(oss, max_line_search_trials),
+    DESERIALIZE_PARAM(iss, max_line_search_trials))
 CUDAQ_ENSMALLEN_ALGORITHM_TYPE(spsa, false, std::optional<double> alpha;
                                std::optional<double> gamma;
-                               std::optional<double> eval_step_size;)
+                               std::optional<double> eval_step_size;
+                               ,
+                               SERIALIZE_PARAM(oss, alpha)
+                                   SERIALIZE_PARAM(oss, gamma)
+                                       SERIALIZE_PARAM(oss, eval_step_size),
+                               DESERIALIZE_PARAM(iss, alpha)
+                                   DESERIALIZE_PARAM(iss, gamma)
+                                       DESERIALIZE_PARAM(iss, eval_step_size))
+CUDAQ_ENSMALLEN_ALGORITHM_TYPE(
+    adam, true, std::optional<std::size_t> batch_size;
+    std::optional<double> beta1; std::optional<double> beta2;
+    std::optional<double> eps;
+    ,
+    SERIALIZE_PARAM(oss, batch_size) SERIALIZE_PARAM(oss, beta1)
+        SERIALIZE_PARAM(oss, beta2) SERIALIZE_PARAM(oss, eps),
+    DESERIALIZE_PARAM(iss, batch_size) DESERIALIZE_PARAM(iss, beta1)
+        DESERIALIZE_PARAM(iss, beta2) DESERIALIZE_PARAM(iss, eps))
+CUDAQ_ENSMALLEN_ALGORITHM_TYPE(gradient_descent, true, , , )
+CUDAQ_ENSMALLEN_ALGORITHM_TYPE(sgd, true, std::optional<std::size_t> batch_size;
+                               , SERIALIZE_PARAM(oss, batch_size),
+                               DESERIALIZE_PARAM(iss, batch_size))
 
-CUDAQ_ENSMALLEN_ALGORITHM_TYPE(adam, true,
-                               std::optional<std::size_t> batch_size;
-                               std::optional<double> beta1;
-                               std::optional<double> beta2;
-                               std::optional<double> eps;)
-
-CUDAQ_ENSMALLEN_ALGORITHM_TYPE(gradient_descent, true, )
-CUDAQ_ENSMALLEN_ALGORITHM_TYPE(sgd, true,
-                               std::optional<std::size_t> batch_size;)
+#undef SERIALIZE_PARAM
+#undef DESERIALIZE_PARAM
 
 } // namespace cudaq::optimizers
--- runtime/cudaq/algorithms/optimizers/nlopt/nlopt.h
+++ runtime/cudaq/algorithms/optimizers/nlopt/nlopt.h
@@ -21,6 +21,87 @@ public:
   std::optional<std::vector<double>> lower_bounds;
   std::optional<std::vector<double>> upper_bounds;
   std::optional<double> f_tol;
+
+  std::string __getstate__() const { return serialize(); }
+
+  void __setstate__(const std::string &data) { deserialize(data); }
+
+  std::string serialize() const {
+    std::ostringstream oss;
+    serializeOptional(oss, max_eval);
+    serializeOptional(oss, initial_parameters);
+    serializeOptional(oss, lower_bounds);
+    serializeOptional(oss, upper_bounds);
+    serializeOptional(oss, f_tol);
+    return oss.str();
+  }
+
+  void deserialize(const std::string &data) {
+    std::istringstream iss(data);
+    deserializeOptional(iss, max_eval);
+    deserializeOptional(iss, initial_parameters);
+    deserializeOptional(iss, lower_bounds);
+    deserializeOptional(iss, upper_bounds);
+    deserializeOptional(iss, f_tol);
+  }
+
+protected:
+  template <typename T>
+  void serializeOptional(std::ostringstream &oss,
+                         const std::optional<T> &opt) const {
+    if (opt.has_value()) {
+      oss << "1 ";
+      serialize(oss, opt.value());
+    } else {
+      oss << "0 ";
+    }
+  }
+
+  template <typename T>
+  void deserializeOptional(std::istringstream &iss, std::optional<T> &opt) {
+    int has_value;
+    iss >> has_value;
+    if (has_value) {
+      T value;
+      deserialize(iss, value);
+      if constexpr (std::is_fundamental_v<T>) {
+        opt = value;
+      } else {
+        opt.emplace(std::move(value));
+      }
+    } else {
+      opt.reset();
+    }
+  }
+
+  void serialize(std::ostringstream &oss,
+                 const std::vector<double> &vec) const {
+    oss << vec.size() << " ";
+    for (const auto &val : vec) {
+      oss << val << " ";
+    }
+  }
+
+  void deserialize(std::istringstream &iss, std::vector<double> &vec) {
+    size_t size;
+    iss >> size;
+    vec.resize(size);
+    for (auto &val : vec) {
+      iss >> val;
+    }
+  }
+
+  void serialize(std::ostringstream &oss, const int &value) const {
+    oss << value << " ";
+  }
+
+  void deserialize(std::istringstream &iss, int &value) { iss >> value; }
+
+  void serialize(std::ostringstream &oss, const double &value) const {
+    oss << value << " ";
+  }
+
+  void deserialize(std::istringstream &iss, double &value) { iss >> value; }
 };
 
 #define CUDAQ_NLOPT_ALGORITHM_TYPE(NAME, NEEDS_GRADIENTS)                      \
