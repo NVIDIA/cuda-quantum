b7519c6e29db65f4999dbe327ba762626b52b52c
Thien Nguyen <58006629+1tnguyen@users.noreply.github.com>
2024-06-13 06:20:32 +1000

    Update runtime/cudaq/platform/default/rest_server/helpers/RestRemoteServer.cpp
    Co-authored-by: Ben Howe <141149032+bmhowe23@users.noreply.github.com>

--- runtime/cudaq/platform/default/rest_server/helpers/RestRemoteServer.cpp
+++ runtime/cudaq/platform/default/rest_server/helpers/RestRemoteServer.cpp
@@ -581,7 +581,7 @@ protected:
         }
         if (errorCode2) {
           LLVMConsumeError(llvm::wrap(std::move(errorCode2)));
-          throw std::runtime_error("Failed to decode input IR");
+          throw std::runtime_error("Failed to decode input IR (request.overlapKernel->ir)");
         }
         std::string_view codeStr1(decodedCodeIr1.data(), decodedCodeIr1.size());
         cudaq::ExecutionContext stateContext1("extract-state");
