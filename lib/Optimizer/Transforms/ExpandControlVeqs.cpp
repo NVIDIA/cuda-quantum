436f7d21401b25435760cc0931029a359ae5d057
Adam T. Geller <adgeller@nvidia.com>
2024-06-17 14:12:04 -0700

    Change to rewrite pattern

--- lib/Optimizer/Transforms/ExpandControlVeqs.cpp
+++ lib/Optimizer/Transforms/ExpandControlVeqs.cpp
@@ -22,42 +22,32 @@ namespace cudaq::opt {
 using namespace mlir;
 
 namespace {
-static Operation *replaceOperator(quake::OperatorInterface oldOp,
-                                  ValueRange controls, OpBuilder &builder) {
-  StringRef name = oldOp->getName().getStringRef();
-  StringAttr nameAttr = builder.getStringAttr(name);
-  ValueRange parameters = oldOp.getParameters();
-  ValueRange targets = oldOp.getTargets();
-  SmallVector<Value> operands(parameters);
-  operands.append(controls.begin(), controls.end());
-  operands.append(targets.begin(), targets.end());
-  auto segmentSizes =
-      builder.getDenseI32ArrayAttr({static_cast<int32_t>(parameters.size()),
-                                    static_cast<int32_t>(controls.size()),
-                                    static_cast<int32_t>(targets.size())});
-  auto newOp = builder.create(oldOp->getLoc(), nameAttr, operands);
-  newOp->setAttr("operand_segment_sizes", segmentSizes);
-  return newOp;
-}
+template <typename OP>
+class ExpandPat : public OpRewritePattern<OP> {
 
-struct ExpandControlVeqsPass
-    : public cudaq::opt::impl::ExpandControlVeqsBase<ExpandControlVeqsPass> {
-  using ExpandControlVeqsBase::ExpandControlVeqsBase;
+public:
+  using OpRewritePattern<OP>::OpRewritePattern;
 
-  LogicalResult expand(quake::OperatorInterface op) const {
-    OpBuilder builder(op);
+  LogicalResult matchAndRewrite(OP op,
+                                PatternRewriter &rewriter) const override {
+    // Unfortunately, we may grow the # of controls as a result of this rewrite,
+    // so we must track a new list of controls and reconstruct the operands
+    // instead of replacing the controls in place.
     SmallVector<Value> newControls;
     bool update = false;
+    rewriter.startRootUpdate(op);
 
+    // Search through the controls for veqs with known sizes
     for (auto [index, control] : llvm::enumerate(op.getControls())) {
-      size_t size = 1;
       if (auto veq = dyn_cast<quake::VeqType>(control.getType())) {
         if (!veq.hasSpecifiedSize())
           return failure();
 
-        size = veq.getSize();
-        for (size_t i = 0; i < size; ++i) {
-          auto ext = builder.create<quake::ExtractRefOp>(op.getLoc(), control, i);
+        // For each of the qubits in the veq, create an extraction instruction
+        // The result of the extraction will be a new control
+        // The veq is not added the newControls, so it will be dropped
+        for (size_t i = 0; i < veq.getSize(); ++i) {
+          auto ext = rewriter.create<quake::ExtractRefOp>(op.getLoc(), control, i);
           newControls.push_back(ext);
           update = true;
         }
@@ -66,27 +56,78 @@ struct ExpandControlVeqsPass
       }
     }
 
-    if (update) {
-        replaceOperator(op, newControls, builder);
-        op.erase();
+    if (!update) {
+      rewriter.cancelRootUpdate(op);
+      return failure();
     }
 
+    // Reconstruct the operands with the new controls
+    ValueRange parameters = op.getParameters();
+    ValueRange targets = op.getTargets();
+    SmallVector<Value> operands(parameters);
+    operands.append(newControls.begin(), newControls.end());
+    operands.append(targets.begin(), targets.end());
+    op->setOperands(operands);
+    auto segmentSizes =
+      rewriter.getDenseI32ArrayAttr({static_cast<int32_t>(parameters.size()),
+                                    static_cast<int32_t>(newControls.size()),
+                                    static_cast<int32_t>(targets.size())});
+    op->setAttr("operand_segment_sizes", segmentSizes);
+
+    rewriter.finalizeRootUpdate(op);
     return success();
   }
+};
 
+struct ExpandControlVeqsPass
+    : public cudaq::opt::impl::ExpandControlVeqsBase<ExpandControlVeqsPass> {
+  using ExpandControlVeqsBase::ExpandControlVeqsBase;
+
+private:
+  template <typename OP>
+  static bool checkLegal(OP op) {
+    for (auto control : op.getControls()) {
+      // Valid ops have no control veqs with a known size
+      if (auto veq = dyn_cast<quake::VeqType>(control.getType()))
+        if (veq.hasSpecifiedSize())
+          return false;
+    }
+
+    return true;
+  }
+
+public:
   void runOnOperation() override {
+    auto *ctx = &getContext();
     func::FuncOp func = getOperation();
-
-    if (func.isExternal())
-      return;
-
-    func.walk([&](quake::OperatorInterface op) {
-      // This pass must be run before conversion to Quake's value semantics form.
-      if (!quake::isAllReferences(op))
-        return;
-      if (failed(expand(op)))
-        return;
-    });
+    RewritePatternSet patterns(ctx);
+    patterns.insert<ExpandPat<quake::HOp>, ExpandPat<quake::PhasedRxOp>,
+                    ExpandPat<quake::R1Op>, ExpandPat<quake::RxOp>,
+                    ExpandPat<quake::RyOp>, ExpandPat<quake::RzOp>,
+                    ExpandPat<quake::SOp>, ExpandPat<quake::SwapOp>,
+                    ExpandPat<quake::TOp>, ExpandPat<quake::U2Op>,
+                    ExpandPat<quake::U3Op>, ExpandPat<quake::XOp>,
+                    ExpandPat<quake::YOp>, ExpandPat<quake::ZOp>>(ctx);
+    ConversionTarget target(*ctx);
+    target.addLegalDialect<quake::QuakeDialect>();
+    target.addDynamicallyLegalOp<quake::HOp>(checkLegal<quake::HOp>);
+    target.addDynamicallyLegalOp<quake::PhasedRxOp>(checkLegal<quake::PhasedRxOp>);
+    target.addDynamicallyLegalOp<quake::R1Op>(checkLegal<quake::R1Op>);
+    target.addDynamicallyLegalOp<quake::RxOp>(checkLegal<quake::RxOp>);
+    target.addDynamicallyLegalOp<quake::RyOp>(checkLegal<quake::RyOp>);
+    target.addDynamicallyLegalOp<quake::RzOp>(checkLegal<quake::RzOp>);
+    target.addDynamicallyLegalOp<quake::SOp>(checkLegal<quake::SOp>);
+    target.addDynamicallyLegalOp<quake::SwapOp>(checkLegal<quake::SwapOp>);
+    target.addDynamicallyLegalOp<quake::TOp>(checkLegal<quake::TOp>);
+    target.addDynamicallyLegalOp<quake::U2Op>(checkLegal<quake::U2Op>);
+    target.addDynamicallyLegalOp<quake::U3Op>(checkLegal<quake::U3Op>);
+    target.addDynamicallyLegalOp<quake::XOp>(checkLegal<quake::XOp>);
+    target.addDynamicallyLegalOp<quake::YOp>(checkLegal<quake::YOp>);
+    target.addDynamicallyLegalOp<quake::ZOp>(checkLegal<quake::ZOp>);
+    if (failed(applyPartialConversion(func.getOperation(), target,
+                                      std::move(patterns)))) {
+      signalPassFailure();
+    }
   }
 };
 
