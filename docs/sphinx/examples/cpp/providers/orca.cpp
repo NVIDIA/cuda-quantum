6993ee4238b499edb0d63444a361197fb1ebc3e6
Omar Bacarreza <obacarreza@orcacomputing.com>
2024-06-19 10:42:32 +0100

    Change examples to be inline with current PT-1

--- docs/sphinx/examples/cpp/providers/orca.cpp
+++ docs/sphinx/examples/cpp/providers/orca.cpp
@@ -6,56 +6,90 @@
 // To use the ORCA target you will need to set the ORCA_ACCESS_URL environment
 // variable or pass the url to the --orca-url flag.
 
+#include "cudaq/orca.h"
+#include "cudaq.h"
+
+// define helper function to generate a linear spaced vectors
+template <typename T>
+void linear_spaced_vector(std::vector<T> &xs, T min, T max, std::size_t N) {
+  T h = (max - min) / static_cast<T>(N - 1);
+  typename std::vector<T>::iterator x;
+  T val;
+  for (x = xs.begin(), val = min; x != xs.end(); ++x, val += h) {
+    *x = val;
+  }
+}
+
 // ORCA's PT-Series implement the boson sampling model of quantum computation,
 // in which multiple photons are interfered with each other within a network of
 // beam splitters, and photon detectors measure where the photons leave this
 // network.
+
 // The parameters needed to define the time bin interferometer are the
-// beam splitter angles, the phase shifter angles, the input state, the loop
-// lengths and optionally the number of samples.
-// The input state is the initial state of the photons in the time bin
-// interferometer, the left-most entry corresponds to the first mode entering
-// the loop.
+// the input state, the loop lengths, beam splitter angles, and optionally the
+// phase shifter angles, and the number of samples.
+
+// The input state is the initial state of the photons in
+//  the time bin interferometer, the left-most entry corresponds to the first
+//  mode entering the loop.
+
 // The loop lengths are the the lengths of the different loops in the time bin
 // interferometer.
 
-#include "cudaq/orca.h"
-#include "cudaq.h"
+// The beam splitter angles and the phase shifter angles are controllable
+// parameters of the time bin interferometer.
 
 int main() {
 
-  // A time-bin boson sampling experiment: An input state of 3 indistinguishable
-  // photons across 3 time bins (modes) entering the time bin interferometer.
-  // The interferometer is composed of one loop and beam splitter with its
-  // correspondent phase shifter. Each photon can either be stored in the loop
-  // to interfere with the next photon or exit the loop to be measured.
-  // Since there are 3 time time bins, there are 2 beam splitters and 2 phase
-  // shifters in the interferometer.
+  // A time-bin boson sampling experiment: An input state of 4 indistinguishable
+  // photons mixed with 4 vacuum states across 8 time bins (modes) enter the
+  // time bin interferometer (TBI). The interferometer is composed of two loops
+  // each with a beam splitter (and optionally with a correspondent phase
+  // shifter). Each photon can either be stored in a loop to interfere with the
+  // next photon or exit the loop to be measured. Since there are 8 time time
+  // bins and 2 loops, there are a total of 14 beam splitters (and optionally 14
+  // phase shifters in the interferometer), which is the number of controllable
+  // parameters.
 
-  // beam splitter angles
-  std::vector<double> bs_angles{
-      M_PI / 3,
-      M_PI / 6,
-  };
+  // half of 8 time bins is filled with a single photon and the other half is
+  // filled with the vacuum state (empty)
+  std::vector<std::size_t> input_state{1, 0, 1, 0, 1, 0, 1, 0};
 
-  // phase shifter angles
-  std::vector<double> ps_angles{
-      M_PI / 4,
-      M_PI / 5,
-  };
+  // The time bin interferometer in this example has two loops, each of length 1
+  std::vector<std::size_t> loop_lengths{1, 1};
 
-  // all time bins are filled witha a single photon
-  std::vector<std::size_t> input_state{1, 1, 1};
+  // helper variables to calculate the number of beam splitters and phase
+  // shifters needed in the TBI
+  std::size_t sum_loop_lengths{std::accumulate(
+      loop_lengths.begin(), loop_lengths.end(), static_cast<std::size_t>(0))};
+  const std::size_t n_loops = loop_lengths.size();
+  const std::size_t n_modes = input_state.size();
+  const std::size_t n_beamsplitters = n_loops * n_modes - sum_loop_lengths;
 
-  // The time bin interferometer in this example has only one loop of length 1
-  std::vector<std::size_t> loop_lengths{1};
+  // beam splitter angles (created a linear spaced vector of angles)
+  std::vector<double> bs_angles(n_beamsplitters);
+  linear_spaced_vector(bs_angles, M_PI / 8, M_PI / 3, n_beamsplitters);
 
+  // Optionally, we can also specify the phase shifter angles (created a linear
+  // spaced vector of angles), if the sytems includes phase shifters
+  // std::vector<double> ps_angles(n_beamsplitters);
+  // linear_spaced_vector(ps_angles, M_PI / 6, M_PI / 3, n_beamsplitters);
+
+  // we can also set number of requested samples
   int n_samples{10000};
 
   // Submit to ORCA synchronously (e.g., wait for the job result to be returned
-  // before proceeding).
-  auto counts = cudaq::orca::sample(bs_angles, ps_angles, input_state,
-                                    loop_lengths, n_samples);
+  // before proceeding with the rest of the execution).
+  auto counts =
+      cudaq::orca::sample(input_state, loop_lengths, bs_angles, n_samples);
+
+  // If the system includes phase shifters, the phase shifter angles can be
+  // inluded in the call
+
+  // auto counts = cudaq::orca::sample(input_state, loop_lengths, bs_angles,
+  //                                   ps_angles, n_samples);
+
+  // Print the results
   counts.dump();
 
   return 0;
--- docs/sphinx/examples/python/providers/orca.py
+++ docs/sphinx/examples/python/providers/orca.py
@@ -10,45 +10,66 @@ import os
 
 # To use the ORCA target you will need to set the ORCA_ACCESS_URL environment variable
 # or pass a url.
-orca_url = os.getenv("ORCA_ACCESS_URL", "http://localhost:8080/sample")
+orca_url = os.getenv(
+    "ORCA_ACCESS_URL", "http://api.orcacomputing.com/v1/sample"
+)  # "http://localhost:8080/sample")
 
 cudaq.set_target("orca", url=orca_url)
 # ORCA's PT-Series implement the boson sampling model of quantum computation,
-# in which multiple photons are interfered with each other within a
-# network of beam splitters, and photon detectors measure where the photons
-# leave this network.
-# The parameters needed to define the time bin interferometer are the beam
-# splitter angles, the phase shifter angles, the input state, the loop lengths
-# and optionally the number of samples.
-# The input state is the initial state of the photons in the time bin
-# interferometer, the left-most entry corresponds to the first mode entering
-# the loop.
-# The loop lengths are the the lengths of the different loops in the
-# time bin interferometer.
+# in which multiple photons are interfered with each other within a network of
+# beam splitters, and photon detectors measure where the photons leave this
+# network.
 
-# A time-bin boson sampling experiment: An input state of 3 indistinguishable
-# photons across 3 time bins (modes) entering the time bin interferometer.
-# The interferometer is composed of one loop and beam splitter with its
-# correspondent phase shifter. Each photon can either be stored in the loop
-# to interfere with the next photon or exit the loop to be measured.
-# Since there are 3 time time bins, there are 2 beam splitters and 2 phase
-# shifters in the interferometer.
+# The parameters needed to define the time bin interferometer are the
+# the input state, the loop lengths, beam splitter angles, and optionally the
+# phase shifter angles, and the number of samples.
+
+# The input state is the initial state of the photons in
+#  the time bin interferometer, the left-most entry corresponds to the first
+#  mode entering the loop.
+
+# The loop lengths are the the lengths of the different loops in the time bin
+# interferometer.
+
+# The beam splitter angles and the phase shifter angles are controllable
+# parameters of the time bin interferometer.
+
+# A time-bin boson sampling experiment: An input state of 4 indistinguishable
+# photons mixed with 4 vacuum states across 8 time bins (modes) enter the
+# time bin interferometer (TBI). The interferometer is composed of two loops
+# each with a beam splitter (and optionally with a correspondent phase
+# shifter). Each photon can either be stored in a loop to interfere with the
+# next photon or exit the loop to be measured. Since there are 8 time time
+# bins and 2 loops, there are a total of 14 beam splitters (and optionally 14
+# phase shifters in the interferometer), which is the number of controllable
+# parameters.
+
+# half of 8 time bins is filled with a single photon and the other half is
+# filled with the vacuum state (empty)
+input_state = [1, 0, 1, 0, 1, 0, 1, 0]
+
+# The time bin interferometer in this example has two loops, each of length 1
+loop_lengths = [1, 1]
+
+# Calculate the number of beam splitters and phase shifters
+n_beamsplitters = len(loop_lengths) * len(input_state) - sum(loop_lengths)
 
 # beam splitter angles
-bs_angles = [np.pi / 3, np.pi / 6]
+bs_angles = np.linspace(np.pi / 8, np.pi / 3, n_beamsplitters)
 
-# phase shifter angles
-ps_angles = [np.pi / 4, np.pi / 5]
+# Optionally, we can also specify the phase shifter angles, if the sytems includes phase shifters
+# ps_angles = np.linspace(np.pi / 6, np.pi / 3, n_beamsplitters)
 
-# all time bins are filled with a single photon
-input_state = [1, 1, 1]
-
-# The time bin interferometer in this example has only one loop of length 1
-loop_lengths = [1]
+# we can also set number of requested samples
+n_samples = 10000
 
 # By using the synchronous `cudaq.orca.sample`, the execution of
 # any remaining classical code in the file will occur only
 # after the job has been returned from ORCA Server.
-counts = cudaq.orca.sample(bs_angles, ps_angles, input_state, loop_lengths)
+counts = cudaq.orca.sample(input_state, loop_lengths, bs_angles, n_samples)
 
+# If the system includes phase shifters, the phase shifter angles can be inluded in the call
+# counts = cudaq.orca.sample(input_state, loop_lengths, bs_angles, ps_angles, n_samples)
+
+# Print the results
 print(counts)
