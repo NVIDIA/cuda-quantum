d477429c09797e1ab311373b66a8927fb1a2df17
Ben Howe <bhowe@nvidia.com>
2024-06-21 01:52:05 +0000

    Revert unintentional changes to simple_vqe.py

--- docs/sphinx/examples/python/simple_vqe.py
+++ docs/sphinx/examples/python/simple_vqe.py
@@ -3,27 +3,21 @@ from cudaq import spin
 
 from typing import List
 
-cudaq.set_target("remote-mqpu", url="localhost:3030")
-
 # We begin by defining the spin Hamiltonian for the system that we are working
 # with. This is achieved through the use of `cudaq.SpinOperator`'s, which allow
 # for the convenient creation of complex Hamiltonians out of Pauli spin operators.
 hamiltonian = 5.907 - 2.1433 * spin.x(0) * spin.x(1) - 2.1433 * spin.y(
     0) * spin.y(1) + .21829 * spin.z(0) - 6.125 * spin.z(1)
 
-@cudaq.kernel
-def nested_kernel(qubits : cudaq.qview):
-    x(qubits[0])
 
 # Next, using the `cudaq.Kernel`, we define the variational quantum circuit
 # that we'd like to use as an ansatz.
 # Create a kernel that takes a list of floats as a function argument.
 @cudaq.kernel
-def kernel(angles: List[float]):#, extraVal: int):
+def kernel(angles: List[float]):
     # Allocate 2 qubits.
     qubits = cudaq.qvector(2)
-    nested_kernel(qubits)
-    # x(qubits[0])
+    x(qubits[0])
     # Apply an `ry` gate that is parameterized by the first value
     # of our `angles`.
     ry(angles[0], qubits[1])
@@ -35,8 +29,6 @@ def kernel(angles: List[float]):#, extraVal: int):
 # We can optionally tune this optimizer through its initial parameters, iterations,
 # optimization bounds, etc. before passing it to `cudaq.vqe`.
 optimizer = cudaq.optimizers.COBYLA()
-# optimizer = cudaq.optimizers.Adam()
-# grad = cudaq.gradients.CentralDifference()
 # optimizer.max_iterations = ...
 # optimizer...
 
@@ -44,14 +36,10 @@ optimizer = cudaq.optimizers.COBYLA()
 # optimization loop, returning a tuple of the minimized eigenvalue of our `spin_operator`
 # and the list of optimal variational parameters.
 
-argmap=lambda x : (x, 0)
-
 energy, parameter = cudaq.vqe(
     kernel=kernel,
-    #gradient_strategy=grad,
     spin_operator=hamiltonian,
     optimizer=optimizer,
-    #argument_mapper=argmap,
     # list of parameters has length of 1:
     parameter_count=1)
 
