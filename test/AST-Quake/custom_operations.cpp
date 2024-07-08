05326324743675d6f3c43ad3083d6086d51939a3
Pradnya Khalate <pkhalate@nvidia.com>
2024-07-08 15:36:22 -0700

    * Fix C++ quake test

--- test/AST-Quake/custom_operations.cpp
+++ test/AST-Quake/custom_operations.cpp
@@ -6,12 +6,9 @@
  * the terms of the Apache License 2.0 which accompanies this distribution.    *
  ******************************************************************************/
 
-// RUN: cudaq-quake %cpp_std %s | cudaq-opt | FileCheck %s
+// RUN: cudaq-quake %cpp_std %s | cudaq-opt -lift-array-value | FileCheck %s
 
 #include <cudaq.h>
-
-#include <complex>
-using namespace std::complex_literals;
 
 CUDAQ_REGISTER_OPERATION(custom_h, 1, 0,
                          {M_SQRT1_2, M_SQRT1_2, M_SQRT1_2, -M_SQRT1_2})
@@ -24,15 +21,8 @@ CUDAQ_REGISTER_OPERATION(custom_cnot, 2, 0,
 CUDAQ_REGISTER_OPERATION(custom_swap, 2, 0,
                          {1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1})
 
-CUDAQ_REGISTER_OPERATION(custom_s, 1, 0, {1, 0, 0, 1i})
-
-CUDAQ_REGISTER_OPERATION(
-    my_u3_generator_0, 1, 3,
-    {std::cos(parameters[0] / 2.),
-     -std::exp(i *parameters[2]) * std::sin(parameters[0] / 2.),
-     std::exp(i *parameters[1]) * std::sin(parameters[0] / 2.),
-     std::exp(i *(parameters[2] + parameters[1])) *
-         std::cos(parameters[0] / 2.)})
+CUDAQ_REGISTER_OPERATION(custom_s, 1, 0,
+                         {1, 0, 0, std::complex<double>{0.0, 1.0}})
 
 CUDAQ_REGISTER_OPERATION(toffoli, 3, 0,
                          {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
@@ -49,8 +39,8 @@ __qpu__ void kernel_1() {
 // CHECK-LABEL:   func.func @__nvqpp__mlirgen__function_kernel_1._Z8kernel_1v() attributes {"cudaq-entrypoint", "cudaq-kernel", no_this} {
 // CHECK:           %[[VAL_0:.*]] = quake.alloca !quake.ref
 // CHECK:           %[[VAL_1:.*]] = quake.alloca !quake.ref
-// CHECK:           quake.custom_op @custom_h_generator_1 %[[VAL_0]] : (!quake.ref) -> ()
-// CHECK:           quake.custom_op @custom_cnot_generator_2 %[[VAL_0]], %[[VAL_1]] : (!quake.ref, !quake.ref) -> ()
+// CHECK:           quake.custom_op @__nvqpp__mlirgen__function_custom_h_generator_1._Z20custom_h_generator_1RKSt6vectorIdSaIdEE.rodata %[[VAL_0]] : (!quake.ref) -> ()
+// CHECK:           quake.custom_op @__nvqpp__mlirgen__function_custom_cnot_generator_2._Z23custom_cnot_generator_2RKSt6vectorIdSaIdEE.rodata %[[VAL_0]], %[[VAL_1]] : (!quake.ref, !quake.ref) -> ()
 // CHECK:           return
 // CHECK:         }
 
@@ -63,8 +53,8 @@ __qpu__ void kernel_2() {
 // CHECK-LABEL:   func.func @__nvqpp__mlirgen__function_kernel_2._Z8kernel_2v() attributes {"cudaq-entrypoint", "cudaq-kernel", no_this} {
 // CHECK:           %[[VAL_0:.*]] = quake.alloca !quake.ref
 // CHECK:           %[[VAL_1:.*]] = quake.alloca !quake.ref
-// CHECK:           quake.custom_op @custom_h_generator_1 %[[VAL_0]] : (!quake.ref) -> ()
-// CHECK:           quake.custom_op @custom_x_generator_1 {{\[}}%[[VAL_0]]] %[[VAL_1]] : (!quake.ref, !quake.ref) -> ()
+// CHECK:           quake.custom_op @__nvqpp__mlirgen__function_custom_h_generator_1._Z20custom_h_generator_1RKSt6vectorIdSaIdEE.rodata %[[VAL_0]] : (!quake.ref) -> ()
+// CHECK:           quake.custom_op @__nvqpp__mlirgen__function_custom_x_generator_1._Z20custom_x_generator_1RKSt6vectorIdSaIdEE.rodata {{\[}}%[[VAL_0]]] %[[VAL_1]] : (!quake.ref, !quake.ref) -> ()
 // CHECK:           return
 // CHECK:         }
 
@@ -78,7 +68,7 @@ __qpu__ void kernel_3() {
 // CHECK:           %[[VAL_0:.*]] = quake.alloca !quake.ref
 // CHECK:           %[[VAL_1:.*]] = quake.alloca !quake.ref
 // CHECK:           quake.x %[[VAL_0]] : (!quake.ref) -> ()
-// CHECK:           quake.custom_op @custom_swap_generator_2 %[[VAL_0]], %[[VAL_1]] : (!quake.ref, !quake.ref) -> ()
+// CHECK:           quake.custom_op @__nvqpp__mlirgen__function_custom_swap_generator_2._Z23custom_swap_generator_2RKSt6vectorIdSaIdEE.rodata %[[VAL_0]], %[[VAL_1]] : (!quake.ref, !quake.ref) -> ()
 // CHECK:           return
 // CHECK:         }
 
@@ -112,7 +102,7 @@ __qpu__ void kernel_4() {
 // CHECK:           %[[VAL_14:.*]] = quake.extract_ref %[[VAL_4]][1] : (!quake.veq<4>) -> !quake.ref
 // CHECK:           %[[VAL_15:.*]] = quake.extract_ref %[[VAL_4]][2] : (!quake.veq<4>) -> !quake.ref
 // CHECK:           %[[VAL_16:.*]] = quake.extract_ref %[[VAL_4]][3] : (!quake.veq<4>) -> !quake.ref
-// CHECK:           quake.custom_op @custom_swap_generator_2 {{\[}}%[[VAL_13]], %[[VAL_14]]] %[[VAL_15]], %[[VAL_16]] : (!quake.ref, !quake.ref, !quake.ref, !quake.ref) -> ()
+// CHECK:           quake.custom_op @__nvqpp__mlirgen__function_custom_swap_generator_2._Z23custom_swap_generator_2RKSt6vectorIdSaIdEE.rodata {{\[}}%[[VAL_13]], %[[VAL_14]]] %[[VAL_15]], %[[VAL_16]] : (!quake.ref, !quake.ref, !quake.ref, !quake.ref) -> ()
 // CHECK:           return
 // CHECK:         }
 
@@ -126,40 +116,18 @@ __qpu__ void kernel_5() {
 // CHECK-LABEL:   func.func @__nvqpp__mlirgen__function_kernel_5._Z8kernel_5v() attributes {"cudaq-entrypoint", "cudaq-kernel", no_this} {
 // CHECK:           %[[VAL_0:.*]] = quake.alloca !quake.ref
 // CHECK:           quake.h %[[VAL_0]] : (!quake.ref) -> ()
-// CHECK:           quake.custom_op @custom_s_generator_1 %[[VAL_0]] : (!quake.ref) -> ()
-// CHECK:           quake.custom_op @custom_s_generator_1<adj> %[[VAL_0]] : (!quake.ref) -> ()
+// CHECK:           quake.custom_op @__nvqpp__mlirgen__function_custom_s_generator_1._Z20custom_s_generator_1RKSt6vectorIdSaIdEE.rodata %[[VAL_0]] : (!quake.ref) -> ()
+// CHECK:           quake.custom_op @__nvqpp__mlirgen__function_custom_s_generator_1._Z20custom_s_generator_1RKSt6vectorIdSaIdEE.rodata<adj> %[[VAL_0]] : (!quake.ref) -> ()
 // CHECK:           return
 // CHECK:         }
 
 __qpu__ void kernel_6() {
-  cudaq::qubit q;
-  my_u3_generator_0(M_PI, M_PI, M_PI_2, q);
-}
-
-// CHECK-LABEL:   func.func @__nvqpp__mlirgen__function_kernel_6._Z8kernel_6v() attributes {"cudaq-entrypoint", "cudaq-kernel", no_this} {
-// CHECK:           %[[VAL_0:.*]] = arith.constant 1.5707963267948966 : f64
-// CHECK:           %[[VAL_1:.*]] = arith.constant 3.1415926535897931 : f64
-// CHECK:           %[[VAL_2:.*]] = quake.alloca !quake.ref
-// CHECK:           %[[VAL_3:.*]] = cc.alloca f64
-// CHECK:           cc.store %[[VAL_1]], %[[VAL_3]] : !cc.ptr<f64>
-// CHECK:           %[[VAL_4:.*]] = cc.alloca f64
-// CHECK:           cc.store %[[VAL_1]], %[[VAL_4]] : !cc.ptr<f64>
-// CHECK:           %[[VAL_5:.*]] = cc.alloca f64
-// CHECK:           cc.store %[[VAL_0]], %[[VAL_5]] : !cc.ptr<f64>
-// CHECK:           %[[VAL_6:.*]] = cc.load %[[VAL_3]] : !cc.ptr<f64>
-// CHECK:           %[[VAL_7:.*]] = cc.load %[[VAL_4]] : !cc.ptr<f64>
-// CHECK:           %[[VAL_8:.*]] = cc.load %[[VAL_5]] : !cc.ptr<f64>
-// CHECK:           quake.custom_op @my_u3_generator_0_generator_1 (%[[VAL_6]], %[[VAL_7]], %[[VAL_8]]) %[[VAL_2]] : (f64, f64, f64, !quake.ref) -> ()
-// CHECK:           return
-// CHECK:         }
-
-__qpu__ void kernel_7() {
   cudaq::qvector q(3);
   x(q);
   toffoli(q[0], q[1], q[2]);
 }
 
-// CHECK-LABEL:   func.func @__nvqpp__mlirgen__function_kernel_7._Z8kernel_7v() attributes {"cudaq-entrypoint", "cudaq-kernel", no_this} {
+// CHECK-LABEL:   func.func @__nvqpp__mlirgen__function_kernel_6._Z8kernel_6v() attributes {"cudaq-entrypoint", "cudaq-kernel", no_this} {
 // CHECK:           %[[VAL_0:.*]] = arith.constant 1 : i64
 // CHECK:           %[[VAL_1:.*]] = arith.constant 0 : i64
 // CHECK:           %[[VAL_2:.*]] = arith.constant 3 : i64
@@ -180,6 +148,13 @@ __qpu__ void kernel_7() {
 // CHECK:           %[[VAL_11:.*]] = quake.extract_ref %[[VAL_3]][0] : (!quake.veq<3>) -> !quake.ref
 // CHECK:           %[[VAL_12:.*]] = quake.extract_ref %[[VAL_3]][1] : (!quake.veq<3>) -> !quake.ref
 // CHECK:           %[[VAL_13:.*]] = quake.extract_ref %[[VAL_3]][2] : (!quake.veq<3>) -> !quake.ref
-// CHECK:           quake.custom_op @toffoli_generator_3 %[[VAL_11]], %[[VAL_12]], %[[VAL_13]] : (!quake.ref, !quake.ref, !quake.ref) -> ()
+// CHECK:           quake.custom_op @__nvqpp__mlirgen__function_toffoli_generator_3._Z19toffoli_generator_3RKSt6vectorIdSaIdEE.rodata %[[VAL_11]], %[[VAL_12]], %[[VAL_13]] : (!quake.ref, !quake.ref, !quake.ref) -> ()
 // CHECK:           return
 // CHECK:         }
+
+// CHECK:         cc.global constant @__nvqpp__mlirgen__function_custom_h_generator_1._Z20custom_h_generator_1RKSt6vectorIdSaIdEE.rodata (dense<[(0.70710678118654757,0.000000e+00), (0.70710678118654757,0.000000e+00), (0.70710678118654757,0.000000e+00), (-0.70710678118654757,0.000000e+00)]> : tensor<4xcomplex<f64>>) : !cc.array<complex<f64> x 4>
+// CHECK:         cc.global constant @__nvqpp__mlirgen__function_custom_x_generator_1._Z20custom_x_generator_1RKSt6vectorIdSaIdEE.rodata (dense<[(0.000000e+00,0.000000e+00), (1.000000e+00,0.000000e+00), (1.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00)]> : tensor<4xcomplex<f64>>) : !cc.array<complex<f64> x 4>
+// CHECK:         cc.global constant @__nvqpp__mlirgen__function_custom_cnot_generator_2._Z23custom_cnot_generator_2RKSt6vectorIdSaIdEE.rodata (dense<[(1.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (1.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (1.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (1.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00)]> : tensor<16xcomplex<f64>>) : !cc.array<complex<f64> x 16>
+// CHECK:         cc.global constant @__nvqpp__mlirgen__function_custom_swap_generator_2._Z23custom_swap_generator_2RKSt6vectorIdSaIdEE.rodata (dense<[(1.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (1.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (1.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (1.000000e+00,0.000000e+00)]> : tensor<16xcomplex<f64>>) : !cc.array<complex<f64> x 16>
+// CHECK:         cc.global constant @__nvqpp__mlirgen__function_custom_s_generator_1._Z20custom_s_generator_1RKSt6vectorIdSaIdEE.rodata (dense<[(1.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,1.000000e+00)]> : tensor<4xcomplex<f64>>) : !cc.array<complex<f64> x 4>
+// CHECK:         cc.global constant @__nvqpp__mlirgen__function_toffoli_generator_3._Z19toffoli_generator_3RKSt6vectorIdSaIdEE.rodata (dense<[(1.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (1.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (1.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (1.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (1.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (1.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (1.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00), (1.000000e+00,0.000000e+00), (0.000000e+00,0.000000e+00)]> : tensor<64xcomplex<f64>>) : !cc.array<complex<f64> x 64>
