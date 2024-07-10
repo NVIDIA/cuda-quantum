4fb04028344ac9d67d8bce712c93ca71a5681c14
Eric Schweitz <eschweitz@nvidia.com>
2024-07-10 11:11:07 -0700

    Add expand measurements pass to IQM pipeline.

--- lib/Optimizer/CodeGen/Pipelines.cpp
+++ lib/Optimizer/CodeGen/Pipelines.cpp
@@ -47,6 +47,7 @@ void cudaq::opt::addPipelineTranslateToOpenQASM(PassManager &pm) {
 
 void cudaq::opt::addPipelineTranslateToIQMJson(PassManager &pm) {
   pm.addNestedPass<func::FuncOp>(createUnwindLoweringPass());
+  pm.addPass(createExpandMeasurementsPass());
   pm.addNestedPass<func::FuncOp>(createLowerToCFGPass());
   pm.addNestedPass<func::FuncOp>(createQuakeAddDeallocs());
   pm.addNestedPass<func::FuncOp>(createCombineQuantumAllocations());
