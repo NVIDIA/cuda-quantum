07141fbd7a87e2200705a361ac213b391984e931
Ben Howe <bhowe@nvidia.com>
2024-07-16 21:18:50 +0000

    Refactor PyRemoteSimulatorQPU.cpp to reduce duplicated code

--- python/runtime/utils/PyRemoteSimulatorQPU.cpp
+++ python/runtime/utils/PyRemoteSimulatorQPU.cpp
@@ -14,6 +14,66 @@ using namespace mlir;
 
 namespace {
 
+// This is a helper function to help reduce duplicated code across
+// PyRemoteSimulatorQPU and PyNvcfSimulatorQPU.
+static void _launchVQE(cudaq::ExecutionContext *executionContextPtr,
+                       std::unique_ptr<cudaq::RemoteRuntimeClient> &m_client,
+                       const std::string &m_simName, const std::string &name,
+                       const void *kernelArgs, cudaq::gradient *gradient,
+                       cudaq::spin_op H, cudaq::optimizer &optimizer,
+                       const int n_params, const std::size_t shots) {
+  auto *wrapper = reinterpret_cast<const cudaq::ArgWrapper *>(kernelArgs);
+  auto m_module = wrapper->mod;
+  auto *mlirContext = m_module->getContext();
+
+  if (executionContextPtr && executionContextPtr->name == "tracer")
+    return;
+
+  auto ctx = std::make_unique<cudaq::ExecutionContext>("observe", shots);
+  ctx->kernelName = name;
+  ctx->spin = &H;
+  if (shots > 0)
+    ctx->shots = shots;
+
+  std::string errorMsg;
+  const bool requestOkay = m_client->sendRequest(
+      *mlirContext, *executionContextPtr, /*serializedCodeContext=*/nullptr,
+      gradient, &optimizer, n_params, m_simName, name, /*kernelFunc=*/nullptr,
+      wrapper->rawArgs, /*argSize=*/0, &errorMsg);
+  if (!requestOkay)
+    throw std::runtime_error("Failed to launch VQE. Error: " + errorMsg);
+}
+
+// This is a helper function to help reduce duplicated code across
+// PyRemoteSimulatorQPU and PyNvcfSimulatorQPU.
+static void _launchKernel(cudaq::ExecutionContext *executionContextPtr,
+                          std::unique_ptr<cudaq::RemoteRuntimeClient> &m_client,
+                          const std::string &m_simName, const std::string &name,
+                          void (*kernelFunc)(void *), void *args,
+                          std::uint64_t voidStarSize,
+                          std::uint64_t resultOffset) {
+  auto *wrapper = reinterpret_cast<cudaq::ArgWrapper *>(args);
+  auto m_module = wrapper->mod;
+  auto callableNames = wrapper->callableNames;
+
+  auto *mlirContext = m_module->getContext();
+
+  // Default context for a 'fire-and-ignore' kernel launch; i.e., no context
+  // was set before launching the kernel. Use a static variable per thread to
+  // set up a single-shot execution context for this case.
+  static thread_local cudaq::ExecutionContext defaultContext("sample",
+                                                             /*shots=*/1);
+  cudaq::ExecutionContext &executionContext =
+      executionContextPtr ? *executionContextPtr : defaultContext;
+  std::string errorMsg;
+  const bool requestOkay = m_client->sendRequest(
+      *mlirContext, executionContext, /*serializedCodeContext=*/nullptr,
+      /*vqe_gradient=*/nullptr, /*vqe_optimizer=*/nullptr, /*vqe_n_params=*/0,
+      m_simName, name, kernelFunc, wrapper->rawArgs, voidStarSize, &errorMsg);
+  if (!requestOkay)
+    throw std::runtime_error("Failed to launch kernel. Error: " + errorMsg);
+}
+
 // Remote QPU: delegating the execution to a remotely-hosted server, which can
 // reinstate the execution context and JIT-invoke the kernel.
 class PyRemoteSimulatorQPU : public cudaq::BaseRemoteSimulatorQPU {
@@ -26,29 +86,8 @@ public:
                  cudaq::gradient *gradient, cudaq::spin_op H,
                  cudaq::optimizer &optimizer, const int n_params,
                  const std::size_t shots) override {
-    cudaq::ExecutionContext *executionContextPtr =
-        getExecutionContextForMyThread();
-
-    auto *wrapper = reinterpret_cast<const cudaq::ArgWrapper *>(kernelArgs);
-    auto m_module = wrapper->mod;
-    auto *mlirContext = m_module->getContext();
-
-    if (executionContextPtr && executionContextPtr->name == "tracer")
-      return;
-
-    auto ctx = std::make_unique<cudaq::ExecutionContext>("observe", shots);
-    ctx->kernelName = name;
-    ctx->spin = &H;
-    if (shots > 0)
-      ctx->shots = shots;
-
-    std::string errorMsg;
-    const bool requestOkay = m_client->sendRequest(
-        *mlirContext, *executionContextPtr, /*serializedCodeContext=*/nullptr,
-        gradient, &optimizer, n_params, m_simName, name, /*kernelFunc=*/nullptr,
-        wrapper->rawArgs, /*argSize=*/0, &errorMsg);
-    if (!requestOkay)
-      throw std::runtime_error("Failed to launch VQE. Error: " + errorMsg);
+    ::_launchVQE(getExecutionContextForMyThread(), m_client, m_simName, name,
+                 kernelArgs, gradient, H, optimizer, n_params, shots);
   }
 
   void launchKernel(const std::string &name, void (*kernelFunc)(void *),
@@ -57,29 +96,8 @@ public:
     cudaq::info("PyRemoteSimulatorQPU: Launch kernel named '{}' remote QPU {} "
                 "(simulator = {})",
                 name, qpu_id, m_simName);
-    auto *wrapper = reinterpret_cast<cudaq::ArgWrapper *>(args);
-    auto m_module = wrapper->mod;
-    auto callableNames = wrapper->callableNames;
-
-    auto *mlirContext = m_module->getContext();
-
-    cudaq::ExecutionContext *executionContextPtr =
-        getExecutionContextForMyThread();
-
-    // Default context for a 'fire-and-ignore' kernel launch; i.e., no context
-    // was set before launching the kernel. Use a static variable per thread to
-    // set up a single-shot execution context for this case.
-    static thread_local cudaq::ExecutionContext defaultContext("sample",
-                                                               /*shots=*/1);
-    cudaq::ExecutionContext &executionContext =
-        executionContextPtr ? *executionContextPtr : defaultContext;
-    std::string errorMsg;
-    const bool requestOkay = m_client->sendRequest(
-        *mlirContext, executionContext, /*serializedCodeContext=*/nullptr,
-        /*vqe_gradient=*/nullptr, /*vqe_optimizer=*/nullptr, /*vqe_n_params=*/0,
-        m_simName, name, kernelFunc, wrapper->rawArgs, voidStarSize, &errorMsg);
-    if (!requestOkay)
-      throw std::runtime_error("Failed to launch kernel. Error: " + errorMsg);
+    ::_launchKernel(getExecutionContextForMyThread(), m_client, m_simName, name,
+                    kernelFunc, args, voidStarSize, resultOffset);
   }
 
   PyRemoteSimulatorQPU(PyRemoteSimulatorQPU &&) = delete;
@@ -100,29 +118,8 @@ public:
                  cudaq::gradient *gradient, cudaq::spin_op H,
                  cudaq::optimizer &optimizer, const int n_params,
                  const std::size_t shots) override {
-    cudaq::ExecutionContext *executionContextPtr =
-        getExecutionContextForMyThread();
-
-    auto *wrapper = reinterpret_cast<const cudaq::ArgWrapper *>(kernelArgs);
-    auto m_module = wrapper->mod;
-    auto *mlirContext = m_module->getContext();
-
-    if (executionContextPtr && executionContextPtr->name == "tracer")
-      return;
-
-    auto ctx = std::make_unique<cudaq::ExecutionContext>("observe", shots);
-    ctx->kernelName = name;
-    ctx->spin = &H;
-    if (shots > 0)
-      ctx->shots = shots;
-
-    std::string errorMsg;
-    const bool requestOkay = m_client->sendRequest(
-        *mlirContext, *executionContextPtr, /*serializedCodeContext=*/nullptr,
-        gradient, &optimizer, n_params, m_simName, name, /*kernelFunc=*/nullptr,
-        wrapper->rawArgs, /*argSize=*/0, &errorMsg);
-    if (!requestOkay)
-      throw std::runtime_error("Failed to launch VQE. Error: " + errorMsg);
+    ::_launchVQE(getExecutionContextForMyThread(), m_client, m_simName, name,
+                 kernelArgs, gradient, H, optimizer, n_params, shots);
   }
 
   void launchKernel(const std::string &name, void (*kernelFunc)(void *),
@@ -131,29 +128,8 @@ public:
     cudaq::info("PyNvcfSimulatorQPU: Launch kernel named '{}' remote QPU {} "
                 "(simulator = {})",
                 name, qpu_id, m_simName);
-    auto *wrapper = reinterpret_cast<cudaq::ArgWrapper *>(args);
-    auto m_module = wrapper->mod;
-    auto callableNames = wrapper->callableNames;
-
-    auto *mlirContext = m_module->getContext();
-
-    cudaq::ExecutionContext *executionContextPtr =
-        getExecutionContextForMyThread();
-
-    // Default context for a 'fire-and-ignore' kernel launch; i.e., no context
-    // was set before launching the kernel. Use a static variable per thread to
-    // set up a single-shot execution context for this case.
-    static thread_local cudaq::ExecutionContext defaultContext("sample",
-                                                               /*shots=*/1);
-    cudaq::ExecutionContext &executionContext =
-        executionContextPtr ? *executionContextPtr : defaultContext;
-    std::string errorMsg;
-    const bool requestOkay = m_client->sendRequest(
-        *mlirContext, executionContext, /*serializedCodeContext=*/nullptr,
-        /*vqe_gradient=*/nullptr, /*vqe_optimizer=*/nullptr, /*vqe_n_params=*/0,
-        m_simName, name, kernelFunc, wrapper->rawArgs, voidStarSize, &errorMsg);
-    if (!requestOkay)
-      throw std::runtime_error("Failed to launch kernel. Error: " + errorMsg);
+    ::_launchKernel(getExecutionContextForMyThread(), m_client, m_simName, name,
+                    kernelFunc, args, voidStarSize, resultOffset);
   }
 
   PyNvcfSimulatorQPU(PyNvcfSimulatorQPU &&) = delete;
