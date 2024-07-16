ba787d73abd5690b0e8f627b2498bd1e1beff077
Eric Schweitz <eschweitz@nvidia.com>
2024-07-16 12:40:34 -0700

    [core] Add constant folding of cc.cast operations.
    These constant folding operations will eliminate cc.cast operations when
    they are converting the type of a constant input op.

--- lib/Optimizer/Dialect/CC/CCOps.cpp
+++ lib/Optimizer/Dialect/CC/CCOps.cpp
@@ -187,10 +187,73 @@ void cudaq::cc::AllocaOp::getCanonicalizationPatterns(
 // CastOp
 //===----------------------------------------------------------------------===//
 
-OpFoldResult cudaq::cc::CastOp::fold(FoldAdaptor) {
+OpFoldResult cudaq::cc::CastOp::fold(FoldAdaptor adaptor) {
   // If cast is a nop, just forward the argument to the uses.
   if (getType() == getValue().getType())
     return getValue();
+  if (auto optConst = adaptor.getValue()) {
+    // Replace a constant + cast with a new constant of an updated type.
+    auto ty = getType();
+    OpBuilder builder(*this);
+    auto fltTy = builder.getF32Type();
+    auto dblTy = builder.getF64Type();
+    auto loc = getLoc();
+    if (auto attr = dyn_cast<IntegerAttr>(optConst)) {
+      auto val = attr.getInt();
+      if (isa<IntegerType>(ty)) {
+        auto width = ty.getIntOrFloatBitWidth();
+        return builder.create<arith::ConstantIntOp>(loc, val, width)
+            .getResult();
+      } else if (ty == fltTy) {
+        if (getZint()) {
+          APFloat fval(static_cast<float>(static_cast<std::uint64_t>(val)));
+          return builder.create<arith::ConstantFloatOp>(loc, fval, fltTy)
+              .getResult();
+        }
+        if (getSint()) {
+          APFloat fval(static_cast<float>(val));
+          return builder.create<arith::ConstantFloatOp>(loc, fval, fltTy)
+              .getResult();
+        }
+      } else if (ty == dblTy) {
+        if (getZint()) {
+          APFloat fval(static_cast<double>(static_cast<std::uint64_t>(val)));
+          return builder.create<arith::ConstantFloatOp>(loc, fval, dblTy)
+              .getResult();
+        }
+        if (getSint()) {
+          APFloat fval(static_cast<double>(val));
+          return builder.create<arith::ConstantFloatOp>(loc, fval, dblTy)
+              .getResult();
+        }
+      }
+    }
+    if (auto attr = dyn_cast<FloatAttr>(optConst)) {
+      auto val = attr.getValue();
+      if (isa<IntegerType>(ty)) {
+        auto width = ty.getIntOrFloatBitWidth();
+        if (getZint()) {
+          auto v = static_cast<std::uint64_t>(val.convertToDouble());
+          return builder.create<arith::ConstantIntOp>(loc, v, width)
+              .getResult();
+        }
+        if (getSint()) {
+          auto v = static_cast<std::int64_t>(val.convertToDouble());
+          return builder.create<arith::ConstantIntOp>(loc, v, width)
+              .getResult();
+        }
+      } else if (ty == fltTy) {
+        float f = val.convertToDouble();
+        APFloat fval(f);
+        return builder.create<arith::ConstantFloatOp>(loc, fval, fltTy)
+            .getResult();
+      } else if (ty == dblTy) {
+        APFloat fval{val.convertToDouble()};
+        return builder.create<arith::ConstantFloatOp>(loc, fval, dblTy)
+            .getResult();
+      }
+    }
+  }
   return nullptr;
 }
 
