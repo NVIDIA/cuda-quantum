516e50e583f076b4e4ec97065e95a2d7caa031b3
Anna Gringauze <agringauze@nvidia.com>
2024-07-17 15:43:57 -0700

    Format

--- runtime/common/Environment.cpp
+++ runtime/common/Environment.cpp
@@ -17,8 +17,9 @@ bool getEnvBool(const char *envName, bool defaultVal = false) {
   if (auto envVal = std::getenv(envName)) {
     std::string tmp(envVal);
     std::transform(tmp.begin(), tmp.end(), tmp.begin(),
-                    [](unsigned char c) { return std::tolower(c); });
-    return (tmp == "1" || tmp == "on" || tmp == "true" || tmp == "y" || tmp == "yes");
+                   [](unsigned char c) { return std::tolower(c); });
+    return (tmp == "1" || tmp == "on" || tmp == "true" || tmp == "y" ||
+            tmp == "yes");
   }
   return defaultVal;
 }
