aa9a6778bc17ac5d46bc2c9f63f797635d2dbab0
Thien Nguyen <thiennguyen@nvidia.com>
2024-07-17 04:00:08 +0000

    Code tidy up: mising eof

--- test/TargetConfig/check_compile.cpp
+++ test/TargetConfig/check_compile.cpp
@@ -6,6 +6,7 @@
  * the terms of the Apache License 2.0 which accompanies this distribution.    *
  ******************************************************************************/
 
+// REQUIRES: c++20
 // Check that we can compile all the targets
 // i.e., all the config files are valid.
 // RUN: for target in $(nvq++ --list-targets); do echo "Testing target: ${target}"; nvq++ %cpp_std --target ${target} %s; done
