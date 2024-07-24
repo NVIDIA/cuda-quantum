fead557383a18a9eadb203682e91bb1419d8bf6e
Eric Schweitz <eschweitz@nvidia.com>
2024-07-24 16:49:13 -0700

    [NFC] merge two small tests running the same script into one file. (#1988)

--- test/AST-Quake/tuple-1.cpp
+++ test/AST-Quake/tuple-1.cpp
@@ -24,7 +24,7 @@ void RzArcTan2(bool input, std::pair<int, double> basis) __qpu__ {
   prepQubit(basis, target);
 }
 
-int main() {
+int main1() {
   RzArcTan2(true, {});
   return 0;
 }
@@ -53,5 +53,51 @@ int main() {
 // CHECK:           cc.store %[[VAL_9]], %[[VAL_8]] : !cc.ptr<!cc.struct<{i32, f64}>>
 // CHECK:           %[[VAL_10:.*]] = cc.load %[[VAL_8]] : !cc.ptr<!cc.struct<{i32, f64}>>
 // CHECK:           call @__nvqpp__mlirgen__function_prepQubit{{.*}}(%[[VAL_10]], %[[VAL_6]]) : (!cc.struct<{i32, f64}>, !quake.ref) -> ()
+// CHECK:           return
+// CHECK:         }
+
+void prepQubit2(std::tuple<bool, float, unsigned> basis,
+               cudaq::qubit &q) __qpu__ {}
+
+void RzArcTan22(bool input, std::tuple<bool, float, unsigned> basis) __qpu__ {
+  cudaq::qubit aux;
+  cudaq::qubit resource;
+  cudaq::qubit target;
+  if (input) {
+    x(target);
+  }
+  prepQubit2(basis, target);
+}
+
+int main2() {
+  RzArcTan22(true, {});
+  return 0;
+}
+
+// CHECK-LABEL:   func.func @__nvqpp__mlirgen__function_prepQubit2
+// CHECK-SAME:        (%[[VAL_0:.*]]: !cc.struct<{i1, f32, i32}>, %[[VAL_1:.*]]: !quake.ref)
+// CHECK:           %[[VAL_2:.*]] = cc.alloca !cc.struct<{i1, f32, i32}>
+// CHECK:           cc.store %[[VAL_0]], %[[VAL_2]] : !cc.ptr<!cc.struct<{i1, f32, i32}>>
+// CHECK:           return
+// CHECK:         }
+
+// CHECK-LABEL:   func.func @__nvqpp__mlirgen__function_RzArcTan22
+// CHECK-SAME:        (%[[VAL_0:.*]]: i1, %[[VAL_1:.*]]: !cc.struct<{i1, f32, i32}>) attributes
+// CHECK:           %[[VAL_2:.*]] = cc.alloca i1
+// CHECK:           cc.store %[[VAL_0]], %[[VAL_2]] : !cc.ptr<i1>
+// CHECK:           %[[VAL_3:.*]] = cc.alloca !cc.struct<{i1, f32, i32}>
+// CHECK:           cc.store %[[VAL_1]], %[[VAL_3]] : !cc.ptr<!cc.struct<{i1, f32, i32}>>
+// CHECK:           %[[VAL_4:.*]] = quake.alloca !quake.ref
+// CHECK:           %[[VAL_5:.*]] = quake.alloca !quake.ref
+// CHECK:           %[[VAL_6:.*]] = quake.alloca !quake.ref
+// CHECK:           %[[VAL_7:.*]] = cc.load %[[VAL_2]] : !cc.ptr<i1>
+// CHECK:           cc.if(%[[VAL_7]]) {
+// CHECK:             quake.x %[[VAL_6]] : (!quake.ref) -> ()
+// CHECK:           }
+// CHECK:           %[[VAL_8:.*]] = cc.alloca !cc.struct<{i1, f32, i32}>
+// CHECK:           %[[VAL_9:.*]] = cc.load %[[VAL_3]] : !cc.ptr<!cc.struct<{i1, f32, i32}>>
+// CHECK:           cc.store %[[VAL_9]], %[[VAL_8]] : !cc.ptr<!cc.struct<{i1, f32, i32}>>
+// CHECK:           %[[VAL_10:.*]] = cc.load %[[VAL_8]] : !cc.ptr<!cc.struct<{i1, f32, i32}>>
+// CHECK:           call @__nvqpp__mlirgen__function_prepQubit2{{.*}}(%[[VAL_10]], %[[VAL_6]]) : (!cc.struct<{i1, f32, i32}>, !quake.ref) -> ()
 // CHECK:           return
 // CHECK:         }
