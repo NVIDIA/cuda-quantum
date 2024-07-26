b6546b93043a752a6b7005c5b40ac5a1a0498e04
Alex McCaskey <amccaskey@nvidia.com>
2024-07-26 13:14:15 +0000

    update to use value semantics for quantum structs, do not use compute_ptr
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>

--- test/AST-Quake/quantum_struct.cpp
+++ test/AST-Quake/quantum_struct.cpp
@@ -41,28 +41,24 @@ int main() { cudaq::sample(entry, 4); }
 // CHECK-SAME:                                                                %[[VAL_0:.*]]: !cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>) attributes {"cudaq-kernel", no_this} {
 // CHECK:           %[[VAL_1:.*]] = arith.constant 1 : i64
 // CHECK:           %[[VAL_2:.*]] = arith.constant 0 : i64
-// CHECK:           %[[VAL_3:.*]] = cc.alloca !cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>
-// CHECK:           cc.store %[[VAL_0]], %[[VAL_3]] : !cc.ptr<!cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>>
-// CHECK:           %[[VAL_4:.*]] = cc.compute_ptr %[[VAL_3]][2] : (!cc.ptr<!cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>>) -> !cc.ptr<!quake.veq<?>>
-// CHECK:           %[[VAL_5:.*]] = cc.load %[[VAL_4]] : !cc.ptr<!quake.veq<?>>
-// CHECK:           %[[VAL_6:.*]] = quake.veq_size %[[VAL_5]] : (!quake.veq<?>) -> i64
-// CHECK:           %[[VAL_7:.*]] = cc.loop while ((%[[VAL_8:.*]] = %[[VAL_2]]) -> (i64)) {
-// CHECK:             %[[VAL_9:.*]] = arith.cmpi slt, %[[VAL_8]], %[[VAL_6]] : i64
-// CHECK:             cc.condition %[[VAL_9]](%[[VAL_8]] : i64)
+// CHECK:           %[[VAL_3:.*]] = cc.extract_value %[[VAL_0]][2] : (!cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>) -> !quake.veq<?>
+// CHECK:           %[[VAL_4:.*]] = quake.veq_size %[[VAL_3]] : (!quake.veq<?>) -> i64
+// CHECK:           %[[VAL_5:.*]] = cc.loop while ((%[[VAL_6:.*]] = %[[VAL_2]]) -> (i64)) {
+// CHECK:             %[[VAL_7:.*]] = arith.cmpi slt, %[[VAL_6]], %[[VAL_4]] : i64
+// CHECK:             cc.condition %[[VAL_7]](%[[VAL_6]] : i64)
 // CHECK:           } do {
-// CHECK:           ^bb0(%[[VAL_10:.*]]: i64):
-// CHECK:             %[[VAL_11:.*]] = quake.extract_ref %[[VAL_5]]{{\[}}%[[VAL_10]]] : (!quake.veq<?>, i64) -> !quake.ref
-// CHECK:             quake.h %[[VAL_11]] : (!quake.ref) -> ()
-// CHECK:             cc.continue %[[VAL_10]] : i64
+// CHECK:           ^bb0(%[[VAL_8:.*]]: i64):
+// CHECK:             %[[VAL_9:.*]] = quake.extract_ref %[[VAL_3]]{{\[}}%[[VAL_8]]] : (!quake.veq<?>, i64) -> !quake.ref
+// CHECK:             quake.h %[[VAL_9]] : (!quake.ref) -> ()
+// CHECK:             cc.continue %[[VAL_8]] : i64
 // CHECK:           } step {
-// CHECK:           ^bb0(%[[VAL_12:.*]]: i64):
-// CHECK:             %[[VAL_13:.*]] = arith.addi %[[VAL_12]], %[[VAL_1]] : i64
-// CHECK:             cc.continue %[[VAL_13]] : i64
+// CHECK:           ^bb0(%[[VAL_10:.*]]: i64):
+// CHECK:             %[[VAL_11:.*]] = arith.addi %[[VAL_10]], %[[VAL_1]] : i64
+// CHECK:             cc.continue %[[VAL_11]] : i64
 // CHECK:           } {invariant}
-// CHECK:           %[[VAL_14:.*]] = cc.compute_ptr %[[VAL_3]][2] : (!cc.ptr<!cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>>) -> !cc.ptr<!quake.veq<?>>
-// CHECK:           %[[VAL_15:.*]] = cc.load %[[VAL_14]] : !cc.ptr<!quake.veq<?>>
-// CHECK:           %[[VAL_16:.*]] = quake.extract_ref %[[VAL_15]][0] : (!quake.veq<?>) -> !quake.ref
-// CHECK:           call @__nvqpp__mlirgen__function_hello._Z5helloRN5cudaq5quditILm2EEE(%[[VAL_16]]) : (!quake.ref) -> ()
+// CHECK:           %[[VAL_12:.*]] = cc.extract_value %[[VAL_0]][2] : (!cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>) -> !quake.veq<?>
+// CHECK:           %[[VAL_13:.*]] = quake.extract_ref %[[VAL_12]][0] : (!quake.veq<?>) -> !quake.ref
+// CHECK:           call @__nvqpp__mlirgen__function_hello._Z5helloRN5cudaq5quditILm2EEE(%[[VAL_13]]) : (!quake.ref) -> ()
 // CHECK:           return
 // CHECK:         }
 
@@ -75,15 +71,11 @@ int main() { cudaq::sample(entry, 4); }
 // CHECK:           %[[VAL_4:.*]] = cc.load %[[VAL_3]] : !cc.ptr<i32>
 // CHECK:           %[[VAL_5:.*]] = cc.cast signed %[[VAL_4]] : (i32) -> i64
 // CHECK:           %[[VAL_6:.*]] = quake.alloca !quake.veq<?>{{\[}}%[[VAL_5]] : i64]
-// CHECK:           %[[VAL_7:.*]] = cc.alloca !cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>
-// CHECK:           %[[VAL_8:.*]] = cc.cast %[[VAL_7]] : (!cc.ptr<!cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>>) -> !cc.ptr<i32>
-// CHECK:           cc.store %[[VAL_2]], %[[VAL_8]] : !cc.ptr<i32>
-// CHECK:           %[[VAL_9:.*]] = cc.compute_ptr %[[VAL_7]][1] : (!cc.ptr<!cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>>) -> !cc.ptr<f64>
-// CHECK:           cc.store %[[VAL_1]], %[[VAL_9]] : !cc.ptr<f64>
-// CHECK:           %[[VAL_10:.*]] = cc.compute_ptr %[[VAL_7]][2] : (!cc.ptr<!cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>>) -> !cc.ptr<!quake.veq<?>>
-// CHECK:           cc.store %[[VAL_6]], %[[VAL_10]] : !cc.ptr<!quake.veq<?>>
-// CHECK:           %[[VAL_11:.*]] = cc.load %[[VAL_7]] : !cc.ptr<!cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>>
-// CHECK:           call @__nvqpp__mlirgen__function_kernel._Z6kernel4test(%[[VAL_11]]) : (!cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>) -> ()
+// CHECK:           %[[VAL_7:.*]] = cc.undef !cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>
+// CHECK:           %[[VAL_8:.*]] = cc.insert_value %[[VAL_2]], %[[VAL_7]][0] : (!cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>, i32) -> !cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>
+// CHECK:           %[[VAL_9:.*]] = cc.insert_value %[[VAL_1]], %[[VAL_8]][1] : (!cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>, f64) -> !cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>
+// CHECK:           %[[VAL_10:.*]] = cc.insert_value %[[VAL_6]], %[[VAL_9]][2] : (!cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>, !quake.veq<?>) -> !cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>
+// CHECK:           call @__nvqpp__mlirgen__function_kernel._Z6kernel4test(%[[VAL_10]]) : (!cc.struct<"test" {i32, f64, !quake.veq<?>} [256,8]>) -> ()
 // CHECK:           return
 // CHECK:         }
 
