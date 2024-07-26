da01ba46345eb57e113f69ee936cc5971855ebaa
Ben Howe <bhowe@nvidia.com>
2024-07-26 19:13:23 +0000

    Add better comment/explanation in remote_test_assert.h

--- targettests/Remote-Sim/remote_test_assert.h
+++ targettests/Remote-Sim/remote_test_assert.h
@@ -6,10 +6,16 @@
  * the terms of the Apache License 2.0 which accompanies this distribution.    *
  ******************************************************************************/
 
-// llvm-lit tests for remote-mqpu tend to hang rather than abort if they hit an
-// assertion error using the regular `assert()` call. Use this instead to
-// cleanly exit/abort from your main function while printing an error message
-// about where the error occurred.
+// llvm-lit tests for remote-mqpu hang rather than abort if they hit an
+// assertion error using the regular `assert()` call. The reason is that the
+// AutoLaunchRestServerProcess destructor is not called when the assertion
+// fails, so the `cudaq-qpud` process stays running in the background, and the
+// llvm-lit wrapper thinks the test has not completed, so it just hangs waiting
+// for completion.
+// Note: this is similar to what would happen if abort() were called.
+// E.g., https://man7.org/linux/man-pages/man3/abort.3.html says:
+// "As with other cases of abnormal termination the functions registered with
+// atexit(3) and on_exit(3) are not called."
 #define REMOTE_TEST_ASSERT(x)                                                  \
   do {                                                                         \
     if (!(x)) {                                                                \
