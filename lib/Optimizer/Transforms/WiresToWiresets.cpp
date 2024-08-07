f8d3e691203d155730d422c84fb829174911d13c
Adam T. Geller <adgeller@nvidia.com>
2024-08-07 15:21:01 -0700

    Address feedback

--- lib/Optimizer/Transforms/WiresToWiresets.cpp
+++ lib/Optimizer/Transforms/WiresToWiresets.cpp
@@ -6,14 +6,6 @@
  * the terms of the Apache License 2.0 which accompanies this distribution.    *
  ******************************************************************************/
 
-/*******************************************************************************
- * Copyright (c) 2022 - 2024 NVIDIA Corporation & Affiliates.                  *
- * All rights reserved.                                                        *
- *                                                                             *
- * This source code and the accompanying materials are made available under    *
- * the terms of the Apache License 2.0 which accompanies this distribution.    *
- ******************************************************************************/
-
 #include "cudaq/Frontend/nvqpp/AttributeNames.h"
 #include "cudaq/Optimizer/Dialect/CC/CCDialect.h"
 #include "cudaq/Optimizer/Dialect/Quake/QuakeDialect.h"
@@ -58,9 +50,8 @@ public:
 };
 
 class SinkOpPat : public OpRewritePattern<quake::SinkOp> {
+  using OpRewritePattern::OpRewritePattern;
 public:
-  SinkOpPat(MLIRContext *context) : OpRewritePattern<quake::SinkOp>(context) {}
-
   LogicalResult matchAndRewrite(quake::SinkOp release,
                                 PatternRewriter &rewriter) const override {
     rewriter.replaceOpWithNewOp<quake::ReturnWireOp>(release,
@@ -94,8 +85,6 @@ struct WiresToWiresetsPass
     target.addLegalDialect<quake::QuakeDialect>();
     target.addIllegalOp<quake::NullWireOp>();
     target.addIllegalOp<quake::SinkOp>();
-    target.addLegalOp<quake::BorrowWireOp>();
-    target.addLegalOp<quake::ReturnWireOp>();
     if (failed(applyPartialConversion(mod, target, std::move(patterns)))) {
       mod->emitOpError("Converting individual wires to wireset wires failed");
       signalPassFailure();
