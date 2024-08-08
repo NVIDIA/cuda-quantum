ce7d8256e3430124b683c7ce19c8cb974261fbd2
Sachin Pisal <spisal@nvidia.com>
2024-08-07 20:27:52 -0700

    adding invalid argument

--- runtime/nvqir/custatevec/CuStateVecCircuitSimulator.cpp
+++ runtime/nvqir/custatevec/CuStateVecCircuitSimulator.cpp
@@ -194,9 +194,8 @@ protected:
       HANDLE_CUDA_ERROR(cudaPointerGetAttributes(&attributes, state));
 
       if (attributes.type == cudaMemoryTypeDevice) {
-        throw std::runtime_error(
-            "[CuStateVecCircuitSimulator] Currently, not supporting device "
-            "memory pointer to the state passed by the user");
+        throw std::invalid_argument(
+            "[CuStateVecCircuitSimulator] Incompatible host pointer");
       }
 
       // First allocation, so just set the user provided data here
