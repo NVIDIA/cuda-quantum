865eea448fb5dceba91ba5ac1950fac6b7f79d91
marwafar <mfarag@nvidia.com>
2024-08-08 03:10:43 +0000

    add space

--- docs/sphinx/examples/python/tutorials/noise_parameters.py
+++ docs/sphinx/examples/python/tutorials/noise_parameters.py
@@ -2,35 +2,35 @@ import cudaq
 import numpy as np
 
 ### Hidden qubit parameters
-T1=1000 #ns
-T2=200 #ns
-detuning_rate=np.pi/250
+T1 = 1000 #ns
+T2 = 200 #ns
+detuning_rate = np.pi/250
 
 ### Define Kraus Channels
 
 def error_prob(t,T12):
-        error=1-np.exp(-t/T12) #nanoseconds
+        error = 1-np.exp(-t/T12) #nanoseconds
         return error
 
 def t1_kraus(t,T1):
 
-    t1_decay=error_prob(t,T1)
-    kraus_0=np.array([[1.0,0.0],[0.0,np.sqrt(1-t1_decay)]],dtype=np.complex128,order='F')
-    kraus_1=np.array([[0.0,np.sqrt(t1_decay)],[0.0,0.0]],dtype=np.complex128,order='F')
-    kraus_op=cudaq.KrausChannel([kraus_0,kraus_1])
+    t1_decay = error_prob(t,T1)
+    kraus_0 = np.array([[1.0,0.0],[0.0,np.sqrt(1-t1_decay)]],dtype=np.complex128,order='F')
+    kraus_1 = np.array([[0.0,np.sqrt(t1_decay)],[0.0,0.0]],dtype=np.complex128,order='F')
+    kraus_op = cudaq.KrausChannel([kraus_0,kraus_1])
     return kraus_op
 
 def coherent_z_kraus(angle):
-    kraus_0=np.array([[np.exp(-1j*angle/2),0.0],[0.0,np.exp(1j*angle/2)]],dtype="complex128",order="F")
-    kraus_op=cudaq.KrausChannel([kraus_0])
+    kraus_0 = np.array([[np.exp(-1j*angle/2),0.0],[0.0,np.exp(1j*angle/2)]],dtype="complex128",order="F")
+    kraus_op = cudaq.KrausChannel([kraus_0])
     return kraus_op
 
 def t2_kraus(t,T2):
     
-    t2_decay=error_prob(t,T2)
-    kraus_0=np.array([[1.0,0.0],[0.0,np.sqrt(1-t2_decay)]],dtype=np.complex128,order='F')
-    kraus_1=np.array([[0.0,0.0],[0.0,np.sqrt(t2_decay)]],dtype=np.complex128,order='F')
-    kraus_op=cudaq.KrausChannel([kraus_0,kraus_1])
+    t2_decay = error_prob(t,T2)
+    kraus_0 = np.array([[1.0,0.0],[0.0,np.sqrt(1-t2_decay)]],dtype=np.complex128,order='F')
+    kraus_1 = np.array([[0.0,0.0],[0.0,np.sqrt(t2_decay)]],dtype=np.complex128,order='F')
+    kraus_op = cudaq.KrausChannel([kraus_0,kraus_1])
     return kraus_op
 
 ### Define Noise model and gates
@@ -63,7 +63,7 @@ def Id(self, time, qubit) -> None:
 
 def custom_rx(self, time, amp, qubit):
      ##hidden frequency
-     f=5.3012314 #GHz
-     amp_adjust=0.1
-     angle=2*np.pi*time*amp*f*amp_adjust
+     f = 5.3012314 #GHz
+     amp_adjust = 0.1
+     angle = 2*np.pi*time*amp*f*amp_adjust
      self.rx(angle,qubit)
