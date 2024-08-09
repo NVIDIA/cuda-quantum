1a7c36f6ea5c9e998fa894ece78227b508d94934
Pradnya Khalate <pkhalate@nvidia.com>
2024-08-09 09:57:45 -0700

    * Simplified TBI examples
    ---------
    
    Co-authored-by: Omar-ORCA

--- docs/sphinx/examples/python/providers/photonics_tbi.py
+++ docs/sphinx/examples/python/providers/photonics_tbi.py
@@ -1,35 +1,18 @@
 import cudaq
 import numpy as np
 
-from dataclasses import dataclass, field
-
 cudaq.set_target("photonics")
-
-# Global variables
-zero = 0
-one = 1
-n_modes = 4
-input_state = [2, 1, 3, 1]
-d = sum(input_state)
-n_loops = 2
-loop_lengths = [1, 2]
-sum_loop_lengths = sum(loop_lengths)
-n_beamsplitters = n_loops * n_modes - sum_loop_lengths
-
-
-@dataclass
-class TBIParameters:
-    bs_angles: list[float]
-    ps_angles: list[float]
-    input_state: list = field(default_factory=lambda: input_state)
-    loop_lengths: list = field(default_factory=lambda: loop_lengths)
-    n_samples: int = 1000000
 
 
 @cudaq.kernel
-def TBI(parameters: TBIParameters):
-    bs_angles = parameters.bs_angles
-    ps_angles = parameters.ps_angles
+def TBI(
+    bs_angles: list[float],
+    ps_angles: list[float],
+    input_state: list[int],
+    loop_lengths: list[int],
+):
+    n_modes = len(input_state)
+    d = sum(input_state) + 1  # qudit level
 
     quds = [qudit(d) for _ in range(n_modes)]
 
@@ -47,10 +30,16 @@ def TBI(parameters: TBIParameters):
     mz(quds)
 
 
-bs_angles = np.linspace(np.pi / 3, np.pi / 6, n_beamsplitters)
-ps_angles = np.linspace(np.pi / 3, np.pi / 5, n_beamsplitters)
+input_state = [2, 1, 3, 1]
+loop_lengths = [1, 2]
+n_beam_splitters = len(loop_lengths) * len(input_state) - sum(loop_lengths)
+bs_angles = np.linspace(np.pi / 3, np.pi / 6, n_beam_splitters)
+ps_angles = np.linspace(np.pi / 3, np.pi / 5, n_beam_splitters)
 
-parameters = TBIParameters(bs_angles, ps_angles)
-
-counts = cudaq.sample(TBI, parameters)
+counts = cudaq.sample(TBI,
+                      bs_angles,
+                      ps_angles,
+                      input_state,
+                      loop_lengths,
+                      shots_count=1000000)
 counts.dump()
