b97b01ccd4a6392a55a051bdf86ee9f09161fca4
Sachin Pisal <spisal@nvidia.com>
2024-08-19 10:34:02 -0700

    keeping only the necessary imports

--- docs/sphinx/examples/python/tutorials/src/vqe_cudaq_qnp.py
+++ docs/sphinx/examples/python/tutorials/src/vqe_cudaq_qnp.py
@@ -4,9 +4,6 @@
 import numpy as np
 import cudaq
 from cudaq import spin as spin_op
-import openfermion as of
-from openfermion.hamiltonians import s_squared_operator
-from openfermion.transforms import jordan_wigner
 
 
 class VQE(object):
