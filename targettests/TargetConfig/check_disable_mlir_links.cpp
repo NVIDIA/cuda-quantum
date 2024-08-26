a3f1e87aa17a3a26c251f941de67fea69f8b4de8
Alex McCaskey <amccaskey@nvidia.com>
2024-08-26 14:16:53 -0400

    enable downstream libraries to turn off direct mlir linking with nvq++ (#2144)
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>
    Co-authored-by: Ben Howe <141149032+bmhowe23@users.noreply.github.com>

---
+++ targettests/TargetConfig/check_disable_mlir_links.cpp
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2024 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+// REQUIRES: c++20
+// RUN: nvq++ --disable-mlir-links %s -o %s.x && ! ldd %s.x | grep -q libcudaq-mlir-runtime.so 
+
+#include "cudaq.h"
+
+__qpu__ void bell() {
+  cudaq::qubit q, r;
+  h(q);
+  x<cudaq::ctrl>(q, r);
+}
+
+int main() {
+  auto counts = cudaq::sample(bell);
+  counts.dump();
+  return 0;
+}
