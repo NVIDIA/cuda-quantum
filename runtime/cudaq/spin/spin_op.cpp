ac2a05ce268c174f3c4d7857b28c2f23405e670b
Sachin Pisal <spisal@nvidia.com>
2024-08-29 10:52:08 -0700

    no need to check for -0 in imaginary part
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- runtime/cudaq/spin/spin_op.cpp
+++ runtime/cudaq/spin/spin_op.cpp
@@ -115,10 +115,6 @@ mult(std::vector<bool> row, std::vector<bool> other_row,
     final_coeff.real(0);
   }
 
-  if (std::abs(final_coeff.imag()) < 1e-12) {
-    final_coeff.imag(0);
-  }
-
   return std::make_pair(final_coeff, result);
 }
 } // namespace details
