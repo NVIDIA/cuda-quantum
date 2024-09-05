7cd5f186e64b9bf3e1cf345e4693cbbb9d2e2d14
Sachin Pisal <spisal@nvidia.com>
2024-09-04 20:03:19 -0700

    adding few more tests
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- unittests/spin_op/SpinOpTester.cpp
+++ unittests/spin_op/SpinOpTester.cpp
@@ -144,19 +144,34 @@ TEST(SpinOpTester, checkMultiplication) {
   EXPECT_EQ((i(2) * i(2)).get_coefficient(), std::complex<double>(1, 0));
 
   std::cout << "I * X: X\n";
-  (i(3) * i(3)).dump();
+  (i(3) * x(3)).dump();
   EXPECT_EQ(x(3), i(3) * x(3));
   EXPECT_EQ((i(3) * x(3)).get_coefficient(), std::complex<double>(1, 0));
 
   std::cout << "I * Y: Y\n";
-  (i(3) * i(3)).dump();
+  (i(3) * y(3)).dump();
   EXPECT_EQ(y(3), i(3) * y(3));
   EXPECT_EQ((i(3) * y(3)).get_coefficient(), std::complex<double>(1, 0));
 
   std::cout << "I * Z: Z\n";
-  (i(3) * i(3)).dump();
+  (i(3) * z(3)).dump();
   EXPECT_EQ(z(3), i(3) * z(3));
   EXPECT_EQ((i(3) * z(3)).get_coefficient(), std::complex<double>(1, 0));
+
+  std::cout << "X * I: X\n";
+  (x(3) * i(3)).dump();
+  EXPECT_EQ(x(3), x(3) * i(3));
+  EXPECT_EQ((x(3) * i(3)).get_coefficient(), std::complex<double>(1, 0));
+
+  std::cout << "Y * I: Y\n";
+  (y(3) * i(3)).dump();
+  EXPECT_EQ(y(3), y(3) * i(3));
+  EXPECT_EQ((y(3) * i(3)).get_coefficient(), std::complex<double>(1, 0));
+
+  std::cout << "Z * I: Z\n";
+  (z(3) * i(3)).dump();
+  EXPECT_EQ(z(3), z(3) * i(3));
+  EXPECT_EQ((z(3) * i(3)).get_coefficient(), std::complex<double>(1, 0));
 
   std::cout << "Eliminate zero coefficient terms\n";
   auto result = (0.0 * x(0)) * z(1);
