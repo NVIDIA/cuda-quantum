8196fc3f3fb958a712494a8269f117ee72901a93
Thien Nguyen <thiennguyen@nvidia.com>
2024-09-09 08:49:26 +0000

    Implement visitor pattern

--- python/cudaq/operator/cusp_solver.py
+++ python/cudaq/operator/cusp_solver.py
@@ -9,6 +9,7 @@ from ..kernel.register_op import register_operation
 from ..mlir._mlir_libs._quakeDialects import cudaq_runtime
 from ..kernel.kernel_decorator import PyKernelDecorator
 from ..runtime.observe import observe
+from .manipulation import OperatorArithmetics
 
 import cupy as cp
 import numpy as np
@@ -16,6 +17,177 @@ import numpy as np
 import cusuperop as cuso
 
 from enum import Enum
+
+class CuSuperOpHamConversion(OperatorArithmetics[cuso.OperatorTerm | cuso.CallbackCoefficient ]):
+    def __init__(self, dimensions: Mapping[int, int]):
+        self._dimensions = dimensions
+    def _scalars_add(self, op1: cuso.CallbackCoefficient, op2: cuso.CallbackCoefficient) -> cuso.CallbackCoefficient:
+        if op1.is_callable or op2.is_callable:
+            raise ValueError("Cannot add CallbackCoefficients")
+        coeff = cuso.CallbackCoefficient(None, op1.scalar + op2.scalar)
+        return coeff 
+    
+    
+    def _wrap_callback_mult_scalar(self, func, scalar):
+        def inplace_func(t, args):
+            return func(t, args) * scalar
+        return inplace_func
+    
+    def _wrap_callback_mult(self, func1, func2):
+        def inplace_func(t, args):
+            return func1(t) * func2(t)
+        return inplace_func
+
+    def _scalars_mult(self, op1: cuso.CallbackCoefficient, op2: cuso.CallbackCoefficient) -> cuso.CallbackCoefficient:
+        if op1.is_callable and op2.is_callable:
+            return cuso.CallbackCoefficient(self._wrap_callback_mult(op1.callback, op2.callback))
+        elif op1.is_callable:
+            if op2.scalar == 1.0:
+                return op1
+            return cuso.CallbackCoefficient(self._wrap_callback_mult_scalar(op1.callback, op2.scalar))
+        elif op2.is_callable:
+            if op1.scalar == 1.0:
+                return op2
+            return cuso.CallbackCoefficient(self._wrap_callback_mult_scalar(op2.callback, op1.scalar))
+        else:
+            return cuso.CallbackCoefficient(None, op1.scalar * op2.scalar)
+        
+    def _scalar_mult_op(self, scalar: cuso.CallbackCoefficient, op: cuso.OperatorTerm) -> cuso.OperatorTerm:
+        if scalar.is_callable:
+            new_opterm = cuso.OperatorTerm(dtype=op.dtype)
+            for term, coeff in zip(op.terms, op.coefficients):
+                if coeff.is_callable:
+                    new_coeff = cuso.CallbackCoefficient(self._wrap_callback_mult(scalar.callback, coeff.callback))
+                    new_opterm.append(term, new_coeff)
+                else:
+                    if coeff.scalar == 1.0:
+                        new_opterm.append(term, scalar.callback)
+                    else:
+                        new_coeff = cuso.CallbackCoefficient(self._wrap_callback_mult_scalar(scalar.callback, coeff.scalar))
+                        new_opterm.append(term, new_coeff)
+            return new_opterm
+        else:
+            return op * scalar.scalar
+
+    def tensor(self, op1: cuso.OperatorTerm | cuso.CallbackCoefficient, op2: cuso.OperatorTerm | cuso.CallbackCoefficient) -> cuso.OperatorTerm | cuso.CallbackCoefficient:
+        if isinstance(op1, cuso.CallbackCoefficient) and isinstance(op2, cuso.CallbackCoefficient):
+            return self._scalars_mult(op1, op2)
+        if isinstance(op1, cuso.CallbackCoefficient):
+            return self._scalar_mult_op(op1, op2)
+        if isinstance(op2, cuso.CallbackCoefficient):
+            return self._scalar_mult_op(op2, op1)
+        new_opterm = cuso.OperatorTerm(dtype=op1.dtype)
+        for term2, coeff2 in zip(op2.terms, op2.coefficients):
+            for term1, coeff1 in zip(op1.terms, op1.coefficients):
+                if coeff1.is_callable or coeff2.is_callable:
+                    raise ValueError("Cannot multiply CallbackCoefficients")
+                mul_term = term1 + term2
+                new_opterm.append(mul_term, coeff1.scalar * coeff2.scalar)
+        return new_opterm     
+
+
+    def mul(self, op1: cuso.OperatorTerm | cuso.CallbackCoefficient, op2: cuso.OperatorTerm | cuso.CallbackCoefficient) -> cuso.OperatorTerm | cuso.CallbackCoefficient:
+        if isinstance(op1, cuso.CallbackCoefficient) and isinstance(op2, cuso.CallbackCoefficient):
+            return self._scalars_mult(op1, op2)
+        if isinstance(op1, cuso.CallbackCoefficient):
+            return self._scalar_mult_op(op1, op2)
+        if isinstance(op2, cuso.CallbackCoefficient):
+            return self._scalar_mult_op(op2, op1)
+        
+        new_opterm = cuso.OperatorTerm(dtype=op1.dtype)
+        for term2, coeff2 in zip(op2.terms, op2.coefficients):
+            for term1, coeff1 in zip(op1.terms, op1.coefficients):
+                if coeff1.is_callable or coeff2.is_callable:
+                    raise ValueError("Cannot multiply CallbackCoefficients")
+                mul_term = term1 + term2
+                new_opterm.append(mul_term, coeff1.scalar * coeff2.scalar)
+        return new_opterm
+
+    def add(self, op1: cuso.OperatorTerm | cuso.CallbackCoefficient, op2: cuso.OperatorTerm | cuso.CallbackCoefficient) -> cuso.OperatorTerm | cuso.CallbackCoefficient:
+        if isinstance(op1, cuso.CallbackCoefficient) and isinstance(op2, cuso.CallbackCoefficient):
+            return self._scalars_add(op1, op2)
+        if isinstance(op1, cuso.CallbackCoefficient) or isinstance(op2, cuso.CallbackCoefficient):
+            raise ValueError("Both add operands must be all scalars or all operators")
+        return op1 + op2
+
+    def _wrap_callback(self, func):
+        def inplace_func(t, args):
+            return func(t)
+        return inplace_func
+    
+    def evaluate(self, op: ElementaryOperator | ScalarOperator) -> cuso.OperatorTerm | cuso.CallbackCoefficient: 
+        if isinstance(op, ScalarOperator):
+            if op._constant_value is None:
+                return cuso.CallbackCoefficient(self._wrap_callback(op.generator))
+            else:
+                return cuso.CallbackCoefficient(None, op._constant_value)
+        else:
+            if op._id == "identity":
+                return cuso.CallbackCoefficient(None, 1.0)
+            op_mat = op.to_matrix(self._dimensions)
+            op_term = cuso.OperatorTerm()
+            op_term.append([cuso.GeneralOperator(cuso.CallbackTensor(op_mat), op.degrees, ((False,) * len(op.degrees)))], 1.0)
+            return op_term
+
+
+def computeLindladOp(hilbert_space_dims: List[int], l1: cuso.OperatorTerm, l2: cuso.OperatorTerm):
+    D_terms = []
+    for term1, coeff1 in zip(l1.terms, l1.coefficients):
+        for term2, coeff2 in zip(l2.terms, l2.coefficients):
+            if coeff1.is_callable or coeff2.is_callable:
+                raise ValueError("Cannot multiply CallbackCoefficients")
+            coeff = coeff1.scalar * numpy.conjugate(coeff2.scalar)
+            d1_terms = []
+            for sub_op_1 in term1:
+                op_mat = sub_op_1.tensor.numpy()
+                degrees = sub_op_1.modes
+                d1_terms.append((op_mat, degrees, (False,)))
+            for sub_op_2 in reversed(term2):
+                op_mat = sub_op_2.tensor.numpy()
+                degrees = sub_op_2.modes
+                d1_terms.append((numpy.conj(op_mat).T, degrees, (True,)))
+            D1 = cuso.tensor_product(*d1_terms, coeff = coeff)
+            D_terms.append(tuple((D1, 1.0)))
+            
+            d2_terms = []
+            for sub_op_2 in reversed(term2):
+                op_mat = sub_op_2.tensor.numpy()
+                degrees = sub_op_2.modes
+                d2_terms.append((numpy.conj(op_mat).T, degrees, (True,)))
+            for sub_op_1 in term1:
+                op_mat = sub_op_1.tensor.numpy()
+                degrees = sub_op_1.modes
+                d2_terms.append((op_mat, degrees, (True,)))
+            D2 = cuso.tensor_product(*d2_terms, coeff = -0.5 * coeff1.scalar * numpy.conjugate(coeff2.scalar))
+            D_terms.append(tuple((D2, 1.0)))
+            
+            d3_terms = []
+            for sub_op_1 in term1:
+                op_mat = sub_op_1.tensor.numpy()
+                degrees = sub_op_1.modes
+                d3_terms.append((op_mat, degrees, (False,)))
+            for sub_op_2 in reversed(term2):
+                op_mat = sub_op_2.tensor.numpy()
+                degrees = sub_op_2.modes
+                d3_terms.append((numpy.conj(op_mat).T, degrees, (False,)))
+            D3 = cuso.tensor_product(*d3_terms, coeff = -0.5 * coeff1.scalar * numpy.conjugate(coeff2.scalar))
+            D_terms.append(tuple((D3, 1.0)))
+    lindblad = cuso.Operator(hilbert_space_dims, *D_terms)
+    return lindblad
+
+
+def constructLiouvillian(hilbert_space_dims: List[int], ham: cuso.OperatorTerm, c_ops: List[cuso.OperatorTerm]):
+    hamiltonian = cuso.Operator(hilbert_space_dims, (ham, 1.0))
+    hamiltonian = hamiltonian * (-1j)
+    liouvillian = hamiltonian - hamiltonian.dual() 
+    
+    for c1 in c_ops:
+        for c2 in c_ops:
+            lindbladian = computeLindladOp(hilbert_space_dims, c1, c2)
+            liouvillian += lindbladian
+
+    return liouvillian
+
 class OpConversionMode(Enum):
     Hamiltonian = 1
     Lindbladian = 2
@@ -23,6 +195,9 @@ class OpConversionMode(Enum):
 # Helper to convert to cuSuperOp format
 def to_cusp_operator(operator: Operator, dimensions: Mapping[int, int], mode: OpConversionMode = OpConversionMode.Hamiltonian) -> cuso.Operator:
     hilbert_space_dims = tuple(dimensions[d] for d in range(len(dimensions)))
+    ham_term = operator._evaluate(CuSuperOpHamConversion(dimensions))
+    hamiltonian = cuso.Operator(hilbert_space_dims, (ham_term, 1.0))
+    return hamiltonian
     # print(f"Op: {operator}, type: {type(operator)}")
     if type(operator) == ElementaryOperator:
         ham_term = cuso.OperatorTerm()
@@ -405,11 +580,18 @@ def evolve_me(hamiltonian: Operator,
     # Conversion of operators to cuSuperOp
     ham_cusp = to_cusp_operator(hamiltonian, dimensions)
 
-    hamiltonian = ham_cusp * (-1j)
-    liouvillian = hamiltonian - hamiltonian.dual() 
-    for op in collapse_operators:
-        l_op = to_cusp_operator(op, dimensions, mode=OpConversionMode.Lindbladian)
-        liouvillian += l_op
+    hilbert_space_dims = tuple(dimensions[d] for d in range(len(dimensions)))
+    ham_term = hamiltonian._evaluate(CuSuperOpHamConversion(dimensions))
+    linblad_terms = []
+    for c_op in collapse_operators:
+        linblad_terms.append(c_op._evaluate(CuSuperOpHamConversion(dimensions)))
+    liouvillian = constructLiouvillian(hilbert_space_dims, ham_term, linblad_terms)
+
+    # hamiltonian = ham_cusp * (-1j)
+    # liouvillian = hamiltonian - hamiltonian.dual() 
+    # for op in collapse_operators:
+    #     l_op = to_cusp_operator(op, dimensions, mode=OpConversionMode.Lindbladian)
+    #     liouvillian += l_op
     
     # Note: we would need a CUDAQ state implementation for cuSuperOp
     if not isinstance(initial_state, cuso.State):
