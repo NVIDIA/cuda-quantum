3e0ece55e077ba527411b8f79734c674dad22f2a
Eric Schweitz <eschweitz@nvidia.com>
2024-10-03 09:56:48 -0700

    Update the python tests. Also change test to eliminate deprecation warnings.

--- python/tests/mlir/quantum_struct.py
+++ python/tests/mlir/quantum_struct.py
@@ -9,25 +9,31 @@
 # RUN: PYTHONPATH=../../ pytest -rP  %s | FileCheck %s
 
 
+import pytest
 import cudaq
 from dataclasses import dataclass
 
-@dataclass
-class patch:
-    q : cudaq.qview
-    r : cudaq.qview 
+def test_quantum_struct():
+  @dataclass
+  class patch:
+      q : cudaq.qview
+      r : cudaq.qview 
 
-@cudaq.kernel 
-def entry():
-    q = cudaq.qvector(2)
-    r = cudaq.qvector(2)
-    p = patch(q, r)
-    h(p.r[0])
+  @cudaq.kernel 
+  def entry():
+      q = cudaq.qvector(2)
+      r = cudaq.qvector(2)
+      p = patch(q, r)
+      h(p.r[0])
 
-print(entry)
+  print(entry)
 
+# CHECK-LABEL:   func.func @__nvqpp__mlirgen__entry()
 # CHECK:           %[[VAL_0:.*]] = quake.alloca !quake.veq<2>
 # CHECK:           %[[VAL_1:.*]] = quake.alloca !quake.veq<2>
+# The struq type is erased in this example.
 # CHECK:           %[[VAL_2:.*]] = quake.extract_ref %[[VAL_1]][0] : (!quake.veq<2>) -> !quake.ref
 # CHECK:           quake.h %[[VAL_2]] : (!quake.ref) -> ()
 # CHECK:           return
+# CHECK:         }
+
