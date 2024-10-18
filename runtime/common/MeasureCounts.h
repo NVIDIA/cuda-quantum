ac7a5c12a89c8b43da4f38e975480b1b092b9557
Markus Pfundstein <markus@life-electronic.nl>
2024-10-18 10:19:00 +0200

    fix

--- runtime/common/MeasureCounts.h
+++ runtime/common/MeasureCounts.h
@@ -107,6 +107,7 @@ class sample_result {
 private:
   /// @brief A mapping of register names to `ExecutionResult`s
   std::unordered_map<std::string, ExecutionResult> sampleResults;
+
   /// @brief Keep track of the total number of shots. We keep this
   /// here so we don't have to keep recomputing it.
   std::size_t totalShots = 0;
