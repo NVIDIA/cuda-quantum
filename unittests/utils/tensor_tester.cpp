a432b548500da867bd082de682c2e81c95859323
Eric Schweitz <eschweitz@nvidia.com>
2024-10-22 13:15:45 -0700

    Update unittests/utils/tensor_tester.cpp
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>

--- unittests/utils/tensor_tester.cpp
+++ unittests/utils/tensor_tester.cpp
@@ -6,7 +6,6 @@
  * the terms of the Apache License 2.0 which accompanies this distribution.    *
  ******************************************************************************/
 #include "cudaq/utils/tensor.h"
-
 #include <algorithm>
 #include <tuple>
 #include <gtest/gtest.h>
