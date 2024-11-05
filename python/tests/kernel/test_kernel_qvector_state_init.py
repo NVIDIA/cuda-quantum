074c60f778f9dc49995199903d99fe3f83eff41b
Anna Gringauze <agringauze@nvidia.com>
2024-11-05 10:02:38 -0800

    Add printing in failing tests
    Signed-off-by: Anna Gringauze <agringauze@nvidia.com>

--- python/tests/kernel/test_kernel_qvector_state_init.py
+++ python/tests/kernel/test_kernel_qvector_state_init.py
@@ -34,6 +34,7 @@ def test_kernel_synthesis_complex():
 
     counts = cudaq.sample(kernel, state)
     print(f"Non-synthesized: ${counts}")
+    print(kernel)
     assert '00' in counts
     assert '10' in counts
     assert len(counts) == 2
@@ -41,6 +42,7 @@ def test_kernel_synthesis_complex():
     synthesized = cudaq.synthesize(kernel, state)
     counts = cudaq.sample(synthesized)
     print(f"Synthesized: ${counts}")
+    print(synthesized)
     assert '00' in counts
     assert '10' in counts
     assert len(counts) == 2
