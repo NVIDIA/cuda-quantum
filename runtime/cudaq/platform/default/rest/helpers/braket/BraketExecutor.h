a69a8774595fb15408831f5a80ef2ef1d48628d7
Ryan Shaffer <3620100+rmshaffer@users.noreply.github.com>
2024-11-05 08:30:41 -0500

    Add Braket backend (#2337)
    * Add Braket backend
    
    Signed-off-by: Ryan <3620100+rmshaffer@users.noreply.github.com>
    
    * Adjust build scripts, add license notice, move result processing to BraketServerHelper
    
    Signed-off-by: Ryan <3620100+rmshaffer@users.noreply.github.com>
    
    * Fix more CI issues
    
    Signed-off-by: Ryan <3620100+rmshaffer@users.noreply.github.com>
    
    * Revert unintentional changes
    
    Signed-off-by: Ryan <3620100+rmshaffer@users.noreply.github.com>
    
    * Build fixes
    
    Co-authored-by: Thien Nguyen <thiennguyen@nvidia.com>
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * Fix for clang compiler warning / error: 'cudaq::OrcaExecutor::execute' hides overloaded virtual function
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * Restore permissions for the script to build CUDA-Q
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Bettina Heim <heimb@outlook.com>
    Signed-off-by: Pradnya Khalate <148914294+khalatepradnya@users.noreply.github.com>
    
    * Change mock port to unused value ('62445')
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * Remove braket target from sudoku tests
    
    Signed-off-by: Ryan Shaffer <3620100+rmshaffer@users.noreply.github.com>
    
    * Add braket to additional tests
    
    Signed-off-by: Ryan Shaffer <3620100+rmshaffer@users.noreply.github.com>
    
    * CMake fixes (with inputs from Bettina Heim and Ryan Shaffer)
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * Remove the anyon test (issue#2249)
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * Restore the use of 'crypto_LIBRARY' and 'crypto_INCLUDE_DIR'
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    ---------
    
    Signed-off-by: Ryan <3620100+rmshaffer@users.noreply.github.com>
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    Signed-off-by: Pradnya Khalate <148914294+khalatepradnya@users.noreply.github.com>
    Signed-off-by: Ryan Shaffer <3620100+rmshaffer@users.noreply.github.com>

---
+++ runtime/cudaq/platform/default/rest/helpers/braket/BraketExecutor.h
@@ -0,0 +1,109 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2024 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+#pragma once
+#include "common/Executor.h"
+#include "common/FmtCore.h"
+#include "common/MeasureCounts.h"
+#include "cudaq.h"
+
+#include <chrono>
+#include <iostream>
+
+#include <aws/core/Aws.h>
+
+#include <aws/braket/BraketClient.h>
+#include <aws/braket/model/CreateQuantumTaskRequest.h>
+#include <aws/braket/model/GetQuantumTaskRequest.h>
+#include <aws/braket/model/QuantumTaskStatus.h>
+
+#include <aws/sts/STSClient.h>
+
+#include <aws/s3-crt/S3CrtClient.h>
+#include <aws/s3-crt/model/GetObjectRequest.h>
+
+#include <aws/core/utils/logging/AWSLogging.h>
+#include <aws/core/utils/logging/ConsoleLogSystem.h>
+#include <aws/core/utils/logging/LogLevel.h>
+
+#include "BraketServerHelper.h"
+#include "common/Logger.h"
+
+#include <nlohmann/json.hpp>
+#include <regex>
+#include <string>
+#include <thread>
+
+namespace cudaq {
+/// @brief The Executor subclass for Amazon Braket
+class BraketExecutor : public Executor {
+  Aws::SDKOptions options;
+
+  class ScopedApi {
+    Aws::SDKOptions &options;
+
+  public:
+    ScopedApi(Aws::SDKOptions &options) : options(options) {
+      cudaq::debug("Initializing AWS API");
+      Aws::InitAPI(options);
+    }
+    ~ScopedApi() { Aws::ShutdownAPI(options); }
+  };
+
+  ScopedApi api;
+  Aws::Braket::BraketClient braketClient;
+  Aws::STS::STSClient stsClient;
+  Aws::S3Crt::S3CrtClient s3Client;
+
+  std::future<std::string> defaultBucketFuture;
+  char const *jobToken;
+
+  std::chrono::microseconds pollingInterval = std::chrono::milliseconds{100};
+
+  static auto getClientConfig() {
+    Aws::Client::ClientConfiguration clientConfig;
+    clientConfig.verifySSL = false;
+    return clientConfig;
+  }
+
+  static auto getS3ClientConfig() {
+    Aws::S3Crt::ClientConfiguration clientConfig;
+    clientConfig.verifySSL = false;
+    return clientConfig;
+  }
+
+public:
+  BraketExecutor()
+      : api(options), braketClient(getClientConfig()),
+        stsClient(getClientConfig()), s3Client(getS3ClientConfig()),
+        jobToken(std::getenv("AMZN_BRAKET_JOB_TOKEN")) {
+    cudaq::debug("Creating BraketExecutor");
+
+    defaultBucketFuture = std::async(std::launch::async, [this] {
+      auto response = stsClient.GetCallerIdentity();
+      std::string bucketName;
+      if (response.IsSuccess()) {
+        bucketName =
+            fmt::format("amazon-braket-{}-{}", getClientConfig().region,
+                        response.GetResult().GetAccount());
+        cudaq::info("Braket task results will use S3 bucket \"{}\"",
+                    bucketName);
+        return bucketName;
+      } else {
+        throw std::runtime_error(response.GetError().GetMessage());
+      }
+    });
+  }
+
+  ~BraketExecutor() = default;
+
+  /// @brief Execute the provided Braket task
+  details::future
+  execute(std::vector<KernelExecution> &codesToExecute) override;
+};
+} // namespace cudaq
