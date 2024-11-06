1c5a3ad8b847b6577dde3331c6f531a00130cc68
Pradnya Khalate <pkhalate@nvidia.com>
2024-11-06 15:54:02 -0800

    * Change the test port for `anyon` target from '5000' to '62446' to   match other targets' ports.
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>

--- unittests/backends/anyon/AnyonTester.cpp
+++ unittests/backends/anyon/AnyonTester.cpp
@@ -15,7 +15,7 @@
 
 // port number and localhost connect to mock_qpu backend server within the
 // container (mock_qpu/anyon).
-std::string mockPort = "5000";
+std::string mockPort = "62446";
 std::string machine = "telegraph-8q";
 std::string backendStringTemplate =
     "anyon;emulate;false;url;http://localhost:{};credentials;{};machine;{}";
