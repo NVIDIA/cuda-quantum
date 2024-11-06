8cf3ce6af199e43608cddd8ee523979776e0181a
Bettina Heim <heimb@outlook.com>
2024-11-06 09:58:30 +0100

    formatting
    Signed-off-by: Bettina Heim <heimb@outlook.com>

--- python/sdist/setup.py
+++ python/sdist/setup.py
@@ -15,10 +15,11 @@ def _log(msg: str) -> None:
     sys.stdout.write(f'[cudaq] {msg}\n')
     sys.stdout.flush()
 
+
 def _get_version_from_library(
-        libnames: list[str],
-        funcname: str,
-        nvrtc: bool = False,
+    libnames: list[str],
+    funcname: str,
+    nvrtc: bool = False,
 ) -> Optional[int]:
     """Returns the library version from list of candidate libraries."""
 
@@ -39,7 +40,6 @@ def _get_version_from_library(
     func.restype = ctypes.c_int
 
     if nvrtc:
-        # nvrtcVersion
         func.argtypes = [
             ctypes.POINTER(ctypes.c_int),
             ctypes.POINTER(ctypes.c_int),
@@ -49,7 +49,6 @@ def _get_version_from_library(
         retval = func(major, minor)
         version = major.value * 1000 + minor.value * 10
     else:
-        # cudaRuntimeGetVersion
         func.argtypes = [
             ctypes.POINTER(ctypes.c_int),
         ]
@@ -57,10 +56,11 @@ def _get_version_from_library(
         retval = func(version_ref)
         version = version_ref.value
 
-    if retval != 0:  # NVRTC_SUCCESS or cudaSuccess
+    if retval != 0:
         raise Exception(f'{libname}: {func} returned error: {retval}')
     _log(f'Detected version: {version}')
     return version
+
 
 def _get_cuda_version() -> Optional[int]:
     """Returns the detected CUDA version or None."""
@@ -89,7 +89,8 @@ def _get_cuda_version() -> Optional[int]:
     ]
     _log(f'Trying to detect CUDA version from libraries: {libnames}')
     try:
-        version = _get_version_from_library(libnames, 'cudaRuntimeGetVersion', False)
+        version = _get_version_from_library(libnames, 'cudaRuntimeGetVersion',
+                                            False)
     except Exception as e:
         _log(f"Error: {e}")  # log and move on
     if version is not None:
@@ -97,6 +98,7 @@ def _get_cuda_version() -> Optional[int]:
 
     _log("Autodetection failed")
     return None
+
 
 def _infer_best_package() -> str:
 
@@ -108,21 +110,21 @@ def _infer_best_package() -> str:
             installed.append(f"cuda-quantum{pkg_suffix}")
         except pkg_resources.DistributionNotFound:
             pass
-    
+
     cuda_version = _get_cuda_version()
     if cuda_version is None:
-        cudaq_bdist='cuda-quantum-cu12'
+        cudaq_bdist = 'cuda-quantum-cu12'
     elif cuda_version < 11000:
         raise Exception(f'Your CUDA version ({cuda_version}) is too old.')
     elif cuda_version < 12000:
-        cudaq_bdist='cuda-quantum-cu11'
+        cudaq_bdist = 'cuda-quantum-cu11'
     elif cuda_version < 13000:
-        cudaq_bdist='cuda-quantum-cu12'
+        cudaq_bdist = 'cuda-quantum-cu12'
     else:
         raise Exception(f'Your CUDA version ({cuda_version}) is too new.')
 
     # Disallow -cu11 & -cu12 wheels from coexisting
-    conflicting=", ".join((pkg for pkg in installed if pkg != cudaq_bdist))
+    conflicting = ", ".join((pkg for pkg in installed if pkg != cudaq_bdist))
     if conflicting != '':
         raise Exception(
             f'You have a conflicting CUDA-Q version installed.'
@@ -130,33 +132,36 @@ def _infer_best_package() -> str:
     return cudaq_bdist
 
 
-# This setup.py handles 3 cases:
-#   1. At the release time, we use it to generate sdist (which contains this script).
-#   2. If the sdist is generated for the deprecated cuda-quantum package name, 
-#      this script raises an exception at install time asking to install cudaq instead.
-#   3. If the sdist is generated for a valid cudaq version, 
+# This setup handles 3 cases:
+#   1. At the release time, we use it to generate source distribution (which contains
+#      this script).
+#   2. If the source distribution is generated for the deprecated cuda-quantum package
+#      name, this script raises an exception at install time asking to install cudaq.
+#   3. If the source distribution is generated for a valid cudaq version,
 #      this script identifies the installed CUDA version at cudaq install time
 #      and downloads the corresponding CUDA-Q binary distribution.
-# For case 1, CUDAQ_META_WHEEL_BUILD is set to 1.
+# For case 1, `CUDAQ_META_WHEEL_BUILD` is set to 1.
 setup_dir = os.path.dirname(os.path.abspath(__file__))
 data_files = []
 install_requires = []
 if os.environ.get('CUDAQ_META_WHEEL_BUILD', '0') == '1':
     # Case 1: generate sdist
     if os.path.exists(os.path.join(setup_dir, "_deprecated.txt")):
-        data_files = [('', ['_deprecated.txt',])]  # extra files to be copied into sdist
+        data_files = [('', [
+            '_deprecated.txt',
+        ])]  # extra files to be copied into sdist
 else:
     # Case 2: install cuda-quantum sdist
     if os.path.exists(os.path.join(setup_dir, "_deprecated.txt")):
         with open(os.path.join(setup_dir, "_deprecated.txt"), "r") as f:
             deprecation = f.read()
-        raise Exception(
-            f'This package is deprecated. \n' + deprecation)
+        raise Exception(f'This package is deprecated. \n' + deprecation)
     # Case 3: install cudaq sdist
     with open(os.path.join(setup_dir, "_version.txt"), "r") as f:
-        __version__ = f.read() 
-    install_requires = [f"{_infer_best_package()}=={__version__}",]
-
+        __version__ = f.read()
+    install_requires = [
+        f"{_infer_best_package()}=={__version__}",
+    ]
 
 setup(
     zip_safe=False,
