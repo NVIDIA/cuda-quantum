aef4036a729c066ab06b18369dcc573ac7f5a1cb
Bettina Heim <heimb@outlook.com>
2024-11-08 19:14:37 +0100

    Metapackage issues and torch install issues (#2368)
    Signed-off-by: Bettina Heim <heimb@outlook.com>

--- python/metapackages/setup.py
+++ python/metapackages/setup.py
@@ -6,7 +6,8 @@
 # the terms of the Apache License 2.0 which accompanies this distribution.     #
 # ============================================================================ #
 
-import ctypes, pkg_resources, os, sys
+import ctypes, os, sys
+import importlib.util
 from setuptools import setup
 from typing import Optional
 
@@ -105,10 +106,16 @@ def _infer_best_package() -> str:
     # Find the existing wheel installation
     installed = []
     for pkg_suffix in ['', '-cu11', '-cu12']:
+        _log(f"Looking for existing installation of cuda-quantum{pkg_suffix}.")
         try:
-            pkg_resources.get_distribution(f"cuda-quantum{pkg_suffix}")
-            installed.append(f"cuda-quantum{pkg_suffix}")
-        except pkg_resources.DistributionNotFound:
+            package_spec = importlib.util.find_spec(f"cuda-quantum{pkg_suffix}")
+            if package_spec is None:
+                _log("No installation found.")
+            else:
+                installed.append(f"cuda-quantum{pkg_suffix}")
+                _log("Installation found.")
+        except:
+            _log("No installation found.")
             pass
 
     cuda_version = _get_cuda_version()
@@ -122,10 +129,13 @@ def _infer_best_package() -> str:
         cudaq_bdist = 'cuda-quantum-cu12'
     else:
         raise Exception(f'Your CUDA version ({cuda_version}) is too new.')
+    _log(f"Identified {cudaq_bdist} as the best package.")
 
     # Disallow -cu11 & -cu12 wheels from coexisting
     conflicting = ", ".join((pkg for pkg in installed if pkg != cudaq_bdist))
+    _log(f"Conflicting packages: {conflicting}")
     if conflicting != '':
+        _log("Abort.")
         raise Exception(
             f'You have a conflicting CUDA-Q version installed.'
             f'Please remove the following package(s): {conflicting}')
