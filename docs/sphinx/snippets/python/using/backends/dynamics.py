7460c7f20e13066377cf881a9256e4bdcb571e94
Sachin Pisal <spisal@nvidia.com>
2024-11-21 14:07:01 -0800

    formatting backend file
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- docs/sphinx/snippets/python/using/backends/dynamics.py
+++ docs/sphinx/snippets/python/using/backends/dynamics.py
@@ -8,7 +8,7 @@
 
 # Made up values - not sure what values are reasonable here.
 omega_z = 6.5
-omega_x = 4.0 
+omega_x = 4.0
 omega_d = 0.5
 
 #[Begin Transmon]
@@ -18,8 +18,8 @@ from cudaq.operator import *
 # Qubit Hamiltonian
 hamiltonian = 0.5 * omega_z * spin.z(0)
 # Add modulated driving term to the Hamiltonian
-hamiltonian += omega_x * ScalarOperator(
-    lambda t: np.cos(omega_d * t)) * spin.x(0)
+hamiltonian += omega_x * ScalarOperator(lambda t: np.cos(omega_d * t)) * spin.x(
+    0)
 #[End Transmon]
 
 # Made up values - not sure what values are reasonable here.
@@ -86,18 +86,18 @@ hamiltonian = omega_c * operators.create(1) * operators.annihilate(1) \
 omega = np.pi
 
 #[Begin Hamiltonian]
-# Define the static (drift) and control terms 
+# Define the static (drift) and control terms
 H0 = spin.z(0)
 H1 = spin.x(0)
 H = H0 + ScalarOperator(lambda t: np.cos(omega * t)) * H1
 #[End Hamiltonian]
-
 
 #[Begin DefineOp]
 import numpy
 import scipy
 from cudaq.operator import *
 from numpy.typing import NDArray
+
 
 def displacement_matrix(
         dimension: int,
@@ -110,21 +110,24 @@ def displacement_matrix(
     """
     displacement = complex(displacement)
     term1 = displacement * operators.create(0).to_matrix({0: dimension})
-    term2 = numpy.conjugate(
-        displacement) * operators.annihilate(0).to_matrix({0: dimension})
+    term2 = numpy.conjugate(displacement) * operators.annihilate(0).to_matrix(
+        {0: dimension})
     return scipy.linalg.expm(term1 - term2)
 
-# The second argument here indicates the the defined operator 
+
+# The second argument here indicates the the defined operator
 # acts on a single degree of freedom, which can have any dimension.
-# An argument [2], for example, would indicate that it can only 
+# An argument [2], for example, would indicate that it can only
 # act on a single degree of freedom with dimension two.
 ElementaryOperator.define("displace", [0], displacement_matrix)
+
 
 def displacement(degree: int) -> ElementaryOperator:
     """
     Instantiates a displacement operator acting on the given degree of freedom.
     """
     return ElementaryOperator("displace", [degree])
+
 
 #[End DefineOp]
 
@@ -133,7 +136,7 @@ import cudaq
 
 # Define a system consisting of a single degree of freedom (0) with dimension 3.
 system_dimensions = {0: 3}
-system_operator = displacement(0) 
+system_operator = displacement(0)
 
 # Define the time dependency of the system operator as a schedule that linearly
 # increases the displacement parameter from 0 to 1.
@@ -142,32 +145,30 @@ initial_state = cudaq.State.from_data(
     numpy.ones(3, dtype=numpy.complex128) / numpy.sqrt(3))
 
 # Simulate the evolution of the system under this time dependent operator.
-cudaq.evolve(system_operator, 
-             system_dimensions, 
-             time_dependence, 
-             initial_state)
+cudaq.evolve(system_operator, system_dimensions, time_dependence, initial_state)
 #[End Schedule1]
 
 #[Begin Schedule2]
 system_operator = displacement(0) + operators.squeeze(0)
 
+
 # Define a schedule such that displacement amplitude increases linearly in time
 # but the squeezing amplitude decreases, that is follows the inverse schedule.
 def parameter_values(time_steps):
+
     def compute_value(param_name, step_idx):
         match param_name:
-            case 'displacement': return time_steps[int(step_idx)]
-            case 'squeezing': return time_steps[-int(step_idx + 1)]
-            case _: raise ValueError(f"value for parameter {param_name} undefined")
-    return Schedule(
-        range(len(time_steps)),
-        system_operator.parameters.keys(), 
-        compute_value)
+            case 'displacement':
+                return time_steps[int(step_idx)]
+            case 'squeezing':
+                return time_steps[-int(step_idx + 1)]
+            case _:
+                raise ValueError(f"value for parameter {param_name} undefined")
+
+    return Schedule(range(len(time_steps)), system_operator.parameters.keys(),
+                    compute_value)
+
 
 time_dependence = parameter_values(numpy.linspace(0, 1, 100))
-cudaq.evolve(system_operator, 
-             system_dimensions, 
-             time_dependence, 
-             initial_state)
+cudaq.evolve(system_operator, system_dimensions, time_dependence, initial_state)
 #[End Schedule2]
-
