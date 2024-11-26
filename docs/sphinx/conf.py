94ea7dc12b16f1bd5ef2240c4f3595aac1131eeb
Bettina Heim <heimb@outlook.com>
2024-11-26 13:26:15 +0100

    install cuquantum for docs generation
    Signed-off-by: Bettina Heim <heimb@outlook.com>

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -178,7 +178,10 @@ breathe_show_enumvalue_initializer = True
 # -- Other options -------------------------------------------------
 
 autosummary_generate = True
-autodoc_mock_imports = ['cuquantum', 'cupy']
+
+# This is unfortunately not sufficient for docs generation, due to the use of Union type;
+# see also https://github.com/sphinx-doc/sphinx/issues/11211. 
+# autodoc_mock_imports = ['cuquantum', 'cupy']
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
