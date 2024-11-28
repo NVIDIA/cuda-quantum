c9d0e4c020ca83b119a0df8a5fdf41911078e12a
Ben Howe <141149032+bmhowe23@users.noreply.github.com>
2024-11-27 23:02:21 -0800

    Follow-up to #2394 - handle remote REST QPU also (#2417)

--- runtime/cudaq/platform/default/rest/helpers/braket/BraketExecutor.cpp
+++ runtime/cudaq/platform/default/rest/helpers/braket/BraketExecutor.cpp
@@ -173,7 +173,8 @@ void BraketExecutor::setOutputNames(const KernelExecution &codeToExecute,
 }
 
 details::future
-BraketExecutor::execute(std::vector<KernelExecution> &codesToExecute) {
+BraketExecutor::execute(std::vector<KernelExecution> &codesToExecute,
+                        bool isObserve) {
   auto [dummy1, dummy2, messages] = checkHelperAndCreateJob(codesToExecute);
 
   std::string const defaultBucket = defaultBucketFuture.get();
