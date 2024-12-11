c46adea442232793c1fbaddfa6a9f3cf98894335
Eric Schweitz <eschweitz@nvidia.com>
2024-12-11 15:44:35 -0800

    Fix bug in test.
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>

--- targettests/execution/state_preparation_vector.cpp
+++ targettests/execution/state_preparation_vector.cpp
@@ -11,7 +11,7 @@
 // RUN: nvq++ %cpp_std %s -o %t && %t | FileCheck %s
 
 // Quantum emulators
-// RUN: nvq++ %cpp_std -target braket     -emulate %s -o %t && %t | FileCheck %s
+// RUN: if %braket_avail; then nvq++ %cpp_std -target braket -emulate %s -o %t && %t | FileCheck %s ; fi
 // RUN: nvq++ %cpp_std -target quantinuum -emulate %s -o %t && %t | FileCheck %s
 // RUN: nvq++ %cpp_std -target ionq       -emulate %s -o %t && %t | FileCheck %s
 // RUN: nvq++ %cpp_std -target oqc        -emulate %s -o %t && %t | FileCheck %s
