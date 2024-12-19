791fb25fd9f3f02af0d257398abc8a953063ae26
Sachin Pisal <spisal@nvidia.com>
2024-12-19 11:50:02 -0800

    formatting
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- docs/sphinx/targets/cpp/infleqtion.cpp
+++ docs/sphinx/targets/cpp/infleqtion.cpp
@@ -1,30 +1,30 @@
 // Compile and run with:
 // ```
-// nvq++ --target infleqtion infleqtion.cpp -o out.x && ./out.x
+// nvq++ --target `infleqtion` `infleqtion`.cpp -o out.x && ./out.x
 // ```
-// This will submit the job to the Infleqtion's ideal simulator,
-// cq_sqale_simulator (default). Alternatively, we can enable hardware noise
-// model simulation by specifying `noise-sim` to the flag `--infleqtion-method`,
+// This will submit the job to the `Infleqtion's` ideal simulator,
+// `cq`_`sqale`_simulator (default). Alternatively, we can enable hardware noise
+// model simulation by specifying `noise-sim` to the flag --`infleqtion`-method,
 // e.g.,
 // ```
-// nvq++ --target infleqtion --infleqtion-machine cq_sqale_qpu
-// --infleqtion-method noise-sim infleqtion.cpp -o out.x && ./out.x
+// nvq++ --target `infleqtion` --`infleqtion`-machine `cq`_`sqale`_qpu
+// --`infleqtion`-method noise-sim `infleqtion`.cpp -o out.x && ./out.x
 // ```
-// where "noise-sim" instructs Superstaq to perform a noisy emulation of the
+// where "noise-sim" instructs `Superstaq` to perform a noisy emulation of the
 // QPU. An ideal dry-run execution on the QPU may be performed by passing
-// `dry-run` to the `--infleqtion-method` flag, e.g.,
+// `dry-run` to the --`infleqtion`-method flag, e.g.,
 // ```
-// nvq++ --target infleqtion --infleqtion-machine cq_sqale_qpu
-// --infleqtion-method dry-run infleqtion.cpp -o out.x && ./out.x
+// nvq++ --target `infleqtion` --`infleqtion`-machine `cq`_`sqale`_qpu
+// --`infleqtion`-method dry-run `infleqtion`.cpp -o out.x && ./out.x
 // ```
-// Note: If targeting ideal cloud simulation, `--infleqtion-machine
-// cq_sqale_simulator` is optional since it is the default configuration if not
+// Note: If targeting ideal cloud simulation, --`infleqtion`-machine
+// `cq`_`sqale`_simulator is optional since it is the default configuration if not
 // provided.
 
 #include <cudaq.h>
 #include <fstream>
 
-// Define a simple quantum kernel to execute on Infleqtion backends.
+// Define a simple quantum kernel to execute on `infleqtion` backends.
 struct ghz {
   // Maximally entangled state between 5 qubits.
   auto operator()() __qpu__ {
@@ -38,7 +38,7 @@ struct ghz {
 };
 
 int main() {
-  // Submit to infleqtion asynchronously (e.g., continue executing
+  // Submit to `infleqtion` asynchronously (e.g., continue executing
   // code in the file until the job has been returned).
   auto future = cudaq::sample_async(ghz{});
   // ... classical code to execute in the meantime ...
@@ -58,7 +58,7 @@ int main() {
   auto async_counts = readIn.get();
   async_counts.dump();
 
-  // OR: Submit to infleqtion synchronously (e.g., wait for the job
+  // OR: Submit to `infleqtion` synchronously (e.g., wait for the job
   // result to be returned before proceeding).
   auto counts = cudaq::sample(ghz{});
   counts.dump();
