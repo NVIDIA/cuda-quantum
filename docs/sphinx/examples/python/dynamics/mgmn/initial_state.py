39b7325555fa8095b1747614235c19f06401cf6e
Thien Nguyen <thiennguyen@nvidia.com>
2025-01-14 06:38:16 +0000

    Update initial state enum example
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>

--- docs/sphinx/examples/python/dynamics/mgmn/initial_state.py
+++ docs/sphinx/examples/python/dynamics/mgmn/initial_state.py
@@ -1,65 +1,68 @@
 import cudaq
-from cudaq import operators, Schedule, RungeKuttaIntegrator
+from cudaq import operators, spin, Schedule, RungeKuttaIntegrator
+
 import numpy as np
-import cupy as cp
-import os
 import matplotlib.pyplot as plt
+import os
+
+cudaq.mpi.initialize()
+
 # Set the target to our dynamics simulator
 cudaq.set_target("dynamics")
 
-# This example demonstrate a simulation of cavity quantum electrodynamics (interaction between light confined in a reflective cavity and atoms)
+# Large number of spins
+N = 20
+dimensions = {}
+for i in range(N):
+    dimensions[i] = 2
 
-# System dimensions: atom (2-level system) and cavity (1024-level system)
-cavity_dim = 1024
-dimensions = {0: 2, 1: cavity_dim}
+# Observable is the average magnetization operator
+avg_magnetization_op = operators.zero()
+for i in range(N):
+    avg_magnetization_op += (spin.z(i) / N)
 
-# Alias for commonly used operators
-# Cavity operators
-a = operators.annihilate(1)
-a_dag = operators.create(1)
+# Arbitrary coupling constant
+g = 1.0 
+# Construct the Hamiltonian
+H = operators.zero()
+for i in range(N):
+    H += 2 * np.pi * spin.x(i)
+    H += 2 * np.pi * spin.y(i)
+for i in range(N - 1):
+    H += 2 * np.pi * g * spin.x(i) * spin.x(i + 1)
+    H += 2 * np.pi * g * spin.y(i) * spin.z(i + 1)
 
-# Atom operators
-sm = operators.annihilate(0)
-sm_dag = operators.create(0)
-
-# Defining the Hamiltonian for the system: self-energy terms and cavity-atom interaction term.
-# This is the so-called Jaynes-Cummings model:
-# https://en.wikipedia.org/wiki/Jaynes%E2%80%93Cummings_model
-hamiltonian = 2 * np.pi * operators.number(1) + 2 * np.pi * operators.number(
-    0) + 2 * np.pi * 0.25 * (sm * a_dag + sm_dag * a)
-
-rho0 = cudaq.operator.InitialState.ZERO
-
-steps = np.linspace(0, 10, 201)
+steps = np.linspace(0.0, 2, 1000)
 schedule = Schedule(steps, ["time"])
 
-evolution_result_decay = cudaq.evolve(
-    hamiltonian,
-    dimensions,
-    schedule,
-    rho0,
-    observables=[operators.number(1), operators.number(0)],
-    collapse_operators=[np.sqrt(0.1) * a],
-    store_intermediate_results=True,
-    integrator=RungeKuttaIntegrator())
+# Initial state (expressed as an enum)
+psi0 = cudaq.operator.InitialState.ZERO
 
-get_result = lambda idx, res: [
-    exp_vals[idx].expectation() for exp_vals in res.expectation_values()
+# Run the simulation
+evolution_result = cudaq.evolve(H,
+                                dimensions,
+                                schedule,
+                                psi0,
+                                observables=[avg_magnetization_op],
+                                collapse_operators=[],
+                                store_intermediate_results=True,
+                                integrator=RungeKuttaIntegrator())
+
+exp_val = [
+    exp_vals[0].expectation()
+    for exp_vals in evolution_result.expectation_values()
 ]
 
-decay_results = [
-    get_result(0, evolution_result_decay),
-    get_result(1, evolution_result_decay)
-]
 
-fig = plt.figure(figsize=(18, 6))
+if cudaq.mpi.rank() == 0:
+    # Plot the results
+    fig = plt.figure(figsize=(12, 6))
+    plt.plot(steps, exp_val)
+    plt.ylabel("Average Magnetization")
+    plt.xlabel("Time")
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+    fig.savefig("spin_model.png", dpi=fig.dpi)
 
-plt.plot(steps, decay_results[0])
-plt.plot(steps, decay_results[1])
-plt.ylabel("Expectation value")
-plt.xlabel("Time")
-plt.legend(("Cavity Photon Number", "Atom Excitation Probability"))
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)
-fig.savefig('cavity_qed.png', dpi=fig.dpi)
+cudaq.mpi.finalize()
