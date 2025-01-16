4dd2615515ab4efa31fa81808826861e033f7745
Sachin Pisal <spisal@nvidia.com>
2025-01-16 14:10:02 -0800

    Adding interface for cudm_helpers
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- runtime/cudaq/cudm_helpers.h
+++ runtime/cudaq/cudm_helpers.h
@@ -8,7 +8,6 @@
 
 #pragma once
 
-#include "cudaq/operators.h"
 #include "cudaq/utils/tensor.h"
 #include <cudensitymat.h>
 #include <iostream>
@@ -17,36 +16,28 @@
 #include <vector>
 
 namespace cudaq {
-std::vector<std::complex<double>> flatten_matrix(const matrix_2 &matrix);
+cudensitymatState_t initialize_state(cudensitymatHandle_t handle,
+                                     cudensitymatStatePurity_t purity,
+                                     int num_modes,
+                                     const std::vector<int64_t> &mode_extents);
 
 void scale_state(cudensitymatHandle_t handle, cudensitymatState_t state,
                  double scale_factor, cudaStream_t stream);
 
+void destroy_state(cudensitymatState_t state);
+
 cudensitymatOperator_t
 compute_lindblad_operator(cudensitymatHandle_t handle,
                           const std::vector<matrix_2> &c_ops,
-                          const std::vector<int64_t> &mode_extents);
+                          const std::vector<int> &mode_extents);
 
-template <typename HandlerTy>
 cudensitymatOperator_t convert_to_cudensitymat_operator(
     cudensitymatHandle_t handle,
-    const std::map<std::string, std::complex<double>> &parameters,
-    const operator_sum<HandlerTy> &op,
-    const std::vector<int64_t> &mode_extents);
+    const std::map<std::string, double> &parameters, const matrix_2 &matrix,
+    const std::vector<int> &mode_extents);
 
 cudensitymatOperator_t construct_liovillian(
     cudensitymatHandle_t handle, const cudensitymatOperator_t &hamiltonian,
     const std::vector<cudensitymatOperator_t> &collapse_operators,
     double gamma);
-
-// Function for creating an array copy in GPU memory
-void *create_array_gpu(const std::vector<std::complex<double>> &cpu_array);
-
-// Function to detsroy a previously created array copy in GPU memory
-void destroy_array_gpu(void *gpu_array);
-
-extern template cudensitymatOperator_t
-convert_to_cudensitymat_operator<cudaq::matrix_operator>(
-    cudensitymatHandle_t, const std::map<std::string, std::complex<double>> &,
-    const operator_sum<cudaq::matrix_operator> &, const std::vector<int64_t> &);
 } // namespace cudaq
