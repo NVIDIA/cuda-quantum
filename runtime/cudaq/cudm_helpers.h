35a1040fb2087d74bf7c1e6aef57db1eea0a93da
Sachin Pisal <spisal@nvidia.com>
2025-01-16 14:10:02 -0800

    Adding interface for cudm_helpers
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

---
+++ runtime/cudaq/cudm_helpers.h
@@ -0,0 +1,43 @@
+/****************************************************************-*- C++ -*-****
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+#pragma once
+
+#include "cudaq/utils/tensor.h"
+#include <cudensitymat.h>
+#include <iostream>
+#include <map>
+#include <stdexcept>
+#include <vector>
+
+namespace cudaq {
+cudensitymatState_t initialize_state(cudensitymatHandle_t handle,
+                                     cudensitymatStatePurity_t purity,
+                                     int num_modes,
+                                     const std::vector<int64_t> &mode_extents);
+
+void scale_state(cudensitymatHandle_t handle, cudensitymatState_t state,
+                 double scale_factor, cudaStream_t stream);
+
+void destroy_state(cudensitymatState_t state);
+
+cudensitymatOperator_t
+compute_lindblad_operator(cudensitymatHandle_t handle,
+                          const std::vector<matrix_2> &c_ops,
+                          const std::vector<int> &mode_extents);
+
+cudensitymatOperator_t convert_to_cudensitymat_operator(
+    cudensitymatHandle_t handle,
+    const std::map<std::string, double> &parameters, const matrix_2 &matrix,
+    const std::vector<int> &mode_extents);
+
+cudensitymatOperator_t construct_liovillian(
+    cudensitymatHandle_t handle, const cudensitymatOperator_t &hamiltonian,
+    const std::vector<cudensitymatOperator_t> &collapse_operators,
+    double gamma);
+} // namespace cudaq
