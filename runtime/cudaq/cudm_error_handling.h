0259b909670f314197e2c2f77455c82b524fedd8
Sachin Pisal <spisal@nvidia.com>
2025-01-21 14:37:29 -0800

    * Adding macro for handling error * Using CUDA_C_64F data type for complex
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

---
+++ runtime/cudaq/cudm_error_handling.h
@@ -0,0 +1,29 @@
+/****************************************************************-*- C++ -*-****
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+#pragma once
+#include <cudensitymat.h>
+#include <stdexcept>
+
+#define HANDLE_CUDM_ERROR(x)                                                     \
+{                                                                                \
+    const auto err = x;                                                          \
+    if (err != CUDENSITYMAT_STATUS_SUCCESS) {                                    \
+        throw std::runtime_error(fmt::format("[cudaq] %{} in {} (line {})", err  \
+                                           __FUNCTION__, __LINE__));             \
+    }                                                                            \
+}
+
+#define HANDLE_CUDA_ERROR(x)                                                     \
+{                                                                                \
+    const auto err = x;                                                          \
+    if (err != cudaSuccess) {                                                    \
+        throw std::runtime_error(fmt::format("[cuda] %{} in {} (line {})", err   \
+                                           __FUNCTION__, __LINE__));             \
+    }                                                                            \
+}
