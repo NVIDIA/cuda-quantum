9c042cb577c537c78ab76a334765cc3b15b43d3e
Sachin Pisal <spisal@nvidia.com>
2025-01-28 15:02:31 -0800

    Adding cudm_time_stepper with unittests
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

---
+++ runtime/cudaq/cudm_time_stepper.h
@@ -0,0 +1,26 @@
+/****************************************************************-*- C++ -*-****
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+#pragma once
+
+#include "cudaq/base_time_stepper.h"
+#include "cudaq/cudm_state.h"
+#include <cudensitymat.h>
+
+namespace cudaq {
+class cudm_time_stepper : public BaseTimeStepper<cudm_state> {
+public:
+    explicit cudm_time_stepper(cudensitymatHandle_t handle, cudensitymatOperator_t liouvillian);
+
+    void compute(cudm_state &state, double t, double step_size) override;
+
+private:
+    cudensitymatHandle_t handle_;
+    cudensitymatOperator_t liouvillian_;
+};
+}
