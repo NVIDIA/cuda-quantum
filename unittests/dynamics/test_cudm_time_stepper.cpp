6544beef2ce2977849ad4bc0445dcfbe4ad73b0f
Sachin Pisal <spisal@nvidia.com>
2025-01-31 09:58:26 -0800

    Adding test_mocks for unittests
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- unittests/dynamics/test_cudm_time_stepper.cpp
+++ unittests/dynamics/test_cudm_time_stepper.cpp
@@ -6,40 +6,23 @@
  * the terms of the Apache License 2.0 which accompanies this distribution.    *
  ******************************************************************************/
 
+#include "test_mocks.h"
 #include <cudaq/cudm_error_handling.h>
 #include <cudaq/cudm_helpers.h>
 #include <cudaq/cudm_state.h>
 #include <cudaq/cudm_time_stepper.h>
 #include <gtest/gtest.h>
+#include <iostream>
+#include <memory>
 
 using namespace cudaq;
-
-// Mock Liouvillian operator creation
-cudensitymatOperator_t mock_liouvillian(cudensitymatHandle_t handle) {
-  cudensitymatOperator_t liouvillian;
-  std::vector<int64_t> dimensions = {2, 2};
-  HANDLE_CUDM_ERROR(cudensitymatCreateOperator(
-      handle, static_cast<int32_t>(dimensions.size()), dimensions.data(),
-      &liouvillian));
-  return liouvillian;
-}
-
-// Mock Hilbert space dimensions
-std::vector<std::complex<double>> mock_initial_state_data() {
-  return {{1.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}};
-}
-
-// Mock initial raw state data
-std::vector<int64_t> mock_hilbert_space_dims() { return {2, 2}; }
 
 class CuDensityMatTimeStepperTest : public ::testing::Test {
 protected:
   cudensitymatHandle_t handle_;
   cudensitymatOperator_t liouvillian_;
-  cudm_time_stepper *time_stepper_;
-  cudm_state *state_;
-
-  // CuDensityMatTimeStepperTest() : state_(mock_initial_state_data()) {};
+  std::unique_ptr<cudm_time_stepper> time_stepper_;
+  std::unique_ptr<cudm_state> state_;
 
   void SetUp() override {
     // Create library handle
@@ -49,12 +32,10 @@ protected:
     liouvillian_ = mock_liouvillian(handle_);
 
     // Initialize the time stepper
-    time_stepper_ = new cudm_time_stepper(handle_, liouvillian_);
+    time_stepper_ = std::make_unique<cudm_time_stepper>(handle_, liouvillian_);
 
-    state_ = new cudm_state(handle_, mock_initial_state_data());
-
-    // Initialize the state
-    state_->init_state(mock_hilbert_space_dims());
+    state_ = std::make_unique<cudm_state>(handle_, mock_initial_state_data(),
+                                          mock_hilbert_space_dims());
 
     ASSERT_TRUE(state_->is_initialized());
   }
@@ -63,17 +44,15 @@ protected:
     // Clean up
     HANDLE_CUDM_ERROR(cudensitymatDestroyOperator(liouvillian_));
     HANDLE_CUDM_ERROR(cudensitymatDestroy(handle_));
-    delete time_stepper_;
-    delete state_;
   }
 };
 
 // Test initialization of cudm_time_stepper
-// TEST_F(CuDensityMatTimeStepperTest, Initialization) {
-//   ASSERT_NE(time_stepper_, nullptr);
-//   ASSERT_TRUE(state_->is_initialized());
-//   ASSERT_FALSE(state_->is_density_matrix());
-// }
+TEST_F(CuDensityMatTimeStepperTest, Initialization) {
+  ASSERT_NE(time_stepper_, nullptr);
+  ASSERT_TRUE(state_->is_initialized());
+  ASSERT_FALSE(state_->is_density_matrix());
+}
 
 // Test a single compute step
 TEST_F(CuDensityMatTimeStepperTest, ComputeStep) {
@@ -81,7 +60,3 @@ TEST_F(CuDensityMatTimeStepperTest, ComputeStep) {
   EXPECT_NO_THROW(time_stepper_->compute(*state_, 0.0, 1.0));
   ASSERT_TRUE(state_->is_initialized());
 }
-
-// // Add test to use construct_liouvillian and then use compute to step using
-// this liouvillian
-// // z0 * z1
