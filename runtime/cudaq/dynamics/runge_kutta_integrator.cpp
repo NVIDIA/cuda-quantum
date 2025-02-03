d32869056096811d92f5dc551a82e601f04ea164
Sachin Pisal <spisal@nvidia.com>
2025-02-03 14:54:10 -0800

    Allocating k1, k2, k3, k4 inside the scope blocks
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- runtime/cudaq/dynamics/runge_kutta_integrator.cpp
+++ runtime/cudaq/dynamics/runge_kutta_integrator.cpp
@@ -27,39 +27,28 @@ void runge_kutta_integrator::integrate(double target_time) {
     throw std::invalid_argument("Invalid time step size for integration.");
   }
 
-  auto handle = state.get_handle();
-  auto hilbertSpaceDims = state.get_hilbert_space_dims();
-
   while (t < target_time) {
     double step_size = std::min(dt, target_time - t);
 
     std::cout << "Runge-Kutta step at time " << t
               << " with step size: " << step_size << std::endl;
 
-    // Empty vectors of same size as state.get_raw_data()
-    std::vector<std::complex<double>> zero_state(state.get_raw_data().size(),
-                                                 {0.0, 0.0});
-
-    cudm_state k1(handle, zero_state, hilbertSpaceDims);
-    cudm_state k2(handle, zero_state, hilbertSpaceDims);
-    cudm_state k3(handle, zero_state, hilbertSpaceDims);
-    cudm_state k4(handle, zero_state, hilbertSpaceDims);
-
     if (substeps_ == 1) {
       // Euler method (1st order)
-      k1 = stepper->compute(state, t, step_size);
+      cudm_state k1 = stepper->compute(state, t, step_size);
       state = std::move(k1);
     } else if (substeps_ == 2) {
       // Midpoint method (2nd order)
-      k1 = stepper->compute(state, t, step_size / 2.0);
-      k2 = stepper->compute(k1, t + step_size / 2.0, step_size);
+      cudm_state k1 = stepper->compute(state, t, step_size / 2.0);
+      cudm_state k2 = stepper->compute(k1, t + step_size / 2.0, step_size);
       state = std::move((k1 + k2) * 0.5);
     } else if (substeps_ == 4) {
       // Runge-Kutta method (4th order)
-      k1 = stepper->compute(state, t, step_size / 2.0);
-      k2 = stepper->compute(k1, t + step_size / 2.0, step_size / 2.0);
-      k3 = stepper->compute(k2, t + step_size / 2.0, step_size);
-      k4 = stepper->compute(k3, t + step_size, step_size);
+      cudm_state k1 = stepper->compute(state, t, step_size / 2.0);
+      cudm_state k2 =
+          stepper->compute(k1, t + step_size / 2.0, step_size / 2.0);
+      cudm_state k3 = stepper->compute(k2, t + step_size / 2.0, step_size);
+      cudm_state k4 = stepper->compute(k3, t + step_size, step_size);
       state = std::move((k1 + (k2 + k3) * 2.0 + k4) * (1.0 / 6.0));
     }
 
