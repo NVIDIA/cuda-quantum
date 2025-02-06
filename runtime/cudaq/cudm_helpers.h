71b87c276faab6d910851b1fb7974adb0e0b02a4
Sachin Pisal <spisal@nvidia.com>
2025-02-05 16:25:09 -0800

    Disabling template method
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- runtime/cudaq/cudm_helpers.h
+++ runtime/cudaq/cudm_helpers.h
@@ -8,6 +8,7 @@
 
 #pragma once
 
+#include "cudaq/cudm_error_handling.h"
 #include "cudaq/operators.h"
 #include "cudaq/utils/tensor.h"
 #include <cudensitymat.h>
@@ -25,12 +26,83 @@ compute_lindblad_operator(cudensitymatHandle_t handle,
                           const std::vector<matrix_2> &c_ops,
                           const std::vector<int64_t> &mode_extents);
 
-template <typename HandlerTy>
-cudensitymatOperator_t convert_to_cudensitymat_operator(
-    cudensitymatHandle_t handle,
-    const std::map<std::string, std::complex<double>> &parameters,
-    const operator_sum<HandlerTy> &op,
-    const std::vector<int64_t> &mode_extents);
+// std::map<int, int>
+// convert_dimensions(const std::vector<int64_t> &mode_extents) {
+//   std::map<int, int> dimensions;
+//   for (size_t i = 0; i < mode_extents.size(); i++) {
+//     dimensions[static_cast<int>(i)] = static_cast<int>(mode_extents[i]);
+//   }
+//   return dimensions;
+// }
+
+// template <typename HandlerTy>
+// cudensitymatOperator_t convert_to_cudensitymat_operator(
+//     cudensitymatHandle_t handle,
+//     const std::map<std::string, std::complex<double>> &parameters,
+//     const operator_sum<HandlerTy> &op,
+//     const std::vector<int64_t> &mode_extents) {
+//   if (op.get_terms().empty()) {
+//     throw std::invalid_argument("Operator sum cannot be empty.");
+//   }
+
+//   try {
+//     cudensitymatOperator_t operator_handle;
+//     HANDLE_CUDM_ERROR(cudensitymatCreateOperator(
+//         handle, static_cast<int32_t>(mode_extents.size()),
+//         mode_extents.data(), &operator_handle));
+
+//     std::vector<cudensitymatElementaryOperator_t> elementary_operators;
+
+//     for (const auto &product_op : op.get_terms()) {
+//       cudensitymatOperatorTerm_t term;
+
+//       HANDLE_CUDM_ERROR(cudensitymatCreateOperatorTerm(
+//           handle, static_cast<int32_t>(mode_extents.size()),
+//           mode_extents.data(), &term));
+
+//       for (const auto &component : product_op.get_terms()) {
+//         if (std::holds_alternative<HandlerTy>(component)) {
+//           const auto &elem_op = std::get<cudaq::matrix_operator>(component);
+
+//           auto subspace_extents =
+//               get_subspace_extents(mode_extents, elem_op.degrees);
+//           auto flat_matrix = flatten_matrix(
+//               elem_op.to_matrix(convert_dimensions(mode_extents),
+//               parameters));
+//           auto cudm_elem_op =
+//               create_elementary_operator(handle, subspace_extents,
+//               flat_matrix);
+
+//           elementary_operators.push_back(cudm_elem_op);
+//           append_elementary_operator_to_term(handle, term, cudm_elem_op,
+//                                              elem_op.degrees);
+//         } else if (std::holds_alternative<cudaq::scalar_operator>(component))
+//         {
+//           auto coeff =
+//               std::get<cudaq::scalar_operator>(component).evaluate(parameters);
+//           append_scalar_to_term(handle, term, coeff);
+//         }
+//       }
+
+//       // Append the product operator term to the top-level operator
+//       HANDLE_CUDM_ERROR(cudensitymatOperatorAppendTerm(
+//           handle, operator_handle, term, 0, make_cuDoubleComplex(1.0, 0.0),
+//           {nullptr, nullptr}));
+
+//       // Destroy the term
+//       HANDLE_CUDM_ERROR(cudensitymatDestroyOperatorTerm(term));
+
+//       // Cleanup
+//       for (auto &elem_op : elementary_operators) {
+//         HANDLE_CUDM_ERROR(cudensitymatDestroyElementaryOperator(elem_op));
+//       }
+//     }
+
+//     return operator_handle;
+//   } catch (const std::exception &e) {
+//     throw std::runtime_error("Error in convert_to_cudensitymat_operator!");
+//   }
+// }
 
 cudensitymatOperator_t construct_liovillian(
     cudensitymatHandle_t handle, const cudensitymatOperator_t &hamiltonian,
