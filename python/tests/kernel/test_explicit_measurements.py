0d8b85c193c6650d8627d73691fd9d533fec3115
Pradnya Khalate <pkhalate@nvidia.com>
2025-02-07 13:26:33 -0800

    * Correct the test (incorrect loop!) * Run test on all available simulator targets
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>

--- python/tests/kernel/test_explicit_measurements.py
+++ python/tests/kernel/test_explicit_measurements.py
@@ -27,14 +27,43 @@ def test_simple():
     for round in range(n_rounds):
         explicit_kernel.h(q[0])
 
-    for i in range(1, n_qubits):
-        explicit_kernel.cx(q[i - 1], q[i])
+        for i in range(1, n_qubits):
+            explicit_kernel.cx(q[i - 1], q[i])
 
-    explicit_kernel.mz(q)
+        explicit_kernel.mz(q)
 
-    for i in range(n_qubits):
-        explicit_kernel.reset(q[i])
+        for i in range(n_qubits):
+            explicit_kernel.reset(q[i])
 
     counts = cudaq.sample(explicit_kernel, explicit_measurements=True)
+    counts.dump()
+
     # With 1000 shots of multiple rounds, we need to see different shot measurements.
-    assert counts["0000"] != 1000
+    assert len(counts) > 1
+
+    seq = counts.get_sequential_data()
+    assert len(seq) == 1000
+    assert len(seq[0]) == n_qubits * n_rounds
+
+
+@pytest.mark.parametrize("target", [
+    'density-matrix-cpu', 'nvidia', 'nvidia-fp64', 'nvidia-mqpu',
+    'nvidia-mqpu-fp64', 'nvidia-mqpu-mps', 'qpp-cpu', 'stim', 'tensornet',
+    'tensornet-mps'
+])
+def test_simulators(target):
+
+    def can_set_target(name):
+        target_installed = True
+        try:
+            cudaq.set_target(name)
+        except RuntimeError:
+            target_installed = False
+        return target_installed
+
+    if can_set_target(target):
+        test_simple()
+    else:
+        pytest.skip("target not available")
+
+    cudaq.reset_target()
