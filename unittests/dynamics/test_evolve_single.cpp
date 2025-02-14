415b6057e2aaa2abd63da71751da62938c17eec0
Thien Nguyen <thiennguyen@nvidia.com>
2025-02-14 06:34:16 +0000

    Support static collapse op and add a test
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>

--- unittests/dynamics/test_evolve_single.cpp
+++ unittests/dynamics/test_evolve_single.cpp
@@ -8,12 +8,12 @@
 
 #include "common/EigenDense.h"
 #include "cudaq/evolution.h"
+#include "cudaq/runge_kutta_integrator.h"
+#include "cudm_state.h"
 #include <cmath>
 #include <gtest/gtest.h>
 #include <iostream>
 #include <unsupported/Eigen/KroneckerProduct>
-#include "cudaq/runge_kutta_integrator.h"
-#include "cudm_state.h"
 
 TEST(EvolveTester, checkSimple) {
   const std::map<int, int> dims = {{0, 2}};
@@ -52,7 +52,7 @@ TEST(EvolveTester, checkSimple) {
       std::complex<double>{1.0, 0.0}, cudaq::matrix_operator("pauli_z", {0}));
   auto initialState =
       cudaq::state::from_data(std::vector<std::complex<double>>{1.0, 0.0});
-  
+
   cudaq::runge_kutta_integrator integrator;
   integrator.dt = 0.001;
   integrator.order = 1;
@@ -108,9 +108,9 @@ TEST(EvolveTester, checkSimpleDensityMatrix) {
       });
   auto pauliZ = cudaq::product_operator<cudaq::matrix_operator>(
       std::complex<double>{1.0, 0.0}, cudaq::matrix_operator("pauli_z", {0}));
-  auto initialState =
-      cudaq::state::from_data(std::vector<std::complex<double>>{1.0, 0.0, 0.0, 0.0});
-  
+  auto initialState = cudaq::state::from_data(
+      std::vector<std::complex<double>>{1.0, 0.0, 0.0, 0.0});
+
   cudaq::runge_kutta_integrator integrator;
   integrator.dt = 0.001;
   integrator.order = 1;
@@ -173,5 +173,57 @@ TEST(EvolveTester, checkCompositeSystem) {
     std::cout << expVals[1] << "\n";
     // This should be an exchanged interaction
     EXPECT_NEAR((double)expVals[0] + (double)expVals[1], num_photons, 1e-2);
+  }
+}
+
+TEST(EvolveTester, checkCompositeSystemWithCollapse) {
+  constexpr int cavity_levels = 10;
+  const std::map<int, int> dims = {{0, 2}, {1, cavity_levels}};
+  auto a = cudaq::matrix_operator::annihilate(1);
+  auto a_dag = cudaq::matrix_operator::create(1);
+
+  auto sm = cudaq::matrix_operator::annihilate(0);
+  auto sm_dag = cudaq::matrix_operator::create(0);
+
+  auto atom_occ_op = cudaq::matrix_operator::number(0);
+  auto cavity_occ_op = cudaq::matrix_operator::number(1);
+  auto hamiltonian = 2 * M_PI * atom_occ_op + 2 * M_PI * cavity_occ_op +
+                     2 * M_PI * 0.25 * (sm * a_dag + sm_dag * a);
+  // auto matrix = hamiltonian.to_matrix(dims);
+  // std::cout << "Matrix:\n" << matrix.dump() << "\n";
+  Eigen::Vector2cd qubit_state;
+  qubit_state << 1.0, 0.0;
+  Eigen::VectorXcd cavity_state = Eigen::VectorXcd::Zero(cavity_levels);
+  const int num_photons = 5;
+  cavity_state[num_photons] = 1.0;
+  Eigen::VectorXcd initial_state_vec =
+      Eigen::kroneckerProduct(cavity_state, qubit_state);
+  Eigen::MatrixXcd rho0 = initial_state_vec * initial_state_vec.transpose();
+  std::cout << "Initial rho:\n" << rho0 << "\n";
+  constexpr int num_steps = 11;
+  const auto timeSteps = cudaq::linspace(0.0, 0.5, num_steps);
+  cudaq::Schedule schedule(timeSteps);
+  auto initialState =
+      cudaq::state::from_data(std::make_pair(rho0.data(), rho0.size()));
+  cudaq::runge_kutta_integrator integrator;
+  integrator.dt = 0.001;
+  integrator.order = 4;
+  constexpr double decayRate = 0.1;
+  auto collapsedOp = std::sqrt(decayRate) * a;
+  auto result = cudaq::evolve_single(hamiltonian, dims, schedule, initialState,
+                                     integrator, {&collapsedOp},
+                                     {&cavity_occ_op, &atom_occ_op}, true);
+  EXPECT_TRUE(result.get_expectation_values().has_value());
+  EXPECT_EQ(result.get_expectation_values().value().size(), num_steps);
+
+  int count = 0;
+  for (auto expVals : result.get_expectation_values().value()) {
+    EXPECT_EQ(expVals.size(), 2);
+    const double totalParticleCount = expVals[0] + expVals[1];
+    const auto time = timeSteps[count++];
+    const double expectedResult = num_photons * std::exp(-decayRate * time);
+    std::cout << "t = " << time << "; particle count = " << totalParticleCount
+              << " vs " << expectedResult << "\n";
+    EXPECT_NEAR(totalParticleCount, expectedResult, 0.1);
   }
 }
