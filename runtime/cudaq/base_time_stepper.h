168be9ddfb99a94da3aa6daa4c537dd647340530
Thien Nguyen <thiennguyen@nvidia.com>
2025-02-17 03:15:41 +0000

    More cleanup: only use cudaq::state at the API surface
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>

--- runtime/cudaq/base_time_stepper.h
+++ runtime/cudaq/base_time_stepper.h
@@ -10,19 +10,6 @@
 #include "cudaq/qis/state.h"
 
 namespace cudaq {
-template <typename TState>
-class BaseTimeStepper {
-public:
-  virtual ~BaseTimeStepper() = default;
-
-  /// @brief Compute the next time step for the given quantum state.
-  /// @param state The quantum state to evolve.
-  /// @param t Current time.
-  /// @param step_size Time step size.
-  /// @return The updated quantum state after stepping.
-  virtual TState compute(TState &state, double t, double step_size) = 0;
-};
-
 class TimeStepper {
 public:
   virtual ~TimeStepper() = default;
