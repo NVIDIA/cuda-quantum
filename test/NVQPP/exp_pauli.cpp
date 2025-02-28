e9af6cce93df00377c05a5388aba97c99abea4da
Eric Schweitz <eschweitz@nvidia.com>
2025-02-28 09:47:29 -0800

    Fix decomposition patterns to handle spans of pauli words. (#2664)
    * Fix decomposition patterns to handle spans of pauli words.
    
    Adds a new test. Changes to decompostion pattern to forward stores to loads
    for Pauli words so that the string literal may be recovered. The forwarding
    is very basic and could be improved.
    
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>
    
    * Improve search efficiency by scanning in reverse from the load.
    
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>
    
    * Add some default output to avoid FileCheck "error" regarding an empty input.
    
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>
    
    ---------
    
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>
    Signed-off-by: Anna Gringauze <agringauze@nvidia.com>

---
+++ test/NVQPP/exp_pauli.cpp
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+// RUN: nvq++ --target quantinuum %s -o %t && %t || echo "passed" |& FileCheck %s
+
+#include <cudaq.h>
+#include <cstdio>
+
+struct Qernel_A {
+  void operator()(std::vector<double> angles,
+                  std::vector<cudaq::pauli_word> paulis) __qpu__ {
+    cudaq::qvector q(3);
+    for (int i = 0; i < angles.size(); i++) {
+      exp_pauli(angles[i], q, paulis[i]);
+    }
+  }
+};
+
+int main() {
+  Qernel_A a;
+  std::vector<double> v = {1.0, 2.0};
+  std::vector<cudaq::pauli_word> p = {"XYZ", "IXX"};
+  printf("calling sample\n");
+  cudaq::sample(a, v, p);
+  return 0;
+}
+
+// CHECK-NOT: cannot determine pauli word string
