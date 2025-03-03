cba0881e39006571ec7ac773adb7e3ac5e980d17
Sachin Pisal <spisal@nvidia.com>
2025-03-03 14:57:18 -0800

    Excluding *.cpp files from dynamics
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- scripts/validate_container.sh
+++ scripts/validate_container.sh
@@ -137,7 +137,7 @@ echo "============================="
 
 # Note: piping the `find` results through `sort` guarantees repeatable ordering.
 tmpFile=$(mktemp)
-for ex in `find examples/ applications/ targets/ -name '*.cpp' | sort`;
+for ex in `find examples/ applications/ targets/ -name '*.cpp' -not -path "examples/cpp/dynamics/*" | sort`;
 do
     filename=$(basename -- "$ex")
     filename="${filename%.*}"
