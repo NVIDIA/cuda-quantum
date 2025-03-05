cfe65095df2e082ecfeef71af60c1ac26fd42791
Mitchell Dzurick <mdzurick@nvidia.com>
2025-03-05 09:48:52 -0700

    docs/notebook_validation.py: initialize skipped array
    Signed-off-by: Mitchell Dzurick <mdzurick@nvidia.com>

--- docs/notebook_validation.py
+++ docs/notebook_validation.py
@@ -107,12 +107,12 @@ if __name__ == "__main__":
         ## See: https://github.com/NVIDIA/cuda-quantum/issues/2577
         ## quantum_transformer.ipynb:
         ## See: https://github.com/NVIDIA/cuda-quantum/issues/2689
-        notebooks_to_skip = {"afqmc.ipynb", "quantum_transformer.ipynb"}
+        notebooks_skipped = ['afqmc.ipynb', 'quantum_transformer.ipynb']
 
         for notebook_filename in notebook_filenames:
-            if os.path.basename(notebook_filename) in notebooks_to_skip:
-                notebooks_skipped.append(notebook_filename)
-            elif validate(notebook_filename, available_backends):
+            base_name = os.path.basename(notebook_filename)
+            if base_name not in notebooks_skipped and validate(
+                    notebook_filename, available_backends):
                 if execute(notebook_filename):
                     notebooks_success.append(notebook_filename)
                 else:
