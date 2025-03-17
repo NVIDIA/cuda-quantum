7f726e9010af12e28766cdd115a5e0e63a27508f
Anna Gringauze <agringauze@nvidia.com>
2025-03-17 12:33:40 -0700

    Cleanup
    Signed-off-by: Anna Gringauze <agringauze@nvidia.com>

--- targettests/execution/complex_vector_synthesis.cpp
+++ targettests/execution/complex_vector_synthesis.cpp
@@ -7,7 +7,7 @@
  ******************************************************************************/
 
 // clang-format off
-// RUN: nvq++ %cpp_std --enable-mlir %s -o %t | FileCheck %s
+// RUN: nvq++ %cpp_std --enable-mlir %s -o %t  && %t | FileCheck %s
 // TODO-FIX-KERNEL-EXEC
 // RUN: nvq++ %cpp_std --target quantinuum --emulate -fkernel-exec-kind=2 %s -o %t  && %t | FileCheck %s
 // RUN: nvq++ %cpp_std --target quantinuum --emulate                      %s -o %t  && %t | FileCheck %s
