b25c6bc739d2a5704edb38835368604752b0b55b
Pradnya Khalate <148914294+khalatepradnya@users.noreply.github.com>
2025-03-20 12:41:44 -0700

    [features/run] Parse primitive types (#2733)
    * * Skeleton for output record logging and parsing
      -- primitive types
    TODO: Handle arrays (vector of results)
    
    * Naive way of parsing integer array log
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * * More tests - labeled arrays, multiple shots, failing test for tuple
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * * Clean-up: Removed the logger functionality since the API was implemented for
    testing only
    * Addressing PR comment
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * * Fix the format of array label - <type x size>
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * * Build fix - back to working stage.
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * WIP: Using the new interface for parser
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * * Handle primitive types
    * Updated tests
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * * Fix test, add "qir-api" to attributes
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * Update runtime/common/RecordLogDecoder.h
    
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>
    
    * review comments
    
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>
    
    ---------
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>
    Co-authored-by: Eric Schweitz <eschweitz@nvidia.com>

--- runtime/common/RecordLogDecoder.h
+++ runtime/common/RecordLogDecoder.h
@@ -8,10 +8,22 @@
 
 #pragma once
 
+#include "cudaq/utils/cudaq_utils.h"
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
 #include <string>
 #include <vector>
 
 namespace cudaq {
+
+/// QIR output schema
+enum struct SchemaType { LABELED, ORDERED };
+enum struct RecordType { HEADER, METADATA, OUTPUT, START, END };
+enum struct OutputType { RESULT, BOOL, INT, DOUBLE };
+enum struct ContainerType { ARRAY, TUPLE };
+
 /// Simple decoder for translating QIR recorded results to a C++ binary data
 /// structure.
 class RecordLogDecoder {
@@ -22,9 +34,7 @@ public:
   /// binary data structure that is compatible with the C++ host code. The data
   /// structure is created in a generic memory buffer. The buffer's address and
   /// length may be queried and returned as a result.
-  void decode(const std::string &outputLog) {
-    // NYI
-  }
+  void decode(const std::string &outputLog);
 
   /// Get a pointer to the data buffer. Note that the data buffer will be
   /// deallocated as soon as the RecordLogDecoder object is deconstructed.
@@ -36,6 +46,33 @@ public:
   std::size_t getBufferSize() const { return buffer.size(); }
 
 private:
+  OutputType extractPrimitiveType(const std::string &label) {
+    if ('i' == label[0]) {
+      auto digits = std::stoi(label.substr(1));
+      if (1 == digits)
+        return OutputType::BOOL;
+      return OutputType::INT;
+    } else if ('f' == label[0]) {
+      return OutputType::DOUBLE;
+    }
+    throw std::runtime_error("Unknown datatype in label");
+  }
+
+  template <typename T>
+  void addPrimitiveRecord(T value) {
+    /// ASKME: Is this efficient?
+    std::size_t position = buffer.size();
+    buffer.resize(position +
+                  std::max(static_cast<std::size_t>(128), sizeof(T)));
+    std::memcpy(buffer.data() + position, &value, sizeof(T));
+  }
+
+  void processSingleRecord(const std::string &recValue,
+                           const std::string &recLabel);
+
   std::vector<char> buffer;
+  SchemaType schema = SchemaType::ORDERED;
+  RecordType currentRecord;
+  OutputType currentOutput;
 };
 } // namespace cudaq
