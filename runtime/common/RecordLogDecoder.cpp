22d638a7a8668544e6d50ce2c2f06f56e5a16e7e
Pradnya Khalate <148914294+khalatepradnya@users.noreply.github.com>
2025-03-20 12:41:44 -0700

    [features/run] Parse primitive types (#2733)
    * * Skeleton for output record logging and parsing
      -- primitive types
    TODO: Handle arrays (vector of results)
    
    * Naive way of parsing integer array log
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * * More tests - labeled arrays, multiple shots, failing test for tuple
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * * Clean-up: Removed the logger functionality since the API was implemented for
    testing only
    * Addressing PR comment
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * * Fix the format of array label - <type x size>
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * * Build fix - back to working stage.
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * WIP: Using the new interface for parser
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * * Handle primitive types
    * Updated tests
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * * Fix test, add "qir-api" to attributes
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    
    * Update runtime/common/RecordLogDecoder.h
    
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>
    
    * review comments
    
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>
    
    ---------
    
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>
    Co-authored-by: Eric Schweitz <eschweitz@nvidia.com>
    Signed-off-by: Anna Gringauze <agringauze@nvidia.com>

---
+++ runtime/common/RecordLogDecoder.cpp
@@ -0,0 +1,116 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+#include "RecordLogDecoder.h"
+
+void cudaq::RecordLogDecoder::decode(const std::string &outputLog) {
+  std::vector<std::string> lines = cudaq::split(outputLog, '\n');
+  if (lines.empty())
+    return;
+
+  for (auto line : lines) {
+    std::vector<std::string> entries = cudaq::split(line, '\t');
+    if (entries.empty())
+      continue;
+
+    if ("HEADER" == entries[0])
+      currentRecord = RecordType::HEADER;
+    else if ("METADATA" == entries[0])
+      currentRecord = RecordType::METADATA;
+    else if ("OUTPUT" == entries[0])
+      currentRecord = RecordType::OUTPUT;
+    else if ("START" == entries[0])
+      currentRecord = RecordType::START;
+    else if ("END" == entries[0])
+      currentRecord = RecordType::END;
+    else
+      throw std::runtime_error("Invalid data");
+
+    switch (currentRecord) {
+    case RecordType::HEADER: {
+      if ("schema_name" == entries[1]) {
+        if ("labeled" == entries[2])
+          schema = SchemaType::LABELED;
+        else if ("ordered" == entries[2])
+          schema = SchemaType::ORDERED;
+        else
+          throw std::runtime_error("Unknown schema type");
+      }
+      /// TODO: Check schema version
+    } break;
+    case RecordType::METADATA:
+      // ignore metadata for now
+      break;
+    case RecordType::START:
+      // indicates start of a shot
+      break;
+    case RecordType::END: {
+      // indicates end of a shot
+      if (entries.size() < 2)
+        throw std::runtime_error("Missing shot status");
+      if ("0" != entries[1])
+        throw std::runtime_error("Cannot handle unsuccessful shot");
+    } break;
+    case RecordType::OUTPUT: {
+      if (entries.size() < 3)
+        throw std::runtime_error("Insufficent data in a record");
+      if ((schema == SchemaType::LABELED) && (entries.size() != 4))
+        throw std::runtime_error("Unexpected record size for a labeled record");
+
+      std::string recType = entries[1];
+      std::string recValue = entries[2];
+      std::string recLabel = (entries.size() == 4) ? entries[3] : "";
+
+      if ("RESULT" == recType)
+        throw std::runtime_error("This type is not yet supported");
+      if ("TUPLE" == recType)
+        throw std::runtime_error("This type is not yet supported");
+      if ("ARRAY" == recType)
+        throw std::runtime_error("This type is not yet supported");
+
+      if ("BOOL" == recType)
+        currentOutput = OutputType::BOOL;
+      else if ("INT" == recType)
+        currentOutput = OutputType::INT;
+      else if ("DOUBLE" == recType)
+        currentOutput = OutputType::DOUBLE;
+      else
+        throw std::runtime_error("Invalid data");
+
+      processSingleRecord(recValue, recLabel);
+    } break;
+    }
+  } // for line
+}
+
+void cudaq::RecordLogDecoder::processSingleRecord(const std::string &recValue,
+                                                  const std::string &recLabel) {
+  if ((!recLabel.empty()) && (extractPrimitiveType(recLabel) != currentOutput))
+    throw std::runtime_error("Type mismatch in label");
+
+  switch (currentOutput) {
+  case OutputType::BOOL: {
+    bool value;
+    if ("true" == recValue)
+      value = true;
+    else if ("false" == recValue)
+      value = false;
+    else
+      throw std::runtime_error("Invalid boolean value");
+    addPrimitiveRecord<bool>(value);
+  } break;
+  case OutputType::INT:
+    addPrimitiveRecord<int>(std::stoi(recValue));
+    break;
+  case OutputType::DOUBLE:
+    addPrimitiveRecord<double>(std::stod(recValue));
+    break;
+  default:
+    throw std::runtime_error("Unsupported output type");
+  }
+}
---
+++ unittests/output_record/RecordParserTester.cpp
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+#include "CUDAQTestUtils.h"
+#include "common/RecordLogDecoder.h"
+#include <cudaq.h>
+
+CUDAQ_TEST(ParserTester, checkSingleBoolean) {
+  const std::string log = "OUTPUT\tBOOL\ttrue";
+  cudaq::RecordLogDecoder parser;
+  parser.decode(log);
+  auto *origBuffer = parser.getBufferPtr();
+  bool value;
+  std::memcpy(&value, origBuffer, sizeof(bool));
+  EXPECT_EQ(true, value);
+}
+
+CUDAQ_TEST(ParserTester, checkIntegers) {
+  const std::string log = "OUTPUT\tINT\t0\n"
+                          "OUTPUT\tINT\t1\n"
+                          "OUTPUT\tINT\t2\n";
+  cudaq::RecordLogDecoder parser;
+  parser.decode(log);
+  auto *origBuffer = parser.getBufferPtr();
+  std::size_t bufferSize = parser.getBufferSize();
+  EXPECT_EQ(3, bufferSize / sizeof(int));
+  int *buffer = static_cast<int *>(malloc(bufferSize));
+  std::memcpy(buffer, origBuffer, bufferSize);
+  for (int i = 0; i < 3; ++i)
+    EXPECT_EQ(i, buffer[i]);
+}
+
+CUDAQ_TEST(ParserTester, checkDoubles) {
+  const std::string log = "START\n"
+                          "OUTPUT\tDOUBLE\t3.14\n"
+                          "OUTPUT\tDOUBLE\t2.717\n"
+                          "END\t0";
+  cudaq::RecordLogDecoder parser;
+  parser.decode(log);
+  auto *origBuffer = parser.getBufferPtr();
+  std::size_t bufferSize = parser.getBufferSize();
+  EXPECT_EQ(2, bufferSize / sizeof(double));
+  double *buffer = static_cast<double *>(malloc(bufferSize));
+  std::memcpy(buffer, origBuffer, bufferSize);
+  EXPECT_EQ(3.14, buffer[0]);
+  EXPECT_EQ(2.717, buffer[1]);
+}
