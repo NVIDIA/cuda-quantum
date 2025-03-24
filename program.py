0e9a893ffdd12401ee62a9bf2d3d202f70c0c36e
Anna Gringauze <agringauze@nvidia.com>
2025-03-24 09:32:05 -0700

    Temp
    Signed-off-by: Anna Gringauze <agringauze@nvidia.com>

---
+++ program.py
@@ -0,0 +1,35 @@
+# ============================================================================ #
+# Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                   #
+# All rights reserved.                                                         #
+#                                                                              #
+# This source code and the accompanying materials are made available under     #
+# the terms of the Apache License 2.0 which accompanies this distribution.     #
+# ============================================================================ #
+
+import cudaq
+
+def test_Ionq_state_synthesis():
+
+    @cudaq.kernel
+    def init(n: int):
+        q = cudaq.qvector(n)
+        h(q[0])
+        x(q[1])
+        mz(q)
+
+    @cudaq.kernel
+    def kernel(s: cudaq.State):
+        q = cudaq.qvector(s)
+        x(q[0])
+        mz(q)
+
+    s = cudaq.get_state(init, 2)
+    s = cudaq.get_state(kernel, s)
+    counts = cudaq.sample(kernel, s)
+    print(counts)
+    assert '01' in counts
+    assert '11' in counts
+    assert len(counts) == 2
+
+cudaq.set_target("ionq", emulate=True)
+test_Ionq_state_synthesis()
