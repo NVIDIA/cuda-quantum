9bd4cf261391788dd1b420bc50dc4ffa8046acf0
Sachin Pisal <spisal@nvidia.com>
2025-04-07 10:06:08 -0700

    Adding measuring_kernels example snippets
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -118,7 +118,7 @@ master_doc = 'index'
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [
-    '**/_*', '.DS_Store', 'examples/python/building_kernels.ipynb'
+    '**/_*', '.DS_Store', 'examples/python/building_kernels.ipynb', 'examples/python/measuring_kernels.ipynb'
 ]
 
 #redirect links 
