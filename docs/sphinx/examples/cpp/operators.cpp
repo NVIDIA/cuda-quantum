6afc733a4a07d5d414c090e0da76711d4242d0d1
Sachin Pisal <spisal@nvidia.com>
2025-04-07 21:24:58 -0700

    sorting and formatting
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- docs/sphinx/examples/cpp/operators.cpp
+++ docs/sphinx/examples/cpp/operators.cpp
@@ -9,14 +9,16 @@
 // [Begin Spin]
 #include <cudaq.h>
 
-auto operator = 2 * cudaq::spin::x(0) * cudaq::spin::y(1) * cudaq::spin::x(2) - 3 * cudaq::spin::z(0) * cudaq::spin::z(1) * cudaq::spin::y(2);
+auto operator= 2 * cudaq::spin::x(0) * cudaq::spin::y(1) * cudaq::spin::x(2) -
+    3 * cudaq::spin::z(0) * cudaq::spin::z(1) * cudaq::spin::y(2);
 // [End Spin]
 
 // [Begin Pauli]
 auto words = {"XYZ", "IXX"};
 auto coefficients = {0.432, 0.324};
 
-__qpu__ void kernel(std::vector<std::string> words, std::vector<double> coefficients) {
+__qpu__ void kernel(std::vector<std::string> words,
+                    std::vector<double> coefficients) {
   cudaq::qvector qvector(3);
   for (int i = 0; i < coefficients.size(); i++) {
     exp_pauli(coefficients[i], qvector, words[i]);
