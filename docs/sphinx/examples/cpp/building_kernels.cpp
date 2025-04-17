92b5b65bd72c247041a2b78c4ab6c68713f71420
Sachin Pisal <spisal@nvidia.com>
2025-04-17 14:52:01 -0700

    formatting
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- docs/sphinx/examples/cpp/building_kernels.cpp
+++ docs/sphinx/examples/cpp/building_kernels.cpp
@@ -27,9 +27,7 @@ __qpu__ void kernel(int N) { cudaq::qvector r(N); }
 
 // [Begin `PassingComplexVector`]
 // Passing complex vectors as parameters
-__qpu__ void kernel(const std::vector<complex> &vec) {
-  cudaq::qubit q(vec);
-}
+__qpu__ void kernel(const std::vector<complex> &vec) { cudaq::qubit q(vec); }
 // [End `PassingComplexVector`]
 
 // [Begin `CapturingComplexVector`]
