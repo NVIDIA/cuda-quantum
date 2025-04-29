dd3cd698df0df72afdcf038e6210fe2f964ab8b6
Sachin Pisal <spisal@nvidia.com>
2025-04-29 11:38:10 -0700

    Documentation for cpp dynamics (#2735)
    Signed-off-by: Anna Gringauze <agringauze@nvidia.com>

--- docs/sphinx/targets/cpp/pasqal.cpp
+++ docs/sphinx/targets/cpp/pasqal.cpp
@@ -4,7 +4,8 @@
 // ./out.x
 // ```
 // Assumes a valid set of credentials (`PASQAL_AUTH_TOKEN`, `PASQAL_PROJECT_ID`)
-// have been set.
+// have been set. To set `PASQAL_AUTH_TOKEN` from Pasqal Cloud username and
+// password, use `pasqal_auth.py` in this folder.
 
 #include "cudaq/algorithms/evolve.h"
 #include "cudaq/algorithms/integrator.h"
@@ -14,8 +15,8 @@
 #include <map>
 #include <vector>
 
-// This example illustrates how to use Pasqal's EMU_MPS emulator over Pasqal's
-// cloud via CUDA-Q. Contact Pasqal at help@pasqal.com or through
+// This example illustrates how to use `Pasqal's` EMU_MPS emulator over
+// `Pasqal's` cloud via CUDA-Q. Contact Pasqal at help@pasqal.com or through
 // https://community.pasqal.com for assistance.
 
 int main() {
