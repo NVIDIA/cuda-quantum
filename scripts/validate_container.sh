e7fe25acc13e862b3f481851fdb86f10253e9b54
Sachin Pisal <spisal@nvidia.com>
2025-05-01 21:34:13 -0700

    Excluding executing_kernels test in absence of a remote server process
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- scripts/validate_container.sh
+++ scripts/validate_container.sh
@@ -218,6 +218,13 @@ do
                 echo ":white_flag: $filename: Incomplete MPI installation. Test skipped." >> "${tmpFile}_$(echo $t | tr - _)"
                 continue
 
+            elif [[ "$ex" == *"executing_kernels"* ]];
+            then
+                let "skipped+=1"
+                echo "Skipping $t target due to a missing remote server process.";
+                echo ":white_flag: $filename: Missing remote server process. Test skipped." >> "${tmpFile}_$(echo $t | tr - _)"
+                continue
+
             else
                 # TODO: remove this once the nvqc backend is part of the validation
                 # tracked in https://github.com/NVIDIA/cuda-quantum/issues/1283
