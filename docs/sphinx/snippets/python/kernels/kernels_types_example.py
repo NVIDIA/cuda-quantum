d11d091d3b9f0c80f1f0049d2a39f7eb188c6086
Abdullah Al Omar Galib <109025616+ahkatlio@users.noreply.github.com>
2025-05-28 16:39:26 +0600

    Create kernels_types_example.py

---
+++ docs/sphinx/snippets/python/kernels/kernels_types_example.py
@@ -0,0 +1,37 @@
+import cudaq
+from cudaq import h, ry, mz # Import necessary operations
+
+# [Begin Kernel Types Python]
+@cudaq.kernel()
+def my_first_entry_point_kernel_py(x : float):
+   q = cudaq.qvector(1)
+   ry(x, q[0])
+   mz(q)
+   print(f"Python lambda-like kernel executed with x = {x}")
+
+@cudaq.kernel()
+def my_second_entry_point_kernel_py(x : float, params : list[float]):
+   q = cudaq.qvector(1)
+   ry(x * params[0], q[0])
+   mz(q)
+   print(f"Python struct-like kernel executed with x = {x}, param0 = {params[0]}")
+
+@cudaq.kernel()
+def my_first_pure_device_kernel_py(qubits : cudaq.qview):
+   h(qubits[0])
+   print(f"Python pure device kernel executed on a qubit.")
+
+@cudaq.kernel()
+def caller_for_pure_device_py():
+    q = cudaq.qvector(1)
+    my_first_pure_device_kernel_py(q)
+    mz(q)
+# [End Kernel Types Python]
+
+# [Begin Kernel Types Python Execution]
+if __name__ == "__main__":
+    my_first_entry_point_kernel_py(1.23)
+    params_py = [0.5]
+    my_second_entry_point_kernel_py(1.23, params_py)
+    cudaq.sample(caller_for_pure_device_py)
+# [End Kernel Types Python Execution]
