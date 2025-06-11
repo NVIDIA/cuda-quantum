e9329386775fbd8f93c506ee41b08ea60da1349d
Thien Nguyen <thiennguyen@nvidia.com>
2025-06-11 05:39:29 +0000

    Add ctest
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>

--- unittests/dynamics/test_EvolveApi.cpp
+++ unittests/dynamics/test_EvolveApi.cpp
@@ -316,3 +316,104 @@ TEST(EvolveAPITester, checkSuperopMasterEquation) {
     EXPECT_NEAR((double)expVals[0], theoryResults[count++], 1e-3);
   }
 }
+
+TEST(EvolveAPITester, checkSuperopMasterEquationInputStateEnum) {
+  const cudaq::dimension_map dims = {{0, 2}};
+  auto ham = 2.0 * M_PI * 0.1 * cudaq::spin_op::x(0);
+  cudaq::super_op sup;
+  // Apply `-i[H, rho]` superop
+  sup += cudaq::super_op::left_multiply(std::complex<double>(0.0, -1.0) * ham);
+  sup += cudaq::super_op::right_multiply(std::complex<double>(0.0, 1.0) * ham);
+  constexpr int numSteps = 10;
+  cudaq::schedule schedule(cudaq::linspace(0.0, 1.0, numSteps), {"t"});
+  auto initialState = cudaq::InitialState::ZERO;
+  cudaq::integrators::runge_kutta integrator(1, 0.001);
+  auto result = cudaq::evolve(ham, dims, schedule, initialState, integrator, {},
+                              {cudaq::spin_op::z(0)}, true);
+  EXPECT_TRUE(result.expectation_values.has_value());
+  EXPECT_EQ(result.expectation_values.value().size(), numSteps);
+
+  std::vector<double> theoryResults;
+  for (const auto &t : schedule) {
+    const double expected = std::cos(2 * 2.0 * M_PI * 0.1 * t.real());
+    theoryResults.emplace_back(expected);
+  }
+
+  int count = 0;
+  for (auto expVals : result.expectation_values.value()) {
+    EXPECT_EQ(expVals.size(), 1);
+    EXPECT_NEAR((double)expVals[0], theoryResults[count++], 1e-3);
+  }
+}
+
+TEST(EvolveAPITester, checkCavityModelSuperOpBatchedState) {
+  constexpr int N = 10;
+  constexpr int numStates = 4;
+  constexpr int numSteps = 101;
+  cudaq::schedule schedule(cudaq::linspace(0.0, 1.0, numSteps), {"t"});
+  const cudaq::dimension_map dimensions{{0, N}};
+  std::vector<cudaq::state> initialStates;
+  for (int i = 0; i < numStates; ++i) {
+    std::vector<std::complex<double>> psi0_(N, 0.0);
+    psi0_[N - i - 1] = 1.0;
+    initialStates.emplace_back(cudaq::state::from_data(psi0_));
+  }
+
+  auto ham = cudaq::boson_op::number(0);
+  cudaq::super_op sup;
+  // Apply `-i[H, rho]` superop
+  sup += cudaq::super_op::left_multiply(std::complex<double>(0.0, -1.0) * ham);
+  sup += cudaq::super_op::right_multiply(std::complex<double>(0.0, 1.0) * ham);
+
+  constexpr double decay_rate = 0.1;
+
+  auto td_function =
+      [decay_rate](const std::unordered_map<std::string, std::complex<double>>
+                       &parameters) {
+        auto entry = parameters.find("t");
+        if (entry == parameters.end())
+          throw std::runtime_error("Cannot find value of expected parameter");
+        const auto t = entry->second.real();
+        const auto result = std::sqrt(decay_rate * std::exp(-t));
+        return result;
+      };
+
+  auto L = cudaq::scalar_operator(td_function) * cudaq::boson_op::annihilate(0);
+  auto L_dagger =
+      cudaq::scalar_operator(td_function) * cudaq::boson_op::create(0);
+  // Lindblad terms
+  // L * rho * L_dagger
+  sup += cudaq::super_op::left_right_multiply(L, L_dagger);
+  // -0.5 * L_dagger * L * rho
+  sup += cudaq::super_op::left_multiply(-0.5 * L_dagger * L);
+  // -0.5 * rho * L_dagger * L
+  sup += cudaq::super_op::right_multiply(-0.5 * L_dagger * L);
+
+  cudaq::integrators::runge_kutta integrator(4, 0.01);
+  auto results = cudaq::evolve(sup, dimensions, schedule, initialStates,
+                               integrator, {cudaq::boson_op::number(0)}, true);
+  EXPECT_EQ(results.size(), numStates);
+  for (int i = 0; i < numStates; ++i) {
+    auto &result = results[i];
+    const auto numPhotons = N - i;
+    std::cout << "Checking results for the initial state with " << numPhotons
+              << " photons.\n";
+    EXPECT_TRUE(result.expectation_values.has_value());
+    EXPECT_EQ(result.expectation_values.value().size(), numSteps);
+    std::vector<double> theoryResults;
+    for (const auto &t : schedule) {
+      const double expected =
+          (numPhotons - 1) *
+          std::exp(-decay_rate * (1.0 - std::exp(-t.real())));
+      theoryResults.emplace_back(expected);
+    }
+
+    int count = 0;
+    for (auto expVals : result.expectation_values.value()) {
+      EXPECT_EQ(expVals.size(), 1);
+      std::cout << "Result = " << (double)expVals[0] << "; expected "
+                << theoryResults[count] << "\n";
+      EXPECT_NEAR((double)expVals[0], theoryResults[count++], 0.01);
+    }
+  }
+}
