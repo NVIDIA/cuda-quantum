55b1d4823327a9684b5712e0ebf0accc88dcedb9
Evgenii Zheltonozhskii <zheltonozhskiy@gmail.com>
2025-06-11 15:03:39 +0000

    Fix spelling
    Signed-off-by: Evgenii Zheltonozhskii <zheltonozhskiy@gmail.com>

--- python/tests/kernel/test_get_unitary.py
+++ python/tests/kernel/test_get_unitary.py
@@ -1,3 +1,10 @@
+# ============================================================================ #
+# Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                   #
+# All rights reserved.                                                         #
+#                                                                              #
+# This source code and the accompanying materials are made available under     #
+# the terms of the Apache License 2.0 which accompanies this distribution.     #
+# ============================================================================ #
 import pytest
 import numpy as np
 from scipy.linalg import block_diag
