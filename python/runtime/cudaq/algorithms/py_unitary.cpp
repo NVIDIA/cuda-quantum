c257ee5ac825f4ba547a2534e373d0c16f5ee424
Evgenii Zheltonozhskii <zheltonozhskiy@gmail.com>
2025-06-11 06:07:25 +0000

    Format
    Signed-off-by: Evgenii Zheltonozhskii <zheltonozhskiy@gmail.com>

--- python/runtime/cudaq/algorithms/py_unitary.cpp
+++ python/runtime/cudaq/algorithms/py_unitary.cpp
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
 #include "cudaq/algorithms/draw.h"
 #include "cudaq/algorithms/unitary.h"
 #include "runtime/cudaq/algorithms/py_draw.h"
