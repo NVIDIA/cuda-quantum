0fd858ba8233cc0abdd0d34d3395eaa43301b955
Adam Geller <adam_geller@msn.com>
2025-06-12 12:57:12 -0700

    Add tests

---
+++ python/tests/backends/test_resource_counter.py
@@ -0,0 +1,47 @@
+# ============================================================================ #
+# Copyright (c) 2025 NVIDIA Corporation & Affiliates.                          #
+# All rights reserved.                                                         #
+#                                                                              #
+# This source code and the accompanying materials are made available under     #
+# the terms of the Apache License 2.0 which accompanies this distribution.     #
+# ============================================================================ #
+
+# Compile and run with:
+
+import cudaq
+
+# Define the random walk phase estimation kernel
+def test_resource_counter():
+    @cudaq.kernel
+    def mykernel():
+        q = cudaq.qubit()
+        p = cudaq.qubit()
+
+        # Alias them
+        h(q)
+
+        m1 = mz(q)
+        if m1:
+            x(p)
+            m2 = mz(p)
+        else:
+            m3 = mz(p)
+
+    def choice():
+        return True
+
+    cudaq.set_target("quantinuum", emulate=True)
+
+    counts1 = cudaq.sample(mykernel, shots_count=5)
+    counts2 = cudaq.count_resources(choice, mykernel)
+    counts3 = cudaq.sample(mykernel, shots_count=10)
+
+    assert(counts1.count("00") + counts1.count("11") == 5)
+    assert(counts2.count("h") == 1)
+    assert(counts2.count("x") == 1)
+    assert(counts3.count("00") + counts3.count("11") == 10)
+
+# leave for gdb debugging
+if __name__ == "__main__":
+    loc = os.path.abspath(__file__)
+    pytest.main([loc, "-s"])
