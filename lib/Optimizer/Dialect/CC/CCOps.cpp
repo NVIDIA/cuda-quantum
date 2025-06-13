8b26decec214a585cd2022adc31c137b902d12f0
Eric Schweitz <eschweitz@nvidia.com>
2025-06-13 08:44:55 -0700

    [core] Workaround overly excitable MLIR inliner. (#3053)
    * [core] Workaround overly excitable MLIR inliner.
    
    Fix #3037.
    
    We generally want the inliner to be super aggressive and inline
    all calls everywhere. Except in the case where the thunk layer
    is calling the kernel proper. In that one instance, inlining
    must be prevented. The existing checks do a decent job of preventing
    this from happening in the typical case, but there were some cases
    (involving trivial kernels) that leaked through.
    
    This adds a special NoInlineCallOp to fool the Inliner into thinking
    it can't perform the inlining.
    
    * Add the test from #3037
    * Fix typo.
    * Changes to regression tests.
    
    ---------
    
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>

--- lib/Optimizer/Dialect/CC/CCOps.cpp
+++ lib/Optimizer/Dialect/CC/CCOps.cpp
@@ -2334,6 +2334,46 @@ MutableOperandRange cudaq::cc::ConditionOp::getMutableSuccessorOperands(
 }
 
 //===----------------------------------------------------------------------===//
+// NoInlineCallOp
+//===----------------------------------------------------------------------===//
+
+LogicalResult cudaq::cc::NoInlineCallOp::verifySymbolUses(
+    SymbolTableCollection &symbolTable) {
+  // Check that the callee attribute was specified.
+  auto fnAttr = (*this)->getAttrOfType<FlatSymbolRefAttr>("callee");
+  if (!fnAttr)
+    return emitOpError("requires a 'callee' symbol reference attribute");
+  auto fn = symbolTable.lookupNearestSymbolFrom<func::FuncOp>(*this, fnAttr);
+  if (!fn)
+    return emitOpError() << "'" << fnAttr.getValue()
+                         << "' does not reference a valid function";
+
+  // Verify that the operand and result types match the callee.
+  auto fnType = fn.getFunctionType();
+  if (fnType.getNumInputs() != getNumOperands())
+    return emitOpError("incorrect number of operands for callee");
+
+  for (unsigned i = 0, e = fnType.getNumInputs(); i != e; ++i)
+    if (getOperand(i).getType() != fnType.getInput(i))
+      return emitOpError("operand type mismatch: expected operand type ")
+             << fnType.getInput(i) << ", but provided "
+             << getOperand(i).getType() << " for operand number " << i;
+
+  if (fnType.getNumResults() != getNumResults())
+    return emitOpError("incorrect number of results for callee");
+
+  for (unsigned i = 0, e = fnType.getNumResults(); i != e; ++i)
+    if (getResult(i).getType() != fnType.getResult(i)) {
+      auto diag = emitOpError("result type mismatch at index ") << i;
+      diag.attachNote() << "      op result types: " << getResultTypes();
+      diag.attachNote() << "function result types: " << fnType.getResults();
+      return diag;
+    }
+
+  return success();
+}
+
+//===----------------------------------------------------------------------===//
 // OffsetOfOp
 //===----------------------------------------------------------------------===//
 
