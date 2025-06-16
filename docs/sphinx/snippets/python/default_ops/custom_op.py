8323a9335d9420db8c33cbf1eaf0db03d2f06521
Abdullah Al Omar Galib <109025616+ahkatlio@users.noreply.github.com>
2025-06-17 00:07:17 +0600

    Update custom_op.py

--- docs/sphinx/snippets/python/default_ops/custom_op.py
+++ docs/sphinx/snippets/python/default_ops/custom_op.py
@@ -2,6 +2,7 @@ import cudaq
 import numpy as np
 
 def main():
+    # [Begin Custom Op]
     cudaq.register_operation("custom_h", 1. / np.sqrt(2.) * np.array([1, 1, 1, -1]))
     cudaq.register_operation("custom_x", np.array([0, 1, 1, 0]))
 
@@ -12,6 +13,7 @@ def main():
         custom_x.ctrl(qubits[0], qubits[1])
 
     cudaq.sample(bell).dump()
+    # [End Custom Op]
 
 if __name__ == "__main__":
     main()
