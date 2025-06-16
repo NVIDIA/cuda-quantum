378a01671139643c5d798bd80190d27d42492cba
Abdullah Al Omar Galib <109025616+ahkatlio@users.noreply.github.com>
2025-06-16 22:03:46 +0600

    Update spin_op_creation.cpp

--- docs/sphinx/snippets/cpp/operators/spin_op_creation.cpp
+++ docs/sphinx/snippets/cpp/operators/spin_op_creation.cpp
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+// Compile and run with: `nvq++ spin_op_creation.cpp && ./a.out`
+
 #include <cudaq.h>
 #include <cudaq/spin_op.h>
 #include <iostream> // For std::cout and std::cerr
@@ -37,3 +47,4 @@ int main() {
   // [End SpinOp Creation C++ Execution]
   return 0; // Indicate success
 }
+
