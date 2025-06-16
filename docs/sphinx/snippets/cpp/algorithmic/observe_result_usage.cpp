b458298a0b4ca1af4a7ea6d58b153216b84a264d
Abdullah Al Omar Galib <109025616+ahkatlio@users.noreply.github.com>
2025-06-16 22:01:17 +0600

    Update observe_result_usage.cpp

--- docs/sphinx/snippets/cpp/algorithmic/observe_result_usage.cpp
+++ docs/sphinx/snippets/cpp/algorithmic/observe_result_usage.cpp
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+// Compile and run with: `nvq++ observe_result_usage.cpp && ./a.out`
+
 #include <cudaq.h>
 #include <stdio.h>
 
@@ -43,3 +53,4 @@ int main() {
   // [End Observe Result Usage C++]
   return 0;
 }
+
