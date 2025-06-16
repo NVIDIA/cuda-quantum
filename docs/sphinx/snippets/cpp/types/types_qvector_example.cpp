986eefc381db2a8de2ab1d4e917149c5b8e230fb
Abdullah Al Omar Galib <109025616+ahkatlio@users.noreply.github.com>
2025-06-16 22:03:59 +0600

    Update types_qvector_example.cpp

--- docs/sphinx/snippets/cpp/types/types_qvector_example.cpp
+++ docs/sphinx/snippets/cpp/types/types_qvector_example.cpp
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+// Compile and run with: `nvq++ types_qvector_example.cpp && ./a.out`
+
 #include <cudaq.h>
 #include <stdio.h>
 #include <vector>
@@ -38,3 +48,4 @@ int main() {
   counts.dump();
   return 0;
 }
+
