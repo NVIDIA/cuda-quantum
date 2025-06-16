8e59ee55ea9eb70e7ec77b7bb8293884fe2713b7
Abdullah Al Omar Galib <109025616+ahkatlio@users.noreply.github.com>
2025-06-16 22:01:26 +0600

    Update sample_bell_state.cpp

--- docs/sphinx/snippets/cpp/algorithmic/sample_bell_state.cpp
+++ docs/sphinx/snippets/cpp/algorithmic/sample_bell_state.cpp
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+// Compile and run with: `nvq++ sample_bell_state.cpp && ./a.out`
+
 #include <cudaq.h>
 #include <stdio.h>
 
@@ -25,3 +35,4 @@ int main() {
   // [End Sample Bell C++]
   return 0;
 }
+
