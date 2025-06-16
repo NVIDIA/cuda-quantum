f0ecab93018d45b56ab029faf651711caa16c557
Abdullah Al Omar Galib <109025616+ahkatlio@users.noreply.github.com>
2025-06-16 22:02:57 +0600

    Update hello_bell_state.cpp

--- docs/sphinx/snippets/cpp/examples/hello_bell_state.cpp
+++ docs/sphinx/snippets/cpp/examples/hello_bell_state.cpp
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+// Compile and run with: `nvq++ hello_bell_state.cpp && ./a.out`
+
 #include <cudaq.h>
 #include <stdio.h> // For printf
 
@@ -25,3 +35,4 @@ int main() {
   return 0;
 }
 // [End Bell State C++]
+
