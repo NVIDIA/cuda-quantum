c34db4d8b890c5486188a1895a6ba948d16e685b
Abdullah Al Omar Galib <109025616+ahkatlio@users.noreply.github.com>
2025-06-17 00:07:00 +0600

    Update adjoint_op.py

--- docs/sphinx/snippets/python/default_ops/adjoint_op.py
+++ docs/sphinx/snippets/python/default_ops/adjoint_op.py
@@ -4,7 +4,7 @@ import cudaq
 def main():
     # Create a kernel and allocate a qubit in a |0> state.
     qubit = cudaq.qubit()
-
+    # [Begin Adjoint Op]
     # Apply the unitary transformation defined by the matrix
     # T = | 1      0     |
     #     | 0  exp(iπ/4) |
@@ -16,6 +16,6 @@ def main():
     #      | 0  exp(-iπ/4) |
     t.adj(qubit)
     # `qubit` is now again in the initial state |0>.
-
+    # [End Adjoint Op]
 if __name__ == "__main__":
     main()
