655653bce13a41e92459ddb80e013d175fd3c305
Abdullah Al Omar Galib <109025616+ahkatlio@users.noreply.github.com>
2025-06-16 22:02:15 +0600

    Update phase_shift_op.cpp

--- docs/sphinx/snippets/cpp/default_ops/phase_shift_op.cpp
+++ docs/sphinx/snippets/cpp/default_ops/phase_shift_op.cpp
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+// Compile and run with: `nvq++ phase_shift_op.cpp && ./a.out`
+
 #include <cudaq.h>
 
 int main() {
@@ -6,3 +16,4 @@ int main() {
 
   return 0;
 }
+
