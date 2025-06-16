e6b06b46a5b1f5794b0a6de06366768199151915
Abdullah Al Omar Galib <109025616+ahkatlio@users.noreply.github.com>
2025-06-16 22:02:34 +0600

    Update swap_op.cpp

--- docs/sphinx/snippets/cpp/default_ops/swap_op.cpp
+++ docs/sphinx/snippets/cpp/default_ops/swap_op.cpp
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                  *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+// Compile and run with: `nvq++ swap_op.cpp && ./a.out`
+
 int main() {
   cudaq::qubit qubit_1, qubit_2;
 
@@ -10,3 +20,4 @@ int main() {
 
   return 0;
 }
+
