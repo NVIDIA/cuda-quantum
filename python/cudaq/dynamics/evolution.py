189f9bec98f081d899f8f923adb8e280d7a28d11
Thien Nguyen <58006629+1tnguyen@users.noreply.github.com>
2025-06-25 07:11:29 +1000

    General `evolve` super-operator construction (#2960)
    * Work on generic superoperator
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    Hook up superop
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    More tests
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    More tests
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    Refactor
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    Add member functions
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    * Add bindings
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    * Hook superop type to Python evolve API
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    * Add docstring + move impl to cpp file
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    * Add all overloads for superop inputs
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    * Code format
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    * Add ctest
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    * Add Python tests
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    * Address code review: code refactor + fix grammar errors in code comments
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    * Add docs + hook C++ dynamics snippets to CI
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    * Fix spelling
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    * Fix merge conflict resolution
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    ---------
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>

--- python/cudaq/dynamics/evolution.py
+++ python/cudaq/dynamics/evolution.py
@@ -19,7 +19,7 @@ from cudaq.kernel.kernel_builder import PyKernel, make_kernel
 from cudaq.kernel.register_op import register_operation
 from cudaq.kernel.utils import ahkPrefix
 from cudaq.mlir._mlir_libs._quakeDialects import cudaq_runtime
-from ..operators import NumericType, Operator, RydbergHamiltonian
+from ..operators import NumericType, Operator, RydbergHamiltonian, SuperOperator
 from .helpers import InitialState, InitialStateArgT, IntermediateResultSave
 from .integrator import BaseIntegrator
 from .schedule import Schedule
@@ -315,7 +315,7 @@ def evolve_single(
 
 # Top level API for the CUDA-Q master equation solver.
 def evolve(
-    hamiltonian: Operator,
+    hamiltonian: Operator | SuperOperator,
     dimensions: Mapping[int, int] = {},
     schedule: Schedule = None,
     initial_state: InitialStateArgT | Sequence[InitialStateArgT] = None,
