34e2886aee2030d8037bdb87e69cbcce74992e81
Pradnya Khalate <pkhalate@nvidia.com>
2025-06-26 09:40:43 -0700

    * Update the test(s) as per PR review comment.
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>

--- targettests/execution/cudaq_run_free_functions.cpp
+++ targettests/execution/cudaq_run_free_functions.cpp
@@ -19,9 +19,9 @@ struct foo_test_result {
 };
 
 foo_test_result foo_test(int x) __qpu__ {
-  cudaq::qvector q(5);
+  cudaq::qvector q(x);
   h(q[0]);
-  for (int i = 1; i < 5; i++) {
+  for (int i = 1; i < x; i++) {
     cx(q[i - 1], q[i]);
   }
   return {9, 10};
@@ -31,12 +31,7 @@ struct bar_test_result {
   bool a;
 };
 
-bar_test_result bar_test(int x) __qpu__ {
-  cudaq::qvector q(5);
-  h(q[0]);
-  for (int i = 1; i < 5; i++) {
-    cx(q[i - 1], q[i]);
-  }
+bar_test_result bar_test() __qpu__ {
   return {true};
 }
 
@@ -46,7 +41,7 @@ struct baz_test_result {
   int64_t c;
 };
 
-baz_test_result baz_test(int x) __qpu__ {
+baz_test_result baz_test() __qpu__ {
   cudaq::qvector q(5);
   h(q[0]);
   for (int i = 1; i < 5; i++) {
@@ -63,13 +58,13 @@ int main() {
     printf("\n");
   }
   {
-    const auto results = cudaq::run(4, bar_test, 2 * 3);
+    const auto results = cudaq::run(4, bar_test);
     for (auto i : results)
       printf("%d\t", (int)i.a);
     printf("\n");
   }
   {
-    const auto results = cudaq::run(3, baz_test, 4 - 3);
+    const auto results = cudaq::run(3, baz_test);
     for (auto i : results)
       printf("%d, %d, %ld\t", (int)i.a, i.b, i.c);
     printf("\n");
