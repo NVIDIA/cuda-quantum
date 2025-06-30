9bdf5a646a956233458757a1a9f18bf74d4ca1bf
Adam Geller <adam_geller@msn.com>
2025-06-30 23:40:08 +0000

    Split subcircuits into separate functions
    Signed-off-by: Adam Geller <adam_geller@msn.com>

--- lib/Optimizer/Transforms/PhasePolynomialPreprocess.cpp
+++ lib/Optimizer/Transforms/PhasePolynomialPreprocess.cpp
@@ -8,6 +8,7 @@
 
 #include "PassDetails.h"
 #include "Subcircuit.h"
+#include "cudaq/Optimizer/Builder/Factory.h"
 #include "cudaq/Optimizer/Dialect/CC/CCOps.h"
 #include "cudaq/Optimizer/Dialect/Quake/QuakeOps.h"
 #include "cudaq/Optimizer/Transforms/Passes.h"
@@ -28,8 +29,157 @@ class PhasePolynomialPreprocessPass
           PhasePolynomialPreprocessPass> {
   using PhasePolynomialPreprocessBase::PhasePolynomialPreprocessBase;
 
-  SetVector<Operation *> processed;
-  SmallVector<Subcircuit> subcircuits;
+  // void markRounds(Value wire, int round) {
+  //   for (auto &uses : wire.getUses()) {
+  //     auto op = uses.getOwner();
+  //     if (op->hasAttr("round") &&
+  //     op->getAttrOfType<IntegerAttr>("round").getInt())
+  //         continue;
+  //     op->setAttr("round", IntegerAttr::get(mlir::IntegerType::get(),
+  //     round)); for (auto result : op->getResults())
+  //       markRounds(wire, round + 1);
+  //   }
+  // }
+
+  class WireStepper {
+    Value old_wire;
+    Value new_wire;
+    Subcircuit *subcircuit;
+
+  public:
+    WireStepper(Subcircuit *circuit, Value initial, Value arg) {
+      subcircuit = circuit;
+      old_wire = initial;
+      new_wire = arg;
+    }
+
+    bool isStopped() {
+      return subcircuit->getTerminalWires().contains(old_wire);
+    }
+
+    Value getNewWire() { return new_wire; }
+
+    Value getOldWire() { return old_wire; }
+
+    void step(DenseMap<Operation *, Operation *> &cloned, OpBuilder &builder) {
+      if (isStopped())
+        return;
+
+      // TODO: Something more elegant here would be nice
+      Operation *op = nullptr;
+      auto opnum = -1;
+      for (auto &use : old_wire.getUses()) {
+        if (use.getOwner()->getBlock() == builder.getInsertionBlock())
+          continue;
+        op = use.getOwner();
+        opnum = use.getOperandNumber();
+      }
+
+      assert(op);
+
+      if (cloned.count(op) == 1) {
+        cloned[op]->setOperand(opnum, new_wire);
+        assert(old_wire.hasOneUse());
+        old_wire = getNextResult(old_wire);
+        new_wire = getNextResult(new_wire);
+        return;
+      }
+
+      // Make sure all dependencies have been cloned
+      for (auto dependency : op->getOperands()) {
+        if (!isa<quake::WireType>(dependency.getType()))
+          continue;
+        auto dop = dependency.getDefiningOp();
+        if (cloned.count(dop) != 1 &&
+            !subcircuit->getInitialWires().contains(dependency))
+          return;
+      }
+
+      auto clone = builder.clone(*op);
+      clone->setOperand(opnum, new_wire);
+
+      // For now, just copy over all classical constants
+      builder.setInsertionPointToStart(clone->getBlock());
+      for (size_t i = 0; i < clone->getNumOperands(); i++) {
+        auto dependency = clone->getOperand(i);
+        if (!isa<quake::WireType>(dependency.getType())) {
+          auto dop = dependency.getDefiningOp();
+          assert(isa<arith::ConstantOp>(dop));
+          auto clone_dop = builder.clone(*dop);
+          clone->setOperand(i, clone_dop->getResult(0));
+        }
+      }
+      builder.setInsertionPointAfter(clone);
+
+      cloned[op] = clone;
+      assert(old_wire.hasOneUse());
+      old_wire = getNextResult(old_wire);
+      new_wire = getNextResult(new_wire);
+    }
+  };
+
+  void removeOld(Subcircuit &subcircuit,
+                 SmallVector<Operation *> &removal_order, Operation *next) {
+    if (!subcircuit.getOps().contains(next) ||
+        std::find(removal_order.begin(), removal_order.end(), next) !=
+            removal_order.end())
+      return;
+
+    for (auto result : next->getResults())
+      for (auto *user : result.getUsers())
+        removeOld(subcircuit, removal_order, user);
+
+    removal_order.push_back(next);
+  }
+
+  void moveToFunc(Subcircuit *subcircuit, size_t subcircuit_num) {
+    auto module = getOperation();
+    SmallVector<Type> types(subcircuit->getInitialWires().size(),
+                            quake::WireType::get(module.getContext()));
+    auto name = std::string("subcircuit") + std::to_string(subcircuit_num);
+    auto fun = cudaq::opt::factory::createFunction(name, types, types, module);
+    fun.setPrivate();
+    auto entry = fun.addEntryBlock();
+    OpBuilder builder(fun);
+
+    DenseMap<Operation *, Operation *> cloned;
+
+    // Need to keep ordering to match returns with arguments
+    SmallVector<Value> wires_in;
+    SmallVector<WireStepper *> steppers;
+    for (auto wire : subcircuit->getInitialWires()) {
+      wires_in.push_back(wire);
+      steppers.push_back(
+          new WireStepper(subcircuit, wire, fun.getArgument(steppers.size())));
+    }
+
+    builder.setInsertionPointToStart(entry);
+    while (true) {
+      auto stepped = false;
+      for (auto stepper : steppers) {
+        if (!stepper->isStopped())
+          stepped = true;
+        stepper->step(cloned, builder);
+      }
+
+      if (!stepped)
+        break;
+    }
+
+    SmallVector<Value> new_wires;
+    for (size_t i = 0; i < steppers.size(); i++)
+      new_wires.push_back(steppers[i]->getNewWire());
+
+    builder.create<cudaq::cc::ReturnOp>(fun.getLoc(), new_wires);
+
+    auto cnot = subcircuit->getStart();
+
+    builder.setInsertionPointAfter(cnot);
+    auto call = builder.create<func::CallOp>(cnot->getLoc(), types,
+                                             fun.getSymNameAttr(), wires_in);
+    for (size_t i = 0; i < steppers.size(); i++)
+      steppers[i]->getOldWire().replaceAllUsesWith(call.getResult(i));
+  }
 
 public:
   // AXIS-SPECIFIC: could allow controlled y and z here
@@ -38,21 +188,31 @@ public:
   }
 
   void runOnOperation() override {
-    func::FuncOp func = getOperation();
+    auto module = getOperation();
+    size_t i = 0;
+    SetVector<Subcircuit *> subcircuits;
 
-    func.walk([&](Operation *op) {
-      if (!isControlledOp(op) || ::processed(op))
-        return;
+    for (auto &op : module) {
+      if (auto func = dyn_cast<func::FuncOp>(op)) {
+        // TODO: this is yucky, having to rewalk the function because we're
+        // mutating it as we go
+        func.walk([&](quake::XOp op) {
+          if (!isControlledOp(op) || ::processed(op))
+            return;
 
-      Subcircuit subcircuit(op);
-      subcircuits.push_back(subcircuit);
-    });
+          auto *subcircuit = new Subcircuit(op);
+          moveToFunc(subcircuit, i++);
+          subcircuits.insert(subcircuit);
+        });
+      }
+    }
 
-    for (auto subcircuit : subcircuits) {
-      llvm::outs() << "Calculated subcircuit: \n";
-      for (auto *op : subcircuit.getOps())
-        op->dump();
-      llvm::outs() << "\n";
+    for (auto *subcircuit : subcircuits) {
+      for (auto op : subcircuit->getOps()) {
+        op->dropAllUses();
+        op->erase();
+      }
+      delete subcircuit;
     }
   }
 };
