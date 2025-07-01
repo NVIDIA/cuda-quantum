7406c2ad3380794a8310890db996f0911ab5542c
Dobri Y <dyordanov@nvidia.com>
2025-07-01 05:02:15 -0700

    Fix CI
    Signed-off-by: Dobri Y <dyordanov@nvidia.com>

--- docs/sphinx/targets/python/quantum_machines_all_gates.py
+++ docs/sphinx/targets/python/quantum_machines_all_gates.py
@@ -1,14 +1,14 @@
 import cudaq
 
-
 # simplest example of quantum machines target run:
 # select a number of qubits (make sure you have this number of qubits available on your computer)
 # and run a Hadamard on each of them.
 # The default executor is mock, use executor name to run on another backend (real or simulator).
 # Configure the address of the QOperator server in the URL argument, and set the API key.
 cudaq.set_target("quantum_machines",
-                url="http://host.docker.internal:8080",
-                api_key="1234567890", executor="mock")
+                 url="http://host.docker.internal:8080",
+                 api_key="1234567890",
+                 executor="mock")
 
 qubit_count = 5
 
@@ -21,12 +21,12 @@ def gates():
     x(qvector[0])
     y(qvector[0])
     z(qvector[0])
-    rx(math.pi/2, qvector[1])
-    ry(math.pi/2, qvector[1])
-    rz(math.pi/2, qvector[1])
+    rx(math.pi / 2, qvector[1])
+    ry(math.pi / 2, qvector[1])
+    rz(math.pi / 2, qvector[1])
     s(qvector[2])
     t(qvector[3])
-    r1(math.pi/2, qvector[4])
+    r1(math.pi / 2, qvector[4])
 
 
 cudaq.sample(gates, shots_count=10000)
--- docs/sphinx/targets/python/quantum_machines_gate_decomposition.py
+++ docs/sphinx/targets/python/quantum_machines_gate_decomposition.py
@@ -1,12 +1,11 @@
 import cudaq
 import math
 
-
 # simplest example of quantum machines target run:
 # select a number of qubits (make sure you have this number of qubits available on your computer)
 # and run a Hadamard on each of them.
 # The default executor is mock, use executor name to run on another backend (real or simulator).
-# Configure the address of the QOperator server in the url argument, and set the api_key
+# Configure the address of the QOperator server in the `url` argument, and set the `api_key`.
 cudaq.set_target("quantum_machines",
                  url="http://host.docker.internal:8080",
                  api_key="1234567890",
@@ -20,12 +19,11 @@ def simplest():
     qvector = cudaq.qvector(qubit_count)
 
     reset(qvector)
-    # t.adj(qvector[0]) - not yet supported as tdg gate is not supported yet
+    # `t.adj(qvector[0])` - not yet supported as `tdg` gate is not supported yet
     x.ctrl(qvector[0], qvector[1])
     h.ctrl(qvector[1], qvector[2])
     rz.adj(math.pi, qvector[3])
     cr1(math.pi, qvector[3], qvector[4])
-
 
 
 cudaq.sample(simplest, shots_count=1000)
