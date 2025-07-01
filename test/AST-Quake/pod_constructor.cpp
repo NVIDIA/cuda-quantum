06bdc285e58adbda00c622581bfffe0f1c672aea
Dobri Yordanov <dyordanov@nvidia.com>
2025-07-01 15:00:37 -0400

    Update pod_constructor.cpp
    Signed-off-by: Dobri Yordanov <dyordanov@nvidia.com>

--- test/AST-Quake/pod_constructor.cpp
+++ test/AST-Quake/pod_constructor.cpp
@@ -16,12 +16,11 @@ struct simple_pod {
 };
 
 // Test POD default constructor - should just allocate memory
-// CHECK-LABEL:   func.func
-// @__nvqpp__mlirgen__function_test_pod_constructor._Z20test_pod_constructorv
+// CHECK-LABEL:   func.func @__nvqpp__mlirgen__function_test_pod_constructor._Z20test_pod_constructorv
 // CHECK:           () -> !cc.struct<"simple_pod" {i64, i64} [128,8]>
-// CHECK:           %[[ALLOC:.*]] = cc.alloca !cc.struct<"simple_pod" {i64, i64}
-// [128,8]> CHECK-NOT:       call @_ZN10simple_podC1Ev CHECK:           return
-// %[[RESULT:.*]] : !cc.struct<"simple_pod" {i64, i64} [128,8]>
+// CHECK:           %[[ALLOC:.*]] = cc.alloca !cc.struct<"simple_pod" {i64, i64} [128,8]>
+// CHECK-NOT:       call @_ZN10simple_podC1Ev
+// CHECK:           return %[[RESULT:.*]] : !cc.struct<"simple_pod" {i64, i64} [128,8]>
 simple_pod test_pod_constructor() __qpu__ {
   simple_pod result;
   result.a = 9;
