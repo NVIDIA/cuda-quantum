62704d1ff6a6657cea5eb87ece040b7cb67c9a8c
Dobri Y <dyordanov@nvidia.com>
2025-07-01 08:34:10 -0700

    Handle default POD constructors in Quake Bridge
    Signed-off-by: Dobri Y <dyordanov@nvidia.com>

--- lib/Frontend/nvqpp/ConvertExpr.cpp
+++ lib/Frontend/nvqpp/ConvertExpr.cpp
@@ -3154,6 +3154,13 @@ bool QuakeBridgeVisitor::VisitCXXConstructExpr(clang::CXXConstructExpr *x) {
     TODO_x(loc, x, mangler, "C++ constructor (non-default)");
   }
 
+  // Handle POD types with default constructors - just allocate memory
+  // No constructor call needed for POD types
+  if (parent->isPOD()) {
+    auto mem = builder.create<cc::AllocaOp>(loc, ctorTy);
+    return pushValue(mem);
+  }
+
   // A regular C++ class constructor lowers as:
   //
   // 1) A unique object must be created, so the type must have a minimum of
