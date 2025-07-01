0d94a8ecaff9674223ea33a72d91b93dd287d29b
Dobri Y <dyordanov@nvidia.com>
2025-07-01 11:46:48 -0700

    lint
    Signed-off-by: Dobri Y <dyordanov@nvidia.com>

--- test/AST-Quake/pod_constructor.cpp
+++ test/AST-Quake/pod_constructor.cpp
@@ -16,11 +16,12 @@ struct simple_pod {
 };
 
 // Test POD default constructor - should just allocate memory
-// CHECK-LABEL:   func.func @__nvqpp__mlirgen__function_test_pod_constructor._Z20test_pod_constructorv
+// CHECK-LABEL:   func.func
+// @__nvqpp__mlirgen__function_test_pod_constructor._Z20test_pod_constructorv
 // CHECK:           () -> !cc.struct<"simple_pod" {i64, i64} [128,8]>
-// CHECK:           %[[ALLOC:.*]] = cc.alloca !cc.struct<"simple_pod" {i64, i64} [128,8]>
-// CHECK-NOT:       call @_ZN10simple_podC1Ev
-// CHECK:           return %[[RESULT:.*]] : !cc.struct<"simple_pod" {i64, i64} [128,8]>
+// CHECK:           %[[ALLOC:.*]] = cc.alloca !cc.struct<"simple_pod" {i64, i64}
+// [128,8]> CHECK-NOT:       call @_ZN10simple_podC1Ev CHECK:           return
+// %[[RESULT:.*]] : !cc.struct<"simple_pod" {i64, i64} [128,8]>
 simple_pod test_pod_constructor() __qpu__ {
   simple_pod result;
   result.a = 9;
@@ -30,6 +31,6 @@ simple_pod test_pod_constructor() __qpu__ {
 
 int main() {
   auto result = cudaq::run(5, test_pod_constructor);
-  
+
   return 0;
-} 
+}
