0c3550d8647c343692409abb04321673309ab110
Sachin Pisal <spisal@nvidia.com>
2025-07-01 17:09:12 -0700

    Removing debug statement
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- python/cudaq/kernel/ast_bridge.py
+++ python/cudaq/kernel/ast_bridge.py
@@ -2140,7 +2140,6 @@ class PyASTBridge(ast.NodeVisitor):
                         f'Invalid cast to integer: {value.type}', node)
 
             elif node.func.id == 'list':
-                print("In list() call ...")
                 if len(self.valueStack) == 2:
                     maybeIterableSize = self.popValue()
                     maybeIterable = self.popValue()
