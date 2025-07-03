f0004186ffb1553006ced8f169f8f365c8403469
Sachin Pisal <spisal@nvidia.com>
2025-07-02 20:41:07 -0700

    Adding test for non-integer value for left shift operator
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- python/tests/kernel/test_run_kernel.py
+++ python/tests/kernel/test_run_kernel.py
@@ -1088,6 +1088,24 @@ def test_run_with_integer_left_shift_operator():
     assert results[1] == 0
 
 
+def test_run_with_non_integer_left_shift_operator():
+
+    @cudaq.kernel(verbose=True)
+    def kernel(n: int) -> int:
+        q = cudaq.qvector(n)
+        m = mz(q)
+        r = 0
+        for i in range(n):
+            r = r & (m[i] << 1.0)
+
+        return r
+
+    with pytest.raises(RuntimeError) as e:
+        results = cudaq.run(kernel, 3, shots_count=2)
+    assert 'unhandled BinOp.LShift types; only integers supported.' in str(
+        e.value)
+
+
 def test_run_with_integer_right_shift_operator():
 
     @cudaq.kernel(verbose=True)
