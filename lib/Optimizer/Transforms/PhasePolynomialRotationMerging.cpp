6e1aa1feb01229e534a12cf1a270422e1a09e5d7
Adam Geller <adam_geller@msn.com>
2025-07-04 18:55:05 +0000

    Initial working prototype of phase polynomial calculations and rotation merging
    Signed-off-by: Adam Geller <adam_geller@msn.com>

---
+++ lib/Optimizer/Transforms/PhasePolynomialRotationMerging.cpp
@@ -0,0 +1,286 @@
+/*******************************************************************************
+ * Copyright (c) 2025 NVIDIA Corporation & Affiliates.                         *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+#include "PassDetails.h"
+#include "Subcircuit.h"
+#include "cudaq/Optimizer/Builder/Factory.h"
+#include "cudaq/Optimizer/Dialect/CC/CCOps.h"
+#include "cudaq/Optimizer/Dialect/Quake/QuakeOps.h"
+#include "cudaq/Optimizer/Transforms/Passes.h"
+#include "mlir/Transforms/Passes.h"
+
+namespace cudaq::opt {
+#define GEN_PASS_DEF_PHASEPOLYNOMIALROTATIONMERGING
+#include "cudaq/Optimizer/Transforms/Passes.h.inc"
+} // namespace cudaq::opt
+
+#define DEBUG_TYPE "phase-polynomial-rotation-merging"
+
+using namespace mlir;
+
+namespace {
+class PhasePolynomialRotationMergingPass
+    : public cudaq::opt::impl::PhasePolynomialRotationMergingBase<
+          PhasePolynomialRotationMergingPass> {
+  using PhasePolynomialRotationMergingBase::PhasePolynomialRotationMergingBase;
+
+  struct PhaseVariable {
+  public:
+    size_t idx;
+    // TODO: do we really need the initial_wire here?
+    // I think it's just useful for debugging
+    Value initial_wire;
+    PhaseVariable(size_t index, Value wire) : idx(index), initial_wire(wire) {}
+
+    bool operator==(PhaseVariable other) { return idx == other.idx; }
+  };
+
+  class Phase {
+    SetVector<PhaseVariable *> vars;
+    bool isInverted;
+
+  public:
+    Phase() : isInverted(false) {}
+
+    Phase(PhaseVariable *var) : isInverted(false) { vars.insert(var); }
+
+    bool operator==(Phase other) {
+      for (auto var : vars)
+        if (!other.vars.contains(var))
+          return false;
+      for (auto var : other.vars)
+        if (!vars.contains(var))
+          return false;
+      return isInverted == other.isInverted;
+    }
+
+    static Phase *combine(Phase *p1, Phase *p2) {
+      Phase *new_phase = new Phase();
+      for (auto var : p1->vars)
+        new_phase->vars.insert(var);
+      for (auto var : p2->vars)
+        if (new_phase->vars.contains(var))
+          new_phase->vars.remove(var);
+        else
+          new_phase->vars.insert(var);
+      return new_phase;
+    }
+
+    static Phase *invert(Phase *p1) {
+      Phase *new_phase = new Phase();
+      for (auto var : p1->vars)
+        new_phase->vars.insert(var);
+      new_phase->isInverted = !p1->isInverted;
+      return new_phase;
+    }
+
+    void dump() {
+      llvm::outs() << "Phase: ";
+      if (isInverted)
+        llvm::outs() << "!";
+      llvm::outs() << "{";
+      auto first = true;
+      for (auto var : vars) {
+        if (!first)
+          llvm::outs() << " ^ ";
+        llvm::outs() << var->idx;
+        first = false;
+      }
+      llvm::outs() << "}\n";
+    }
+
+    std::optional<int64_t> getIntRepresentation() {
+      int64_t sum = 0;
+      for (auto var : vars) {
+        if (var->idx > sizeof(int64_t) - 1)
+          return std::nullopt;
+        sum += 1 << var->idx;
+      }
+    }
+  };
+
+  class PhaseStorage {
+    // TODO: If SmallVector, no need for pointers here, so make not
+    // pointer to avoid memory leaks
+    SmallVector<Phase *> phases;
+    SmallVector<quake::RzOp> rotations;
+
+    void combineRotations(size_t prev_idx, quake::RzOp rzop) {
+      auto old_rzop = rotations[prev_idx];
+      auto builder = OpBuilder(old_rzop);
+      auto rot_arg1 = old_rzop.getOperand(0);
+      auto rot_arg2 = rzop.getOperand(0);
+      auto new_rot_arg =
+          builder.create<arith::AddFOp>(old_rzop.getLoc(), rot_arg1, rot_arg2);
+      auto new_rot = builder.clone(*old_rzop.getOperation());
+      new_rot->setOperand(0, new_rot_arg.getResult());
+      old_rzop.getResult(0).replaceAllUsesWith(new_rot->getResult(0));
+      old_rzop.erase();
+      rzop.getResult(0).replaceAllUsesWith(rzop.getOperand(1));
+      rzop.erase();
+    }
+
+  public:
+    /// @brief registers a new rotation op for the given phase
+    /// @returns true if the rotation was combined, false otherwise
+    bool addOrCombineRotationForPhase(quake::RzOp op, Phase *phase) {
+      for (size_t i = 0; i < phases.size(); i++)
+        if (*phases[i] == *phase) {
+          combineRotations(i, op);
+          return true;
+        }
+
+      phases.push_back(phase);
+      rotations.push_back(op);
+      return false;
+    }
+  };
+
+  class PhaseStepper {
+    Value wire;
+    Subcircuit *subcircuit;
+    PhaseStorage *store;
+    Phase *current_phase;
+
+  public:
+    class StepperContainer {
+      SmallVector<PhaseStepper *> steppers;
+      PhaseStorage *store;
+      SetVector<PhaseVariable *> vars;
+
+      PhaseStepper *getStepperForValue(Value v) {
+        for (auto *stepper : steppers)
+          if (stepper->wire == v)
+            return stepper;
+        return nullptr;
+      }
+
+    public:
+      // Caller is responsible for cleaning up circuit
+      StepperContainer(Subcircuit *circuit) {
+        store = new PhaseStorage();
+        size_t i = 0;
+        for (auto wire : circuit->getInitialWires()) {
+          auto *new_var = new PhaseVariable(i++, wire);
+          // StepperContainer is responsible for cleaning up PhaseSteppers
+          steppers.push_back(new PhaseStepper(circuit, store, wire, new_var));
+          // StepperContainer is responsible for cleaning up PhaseVariables
+          vars.insert(new_var);
+        }
+      }
+
+      ~StepperContainer() {
+        delete store;
+        for (auto stepper : steppers)
+          delete stepper;
+        for (auto var : vars)
+          delete var;
+      }
+
+      bool isStopped() {
+        for (auto *stepper : steppers)
+          if (!stepper->isStopped())
+            return false;
+        return true;
+      }
+
+      void stepAll() {
+        if (isStopped())
+          return;
+        for (auto *stepper : steppers)
+          stepper->step(this);
+      }
+
+      std::optional<Phase *>
+      maybeGetControlPhase(quake::OperatorInterface opi) {
+        assert(isControlledOp(opi.getOperation()));
+        auto control = opi.getControls().front();
+        auto *stepper = getStepperForValue(control);
+        if (stepper)
+          return stepper->current_phase;
+        return std::nullopt;
+      }
+
+      bool targetVisited(quake::OperatorInterface opi) {
+        assert(isControlledOp(opi.getOperation()));
+        auto next_result = getNextResult(opi.getTarget(0));
+        // Wait until target wire stepper steps to ensure
+        // control phase is available
+        if (getStepperForValue(next_result))
+          return true;
+        return false;
+      }
+    };
+
+    PhaseStepper(Subcircuit *circuit, PhaseStorage *store, Value initial,
+                 PhaseVariable *var) {
+      subcircuit = circuit;
+      this->store = store;
+      wire = initial;
+      current_phase = new Phase(var);
+    }
+
+    bool isStopped() {
+      Operation *op = *wire.getUsers().begin();
+      assert(op);
+      return isTerminationPoint(op);
+    }
+
+    void step(StepperContainer *container) {
+      if (isStopped())
+        return;
+      assert(wire.hasOneUse());
+
+      Operation *op = *wire.getUsers().begin();
+      assert(op);
+      auto opi = dyn_cast<quake::OperatorInterface>(op);
+      assert(opi);
+
+      if (isControlledOp(op)) {
+        // Controlled not, and we are the target, so update phase
+        if (opi.getTarget(0) == wire) {
+          auto phase_opt = container->maybeGetControlPhase(opi);
+          // Wait until we have the phase for the other wire
+          if (!phase_opt.has_value())
+            return;
+          current_phase = Phase::combine(current_phase, phase_opt.value());
+        } else {
+          // Wait until the target has visited the operation so it can
+          // access our phase (the control phase)
+          if (!container->targetVisited(opi))
+            return;
+        }
+      } else if (isa<quake::XOp>(op) && opi.getControls().size() == 0) {
+        // Simple not, invert phase
+        // AXIS-SPECIFIC: Would want to handle y and z gates here too
+        current_phase = Phase::invert(current_phase);
+      } else if (auto rzop = dyn_cast<quake::RzOp>(op)) {
+        if (store->addOrCombineRotationForPhase(rzop, current_phase))
+          return;
+      }
+
+      wire = getNextResult(wire);
+    }
+  };
+
+public:
+  void runOnOperation() override {
+    auto func = getOperation();
+
+    if (!func.getOperation()->hasAttr("subcircuit"))
+      return;
+
+    auto subcircuit = new Subcircuit(func);
+    auto container = PhaseStepper::StepperContainer(subcircuit);
+
+    while (!container.isStopped())
+      container.stepAll();
+    delete subcircuit;
+  }
+};
+} // namespace
