7659447c670d9b2779ca6e7b9d5c3ad7e772f3b7
Adam Geller <adam_geller@msn.com>
2025-07-11 00:27:58 +0000

    Add opt pipeline and test using CircuitCheck
    Signed-off-by: Adam Geller <adam_geller@msn.com>

--- include/cudaq/Optimizer/Transforms/Passes.h
+++ include/cudaq/Optimizer/Transforms/Passes.h
@@ -25,6 +25,7 @@ void addAggressiveEarlyInlining(mlir::OpPassManager &pm,
                                 bool fatalCheck = false);
 void registerAggressiveEarlyInliningPipeline();
 
+void registerPhasePolynomialOptimizationPipeline();
 void registerUnrollingPipeline();
 void registerClassicalOptimizationPipeline();
 void registerMappingPipeline();
