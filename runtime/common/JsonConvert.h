94913a796330f48fdc943c1c66a8c8803bad0b91
Sachin Pisal <spisal@nvidia.com>
2025-07-17 10:44:04 -0700

    Adding step_size hyperparameter for adam and sgd algo
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- runtime/common/JsonConvert.h
+++ runtime/common/JsonConvert.h
@@ -295,6 +295,7 @@ inline void to_json(json &j, const cudaq::optimizers::adam &p) {
   TO_JSON_OPT_HELPER(beta1);
   TO_JSON_OPT_HELPER(beta2);
   TO_JSON_OPT_HELPER(eps);
+  TO_JSON_OPT_HELPER(step_size);
   to_json(j, dynamic_cast<const cudaq::optimizers::BaseEnsmallen &>(p));
 }
 
@@ -304,6 +305,7 @@ inline void to_json(json &j, const cudaq::optimizers::gradient_descent &p) {
 
 inline void to_json(json &j, const cudaq::optimizers::sgd &p) {
   TO_JSON_OPT_HELPER(batch_size);
+  TO_JSON_OPT_HELPER(step_size);
   to_json(j, dynamic_cast<const cudaq::optimizers::BaseEnsmallen &>(p));
 }
 
