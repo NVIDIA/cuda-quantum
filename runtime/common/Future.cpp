1503efb7e3e551e2bc1587907319608f92ac6432
Thien Nguyen <thiennguyen@nvidia.com>
2025-07-23 04:22:02 +0000

    Support bit register result parsing
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>

--- runtime/common/Future.cpp
+++ runtime/common/Future.cpp
@@ -24,7 +24,6 @@ sample_result future::get() {
   auto serverHelper = registry::get<ServerHelper>(qpuName);
   serverHelper->initialize(serverConfig);
   auto headers = serverHelper->getHeaders();
-  auto cookies = serverHelper->getCookies();
   std::vector<ExecutionResult> results;
   for (auto &id : jobs) {
     cudaq::info("Future retrieving results for {}.", id.first);
@@ -32,12 +31,14 @@ sample_result future::get() {
     auto jobGetPath = serverHelper->constructGetJobPath(id.first);
 
     cudaq::info("Future got job retrieval path as {}.", jobGetPath);
-    auto resultResponse = client.get(jobGetPath, "", headers, false, cookies);
+    auto resultResponse =
+        client.get(jobGetPath, "", headers, false, serverHelper->getCookies());
     while (!serverHelper->jobIsDone(resultResponse)) {
       auto polling_interval =
           serverHelper->nextResultPollingInterval(resultResponse);
       std::this_thread::sleep_for(polling_interval);
-      resultResponse = client.get(jobGetPath, "", headers, false, cookies);
+      resultResponse = client.get(jobGetPath, "", headers, false,
+                                  serverHelper->getCookies());
     }
     auto c = serverHelper->processResults(resultResponse, id.first);
 
