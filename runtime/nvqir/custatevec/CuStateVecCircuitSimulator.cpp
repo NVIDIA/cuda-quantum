ed50d4e14d4517c29251ad1b676091627a6be563
Thien Nguyen <thiennguyen@nvidia.com>
2025-08-12 06:36:17 +0000

    Accidentally committed file
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>

--- runtime/nvqir/custatevec/CuStateVecCircuitSimulator.cpp
+++ runtime/nvqir/custatevec/CuStateVecCircuitSimulator.cpp
@@ -430,7 +430,7 @@ public:
         handle, deviceStateVector, cuStateVecCudaDataType, nQubitsAllocated,
         &parity, basisBits, /*N Bits*/ 1, rand,
         CUSTATEVEC_COLLAPSE_NORMALIZE_AND_ZERO));
-    CUDAQ_INFO("Measured qubit {} -> {} (random number = {})", qubitIdx, parity, rand);
+    CUDAQ_INFO("Measured qubit {} -> {}", qubitIdx, parity);
     return parity == 1 ? true : false;
   }
 
