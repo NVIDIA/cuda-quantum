0972709a01c49b230fbac42c23e2a218686fe2ee
Eric Schweitz <eschweitz@nvidia.com>
2025-08-22 23:32:05 -0700

    Follow up work from #3223. (#3365)
    * Follow up work from #3223.
    
    See also #3312.
    
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>
    
    * Modify the mock servers to NOT use an environment variable.
    
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>
    
    * Spelling check dodge.
    
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>
    
    * Changes to make codegen-emission work with the new standardized
    transport triple instead of using its own one-off string encoding
    scheme.
    
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>
    
    * Add work to dictionary.
    
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>
    
    ---------
    
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>
    Signed-off-by: ikkoham <ikkoham@users.noreply.github.com>

--- include/cudaq/Optimizer/CodeGen/Peephole.h
+++ include/cudaq/Optimizer/CodeGen/Peephole.h
@@ -56,14 +56,13 @@ inline mlir::Value createMeasureCall(mlir::PatternRewriter &builder,
 }
 
 inline mlir::Value createReadResultCall(mlir::PatternRewriter &builder,
-                                        mlir::Location loc, mlir::Value result,
-                                        bool qirVersionUnderDevelopment) {
-
+                                        mlir::Location loc,
+                                        mlir::Value result) {
+  // NB: This code is only used from a deprecated pass.
   auto i1Ty = mlir::IntegerType::get(builder.getContext(), 1);
   return builder
-      .create<mlir::LLVM::CallOp>(
-          loc, mlir::TypeRange{i1Ty},
-          cudaq::opt::getQIRReadResultBody(qirVersionUnderDevelopment),
-          mlir::ArrayRef<mlir::Value>{result})
+      .create<mlir::LLVM::CallOp>(loc, mlir::TypeRange{i1Ty},
+                                  cudaq::opt::qir0_1::ReadResultBody,
+                                  mlir::ArrayRef<mlir::Value>{result})
       .getResult();
 }
