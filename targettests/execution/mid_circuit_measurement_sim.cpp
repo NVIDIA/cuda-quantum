be5f541a9989148a3d8858423195d2fc563b66da
Eric Schweitz <eschweitz@nvidia.com>
2025-09-03 14:04:47 -0700

    Parallelize mid_circuit_measurement tests. (#3415)
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>

---
+++ targettests/execution/mid_circuit_measurement_sim.cpp
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates and Contributors. *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+// Simulators
+// RUN: nvq++ %cpp_std --enable-mlir  %s -o %t && %t | FileCheck %s
+// RUN: nvq++ %cpp_std --library-mode %s -o %t && %t | FileCheck %s
+
+#include "mid_circuit_measurement.inc"
+
+// CHECK: 1
+// CHECK: 1
+// CHECK: 1
+// CHECK: 1
+
+// CHECK: 1
+// CHECK: 1
+// CHECK: 1
+// CHECK: 1
+
+// CHECK: done
