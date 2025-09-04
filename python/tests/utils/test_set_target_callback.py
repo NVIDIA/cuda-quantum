307af819e643b9d67056c5bde6a6ec92105f9bee
Thien Nguyen <58006629+1tnguyen@users.noreply.github.com>
2025-09-04 17:18:43 +1000

    [Python] Add utility to register callbacks for Python target changes  (#3419)
    * Callback mechanism for set_target
    
    * Code review: add mutex lock
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    * Fix clang build
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    ---------
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>

---
+++ python/tests/utils/test_set_target_callback.py
@@ -0,0 +1,74 @@
+# ============================================================================ #
+# Copyright (c) 2022 - 2025 NVIDIA Corporation & Affiliates.                   #
+# All rights reserved.                                                         #
+#                                                                              #
+# This source code and the accompanying materials are made available under     #
+# the terms of the Apache License 2.0 which accompanies this distribution.     #
+# ============================================================================ #
+
+import cudaq
+import os
+import pytest
+
+
+def test_register_callback():
+    cudaq.set_target("density-matrix-cpu")
+
+    sim_name = ""  # to be updated by the callback
+
+    def callback(target):
+        nonlocal sim_name
+        sim_name = target.name
+
+    cudaq.register_set_target_callback(callback, "my_callback")
+    assert sim_name == "density-matrix-cpu"
+
+    # Register another one
+    called = False
+
+    def another_callback(target):
+        nonlocal called
+        called = True
+
+    cudaq.register_set_target_callback(another_callback, "another_callback")
+    assert called
+
+    called = False
+    sim_name = ""
+
+    # Change target
+    cudaq.set_target("stim")
+    assert sim_name == "stim"
+    assert called
+
+    called = False
+    sim_name = ""
+
+    # Unregister
+    cudaq.unregister_set_target_callback("another_callback")
+    cudaq.set_target("density-matrix-cpu")
+    assert sim_name == "density-matrix-cpu"
+    assert not called
+
+    # Reset target also triggers callback
+    cudaq.reset_target()
+    assert sim_name == "nvidia" or sim_name == "qpp-cpu"
+
+    # Check other target info
+    is_remote = None
+
+    def yet_another_callback(target):
+        nonlocal is_remote
+        is_remote = target.is_remote()
+
+    cudaq.set_target("quantinuum", emulated=True)
+    cudaq.register_set_target_callback(yet_another_callback,
+                                       "yet_another_callback")
+    assert is_remote
+    assert sim_name == "quantinuum"
+
+
+# leave for gdb debugging
+if __name__ == "__main__":
+    loc = os.path.abspath(__file__)
+    pytest.main([loc, "-rP"])
