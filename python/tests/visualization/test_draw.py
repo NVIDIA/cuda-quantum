3221e3c2f411ee802c57465ef1710d4abb3a671c
Pradnya Khalate <pkhalate@nvidia.com>
2025-09-10 16:28:14 -0700

    * Refactor `uccsd` tests by creating a copy / "fresh" version of the UCCSD module that bypasses kernel registry dependencies, so as to ensure reliable test execution.
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>

--- python/tests/visualization/test_draw.py
+++ python/tests/visualization/test_draw.py
@@ -6,9 +6,10 @@
 # the terms of the Apache License 2.0 which accompanies this distribution.     #
 # ============================================================================ #
 
+import os
+
 import cudaq
 import numpy as np
-import os
 import pytest
 
 
