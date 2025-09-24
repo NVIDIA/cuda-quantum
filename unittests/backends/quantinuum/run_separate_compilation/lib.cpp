63d62ece6d56a2ebf107d4a3cfb493bf73a4dd51
Anna Gringauze <agringauze@nvidia.com>
2025-09-23 19:07:32 -0700

    Remove configuration environment variables (#3406)
    * Remove stale update to tpls/Stim
    * Remove environment variables and add codegen configuration
    
    Signed-off-by: Anna Gringauze <agringauze@nvidia.com>
    
    * Retrieve target config
    * Remove usage of env variable
    * Try fixing CI tests
    * Fixes for CI tests
    * Fixes for Python tests
    * Fixing the last failing test
    * Add more tests for coverage of codegen w/ output log
    * Fix doc build
    * Add more tests
    * Remove custom compilation mode for cudaq::run
    * Debug the installer CI: exception handling with static linking
    * Address code review comments
    
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    
    ---------
    
    Signed-off-by: Anna Gringauze <agringauze@nvidia.com>
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>
    Co-authored-by: Thien Nguyen <thiennguyen@nvidia.com>
    Co-authored-by: Thien Nguyen <58006629+1tnguyen@users.noreply.github.com>
    Signed-off-by: ikkoham <ikkoham@users.noreply.github.com>

--- unittests/backends/quantinuum/run_separate_compilation/lib.cpp
+++ unittests/backends/quantinuum/run_separate_compilation/lib.cpp
@@ -6,22 +6,30 @@
  * the terms of the Apache License 2.0 which accompanies this distribution.    *
  ******************************************************************************/
 
-#include "RuntimeTarget.h"
-#include <sstream>
+#include <cstdio>
+#include <cudaq.h>
 
-namespace cudaq {
-
-simulation_precision RuntimeTarget::get_precision() const { return precision; }
-
-std::string RuntimeTarget::get_target_args_help_string() const {
-  std::stringstream ss;
-  for (const auto &argConfig : config.TargetArguments) {
-    ss << "  - " << argConfig.KeyName;
-    if (!argConfig.HelpString.empty()) {
-      ss << " (" << argConfig.HelpString << ")";
-    }
-    ss << "\n";
+__qpu__ void entangle_all(cudaq::qview<> qubits) {
+  h(qubits[0]);
+  for (std::size_t i = 1; i < qubits.size(); i++) {
+    x<cudaq::ctrl>(qubits[0], qubits[i]);
   }
-  return ss.str();
 }
-} // namespace cudaq
+
+// Define a quantum kernel that returns an integer
+__qpu__ int run_ghz(int num_qubits) {
+  // Allocate qubits
+  cudaq::qvector qubits(num_qubits);
+
+  // Call kernel
+  entangle_all(qubits);
+
+  // Measure and return total number of qubits in state |1⟩
+  int result = 0;
+  for (int i = 0; i < num_qubits; i++) {
+    if (mz(qubits[i])) {
+      result += 1;
+    }
+  }
+  return result;
+}
