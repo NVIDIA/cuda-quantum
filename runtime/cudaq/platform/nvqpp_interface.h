ac6ca134177042c69d9f11fdc19da3dbd0ef2654
Eric Schweitz <eschweitz@nvidia.com>
2025-10-06 10:22:47 -0700

    Remove declarations needed for reworked runtime. (Will be added later.)
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>

--- runtime/cudaq/platform/nvqpp_interface.h
+++ runtime/cudaq/platform/nvqpp_interface.h
@@ -36,39 +36,5 @@ streamlinedLaunchKernel(const char *kernelName,
 hybridLaunchKernel(const char *kernelName, KernelThunkType kernel, void *args,
                    std::uint64_t argsSize, std::uint64_t resultOffset,
                    const std::vector<void *> &rawArgs);
-
-//===----------------------------------------------------------------------===//
-// Launch module entry points.
-//
-// In some environments (e.g., Python), the ModuleOp of the source can be
-// provided immediately to be launched, unlike with statically compiled systems
-// (C++). These entry points allow the managed runtime to provide the ModuleOp
-// directly.
-//===----------------------------------------------------------------------===//
-
-// Client-server interface. The caller must provide an mlir::ModuleOp and the
-// exact name of the entry point kernel function to be called, which is
-// typically the .thunk unmarshalling function. Passing short names is
-// considered incorrect.
-[[nodiscard]] KernelThunkResultType
-altLaunchModule(const char *exactEntryPointName, void *moduleOp, void *args,
-                std::uint64_t argsSize, std::uint64_t resultOffset);
-
-// Streamlined interface for launching kernels. Argument synthesis and JIT
-// compilation *must* happen on the local machine. The caller must provide an
-// mlir::ModuleOp and the exact name of the entry point kernel function to be
-// called,
-[[nodiscard]] KernelThunkResultType
-streamlinedLaunchModule(const char *exactEntryPointName, void *moduleOp,
-                        const std::vector<void *> &rawArgs);
-
-// Hybrid of the client-server and streamlined approaches. Letting JIT
-// compilation happen either early or late and can handle return values from
-// each kernel launch. The caller must provide an mlir::ModuleOp and the exact
-// name of the entry point kernel function to be called,
-[[nodiscard]] KernelThunkResultType
-hybridLaunchModule(const char *exactEntryPointName, void *moduleOp, void *args,
-                   std::uint64_t argsSize, std::uint64_t resultOffset,
-                   const std::vector<void *> &rawArgs);
 } // extern "C"
 } // namespace cudaq
