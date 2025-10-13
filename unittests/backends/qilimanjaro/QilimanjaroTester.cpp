475c79fc6fd0aca96d52bc4658d06c1a8f1eb335
Vyron Vasileiadis <hi@fedonman.com>
2025-10-13 09:12:58 +0200

    update QilimanjaroTester

--- unittests/backends/qilimanjaro/QilimanjaroTester.cpp
+++ unittests/backends/qilimanjaro/QilimanjaroTester.cpp
@@ -65,12 +65,38 @@ const std::string sampleTimeEvolutionPayload = R"(
 )";
 
 CUDAQ_TEST(QilimanjaroTester, checkTimeEvolutionJson) {
+    // Define Hamiltonians
     cudaq::spin_op Hinit = 0.8 * cudaq::spin::x(0) + 0.6 * cudaq::spin::x(1) + 0.4 * cudaq::spin::x(0) * cudaq::spin::x(1);
     cudaq::spin_op Hinter = 1.0 * cudaq::spin::z(0) + 0.9 * cudaq::spin::z(1) + 0.5 * cudaq::spin::z(0) * cudaq::spin::z(1);
     cudaq::spin_op Hfinal = 1.2 * cudaq::spin::z(0) + 1.0 * cudaq::spin::z(1) + 0.7 * cudaq::spin::z(0) * cudaq::spin::z(1);
     
-    std::vector<std::complex<double>> psi_data = {
+    // Define initial state
+    std::vector<std::complex<double>> initial_state_data = {
         {0.7071, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.7071, 0.0}
     };
-    cudaq::state psi = cudaq::state::from_data(psi_data);
+    cudaq::state initial_state = cudaq::state::from_data(initial_state_data);
+
+    // Define observables
+    std::vector<cudaq::spin_op> observables = {
+        1.0 * cudaq::spin::z(0) + 0.7 * cudaq::spin::z(1) + 0.6 * cudaq::spin::z(0) * cudaq::spin::z(1),
+        0.9 * cudaq::spin::x(0) + 0.7 * cudaq::spin::x(1) + 0.5 * cudaq::spin::x(0) * cudaq::spin::x(1)
+    };
+
+    // Define schedules (Is there any cudaq class?)
+    std::vector<std::map<std::string, std::vector<double>>> schedules = {
+        {
+            {"Hinit", {1.0, 0.75, 0.5, 0.25, 0.0}},
+            {"Hinter", {0.0, 0.25, 0.5, 0.75, 1.0}},
+            {"Hfinal", {0.0, 0.0, 0.0, 0.0, 0.0}}
+        },
+        {
+            {"Hinit",  {0.0, 0.0, 0.0, 0.0, 0.0}},
+            {"Hinter", {1.0, 0.75, 0.5, 0.25, 0.0}},
+            {"Hfinal", {0.0, 0.25, 0.5, 0.75, 1.0}}
+        }
+    };
+
+    // TODO: How will we construct the payload and which is the class we should parse into?
+
+    auto refPayload = nlohmann::json::parse(sampleTimeEvolutionPayload);
 }
