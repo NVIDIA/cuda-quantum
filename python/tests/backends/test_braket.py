008e6d5ec942846841ce4e259e8d96e62f03ab6f
Thien Nguyen <thiennguyen@nvidia.com>
2025-10-27 01:29:32 +0000

    Another round of audit
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>

--- python/tests/backends/test_braket.py
+++ python/tests/backends/test_braket.py
@@ -433,7 +433,8 @@ def test_state_synthesis_from_simulator():
         mz(qubits)
 
     state = cudaq.State.from_data(
-        np.array([1. / np.sqrt(2.), 1. / np.sqrt(2.), 0., 0.], dtype=complex))
+        np.array([1. / np.sqrt(2.), 1. / np.sqrt(2.), 0., 0.],
+                 dtype=cudaq.complex()))
 
     counts = cudaq.sample(kernel, state)
     assert "00" in counts
