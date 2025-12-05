be811fc76e90daf621c723456ad4ca9a5f210b1b
Eric Schweitz <eschweitz@nvidia.com>
2025-12-05 13:35:01 -0800

    Eliminate tests that were using state for other than local simulation.
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>

--- targettests/execution/qvector_init_from_state.cpp
+++ targettests/execution/qvector_init_from_state.cpp
@@ -6,20 +6,9 @@
  * the terms of the Apache License 2.0 which accompanies this distribution.    *
  ******************************************************************************/
 
-// clang-format off
 // Simulators
-// RUN: nvq++ %cpp_std --enable-mlir  %s -o %t && %t | FileCheck %s
-// RUN: nvq++ %cpp_std --library-mode %s -o %t && %t | FileCheck %s
-
-// Quantum emulators
-// RUN: nvq++ %cpp_std --target quantinuum --emulate %s -o %t && %t | FileCheck %s
-// RUN: nvq++ %cpp_std --target ionq       --emulate %s -o %t && %t | FileCheck %s
-// RUN: nvq++ %cpp_std --target anyon      --emulate %s -o %t && %t | FileCheck %s
-// RUN: nvq++ %cpp_std --target iqm        --emulate %s -o %t && IQM_QPU_QA=%iqm_tests_dir/Crystal_5.txt %t | FileCheck %s
-// RUN: nvq++ %cpp_std --target oqc        --emulate %s -o %t && %t | FileCheck %s
-// RUN: if %braket_avail; then nvq++ %cpp_std --target braket --emulate %s -o %t && %t | FileCheck %s; fi
-// RUN: if %qci_avail; then nvq++ %cpp_std --target qci --emulate %s -o %t && %t | FileCheck %s; fi
-// clang-format on
+// RUN: nvq++ --enable-mlir  %s -o %t && %t | FileCheck %s
+// RUN: nvq++ --library-mode %s -o %t && %t | FileCheck %s
 
 #include <cudaq.h>
 #include <iostream>
