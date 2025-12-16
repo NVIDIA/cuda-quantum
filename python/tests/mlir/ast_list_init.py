0161fa6a9a190ced25229919d358735a325d655c
Eric Schweitz <eschweitz@nvidia.com>
2025-12-16 14:49:23 -0800

    More fixes due to new loop lowering.
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>

--- python/tests/mlir/ast_list_init.py
+++ python/tests/mlir/ast_list_init.py
@@ -27,39 +27,41 @@ def test_list_init():
 
 # CHECK-LABEL:   func.func @__nvqpp__mlirgen__kernel..
 # CHECK-SAME: () attributes {"cudaq-entrypoint", "cudaq-kernel"} {
-# CHECK-DAG:           %[[VAL_0:.*]] = arith.constant 1 : i64
-# CHECK-DAG:           %[[VAL_1:.*]] = arith.constant 0 : i64
-# CHECK-DAG:           %[[VAL_2:.*]] = arith.constant 4 : i64
-# CHECK-DAG:           %[[VAL_3:.*]] = arith.constant 4.000000e+00 : f64
-# CHECK-DAG:           %[[VAL_4:.*]] = arith.constant 3.000000e+00 : f64
-# CHECK-DAG:           %[[VAL_5:.*]] = arith.constant 2.000000e+00 : f64
-# CHECK-DAG:           %[[VAL_6:.*]] = arith.constant 1.000000e+00 : f64
-# CHECK-DAG:           %[[VAL_7:.*]] = quake.alloca !quake.veq<6>
-# CHECK-DAG:           %[[VAL_8:.*]] = cc.alloca !cc.array<f64 x 4>
-# CHECK:           %[[VAL_9:.*]] = cc.cast %[[VAL_8]] : (!cc.ptr<!cc.array<f64 x 4>>) -> !cc.ptr<f64>
-# CHECK:           cc.store %[[VAL_6]], %[[VAL_9]] : !cc.ptr<f64>
-# CHECK:           %[[VAL_10:.*]] = cc.compute_ptr %[[VAL_8]][1] : (!cc.ptr<!cc.array<f64 x 4>>) -> !cc.ptr<f64>
-# CHECK:           cc.store %[[VAL_5]], %[[VAL_10]] : !cc.ptr<f64>
-# CHECK:           %[[VAL_11:.*]] = cc.compute_ptr %[[VAL_8]][2] : (!cc.ptr<!cc.array<f64 x 4>>) -> !cc.ptr<f64>
-# CHECK:           cc.store %[[VAL_4]], %[[VAL_11]] : !cc.ptr<f64>
-# CHECK:           %[[VAL_12:.*]] = cc.compute_ptr %[[VAL_8]][3] : (!cc.ptr<!cc.array<f64 x 4>>) -> !cc.ptr<f64>
-# CHECK:           cc.store %[[VAL_3]], %[[VAL_12]] : !cc.ptr<f64>
-# CHECK:           %[[VAL_13:.*]] = cc.loop while ((%[[VAL_14:.*]] = %[[VAL_1]]) -> (i64)) {
-# CHECK:             %[[VAL_15:.*]] = arith.cmpi slt, %[[VAL_14]], %[[VAL_2]] : i64
-# CHECK:             cc.condition %[[VAL_15]](%[[VAL_14]] : i64)
+# CHECK-DAG:       %[[VAL_0:.*]] = arith.constant 1 : i64
+# CHECK-DAG:       %[[VAL_1:.*]] = arith.constant 0 : i64
+# CHECK-DAG:       %[[VAL_2:.*]] = arith.constant 4 : i64
+# CHECK-DAG:       %[[VAL_3:.*]] = arith.constant 4.000000e+00 : f64
+# CHECK-DAG:       %[[VAL_4:.*]] = arith.constant 3.000000e+00 : f64
+# CHECK-DAG:       %[[VAL_5:.*]] = arith.constant 2.000000e+00 : f64
+# CHECK-DAG:       %[[VAL_6:.*]] = arith.constant 1.000000e+00 : f64
+# CHECK-DAG:       %[[VAL_7:.*]] = cc.undef f64
+# CHECK-DAG:       %[[VAL_8:.*]] = cc.undef i64
+# CHECK-DAG:       %[[VAL_9:.*]] = quake.alloca !quake.veq<6>
+# CHECK-DAG:       %[[VAL_10:.*]] = cc.alloca !cc.array<f64 x 4>
+# CHECK:           %[[VAL_11:.*]] = cc.cast %[[VAL_10]] : (!cc.ptr<!cc.array<f64 x 4>>) -> !cc.ptr<f64>
+# CHECK:           cc.store %[[VAL_6]], %[[VAL_11]] : !cc.ptr<f64>
+# CHECK:           %[[VAL_12:.*]] = cc.compute_ptr %[[VAL_10]][1] : (!cc.ptr<!cc.array<f64 x 4>>) -> !cc.ptr<f64>
+# CHECK:           cc.store %[[VAL_5]], %[[VAL_12]] : !cc.ptr<f64>
+# CHECK:           %[[VAL_13:.*]] = cc.compute_ptr %[[VAL_10]][2] : (!cc.ptr<!cc.array<f64 x 4>>) -> !cc.ptr<f64>
+# CHECK:           cc.store %[[VAL_4]], %[[VAL_13]] : !cc.ptr<f64>
+# CHECK:           %[[VAL_14:.*]] = cc.compute_ptr %[[VAL_10]][3] : (!cc.ptr<!cc.array<f64 x 4>>) -> !cc.ptr<f64>
+# CHECK:           cc.store %[[VAL_3]], %[[VAL_14]] : !cc.ptr<f64>
+# CHECK:           %[[VAL_15:.*]]:3 = cc.loop while ((%[[VAL_16:.*]] = %[[VAL_1]], %[[VAL_17:.*]] = %[[VAL_8]], %[[VAL_18:.*]] = %[[VAL_7]]) -> (i64, i64, f64)) {
+# CHECK:             %[[VAL_19:.*]] = arith.cmpi slt, %[[VAL_16]], %[[VAL_2]] : i64
+# CHECK:             cc.condition %[[VAL_19]](%[[VAL_16]], %[[VAL_17]], %[[VAL_18]] : i64, i64, f64)
 # CHECK:           } do {
-# CHECK:           ^bb0(%[[VAL_16:.*]]: i64):
-# CHECK:             %[[VAL_17:.*]] = cc.compute_ptr %[[VAL_8]]{{\[}}%[[VAL_16]]] : (!cc.ptr<!cc.array<f64 x 4>>, i64) -> !cc.ptr<f64>
-# CHECK:             %[[VAL_18:.*]] = cc.load %[[VAL_17]] : !cc.ptr<f64>
-# CHECK:             %[[VAL_19:.*]] = quake.extract_ref %[[VAL_7]]{{\[}}%[[VAL_16]]] : (!quake.veq<6>, i64) -> !quake.ref
-# CHECK:             quake.ry (%[[VAL_18]]) %[[VAL_19]] : (f64, !quake.ref) -> ()
-# CHECK:             cc.continue %[[VAL_16]] : i64
+# CHECK:           ^bb0(%[[VAL_20:.*]]: i64, %[[VAL_21:.*]]: i64, %[[VAL_22:.*]]: f64):
+# CHECK:             %[[VAL_23:.*]] = cc.compute_ptr %[[VAL_10]]{{\[}}%[[VAL_20]]] : (!cc.ptr<!cc.array<f64 x 4>>, i64) -> !cc.ptr<f64>
+# CHECK:             %[[VAL_24:.*]] = cc.load %[[VAL_23]] : !cc.ptr<f64>
+# CHECK:             %[[VAL_25:.*]] = quake.extract_ref %[[VAL_9]]{{\[}}%[[VAL_20]]] : (!quake.veq<6>, i64) -> !quake.ref
+# CHECK:             quake.ry (%[[VAL_24]]) %[[VAL_25]] : (f64, !quake.ref) -> ()
+# CHECK:             cc.continue %[[VAL_20]], %[[VAL_20]], %[[VAL_24]] : i64, i64, f64
 # CHECK:           } step {
-# CHECK:           ^bb0(%[[VAL_20:.*]]: i64):
-# CHECK:             %[[VAL_21:.*]] = arith.addi %[[VAL_20]], %[[VAL_0]] : i64
-# CHECK:             cc.continue %[[VAL_21]] : i64
+# CHECK:           ^bb0(%[[VAL_26:.*]]: i64, %[[VAL_27:.*]]: i64, %[[VAL_28:.*]]: f64):
+# CHECK:             %[[VAL_29:.*]] = arith.addi %[[VAL_26]], %[[VAL_0]] : i64
+# CHECK:             cc.continue %[[VAL_29]], %[[VAL_27]], %[[VAL_28]] : i64, i64, f64
 # CHECK:           }
-# CHECK:           quake.dealloc %[[VAL_7]] : !quake.veq<6>
+# CHECK:           quake.dealloc %[[VAL_9]] : !quake.veq<6>
 # CHECK:           return
 # CHECK:         }
 
