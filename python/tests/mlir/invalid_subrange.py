ad050eb6e4d6f96d59a64b902c0a81644f05a6b6
Eric Schweitz <eschweitz@nvidia.com>
2025-12-16 15:49:18 -0800

    Fix another raft of tests for which the loop changes broke the tests.
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>

--- python/tests/mlir/invalid_subrange.py
+++ python/tests/mlir/invalid_subrange.py
@@ -26,45 +26,46 @@ def test_banjo():
                 x.ctrl([ancilla, *qubits[0:i]], qubits[i])
 
     shots = 10000
-    print('sample bar:')
-    x = cudaq.sample(bar, shots_count=shots)
-    print(x)
     print(bar)
+    x = cudaq.sample(bar, shots_count=shots)
+    print('sample bar:')
+    print(x)
 
 
-# CHECK-LABEL: sample bar:
-# CHECK: { 00000:10000 }
-
-# CHECK-LABEL:   func.func @__nvqpp__mlirgen__bar
+# CHECK-LABEL:   func.func @__nvqpp__mlirgen__bar..
 # CHECK-SAME: () attributes {"cudaq-entrypoint", "cudaq-kernel"} {
 # CHECK-DAG:       %[[VAL_0:.*]] = arith.constant 4 : i64
 # CHECK-DAG:       %[[VAL_1:.*]] = arith.constant 1 : i64
 # CHECK-DAG:       %[[VAL_2:.*]] = arith.constant 0 : i64
-# CHECK-DAG:       %[[VAL_3:.*]] = quake.alloca !quake.ref
-# CHECK-DAG:       %[[VAL_4:.*]] = quake.alloca !quake.veq<4>
-# CHECK:           %[[VAL_5:.*]] = cc.loop while ((%[[VAL_6:.*]] = %[[VAL_2]]) -> (i64)) {
-# CHECK:             %[[VAL_7:.*]] = arith.cmpi slt, %[[VAL_6]], %[[VAL_0]] : i64
-# CHECK:             cc.condition %[[VAL_7]](%[[VAL_6]] : i64)
+# CHECK-DAG:       %[[VAL_3:.*]] = cc.undef i64
+# CHECK-DAG:       %[[VAL_4:.*]] = quake.alloca !quake.ref
+# CHECK-DAG:       %[[VAL_5:.*]] = quake.alloca !quake.veq<4>
+# CHECK:           %[[VAL_6:.*]]:2 = cc.loop while ((%[[VAL_7:.*]] = %[[VAL_2]], %[[VAL_8:.*]] = %[[VAL_3]]) -> (i64, i64)) {
+# CHECK:             %[[VAL_9:.*]] = arith.cmpi slt, %[[VAL_7]], %[[VAL_0]] : i64
+# CHECK:             cc.condition %[[VAL_9]](%[[VAL_7]], %[[VAL_8]] : i64, i64)
 # CHECK:           } do {
-# CHECK:           ^bb0(%[[VAL_8:.*]]: i64):
-# CHECK:             %[[VAL_9:.*]] = arith.cmpi eq, %[[VAL_8]], %[[VAL_2]] : i64
-# CHECK:             cc.if(%[[VAL_9]]) {
-# CHECK:               %[[VAL_10:.*]] = quake.extract_ref %[[VAL_4]][0] : (!quake.veq<4>) -> !quake.ref
-# CHECK:               quake.x {{\[}}%[[VAL_3]]] %[[VAL_10]] : (!quake.ref, !quake.ref) -> ()
+# CHECK:           ^bb0(%[[VAL_10:.*]]: i64, %[[VAL_11:.*]]: i64):
+# CHECK:             %[[VAL_12:.*]] = arith.cmpi eq, %[[VAL_10]], %[[VAL_2]] : i64
+# CHECK:             cc.if(%[[VAL_12]]) {
+# CHECK:               %[[VAL_13:.*]] = quake.extract_ref %[[VAL_5]][0] : (!quake.veq<4>) -> !quake.ref
+# CHECK:               quake.x {{\[}}%[[VAL_4]]] %[[VAL_13]] : (!quake.ref, !quake.ref) -> ()
 # CHECK:             } else {
-# CHECK:               %[[VAL_11:.*]] = arith.subi %[[VAL_8]], %[[VAL_1]] : i64
-# CHECK:               %[[VAL_12:.*]] = quake.subveq %[[VAL_4]], 0, %[[VAL_11]] : (!quake.veq<4>, i64) -> !quake.veq<?>
-# CHECK:               %[[VAL_13:.*]] = quake.concat %[[VAL_3]], %[[VAL_12]] : (!quake.ref, !quake.veq<?>) -> !quake.veq<?>
-# CHECK:               %[[VAL_14:.*]] = quake.extract_ref %[[VAL_4]]{{\[}}%[[VAL_8]]] : (!quake.veq<4>, i64) -> !quake.ref
-# CHECK:               quake.x {{\[}}%[[VAL_13]]] %[[VAL_14]] : (!quake.veq<?>, !quake.ref) -> ()
+# CHECK:               %[[VAL_14:.*]] = arith.subi %[[VAL_10]], %[[VAL_1]] : i64
+# CHECK:               %[[VAL_15:.*]] = quake.subveq %[[VAL_5]], 0, %[[VAL_14]] : (!quake.veq<4>, i64) -> !quake.veq<?>
+# CHECK:               %[[VAL_16:.*]] = quake.concat %[[VAL_4]], %[[VAL_15]] : (!quake.ref, !quake.veq<?>) -> !quake.veq<?>
+# CHECK:               %[[VAL_17:.*]] = quake.extract_ref %[[VAL_5]]{{\[}}%[[VAL_10]]] : (!quake.veq<4>, i64) -> !quake.ref
+# CHECK:               quake.x {{\[}}%[[VAL_16]]] %[[VAL_17]] : (!quake.veq<?>, !quake.ref) -> ()
 # CHECK:             }
-# CHECK:             cc.continue %[[VAL_8]] : i64
+# CHECK:             cc.continue %[[VAL_10]], %[[VAL_10]] : i64, i64
 # CHECK:           } step {
-# CHECK:           ^bb0(%[[VAL_15:.*]]: i64):
-# CHECK:             %[[VAL_16:.*]] = arith.addi %[[VAL_15]], %[[VAL_1]] : i64
-# CHECK:             cc.continue %[[VAL_16]] : i64
-# CHECK:           } {invariant}
-# CHECK-DAG:       quake.dealloc %[[VAL_4]] : !quake.veq<4>
-# CHECK-DAG:       quake.dealloc %[[VAL_3]] : !quake.ref
+# CHECK:           ^bb0(%[[VAL_18:.*]]: i64, %[[VAL_19:.*]]: i64):
+# CHECK:             %[[VAL_20:.*]] = arith.addi %[[VAL_18]], %[[VAL_1]] : i64
+# CHECK:             cc.continue %[[VAL_20]], %[[VAL_19]] : i64, i64
+# CHECK:           }
+# CHECK:           quake.dealloc %[[VAL_5]] : !quake.veq<4>
+# CHECK:           quake.dealloc %[[VAL_4]] : !quake.ref
 # CHECK:           return
 # CHECK:         }
+
+# CHECK-LABEL: sample bar:
+# CHECK: { 00000:10000 }
