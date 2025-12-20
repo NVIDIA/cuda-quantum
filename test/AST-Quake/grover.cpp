60f3ca324ba1262fca4f367184b32f87f8e177c3
Sachin Pisal <spisal@nvidia.com>
2025-12-20 04:45:02 -0800

    Update RUN command for cudaq-quake
    remove %cpp_std
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- test/AST-Quake/grover.cpp
+++ test/AST-Quake/grover.cpp
@@ -7,7 +7,7 @@
  ******************************************************************************/
 
 // clang-format off
-// RUN: cudaq-quake %cpp_std %s | cudaq-opt -lambda-lifting=constant-prop=1 -apply-op-specialization | FileCheck %s
+// RUN: cudaq-quake %s | cudaq-opt -lambda-lifting=constant-prop=1 -apply-op-specialization | FileCheck %s
 // clang-format on
 
 #include <cmath>
