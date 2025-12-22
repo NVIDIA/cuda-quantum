01e65fddcf39271ad223dfddd02b90a91936fbfc
Pradnya Khalate <pkhalate@nvidia.com>
2025-12-22 15:45:02 -0800

    * Simply pass logic by invoking after QIR conversion and simply counting   exisiting result recording calls. * Allow array recording call in base profile (matches spec)
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>

--- lib/Optimizer/CodeGen/QirInsertArrayRecord.cpp
+++ lib/Optimizer/CodeGen/QirInsertArrayRecord.cpp
@@ -28,134 +28,19 @@ using namespace mlir;
 
 namespace {
 
-// Trace a pointer to back to its corresponding `AllocaOp`
-static cudaq::cc::AllocaOp tracePointerToAlloca(Value ptr) {
-  llvm::DenseSet<Value> visited;
-  while (ptr) {
-    if (!visited.insert(ptr).second)
-      return {};
-    Operation *defOp = ptr.getDefiningOp();
-    if (!defOp)
-      return {};
-    if (auto allocaOp = dyn_cast<cudaq::cc::AllocaOp>(defOp))
-      return allocaOp;
-    if (auto castOp = dyn_cast<cudaq::cc::CastOp>(defOp)) {
-      ptr = castOp.getValue();
-      continue;
-    }
-    if (auto computePtrOp = dyn_cast<cudaq::cc::ComputePtrOp>(defOp)) {
-      ptr = computePtrOp.getBase();
-      continue;
-    }
-    return {};
-  }
-  return {};
-}
-
-// Walk a function to identify all the measure-discriminate-store patterns and
-// collect the associated `AllocaOp` when the measurement results are stored.
-// Collect only unique AllocaOps - since each may correspond to multiple
-// measurement operations. When there are no explicit stores, track the first
-// measurement operation and the get the total number of measurements.
-struct AllocaMeasureStoreAnalysis {
-  AllocaMeasureStoreAnalysis() = default;
-
-  explicit AllocaMeasureStoreAnalysis(func::FuncOp funcOp) {
-    size_t totalMeasurementCount = 0;
-    Operation *firstMeasureOp = nullptr;
-    DenseMap<Value, Operation *> valueToMeasurement;
-    llvm::SetVector<cudaq::cc::AllocaOp> uniqueAllocaOps;
-
-    // First pass: identify measurements and propagate through uses
-    funcOp.walk([&](Operation *op) {
-      if (op->hasTrait<cudaq::QuantumMeasure>()) {
-        if (op->hasAttr(cudaq::opt::ResultIndexAttrName)) {
-          totalMeasurementCount++;
-          if (!firstMeasureOp)
-            firstMeasureOp = op;
-        }
-        for (auto result : op->getResults())
-          valueToMeasurement[result] = op;
-        return WalkResult::advance();
-      }
-
-      // TODO: Check if more operations need to be added here.
-      if (!isa<quake::DiscriminateOp, cudaq::cc::CastOp>(op)) {
-        return WalkResult::advance();
-      }
-
-      // Find the operands derived from measurements
-      for (auto operand : op->getOperands()) {
-        if (valueToMeasurement.count(operand)) {
-          for (auto result : op->getResults())
-            valueToMeasurement[result] = valueToMeasurement[operand];
-        }
-        break; // Checking one operand is enough
-      }
-      return WalkResult::advance();
-    });
-
-    // Second pass: find stores of measurement values and trace to `alloca` ops
-    funcOp.walk([&](cudaq::cc::StoreOp storeOp) {
-      if (valueToMeasurement.count(storeOp.getValue())) {
-        Value ptr = storeOp.getPtrvalue();
-        auto allocaOp = tracePointerToAlloca(ptr);
-        if (allocaOp)
-          uniqueAllocaOps.insert(allocaOp);
-      }
-    });
-
-    if (!uniqueAllocaOps.empty()) {
-      // Use array sizes when explicit storage exists
-      for (auto allocaOp : uniqueAllocaOps) {
-        if (auto arrType =
-                allocaOp.getElementType().dyn_cast<cudaq::cc::ArrayType>()) {
-          arraySize += arrType.getSize();
-        } else {
-          arraySize += 1;
-        }
-      }
-      allocaOps.append(uniqueAllocaOps.begin(), uniqueAllocaOps.end());
-    } else if (totalMeasurementCount > 0) {
-      // This could be individual qubit(s)
-      arraySize = totalMeasurementCount;
-      firstMeasurementOp = firstMeasureOp;
-    }
-  }
-
-  SmallVector<cudaq::cc::AllocaOp> allocaOps;
-  size_t arraySize = 0;
-  Operation *firstMeasurementOp = nullptr;
-};
-
 // Inserts a QIR array record output call to declare measurement result storage.
 // QIR requires `__quantum__rt__array_record_output()` be called before multiple
 // measurements to declare the output array size and type label. This is
 // required in `sample` API since it always returns a vector of measurement
-// results. Following logic is used to determine the insertion point:
-//   1. After first alloca (if explicit array storage exists)
-//   2. Before first measurement (if no explicit storage)
-// The label string is created as "array<i1 x N>" where N is the total number of
-// measurement results. The array record output call is created as:
+// results. The call is inserted before the first
+// `__quantum__rt__result_record_output` call. The label string is created as
+// "array<i1 x N>" where N is the total number of measurement results. The array
+// record output call is created as:
 // `__quantum__rt__array_record_output(N, label);`
-LogicalResult
-insertArrayRecordingCalls(func::FuncOp funcOp, size_t resultCount,
-                          const SmallVector<cudaq::cc::AllocaOp> &allocaOps,
-                          Operation *firstMeasureOp) {
+LogicalResult insertArrayRecordingCall(OpBuilder &builder, mlir::Location loc,
+                                       size_t resultCount) {
   if (resultCount == 0)
     return success();
-
-  auto ctx = funcOp.getContext();
-  OpBuilder builder(ctx);
-  mlir::Location loc = funcOp.getLoc();
-  // We insert only one array record call
-  if (!allocaOps.empty())
-    builder.setInsertionPointAfter(allocaOps[0]);
-  else if (firstMeasureOp)
-    builder.setInsertionPoint(firstMeasureOp);
-  else
-    return failure();
-
   // Create the label string: "array<i1 x N>"
   std::string labelStr = "array<i1 x " + std::to_string(resultCount) + ">";
   auto strLitTy = cudaq::cc::PointerType::get(cudaq::cc::ArrayType::get(
@@ -168,16 +53,6 @@ insertArrayRecordingCalls(func::FuncOp funcOp, size_t resultCount,
   builder.create<func::CallOp>(loc, TypeRange{},
                                cudaq::opt::QIRArrayRecordOutput,
                                ArrayRef<Value>{size, label});
-
-  // Add the declaration to the module if it doesn't already exist
-  auto module = funcOp->getParentOfType<ModuleOp>();
-  if (!module.lookupSymbol(cudaq::opt::QIRArrayRecordOutput)) {
-    auto irBuilder = cudaq::IRBuilder::atBlockEnd(module.getBody());
-    if (failed(irBuilder.loadIntrinsic(module,
-                                       cudaq::opt::QIRArrayRecordOutput))) {
-      return failure();
-    }
-  }
   return success();
 }
 
@@ -195,15 +70,36 @@ struct QirInsertArrayRecordPass
           funcOp->hasAttr(cudaq::runtime::enableCudaqRun))
         continue;
 
-      AllocaMeasureStoreAnalysis analysis(funcOp);
-      if (analysis.arraySize == 0)
+      SmallVector<func::CallOp> recordOutputCalls;
+      bool arrayRecordExists = false;
+
+      funcOp.walk([&](func::CallOp callOp) {
+        if (callOp.getCallee() == cudaq::opt::QIRArrayRecordOutput) {
+          arrayRecordExists = true;
+          return;
+        }
+        if (callOp.getCallee() == cudaq::opt::QIRRecordOutput)
+          recordOutputCalls.push_back(callOp);
+      });
+
+      if (arrayRecordExists || recordOutputCalls.empty())
         continue;
 
       LLVM_DEBUG(llvm::dbgs() << "Before adding array recording call:\n"
                               << *funcOp);
-      if (failed(insertArrayRecordingCalls(funcOp, analysis.arraySize,
-                                           analysis.allocaOps,
-                                           analysis.firstMeasurementOp)))
+      // Add the declaration of array recording call to the module
+      auto module = funcOp->getParentOfType<ModuleOp>();
+      if (!module.lookupSymbol(cudaq::opt::QIRArrayRecordOutput)) {
+        auto irBuilder = cudaq::IRBuilder::atBlockEnd(module.getBody());
+        if (failed(irBuilder.loadIntrinsic(module,
+                                           cudaq::opt::QIRArrayRecordOutput))) {
+          return signalPassFailure();
+        }
+      }
+      // Insert array record before first result result recording call.
+      OpBuilder builder(recordOutputCalls.front());
+      if (failed(insertArrayRecordingCall(builder, funcOp.getLoc(),
+                                          recordOutputCalls.size())))
         return signalPassFailure();
       LLVM_DEBUG(llvm::dbgs() << "After adding array recording call:\n"
                               << *funcOp);
