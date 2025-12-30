327d5e8ada593d6e6610e521b4b844d883116dbf
Thomas Alexander <thomasalexander2718@gmail.com>
2025-12-29 22:14:31 -0400

    Force loading of LLVMCodeGen to fix availability issues with fast-isel due to two-level namespacing in OSX.
    Bugs uncovered on OSX:
    - ConvertStmt.cpp: Fix use-after-free by saving operands before erasing call.
    - CombineMeasurements.cpp: Return success() after erase/replace ops for rewriter/
    - RegToMem.cpp: Return WalkResult::skip() after erasing ops during walk.
    - ArgumentConversion.cpp: Fix lambda capture by value instead of reference.
    
    Updating tests for OSX:
    - Fix regex patterns for OSX ABI.
    - A few floating point regexes
    - Mark one of the KAK tests as unsupported on OSX as the values are different due to a different found decomposition. We could have used the OSX values but it seemed lik
    e overkill.
    
    Signed-off-by: Thomas Alexander <thomasalexander2718@gmail.com>
    Signed-off-by: Thomas Alexander <talexander@nvidia.com>
    Signed-off-by: Thomas Alexander <thomasalexander2718@gmail.com>

--- lib/Optimizer/Transforms/CombineMeasurements.cpp
+++ lib/Optimizer/Transforms/CombineMeasurements.cpp
@@ -140,7 +140,6 @@ public:
         return success();
       }
       if (measure.use_empty()) {
-        // Erasing an unused intermediate measurement is a valid rewrite action.
         rewriter.eraseOp(measure);
         return success();
       }
