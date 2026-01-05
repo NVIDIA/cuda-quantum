eede208b3da042e9663d80d986c6cf85f5e43131
Renaud Kauffmann <rkauffmann@nvidia.com>
2026-01-05 08:55:03 -0800

    Correcting include
    Signed-off-by: Renaud Kauffmann <rkauffmann@nvidia.com>

--- runtime/cudaq/version2/cudaq/utils/type_erased_registry.h
+++ runtime/cudaq/version2/cudaq/utils/type_erased_registry.h
@@ -6,7 +6,7 @@
  * the terms of the Apache License 2.0 which accompanies this distribution.    *
  ******************************************************************************/
 #include "common/logger_lite.h"
-#include "iostream"
+#include <iostream>
 #include <memory>
 #include <unordered_map>
 
