642665c14a430018a53325e61eea615f5e49e69b
GabrielePalazzo <73099233+GabrielePalazzo@users.noreply.github.com>
2026-01-23 20:30:37 +0100

    add test for 'sample_async'

--- unittests/backends/qibo/QiboTester.cpp
+++ unittests/backends/qibo/QiboTester.cpp
@@ -24,7 +24,7 @@ bool result_maps_are_matching(
     return true;
 }
 
-TEST(QiboTester, checkSimpleCircuit) {
+TEST(QiboTester, checkSimpleCircuitSync) {
   // Initialize the platform
   auto backendString = fmt::format(fmt::runtime(backendStringTemplate),
                                    mockPort, auth_token);
@@ -39,6 +39,31 @@ TEST(QiboTester, checkSimpleCircuit) {
 
   // Execute the circuit
   auto counts = cudaq::sample(kernel);
+  counts.dump();
+  // Check results
+  EXPECT_EQ(counts.size(), 2);
+  std::unordered_map<std::string, std::size_t> expected = {
+    {"00", 500},
+    {"11", 500}
+  };
+  EXPECT_TRUE(result_maps_are_matching(counts.to_map(), expected));
+}
+
+TEST(QiboTester, checkSimpleCircuitAsync) {
+  // Initialize the platform
+  auto backendString = fmt::format(fmt::runtime(backendStringTemplate),
+                                   mockPort, auth_token);
+  auto &platform = cudaq::get_platform();
+  platform.setTargetBackend(backendString);
+
+  // Create a simple circuit
+  auto kernel = cudaq::make_kernel();
+  auto qubits = kernel.qalloc(2);
+  kernel.h(qubits[0]);
+  kernel.mz(qubits);
+
+  // Execute the circuit
+  auto counts = cudaq::sample_async(kernel).get();
   counts.dump();
   // Check results
   EXPECT_EQ(counts.size(), 2);
