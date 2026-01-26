a2e5143aa86cb10db820f7393fa28584ad9f3124
GabrielePalazzo <73099233+GabrielePalazzo@users.noreply.github.com>
2026-01-26 17:04:24 +0100

    add qibo.cpp example in the docs

---
+++ docs/sphinx/targets/cpp/qibo.cpp
@@ -0,0 +1,24 @@
+// Compile and run with:
+// ```
+// nvq++ --library-mode --target qibo qibo.cpp
+// ./a.out
+// ```
+
+#include <cudaq.h>
+#include <fstream>
+
+
+struct bell_state {
+  auto operator()() __qpu__ {
+    cudaq::qvector q(2);
+    h(q[0]);
+    x<cudaq::ctrl>(q[0], q[1]);
+    auto result = mz(q);
+  }
+};
+
+int main() {
+  // OR: Submit to Qibo synchronously.
+  auto result_counts = cudaq::sample(bell_state{});
+  result_counts.dump();
+}
