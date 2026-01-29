63eafe3eb6cfdbb90dc590bbc22c2aca0d2de691
Sachin Pisal <spisal@nvidia.com>
2026-01-29 14:37:26 -0800

    Adding PTSBE data structures (#3808)
    * * Adding the following data structures along with unittests for ptsbe
    	- KrausSelection
    	- KrausTrajectory
    	- PTSBEResult
    	- ShotAllocationStrategy
    	- TrajectoryMetadata
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>
    
    * formatting, spell fix
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>
    
    * removing metadata
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>
    
    * removing description
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>
    
    * renaming KrausOperatorIndex to KrausOperatorType
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>
    
    * pulling out 1e-9 as a constant
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>
    
    * moving countErrors to trajectory
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>
    
    * allocating remaining shots in a batch mode
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>
    
    * allocating shots in one iteration for uniform shots allocation strategy
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>
    
    * adding ordering validation for krausSelection
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>
    
    * * Adding functions for shot allocation strategies
    * Throwing runtime error on 0 shots
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>
    
    * Updating year in the header
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>
    
    * updating year
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>
    
    * updating year
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>
    
    * adding a builder pattern for building a KrausTrajectory object
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>
    
    * fix spell check
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>
    
    * removing a test
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>
    
    ---------
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

---
+++ unittests/ptsbe/KrausSelectionTester.cpp
@@ -0,0 +1,108 @@
+/*******************************************************************************
+ * Copyright (c) 2026 NVIDIA Corporation & Affiliates.                         *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
+ ******************************************************************************/
+
+#include "CUDAQTestUtils.h"
+#include "cudaq/ptsbe/KrausSelection.h"
+
+using namespace cudaq;
+
+CUDAQ_TEST(KrausSelectionTest, DefaultConstruction) {
+  KrausSelection selection;
+  EXPECT_EQ(selection.circuit_location, 0);
+  EXPECT_TRUE(selection.qubits.empty());
+  EXPECT_TRUE(selection.op_name.empty());
+  EXPECT_EQ(static_cast<std::size_t>(selection.kraus_operator_index), 0);
+}
+
+CUDAQ_TEST(KrausSelectionTest, ParameterizedConstruction) {
+  KrausSelection selection(5,                   // circuit_location
+                           {0, 1},              // qubits
+                           "cx",                // op_name
+                           KrausOperatorType{2} // kraus_operator_index
+  );
+
+  EXPECT_EQ(selection.circuit_location, 5);
+  EXPECT_EQ(selection.qubits.size(), 2);
+  EXPECT_EQ(selection.qubits[0], 0);
+  EXPECT_EQ(selection.qubits[1], 1);
+  EXPECT_EQ(selection.op_name, "cx");
+  EXPECT_EQ(static_cast<std::size_t>(selection.kraus_operator_index), 2);
+}
+
+CUDAQ_TEST(KrausSelectionTest, Equality) {
+  KrausSelection sel1(0, {0}, "h", KrausOperatorType{1});
+  KrausSelection sel2(0, {0}, "h", KrausOperatorType{1});
+  KrausSelection sel3(0, {0}, "h", KrausOperatorType{2}); // Different index
+  KrausSelection sel4(1, {0}, "h", KrausOperatorType{1}); // Different location
+
+  EXPECT_TRUE(sel1 == sel2);
+  EXPECT_FALSE(sel1 == sel3);
+  EXPECT_FALSE(sel1 == sel4);
+}
+
+CUDAQ_TEST(KrausSelectionTest, KrausOperatorTypeIdentity) {
+  EXPECT_EQ(static_cast<std::size_t>(KrausOperatorType::IDENTITY), 0);
+
+  KrausOperatorType idx{3};
+  EXPECT_EQ(static_cast<std::size_t>(idx), 3);
+}
+
+CUDAQ_TEST(KrausSelectionTest, SingleQubitNoise) {
+  KrausSelection h_noise(0,                   // circuit_location
+                         {0},                 // Single qubit
+                         "h",                 // op_name
+                         KrausOperatorType{1} // X error
+  );
+
+  EXPECT_EQ(h_noise.qubits.size(), 1);
+  EXPECT_EQ(h_noise.qubits[0], 0);
+  EXPECT_EQ(h_noise.op_name, "h");
+}
+
+CUDAQ_TEST(KrausSelectionTest, TwoQubitNoise) {
+  KrausSelection cx_noise(5,                   // circuit_location
+                          {0, 1},              // Both qubits affected
+                          "cx",                // op_name
+                          KrausOperatorType{7} // IX error
+  );
+
+  EXPECT_EQ(cx_noise.qubits.size(), 2);
+  EXPECT_EQ(cx_noise.qubits[0], 0);
+  EXPECT_EQ(cx_noise.qubits[1], 1);
+  EXPECT_EQ(static_cast<std::size_t>(cx_noise.kraus_operator_index), 7);
+}
+
+CUDAQ_TEST(KrausSelectionTest, CopySemantics) {
+  KrausSelection original(1, {0, 1}, "cx", KrausOperatorType{3});
+
+  KrausSelection copied = original;
+  EXPECT_TRUE(copied == original);
+  EXPECT_EQ(copied.qubits[0], 0);
+
+  KrausSelection assigned;
+  assigned = original;
+  EXPECT_TRUE(assigned == original);
+}
+
+CUDAQ_TEST(KrausSelectionTest, MoveSemantics) {
+  KrausSelection original(1, {0, 1, 2}, "ccx", KrausOperatorType{5});
+
+  KrausSelection moved = std::move(original);
+  EXPECT_EQ(moved.circuit_location, 1);
+  EXPECT_EQ(moved.qubits.size(), 3);
+  EXPECT_EQ(moved.op_name, "ccx");
+}
+
+CUDAQ_TEST(KrausSelectionTest, ConstexprEquality) {
+  KrausSelection sel1(0, {0}, "h", KrausOperatorType{1});
+  KrausSelection sel2(0, {0}, "h", KrausOperatorType{1});
+  KrausSelection sel3(1, {0}, "h", KrausOperatorType{1});
+
+  EXPECT_TRUE(sel1 == sel2);
+  EXPECT_FALSE(sel1 == sel3);
+}
