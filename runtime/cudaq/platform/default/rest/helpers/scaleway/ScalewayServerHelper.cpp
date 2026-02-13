06495efaa79dffc1187df003028e04a3ceca322d
Valentin Macheret <vmacheret@scaleway.com>
2026-02-13 16:18:11 +0100

    feat(scaleway): wip register

--- runtime/cudaq/platform/default/rest/helpers/scaleway/ScalewayServerHelper.cpp
+++ runtime/cudaq/platform/default/rest/helpers/scaleway/ScalewayServerHelper.cpp
@@ -190,13 +190,9 @@ ScalewayServerHelper::processResults(ServerMessage &postJobResponse,
 
     auto sampleResult = qioResult.toCudaqSampleResult();
 
-    // CUDAQ_INFO("sampleResult {}", sampleResult);
-
     std::vector<ExecutionResult> execResults;
 
     auto &output_names = outputNames[jobId];
-
-    CUDAQ_INFO("Output names {}", output_names);
 
     // Get a reduced list of qubit numbers that were in the original program
     // so that we can slice the output data and extract the bits that the user
@@ -206,8 +202,6 @@ ScalewayServerHelper::processResults(ServerMessage &postJobResponse,
     for (auto &[result, info] : output_names) {
       qubitNumbers.push_back(info.qubitNum);
     }
-
-    CUDAQ_INFO("qubitNumbers {}", qubitNumbers);
 
     // For each original counts entry in the full sample results, reduce it
     // down to the user component and add to userGlobal. If qubitNumbers is empty,
@@ -221,8 +215,6 @@ ScalewayServerHelper::processResults(ServerMessage &postJobResponse,
 
     // Now add to `execResults` one register at a time
     for (const auto &[result, info] : output_names) {
-      // CUDAQ_INFO("result info {}", info);
-
       CountsDictionary regCounts;
       for (const auto &[bits, count] : sampleResult)
         regCounts[std::string{bits[info.qubitNum]}] += count;
@@ -232,8 +224,6 @@ ScalewayServerHelper::processResults(ServerMessage &postJobResponse,
     // Return a sample result including the global register and all individual
     // registers.
     auto ret = cudaq::sample_result(execResults);
-
-    // CUDAQ_INFO("sampleResult ret {}", ret);
 
     return ret;
   } catch (const std::exception &e) {
