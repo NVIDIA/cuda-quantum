d5c8c009f74d537a28267d6414153b92361b657b
Adam Geller <adgeller@nvidia.com>
2026-02-13 19:24:21 -0800

    Change test order to avoid sequential test fail
    Signed-off-by: Adam Geller <adgeller@nvidia.com>

--- python/tests/kernel/test_kernel_features.py
+++ python/tests/kernel/test_kernel_features.py
@@ -164,37 +164,6 @@ def test_adjoint():
     #assert len(counts) == 1 and '00000000' in counts
 
 
-# TODO: Update when `ApplyOpSpecialization` can handle multi-argument loops
-# See: https://github.com/NVIDIA/cuda-quantum/issues/3818
-@pytest.mark.xfail(raises=RuntimeError)
-def test_adjoint_bug():
-    num_electrons = 2
-    num_qubits = 8
-
-    thetas = [
-        -0.00037043841404585794, 0.0003811110195084151, 0.2286823796532558,
-        -0.00037043841404585794, 0.0003811110195084151, 0.2286823796532558,
-        -0.00037043841404585794, 0.0003811110195084151, 0.2286823796532558,
-        -0.00037043841404585794, 0.0003811110195084151, 0.2286823796532558,
-        -0.00037043841404585794, 0.0003811110195084151, 0.2286823796532558,
-        -0.00037043841404585794, 0.0003811110195084151, 0.2286823796532558,
-        -0.00037043841404585794, 0.0003811110195084151, 0.2286823796532558,
-        -0.00037043841404585794, 0.0003811110195084151, 0.2286823796532558
-    ]
-
-    @cudaq.kernel
-    def kernel(withAdj: bool):
-        qubits = cudaq.qvector(num_qubits)
-        for i in range(num_electrons):
-            x(qubits[i])
-        cudaq.kernels.uccsd(qubits, thetas, num_electrons, num_qubits)
-        if withAdj:
-            cudaq.adjoint(cudaq.kernels.uccsd, qubits, thetas, num_electrons,
-                          num_qubits)
-
-    counts = cudaq.sample(kernel, True, shots_count=1000)
-
-
 def test_control():
     """Test that we can control on kernel functions."""
 
@@ -2827,6 +2796,37 @@ def test_named_reg_in_sample(capfd):
     cudaq.sample(baz)
     captured = capfd.readouterr()
     assert "WARNING" in captured.err
+
+
+# TODO: Update when `ApplyOpSpecialization` can handle multi-argument loops
+# See: https://github.com/NVIDIA/cuda-quantum/issues/3818
+@pytest.mark.xfail(raises=RuntimeError)
+def test_adjoint_bug():
+    num_electrons = 2
+    num_qubits = 8
+
+    thetas = [
+        -0.00037043841404585794, 0.0003811110195084151, 0.2286823796532558,
+        -0.00037043841404585794, 0.0003811110195084151, 0.2286823796532558,
+        -0.00037043841404585794, 0.0003811110195084151, 0.2286823796532558,
+        -0.00037043841404585794, 0.0003811110195084151, 0.2286823796532558,
+        -0.00037043841404585794, 0.0003811110195084151, 0.2286823796532558,
+        -0.00037043841404585794, 0.0003811110195084151, 0.2286823796532558,
+        -0.00037043841404585794, 0.0003811110195084151, 0.2286823796532558,
+        -0.00037043841404585794, 0.0003811110195084151, 0.2286823796532558
+    ]
+
+    @cudaq.kernel
+    def kernel(withAdj: bool):
+        qubits = cudaq.qvector(num_qubits)
+        for i in range(num_electrons):
+            x(qubits[i])
+        cudaq.kernels.uccsd(qubits, thetas, num_electrons, num_qubits)
+        if withAdj:
+            cudaq.adjoint(cudaq.kernels.uccsd, qubits, thetas, num_electrons,
+                          num_qubits)
+
+    cudaq.sample(kernel, True, shots_count=1000)
 
 
 # leave for gdb debugging
