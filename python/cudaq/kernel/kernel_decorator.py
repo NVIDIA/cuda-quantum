c0b65d09058fd2bde63dddb86214f3701b386513
Adam Geller <adgeller@nvidia.com>
2026-02-24 11:58:45 -0800

    Formatting
    Signed-off-by: Adam Geller <adgeller@nvidia.com>

--- python/cudaq/kernel/kernel_decorator.py
+++ python/cudaq/kernel/kernel_decorator.py
@@ -551,7 +551,7 @@ class PyKernelDecorator(object):
         """
         # Make sure this hasn't already been cleaned up as we're winding down
         if (cudaq_runtime is not None and
-            cudaq_runtime.delete_cache_execution_engine is not None):
+                cudaq_runtime.delete_cache_execution_engine is not None):
             cudaq_runtime.delete_cache_execution_engine(key)
 
     def resolve_decorator_at_callsite(self, callingMod):
