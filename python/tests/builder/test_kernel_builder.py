3753090f7423280a5c958448c6b24ee6471b1816
Luca Mondada <luca@mondada.net>
2026-02-24 17:10:09 +0000

    add more asserts for error msg
    Signed-off-by: Luca Mondada <luca@mondada.net>

--- python/tests/builder/test_kernel_builder.py
+++ python/tests/builder/test_kernel_builder.py
@@ -1274,6 +1274,9 @@ def test_call_kernel_deferred_compilation():
         kernel.apply_call(notPrecompiledKernel)
 
     assert "must be compiled to be used in the kernel builder" in str(e.value)
+    assert "Call `notPrecompiledKernel.compile()` before initialzing" in str(
+        e.value)
+    assert "or deactivate deferred compilation" in str(e.value)
 
 
 def test_sample_with_no_qubits():
