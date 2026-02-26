bff4df95f556991fd2384dfa3b95ce15b613fb42
Thomas Alexander <talexander@nvidia.com>
2026-02-26 14:51:07 -0400

    Clean up py_sample_ptsbe: anonymous namespace, positive logic, fix docstring, remove pybind alias
    Signed-off-by: Thomas Alexander <talexander@nvidia.com>

--- python/runtime/cudaq/algorithms/py_sample_ptsbe.h
+++ python/runtime/cudaq/algorithms/py_sample_ptsbe.h
@@ -10,8 +10,6 @@
 
 #include <pybind11/pybind11.h>
 
-namespace py = pybind11;
-
 namespace cudaq {
-void bindSamplePTSBE(py::module &mod);
+void bindSamplePTSBE(pybind11::module &mod);
 } // namespace cudaq
