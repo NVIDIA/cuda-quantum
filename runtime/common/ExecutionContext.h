d5bea816b24ed101b10cf75ef3bf6d1d27ee9f4c
Adam T. Geller <adgeller@nvidia.com>
2026-03-04 15:11:08 -0800

    Add errors for misuse of `reuse_compiler_artifact` (#4067)
    This PR builds on #4054, adding checks and throwing runtime errors in
    the following situations where the compiler artifacts are incorrectly
    reused across launches:
    1. The second launch is for a kernel with a different name.
    2. The second launch has different explicit arguments.
    
    This does not handle cases where any captured arguments (python kernel
    decorators) are changed in between launches when reusing artifacts.
    
    ---------
    
    Signed-off-by: Adam Geller <adgeller@nvidia.com>

--- runtime/common/ExecutionContext.h
+++ runtime/common/ExecutionContext.h
@@ -205,9 +205,23 @@ void setExecutionContext(ExecutionContext *ctx);
 /// Use `quantum_platform::with_execution_context` instead of setting/resetting
 /// the execution context manually.
 void resetExecutionContext();
-
-void enablePersistentJITEngine();
-void disablePersistentJITEngine();
 } // namespace detail
 
+namespace compiler_artifact {
+/// Saves and reuses the JITEngine across launches
+///
+/// This will exhibit undefined behavior if the launch arguments/context
+/// in any way differs from the saved launch.
+void enablePersistentJITEngine();
+void disablePersistentJITEngine();
+bool isPersistingJITEngine();
+
+/// Checks that the compiler artifact (if present) can be reused
+/// for the given explicit launch arguments.
+///
+/// `argsCreatorPtr` must point to the `.argsCreator` function from `jit`
+void checkArtifactReuse(const std::string kernelName,
+                        const std::vector<void *> &args, const JitEngine jit,
+                        std::function<void *()> argsCreatorThunk);
+}; // namespace compiler_artifact
 } // namespace cudaq
