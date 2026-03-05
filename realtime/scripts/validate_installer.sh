7fd0fe22b7476271e878a1f73b803b6ef7817585
Thien Nguyen <thiennguyen@nvidia.com>
2026-03-06 09:44:02 +1100

    Some fixes running the test
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>

--- realtime/scripts/validate_installer.sh
+++ realtime/scripts/validate_installer.sh
@@ -11,25 +11,30 @@
 # Default installation location: /opt/nvidia/cudaq/realtime
 install_dir=/opt/nvidia/cudaq/realtime
 
-# First, make sure the "--hololink-dir=<path>"" is provided and valid, since it is required for validation.
+# First, make sure the "--hololink-dir <path>"" is provided and valid, since it is required for validation.
 hololink_dir=""
 
 # Process command line arguments
 while [[ $# -gt 0 ]]; do
-  case $1 in
-    --hololink-dir=*)
-      hololink_dir="${1#*=}"
-      shift
-      ;;
-    *)
-      # Ignore other command line arguments, they will be forwarded to the `hololink_test.sh` script.
-      shift
-      ;;
-  esac
+    case "$1" in
+      --hololink-dir)
+        if [[ -n "$2" ]]; then
+          hololink_dir="$2"
+          shift 2
+        else
+          echo "Error: --hololink-dir requires a non-empty argument." >&2
+          exit 1
+        fi
+        ;;
+      *)
+        # Ignore other command line arguments, they will be forwarded to the `hololink_test.sh` script.
+        shift
+        ;;
+    esac
 done
 
 if [[ -z "$hololink_dir" ]]; then
-  echo "Error: --hololink-dir=<path> argument is required." >&2
+  echo "Error: --hololink-dir <path> argument is required." >&2
   exit 1
 fi
 
@@ -77,10 +82,10 @@ fi
 
 echo "Successfully built hololink test application for validation."
 
-# Add "--bin-dir=<path>" argument to specify the location of the built test application, so that the `hololink_test.sh` script can find and run it.
+# Add "--bin-dir <path>" argument to specify the location of the built test application, so that the `hololink_test.sh` script can find and run it.
 bin_dir="$build_dir"
 # Call the `hololink_test.sh` script to validate the installation, forward all the command line arguments to it.
-bash "$install_dir/utils/hololink_test.sh" "$@"
+bash "$install_dir/utils/hololink_test.sh" "$@" --bin-dir "$bin_dir"
 
 # Check the status of the hololink test script to determine if the validation was successful.
 if [[ $? -ne 0 ]]; then
