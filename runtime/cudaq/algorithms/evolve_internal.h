33b72016b81a3bc404096f191e800f824594ace2
Sachin Pisal <spisal@nvidia.com>
2026-03-07 17:08:56 -0800

    Fix seg fault in test_evolve_async (#4114)
    In evolve_internal.h, evolve_async called `p.set_value(evolve(...))` and
    then called `platform.set_noise(nullptr)`. Here the background thread
    held a reference to the platform and was executing `set_noise(nullptr)`
    even when `.get` returned. When the successive test's `set_target()`
    call triggered `platformQPUs.clear()`, it tried to execute even when the
    thread with `platform.set_noise(nullptr)` from the previous test was
    still running. That caused a segfault.
    
    Fixes #3678. 
    
    Also fixes the [deployment failure in
    main](https://github.com/NVIDIA/cuda-quantum/actions/runs/22787780036/job/66113787006#step:9:600).
    
    ---------
    
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- runtime/cudaq/algorithms/evolve_internal.h
+++ runtime/cudaq/algorithms/evolve_internal.h
@@ -107,9 +107,10 @@ evolve_async(state initial_state, QuantumKernel &&kernel,
        &platform]() mutable {
         if (noise_model.has_value())
           platform.set_noise(&noise_model.value());
-        p.set_value(evolve(initial_state, func, observables, shots_count));
+        auto result = evolve(initial_state, func, observables, shots_count);
         if (noise_model.has_value())
           platform.set_noise(nullptr);
+        p.set_value(std::move(result));
       });
 
   platform.enqueueAsyncTask(qpu_id, wrapped);
@@ -132,10 +133,11 @@ evolve_async(state initial_state, std::vector<QuantumKernel> kernels,
        shots_count, &platform, save_intermediate_states]() mutable {
         if (noise_model.has_value())
           platform.set_noise(&noise_model.value());
-        p.set_value(evolve(initial_state, kernels, observables, shots_count,
-                           save_intermediate_states));
+        auto result = evolve(initial_state, kernels, observables, shots_count,
+                             save_intermediate_states);
         if (noise_model.has_value())
           platform.set_noise(nullptr);
+        p.set_value(std::move(result));
       });
 
   platform.enqueueAsyncTask(qpu_id, wrapped);
