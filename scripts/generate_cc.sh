d891bf649456d8ca7f710c67f1bac9c81981d852
Mitchell <mitch_dz@hotmail.com>
2026-03-13 08:41:16 -0700

    use requirements file to install dependencies
    Signed-off-by: Mitchell <mitch_dz@hotmail.com>

--- scripts/generate_cc.sh
+++ scripts/generate_cc.sh
@@ -93,7 +93,7 @@ if $gen_cpp_coverage; then
     use_llvm_cov=true
 
     # Run tests (C++ Unittests)
-    python3 -m pip install iqm-client==28.0.0 --break-system-packages
+    python3 -m pip install -r ${repo_root}/requirements-tests-backend.txt --break-system-packages
     ctest --output-on-failure --test-dir ${repo_root}/build -E ctest-nvqpp -E ctest-targettests
     ctest_status=$?
     /usr/local/llvm/bin/llvm-lit -v --param nvqpp_site_config=${repo_root}/build/test/lit.site.cfg.py ${repo_root}/build/test
@@ -184,7 +184,7 @@ if $gen_py_coverage; then
     apt install -y python${PY_VER}-venv
  
     # Needs to be installed outside of venv
-    python3 -m pip install iqm-client==28.0.0 --break-system-packages
+    python3 -m pip install -r ${repo_root}/requirements-tests-backend.txt --break-system-packages
 
     venv_dir=${repo_root}/build/venv-coverage
     python3 -m venv "$venv_dir"
