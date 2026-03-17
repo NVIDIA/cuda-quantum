e1da545a6a1d7074fff68155e5e5e2b007a9079f
Eric Schweitz <eschweitz@nvidia.com>
2026-03-17 13:57:13 -0700

    Update file path names. Get rid of redundant RUN lines.
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>

--- targettests/quantinuum/qspan_slices.cpp
+++ targettests/quantinuum/qspan_slices.cpp
@@ -6,5 +6,6 @@
  * the terms of the Apache License 2.0 which accompanies this distribution.    *
  ******************************************************************************/
 
-// RUN: nvq++ --target quantinuum --emulate %s -o %t && echo skipping
-#include "../execution/qspan_slices.cpp"
+// RUN: echo skipping
+
+#include "../execution/qspan_slice.cpp"
