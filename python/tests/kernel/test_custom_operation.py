1d1c40f3c0d4f953c4b8ac706d82b6a24290e10c
Sachin Pisal <spisal@nvidia.com>
2026-03-16 21:18:25 -0700

    adding more tests
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- python/tests/kernel/test_custom_operation.py
+++ python/tests/kernel/test_custom_operation.py
@@ -58,3 +58,89 @@ def test_starred_qvector():
 
     counts = cudaq.sample(kernel)
     assert counts.most_probable() == "01"
+
+
+def test_mixed_starred_qvec_and_qref():
+    """custom_swap(*qvec, qbit)"""
+
+    @cudaq.kernel
+    def kernel():
+        qvec = cudaq.qvector(1)
+        qbit = cudaq.qubit()
+        x(qvec[0])
+        custom_swap(*qvec, qbit)
+
+    counts = cudaq.sample(kernel)
+    assert counts.most_probable() == "01"
+
+
+def test_unstarred_qvec_and_qref():
+    """custom_swap(qvec, qbit)"""
+
+    @cudaq.kernel
+    def kernel():
+        qvec = cudaq.qvector(1)
+        qbit = cudaq.qubit()
+        x(qvec[0])
+        custom_swap(qvec, qbit)
+
+    counts = cudaq.sample(kernel)
+    assert counts.most_probable() == "01"
+
+
+def test_too_few_qubits_raises_error():
+    """custom_swap with only 1 qubit when 2 are required"""
+
+    with pytest.raises(RuntimeError) as error:
+
+        @cudaq.kernel
+        def kernel():
+            qbit = cudaq.qubit()
+            custom_swap(qbit)
+
+        kernel.compile()
+    assert 'custom operation requires 2 qubit target(s), but 1 were provided' in repr(
+        error)
+
+
+def test_too_many_qubits_raises_error():
+    """custom_swap with 3 qubits when 2 are required"""
+
+    with pytest.raises(RuntimeError) as error:
+
+        @cudaq.kernel
+        def kernel():
+            q1 = cudaq.qubit()
+            q2 = cudaq.qubit()
+            q3 = cudaq.qubit()
+            custom_swap(q1, q2, q3)
+
+        kernel.compile()
+    assert 'custom operation requires 2 qubit target(s), but 3 were provided' in repr(
+        error)
+
+
+def test_unknown_veq_size_correct_count():
+    """custom_swap(*qvec), qvec size is a runtime parameter"""
+
+    @cudaq.kernel
+    def kernel(n: int):
+        qvec = cudaq.qvector(n)
+        x(qvec[0])
+        custom_swap(*qvec)
+
+    counts = cudaq.sample(kernel, 2)
+    assert counts.most_probable() == "01"
+
+
+def test_unknown_veq_size_incorrect_count():
+    """custom_swap(*qvec), qvec has more qubits than the operation requires."""
+
+    @cudaq.kernel
+    def kernel(n: int):
+        qvec = cudaq.qvector(n)
+        x(qvec[0])
+        custom_swap(*qvec)
+
+    counts = cudaq.sample(kernel, 3)
+    assert counts.most_probable() == "010"
