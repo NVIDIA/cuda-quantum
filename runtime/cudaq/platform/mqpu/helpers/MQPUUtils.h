1a23e0efb2416122f917c1ebffada3dd058ff50c
Thien Nguyen <thiennguyen@nvidia.com>
2026-04-08 00:31:42 +0000

    Clean up
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>

--- runtime/cudaq/platform/mqpu/helpers/MQPUUtils.h
+++ runtime/cudaq/platform/mqpu/helpers/MQPUUtils.h
@@ -8,28 +8,7 @@
 
 #pragma once
 
-#include "cudaq/distributed/mpi_plugin.h"
-#include <string>
-#include <vector>
-
 namespace cudaq {
-// Helper struct to start a REST server (`cudaq-qpud`) instance on a random
-// TCP/IP port. This will wait until the server is ready to serve incoming HTTP
-// requests. The server process is terminated automatically by the destructor.
-struct AutoLaunchRestServerProcess {
-  AutoLaunchRestServerProcess(int seed_offset);
-  ~AutoLaunchRestServerProcess();
-  std::string getUrl() const;
-  AutoLaunchRestServerProcess(const AutoLaunchRestServerProcess &) = delete;
-  AutoLaunchRestServerProcess &
-  operator=(const AutoLaunchRestServerProcess &) = delete;
-
-private:
-  int m_pid;
-  std::string m_url;
-  std::string m_libPathEnv;
-};
-
 // Helper to retrieve the number of GPU.
 // It works with or without CUDA dependency.
 // If CUDA is present, returns the actual number of GPU devices. Otherwise,
