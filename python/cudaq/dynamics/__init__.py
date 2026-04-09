a81337181f9a6595092631974177d07dc0c406a7
Sachin Pisal <spisal@nvidia.com>
2026-04-09 09:03:58 -0700

    preloading dynamics libs libcudaq-logger.so and libnvqir-dynamics.so
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- python/cudaq/dynamics/__init__.py
+++ python/cudaq/dynamics/__init__.py
@@ -6,5 +6,20 @@
 # the terms of the Apache License 2.0 which accompanies this distribution.     #
 # ============================================================================ #
 
+
+def _preload_dynamics_libs():
+    import ctypes, os
+    lib_dir = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '..', '..', 'lib'))
+    for lib in ('libcudaq-logger.so', 'libnvqir-dynamics.so'):
+        try:
+            ctypes.CDLL(os.path.join(lib_dir, lib), ctypes.RTLD_GLOBAL)
+        except OSError:
+            pass
+
+
+_preload_dynamics_libs()
+del _preload_dynamics_libs
+
 from .helpers import InitialState
 from .schedule import Schedule
