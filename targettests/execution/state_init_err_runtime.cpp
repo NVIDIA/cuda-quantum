515d6ffa29fce8fcef3dec48d2d601118884e438
Thien Nguyen <thiennguyen@nvidia.com>
2026-04-10 01:36:20 +0000

    multi-line deletion error
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>

--- targettests/execution/state_init_err_runtime.cpp
+++ targettests/execution/state_init_err_runtime.cpp
@@ -8,7 +8,6 @@
 
 // RUN: nvq++ -target quantinuum -emulate -fkernel-exec-kind=1 %s -o %t \
 // RUN:   && %t 2>&1 | FileCheck %s
-// RUN:   -fkernel-exec-kind=1 %s -o %t && %t 2>&1 | FileCheck %s
 
 #include <cudaq.h>
 #include <iostream>
