b234f1618ecc182c02f54acb13ccd43957c78180
Adam Geller <adgeller@nvidia.com>
2026-04-13 12:21:20 -0700

    Fix test command
    Signed-off-by: Adam Geller <adgeller@nvidia.com>

--- targettests/execution/cudaq_run_dynamic_result.cpp
+++ targettests/execution/cudaq_run_dynamic_result.cpp
@@ -1,5 +1,5 @@
 // RUN: nvq++ %s -o %t && %t | FileCheck %s
-// RUN: nvq++ --target quantinuum --quantinuum-machine Helios-1SC --emulate %s -o %t && %t | FileCheck %s -check-prefix=FAIL
+// RUN: not nvq++ --target quantinuum --quantinuum-machine Helios-1SC --emulate %s -o %t && %t 2>&1 | FileCheck %s -check-prefix=FAIL
 
 #include <cudaq.h>
 
