1864137fe0cf1db94c2321897b490171ffdf4c3f
hanag <hana.giat@quantum-machines.co>
2026-04-15 15:08:30 +0300

    Server helper changes for quake payload

--- runtime/cudaq/platform/default/rest/helpers/quantum_machines/QuantumMachinesServerHelper.cpp
+++ runtime/cudaq/platform/default/rest/helpers/quantum_machines/QuantumMachinesServerHelper.cpp
@@ -99,8 +99,11 @@ public:
     if (!postResponse.contains("id"))
       return "";
 
+    // Extract the job ID from the response
+    std::string id = postResponse.at("id");
+
     // Return the job ID from the response
-    return postResponse.at("id");
+    return id;
   }
 
   /// @brief Constructs the URL for retrieving a job based on the server's
@@ -151,6 +154,14 @@ public:
     CUDAQ_INFO("nextResultPollingInterval");
     return std::chrono::seconds(1);
   }
+
+  std::string extractOutputLog(ServerMessage &jobResponse,
+                                                       std::string &jobId) {
+    CUDAQ_INFO("extractOutputLog: {}, {}", jobId, jobResponse.dump());
+    //TODO: implement
+    return "";
+  }
+
 };
 
 } // namespace cudaq
