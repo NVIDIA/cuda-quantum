ef487935760d6f17574bbb5b2624a34a33a8f1fa
Sachin Pisal <spisal@nvidia.com>
2026-05-14 10:20:09 -0700

    implementing addCliffordTSynthesis helper
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- cudaq/lib/Optimizer/Transforms/Pipelines.cpp
+++ cudaq/lib/Optimizer/Transforms/Pipelines.cpp
@@ -144,6 +144,18 @@ void cudaq::opt::addDecomposition(OpPassManager &pm,
   pm.addPass(cudaq::opt::createDecomposition(opts));
 }
 
+void cudaq::opt::addCliffordTSynthesis(OpPassManager &pm, double epsilon) {
+  pm.addPass(cudaq::opt::createUnitarySynthesis());
+  pm.addPass(cudaq::opt::createApplySpecialization());
+  pm.addNestedPass<func::FuncOp>(cudaq::opt::createConstantPropagation());
+  cudaq::opt::CliffordTSynthesisOptions ctsOpts;
+  ctsOpts.epsilon = epsilon;
+  pm.addPass(cudaq::opt::createCliffordTSynthesis(ctsOpts));
+  cudaq::opt::DecompositionOptions decOpts;
+  decOpts.basis = {"h", "s", "t", "x", "z", "x(1)"};
+  pm.addPass(cudaq::opt::createDecomposition(decOpts));
+}
+
 static void
 createTargetDeployPipeline(OpPassManager &pm,
                            const TargetDeployPipelineOptions &options) {
