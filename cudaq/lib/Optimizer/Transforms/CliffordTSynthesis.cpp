7660126faab34522b19a1615a7ff053b97a11e66
Sachin Pisal <spisal@nvidia.com>
2026-05-13 17:18:05 -0700

    adding R1Pattern to clifford-t-synthesis
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- cudaq/lib/Optimizer/Transforms/CliffordTSynthesis.cpp
+++ cudaq/lib/Optimizer/Transforms/CliffordTSynthesis.cpp
@@ -239,6 +239,38 @@ struct RyPattern : OpRewritePattern<cudaq::quake::RyOp> {
   RotationOptions opts;
 };
 
+// R1(theta) = e^{i theta/2} . Rz(theta). The leading global phase is
+// dropped for now and we will revisit when controlled rotations are
+// reintroduced.
+struct R1Pattern : OpRewritePattern<cudaq::quake::R1Op> {
+  R1Pattern(MLIRContext *ctx, RotationOptions opts)
+      : OpRewritePattern(ctx), opts(std::move(opts)) {}
+
+  LogicalResult matchAndRewrite(cudaq::quake::R1Op op,
+                                PatternRewriter &rewriter) const override {
+    auto check = validateRotationOperands(op, op.getParameter(),
+                                          op.getControls(), rewriter, opts);
+    switch (check.action) {
+    case PreCheck::Action::LeaveInPlace:
+      return failure();
+    case PreCheck::Action::Erased:
+      return success();
+    case PreCheck::Action::Lower:
+      break;
+    }
+
+    Location loc = op.getLoc();
+    Value target = op.getTarget();
+    Value angle = op.getParameter();
+    cudaq::quake::RzOp::create(rewriter, loc, ValueRange{angle}, ValueRange{},
+                               ValueRange{target});
+    rewriter.eraseOp(op);
+    return success();
+  }
+
+  RotationOptions opts;
+};
+
 } // namespace
 
 #endif // CUDAQ_HAS_CLIFFORD_T_SYNTHESIS
@@ -276,7 +308,7 @@ public:
 
     MLIRContext *ctx = &getContext();
     RewritePatternSet patterns(ctx);
-    patterns.add<RxPattern, RyPattern, RzPattern>(ctx, opts);
+    patterns.add<R1Pattern, RxPattern, RyPattern, RzPattern>(ctx, opts);
 
     if (failed(applyPatternsGreedily(getOperation(), std::move(patterns))))
       signalPassFailure();
