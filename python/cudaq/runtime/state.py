59fa3938d0b2b47f7bc0f0b30d13f3398bf296b6
Minh Vu <vuhoangminh97@gmail.com>
2026-05-29 23:45:26 +0200

    Fix cudaq.to_cupy GPU buffer wrapping
    Signed-off-by: Minh Vu <vuhoangminh97@gmail.com>

--- python/cudaq/runtime/state.py
+++ python/cudaq/runtime/state.py
@@ -100,7 +100,8 @@ def to_cupy(state, dtype=None):
 
     arrays = []
     for tensor in state.getTensors():
-        mem = cp.cuda.UnownedMemory(tensor.data(), 1024, owner=None)
+        sizeBytes = tensor.get_num_elements() * tensor.get_element_size()
+        mem = cp.cuda.UnownedMemory(tensor.data(), sizeBytes, owner=state)
         memptr = cp.cuda.MemoryPointer(mem, offset=0)
         arrays.append(cp.ndarray(tensor.extents, dtype=dtype, memptr=memptr))
     return arrays
