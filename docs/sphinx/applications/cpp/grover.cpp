632663e8d06266aaf2b8b046f410e5527f21eb39
Sachin Pisal <spisal@nvidia.com>
2026-06-02 16:30:11 -0700

    writing grover example for MLIR mode
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- docs/sphinx/applications/cpp/grover.cpp
+++ docs/sphinx/applications/cpp/grover.cpp
@@ -5,7 +5,6 @@
 
 #include <cmath>
 #include <cudaq.h>
-#include <numbers>
 
 __qpu__ void reflect_about_uniform(cudaq::qvector<> &qs) {
   auto ctrlQubits = qs.front(qs.size() - 1);
@@ -23,41 +22,39 @@ __qpu__ void reflect_about_uniform(cudaq::qvector<> &qs) {
 
 struct run_grover {
   template <typename CallableKernel>
-  __qpu__ auto operator()(const int n_qubits, CallableKernel &&oracle) {
-    int n_iterations = round(0.25 * std::numbers::pi * sqrt(2 ^ n_qubits));
+  __qpu__ auto operator()(const int n_qubits, const long target_state,
+                          CallableKernel &&oracle) {
+    int n_iterations = round(0.25 * M_PI * sqrt(2 ^ n_qubits));
 
     cudaq::qvector qs(n_qubits);
     h(qs);
     for (int i = 0; i < n_iterations; i++) {
-      oracle(qs);
+      oracle(target_state, qs);
       reflect_about_uniform(qs);
     }
     mz(qs);
   }
 };
 
-struct oracle {
-  const long target_state;
+__qpu__ void apply_target_mask(cudaq::qvector<> &qs, const long target_state) {
+  for (int i = 1; i <= qs.size(); ++i) {
+    auto target_bit_set = (1 << (qs.size() - i)) & target_state;
+    if (!target_bit_set)
+      x(qs[i - 1]);
+  }
+}
 
-  void operator()(cudaq::qvector<> &qs) __qpu__ {
-    cudaq::compute_action(
-        [&]() {
-          for (int i = 1; i <= qs.size(); ++i) {
-            auto target_bit_set = (1 << (qs.size() - i)) & target_state;
-            if (!target_bit_set)
-              x(qs[i - 1]);
-          }
-        },
-        [&]() {
-          auto ctrlQubits = qs.front(qs.size() - 1);
-          z<cudaq::ctrl>(ctrlQubits, qs.back());
-        });
+struct oracle {
+  void operator()(const long target_state, cudaq::qvector<> &qs) __qpu__ {
+    apply_target_mask(qs, target_state);
+    auto ctrlQubits = qs.front(qs.size() - 1);
+    z<cudaq::ctrl>(ctrlQubits, qs.back());
+    apply_target_mask(qs, target_state);
   }
 };
 
 int main(int argc, char *argv[]) {
   auto secret = 1 < argc ? strtol(argv[1], NULL, 2) : 0b1011;
-  oracle compute_oracle{.target_state = secret};
-  auto counts = cudaq::sample(run_grover{}, 4, compute_oracle);
+  auto counts = cudaq::sample(run_grover{}, 4, secret, oracle{});
   printf("Found string %s\n", counts.most_probable().c_str());
 }
