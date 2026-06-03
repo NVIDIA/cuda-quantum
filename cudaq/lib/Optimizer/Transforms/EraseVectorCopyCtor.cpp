e85be51972a725e6a92a7af6514259f602bd32ce
Eric Schweitz <eschweitz@nvidia.com>
2026-06-03 09:08:14 -0700

    [core] Update the erase-vector-copy-ctor pass.
    This pass needs a new pattern to be better aligned with the new
    inlining scope preservation in the compiler. When returning a span,
    a kernel necessarily must move the data off the stack. But after
    inlining a call to such a function, the span can be retained on the
    calling function's stack trivially and without a copy. This adds
    a new rewrite pattern to deal with the ScopeOp, etc.
    
    Rename the test to better reflect the regression.
    
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>

--- cudaq/lib/Optimizer/Transforms/EraseVectorCopyCtor.cpp
+++ cudaq/lib/Optimizer/Transforms/EraseVectorCopyCtor.cpp
@@ -82,7 +82,8 @@ public:
     rewriter.eraseOp(analysis.copyFrom);
     rewriter.eraseOp(analysis.copyTo);
     rewriter.eraseOp(analysis.freeMem);
-    rewriter.eraseOp(call);
+    rewriter.replaceOpWithNewOp<cudaq::cc::UndefOp>(
+        call, call.getResult(0).getType());
     return success();
   }
 
@@ -114,6 +115,149 @@ public:
   }
 };
 
+// When inlining a function that returns a span, we can have the span buffer and
+// creation code captured in a scope block. In that case, we have to look at the
+// user(s) of the scope's return value. If that checks, we have to hoist the
+// span buffer to the parent scope.
+//
+// Transformation is:
+//
+//  %30 = cc.scope -> (!cc.stdvec<i1>) {
+//    ...
+//    %34 = cc.alloca i8[%15 : i64]
+//    ...
+//    %36 = func.call @malloc(%15) : (i64) -> !cc.ptr<i8>
+//    func.call @llvm.memcpy.p0.p0.i64(%36, %34, %35, %false) :
+//      (!cc.ptr<i8>, !cc.ptr<i8>, i64, i1) -> () [dead]
+//    %37 = cc.stdvec_init %36, %15 : (!cc.ptr<i8>, i64) -> !cc.stdvec<i1>
+//    cc.continue %37
+//  }
+//  %37 = cc.stdvec_data %30 : (!stdvec<i1>) -> !cc.ptr<i8>
+//  %38 = cc.stdvec_size %30 : (!stdvec<i1>) -> i64
+//  %39 = cc.alloca i8[%38 : i64]
+//  %40 = cc.cast %39 : (!cc.ptr<!cc.array<i8 x ?>>) -> !cc.ptr<i8>
+//  func.call @llvm.memcpy.p0.p0.i64(%40, %37, %38, %false) :
+//      (!cc.ptr<i8>, !cc.ptr<i8>, i64, i1) -> ()
+//  func.call @free(%37) : (!cc.ptr<i8>) -> ()
+//  ───────────────────────────────────────────────────────────────
+//  %29 = cc.alloca i8[%15 : i64]
+//  %30 = cc.scope ...
+//    ...
+//  func.call @free(%37) : (!cc.ptr<i8>) -> () [dead]
+//  [{ %34 \ %29;  %39 \ %29 }]   // i.e., replace %34 and %39 with %29
+class EraseScopedVectorCopyCtorPattern : public OpRewritePattern<func::CallOp> {
+public:
+  using OpRewritePattern::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(func::CallOp call,
+                                PatternRewriter &rewriter) const override {
+    if (call.getCallee() != "malloc")
+      return failure();
+    auto scope = dyn_cast_if_present<cudaq::cc::ScopeOp>(call->getParentOp());
+    if (!(scope && scope.getResults().size() == 1 &&
+          isa<cudaq::cc::StdvecType>(scope->getResult(0).getType()) &&
+          std::distance(call->getUsers().begin(), call->getUsers().end()) == 2))
+      return failure();
+
+    // Check call (malloc) users.
+    func::CallOp copyFrom;
+    cudaq::cc::StdvecInitOp initOp;
+    for (auto *u : call->getUsers()) {
+      auto c = dyn_cast<func::CallOp>(u);
+      if (c && c.getCallee().starts_with("llvm.memcpy") &&
+          c.getOperand(0) == call.getResult(0) && !copyFrom) {
+        copyFrom = c;
+        continue;
+      }
+      auto io = dyn_cast<cudaq::cc::StdvecInitOp>(u);
+      if (io && !initOp) {
+        initOp = io;
+        continue;
+      }
+      return failure();
+    }
+
+    // Check initOp users.
+    if (!initOp->hasOneUse())
+      return failure();
+    auto contOp = dyn_cast<cudaq::cc::ContinueOp>(*initOp->getUsers().begin());
+    if (!contOp)
+      return failure();
+
+    // Check scope users.
+    if (std::distance(scope->getUsers().begin(), scope->getUsers().end()) != 2)
+      return failure();
+    cudaq::cc::StdvecDataOp data;
+    cudaq::cc::StdvecSizeOp size;
+    for (auto *u : scope->getUsers()) {
+      auto d = dyn_cast<cudaq::cc::StdvecDataOp>(u);
+      if (d && !data) {
+        data = d;
+        continue;
+      }
+      auto s = dyn_cast<cudaq::cc::StdvecSizeOp>(u);
+      if (s && !size) {
+        size = s;
+        continue;
+      }
+      return failure();
+    }
+
+    // Check vec data users.
+    if (std::distance(data->getUsers().begin(), data->getUsers().end()) != 2)
+      return failure();
+    func::CallOp copyTo;
+    func::CallOp freeHeap;
+    for (auto *u : data->getUsers()) {
+      auto c = dyn_cast<func::CallOp>(u);
+      if (c && c.getCallee().starts_with("llvm.memcpy") &&
+          c.getOperand(1) == data.getResult() && !copyTo) {
+        copyTo = c;
+        continue;
+      }
+      if (c && c.getCallee() == "free" && c.getOperand(0) == data.getResult() &&
+          !freeHeap) {
+        freeHeap = c;
+        continue;
+      }
+      return failure();
+    }
+
+    // Find the to-buffer.
+    auto *buff = copyTo.getOperand(0).getDefiningOp();
+    while (auto cast = dyn_cast<cudaq::cc::CastOp>(buff))
+      buff = cast.getValue().getDefiningOp();
+    auto toBuff = dyn_cast<cudaq::cc::AllocaOp>(buff);
+    if (!toBuff)
+      return failure();
+
+    // Hoist the allocation.
+    auto *alloc = copyFrom.getOperand(1).getDefiningOp();
+    while (auto cast = dyn_cast<cudaq::cc::CastOp>(alloc))
+      alloc = cast.getValue().getDefiningOp();
+    auto al = dyn_cast<cudaq::cc::AllocaOp>(alloc);
+    if (!al || al->getNumOperands())
+      return failure();
+    auto newAlloc = [&]() -> cudaq::cc::AllocaOp {
+      OpBuilder::InsertionGuard guard(rewriter);
+      rewriter.setInsertionPoint(scope);
+      return rewriter.replaceOpWithNewOp<cudaq::cc::AllocaOp>(
+          al, al.getElementType());
+    }();
+
+    // Replace the uses.
+    rewriter.replaceOp(toBuff, newAlloc);
+
+    // Remove dead calls.
+    rewriter.eraseOp(copyFrom);
+    rewriter.eraseOp(copyTo);
+    rewriter.eraseOp(freeHeap);
+    rewriter.replaceOpWithNewOp<cudaq::cc::UndefOp>(
+        call, call->getResult(0).getType());
+    return success();
+  }
+};
+
 class EraseVectorCopyCtorPass
     : public cudaq::opt::impl::EraseVectorCopyCtorBase<
           EraseVectorCopyCtorPass> {
@@ -125,7 +269,9 @@ public:
     LLVM_DEBUG(llvm::dbgs() << "Before erasure:\n" << *op << "\n\n");
     auto *ctx = &getContext();
     RewritePatternSet patterns(ctx);
-    patterns.insert<EraseVectorCopyCtorPattern>(ctx);
+    patterns
+        .insert<EraseVectorCopyCtorPattern, EraseScopedVectorCopyCtorPattern>(
+            ctx);
     if (failed(applyPatternsGreedily(op, std::move(patterns))))
       signalPassFailure();
     LLVM_DEBUG(llvm::dbgs() << "After erasure:\n" << *op << "\n\n");
