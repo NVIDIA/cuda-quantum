3cbca2f9e39f45d02ff8685f984a6e36951a1cfa
hanag <hana.giat@quantum-machines.co>
2026-07-01 12:23:27 +0300

    After upstream sync

--- docs/sphinx/targets/python/quantum_machines.py
+++ docs/sphinx/targets/python/quantum_machines.py
@@ -4,9 +4,13 @@ import math
 # The default executor is mock, use executor name to run on another backend (real or simulator).
 # Configure the address of the QOperator server in the `url` argument, and set the `api_key`.
 cudaq.set_target("quantum_machines",
-                 url="http://host.docker.internal:8080",
-                 api_key="1234567890",
-                 executor="mock")
+                 url="http://host.docker.internal:8000",
+                 #url="http://172.16.32.154:8000",
+                 #api_key="1234567890",
+                 #qubit_mapping_mode="backend",
+                 #qubit_mapping_mode="local_file",
+                 qubit_mapping_mode="local_get_latest",
+                 executor="sim")
 
 qubit_count = 5
 
@@ -16,13 +20,23 @@ qubit_count = 5
 def all_h():
     qvector = cudaq.qvector(qubit_count)
 
-    for i in range(qubit_count - 1):
-        h(qvector[i])
+    for i in range(8):
+        #h(qvector[i])
+        h(qvector[0])
+        h(qvector[1])
+        h(qvector[2])
+    # mz(qvector[0])
+    # mz(qvector[0])
 
-    s(qvector[0])
-    r1(math.pi / 2, qvector[1])
-    mz(qvector)
+    rz(math.pi / 2, qvector[0])
 
+# Define the Hamiltonian (Observable)
+#hamiltonian = 5.0 * cudaq.spin.z(0)
+
+# 2. Observe the observable (Expectation value)
+#result = cudaq.observe(all_h, hamiltonian)
+#print(f"Expectation Value: {result.expectation()}")
 
 # Submit synchronously
-cudaq.sample(all_h).dump()
+cudaq.sample(all_h, shots_count=4).dump()
+
