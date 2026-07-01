40761a382408865dba958c08ddf816a3a6ec13ab
Thien Nguyen <thiennguyen@nvidia.com>
2026-07-01 15:56:51 +1000

    Add realtime to deployment images
    Signed-off-by: Thien Nguyen <thiennguyen@nvidia.com>

--- realtime/include/cudaq/realtime/daemon/dispatcher/host_dispatcher.h
+++ realtime/include/cudaq/realtime/daemon/dispatcher/host_dispatcher.h
@@ -1,9 +1,9 @@
-/*******************************************************************************
- * Copyright (c) 2026 NVIDIA Corporation & Affiliates.
- * All rights reserved.
- *
- * This source code and the accompanying materials are made available under
- * the terms of the Apache License 2.0 which accompanies this distribution.
+/****************************************************************-*- C++ -*-****
+ * Copyright (c) 2026 NVIDIA Corporation & Affiliates.                         *
+ * All rights reserved.                                                        *
+ *                                                                             *
+ * This source code and the accompanying materials are made available under    *
+ * the terms of the Apache License 2.0 which accompanies this distribution.    *
  ******************************************************************************/
 
 #pragma once
@@ -16,7 +16,7 @@
 
 #ifndef CUDAQ_REALTIME_CPU_RELAX
 #if defined(__x86_64__)
-#include <immintrin.h>
+#include <xmmintrin.h> // _mm_pause()
 #define CUDAQ_REALTIME_CPU_RELAX() _mm_pause()
 #elif defined(__aarch64__)
 #define CUDAQ_REALTIME_CPU_RELAX() __asm__ volatile("yield" ::: "memory")
