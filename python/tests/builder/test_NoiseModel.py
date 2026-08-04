4f8c8469f858c79498ba35e5847846894d8c0e5f
Eric Schweitz <eschweitz@nvidia.com>
2026-08-04 12:15:42 -0700

    [python] Re-enable some of the disabled tests.
    By hook or by crook, we want to re-enable our regression tests.
    
    Re-enable more tests.
    
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>

--- python/tests/builder/test_NoiseModel.py
+++ python/tests/builder/test_NoiseModel.py
@@ -761,17 +761,13 @@ def test_apply_noise_builtin(target: str):
     cudaq.reset_target()
 
 
-skipIfValueSemantics = pytest.mark.skipif(True,
-                                          reason="broken in value semantics")
-
-
-@skipIfValueSemantics
 @pytest.mark.parametrize('target', ['density-matrix-cpu', 'stim'])
 def test_depolarization2_standard_formula(target: str):
     cudaq.set_target(target)
     cudaq.set_random_seed(42)
 
-    @cudaq.kernel
+    # x.ctrl gates can be eliminated by optimization
+    @cudaq.kernel(disable_quantum_optimization=True)
     def cnot_echo():
         q = cudaq.qvector(2)
         x.ctrl(q[0], q[1])
