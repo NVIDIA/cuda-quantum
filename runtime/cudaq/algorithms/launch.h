d82223a88bf742d35af8617a0eaafefacfe89135
Luca Mondada <luca@mondada.net>
2026-08-10 16:03:41 +0200

    check for analysis scope re-entry in launch.h
    Signed-off-by: Luca Mondada <luca@mondada.net>

--- runtime/cudaq/algorithms/launch.h
+++ runtime/cudaq/algorithms/launch.h
@@ -104,6 +104,12 @@ auto launch(const Policy &policy, std::size_t qpu_id, ExecutionContext &ctx,
   else
     CUDAQ_INFO("Launching kernel in sync mode with policy {}", policy.name);
 
+  if constexpr (!std::is_same_v<Policy, dem_policy>) {
+    if (nvqir::AnalysisScope::is_active()) {
+      throw std::runtime_error(
+          "Invalid kernel launch within an active AnalysisScope");
+    }
+  }
   detail::try_finally(
       [&] {
         detail::with_policy_and_ctx(policy, ctx, std::forward<Callable>(f),
