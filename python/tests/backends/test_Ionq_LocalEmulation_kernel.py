db262977759db3df18d77c5bd9a82b57a27bace5
Eric Schweitz <eschweitz@nvidia.com>
2026-08-16 15:21:25 -0700

    Fix one bug and confirm pauli is broken bad.
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>

--- python/tests/backends/test_Ionq_LocalEmulation_kernel.py
+++ python/tests/backends/test_Ionq_LocalEmulation_kernel.py
@@ -112,7 +112,7 @@ def test_Ionq_state_synthesis():
     s = cudaq.get_state(init, 2)
     s = cudaq.get_state(kernel, s)
     counts = cudaq.sample(kernel, s)
-    assert '1' in counts
+    assert '10' in counts
     assert len(counts) == 1
 
 
