dc28bc2ff102ad7d7d3ea6affa1949536a8a87e2
Anthony Cabrera <antcabrera@nvidia.com>
2026-08-30 15:51:53 +0000

    remove duplicate header
    Signed-off-by: Anthony Cabrera <antcabrera@nvidia.com>

--- cudaq/lib/Optimizer/Transforms/PhaseUtilities.h
+++ cudaq/lib/Optimizer/Transforms/PhaseUtilities.h
@@ -6,14 +6,6 @@
  * the terms of the Apache License 2.0 which accompanies this distribution.    *
  ******************************************************************************/
 
-/*******************************************************************************
- * Copyright (c) 2026 NVIDIA Corporation & Affiliates.                         *
- * All rights reserved.                                                        *
- *                                                                             *
- * This source code and the accompanying materials are made available under    *
- * the terms of the Apache License 2.0 which accompanies this distribution.    *
- *******************************************************************************/
-
 #pragma once
 
 #include "cudaq/Optimizer/Dialect/Quake/QuakeOps.h"
