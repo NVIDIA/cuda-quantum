cc3b96d5d88dade755250a6a2a5001688275f2d8
Adam T. Geller <adgeller@nvidia.com>
2026-08-31 08:38:12 +0000

    Add flag to allow endpoints to avoid JIT engine creation at runtime (#5320)
    Signed-off-by: Adam Geller <adgeller@nvidia.com>
    Signed-off-by: Luca Mondada <luca@mondada.net>
    Co-authored-by: Luca Mondada <luca@mondada.net>

--- runtime/cudaq/algorithms/launch.h
+++ runtime/cudaq/algorithms/launch.h
@@ -89,6 +89,8 @@ auto launch(const Policy &policy, std::size_t qpu_id, ExecutionContext &ctx,
       const bool isRemote = platform.is_remote(qpu_id);
       options.emulate = isEmulated;
       options.emitJit |= !isRemote;
+      if (!platform.supports_jit(qpu_id))
+        options.emitJit = false;
       options.boolVecBitPacked = !isRemote && !isEmulated;
       compiled = cudaq_internal::compiler::compileModule(target, options,
                                                          *source, args,
