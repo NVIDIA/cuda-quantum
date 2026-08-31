134d23512a44b40ef82e5a05cbb421ce354d013f
Pradnya Khalate <pkhalate@nvidia.com>
2026-08-31 15:49:11 -0700

    Add a test
    Signed-off-by: Pradnya Khalate <pkhalate@nvidia.com>

--- cudaq/test/NVQPP/obj_file.cpp
+++ cudaq/test/NVQPP/obj_file.cpp
@@ -6,8 +6,11 @@
  * the terms of the Apache License 2.0 which accompanies this distribution.    *
  ******************************************************************************/
 
-// RUN: nvq++ -c %s -o %t && [ -e %t ]
+// RUN: nvq++ -c %s -o %t 2>&1 | FileCheck %s
+// RUN: [ -e %t ]
 
 #include <cudaq.h>
 
 int plain_old_function() { return 0; }
+
+// CHECK: warning: The CUDA-Q `sample` and `observe` algorithmic primitives will change in a future release.
