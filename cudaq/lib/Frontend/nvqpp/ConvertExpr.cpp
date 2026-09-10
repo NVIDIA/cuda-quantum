bfb0dd356b51902cab0772168a9e1823c0132f4a
Sachin Pisal <spisal@nvidia.com>
2026-09-10 10:57:42 -0700

    loading comma expression result when it is a prvalue
    Signed-off-by: Sachin Pisal <spisal@nvidia.com>

--- cudaq/lib/Frontend/nvqpp/ConvertExpr.cpp
+++ cudaq/lib/Frontend/nvqpp/ConvertExpr.cpp
@@ -1144,7 +1144,8 @@ bool QuakeBridgeVisitor::VisitBinaryOperator(clang::BinaryOperator *x) {
   case clang::BinaryOperatorKind::BO_AndAssign:
     return true; // see CompoundAssignOperator
   case clang::BinaryOperatorKind::BO_Comma:
-    return pushValue(rhs);
+    // A comma expression is an lvalue exactly when its rhs is one.
+    return pushValue(x->isGLValue() ? rhs : maybeLoadValue(rhs));
   default:
     break;
   }
--- cudaq/test/Frontend/comma_expression.cpp
+++ cudaq/test/Frontend/comma_expression.cpp
@@ -12,11 +12,53 @@
 
 // A comma expression evaluates its lhs for side-effects only and takes its
 // value from the rhs.
-struct multi_counter_step {
+
+// Two induction variables, stepped with a comma.
+struct comma_in_for_loop {
   void operator()() __qpu__ {
     cudaq::qvector q(2);
     for (int i = 0, j = 3; i < 4; i++, j--)
       h(q[0]);
+  }
+};
+
+// clang-format off
+// CHECK-LABEL:   func.func @__nvqpp__mlirgen__comma_in_for_loop() attributes {"cudaq-entrypoint", "cudaq-kernel"} {
+// CHECK:           %[[VAL_0:.*]] = arith.constant 1 : i32
+// CHECK:           %[[VAL_1:.*]] = arith.constant 4 : i32
+// CHECK:           %[[VAL_2:.*]] = arith.constant 3 : i32
+// CHECK:           %[[VAL_3:.*]] = arith.constant 0 : i32
+// CHECK:           %[[VAL_4:.*]] = quake.alloca !quake.veq<2>
+// CHECK:           cc.scope {
+// CHECK:             %[[VAL_5:.*]] = cc.alloca i32
+// CHECK:             cc.store %[[VAL_3]], %[[VAL_5]] : !cc.ptr<i32>
+// CHECK:             %[[VAL_6:.*]] = cc.alloca i32
+// CHECK:             cc.store %[[VAL_2]], %[[VAL_6]] : !cc.ptr<i32>
+// CHECK:             cc.loop while {
+// CHECK:               %[[VAL_7:.*]] = cc.load %[[VAL_5]] : !cc.ptr<i32>
+// CHECK:               %[[VAL_8:.*]] = arith.cmpi slt, %[[VAL_7]], %[[VAL_1]] : i32
+// CHECK:               cc.condition %[[VAL_8]]
+// CHECK:             } do {
+// CHECK:               %[[VAL_9:.*]] = quake.extract_ref %[[VAL_4]][0] : (!quake.veq<2>) -> !quake.ref
+// CHECK:               quake.h %[[VAL_9]] : (!quake.ref) -> ()
+// CHECK:               cc.continue
+// CHECK:             } step {
+// CHECK:               %[[VAL_10:.*]] = cc.load %[[VAL_5]] : !cc.ptr<i32>
+// CHECK:               %[[VAL_11:.*]] = arith.addi %[[VAL_10]], %[[VAL_0]] : i32
+// CHECK:               cc.store %[[VAL_11]], %[[VAL_5]] : !cc.ptr<i32>
+// CHECK:               %[[VAL_12:.*]] = cc.load %[[VAL_6]] : !cc.ptr<i32>
+// CHECK:               %[[VAL_13:.*]] = arith.subi %[[VAL_12]], %[[VAL_0]] : i32
+// CHECK:               cc.store %[[VAL_13]], %[[VAL_6]] : !cc.ptr<i32>
+// CHECK:             }
+// CHECK:           }
+// CHECK:           return
+// CHECK:         }
+// clang-format on
+
+// The value is the rhs, `b + 3`.
+struct comma_takes_rhs_value {
+  void operator()() __qpu__ {
+    cudaq::qvector q(2);
     int a = 0, b = 0;
     a = (b = 2, b + 3);
     if (a == 5)
@@ -25,49 +67,194 @@ struct multi_counter_step {
 };
 
 // clang-format off
-// CHECK-LABEL:   func.func @__nvqpp__mlirgen__multi_counter_step() attributes
-// CHECK-DAG:       %[[VAL_0:.*]] = arith.constant 5 : i32
-// CHECK-DAG:       %[[VAL_1:.*]] = arith.constant 1 : i32
-// CHECK-DAG:       %[[VAL_2:.*]] = arith.constant 4 : i32
-// CHECK-DAG:       %[[VAL_3:.*]] = arith.constant 3 : i32
-// CHECK-DAG:       %[[VAL_4:.*]] = arith.constant 0 : i32
-// CHECK-DAG:       %[[VAL_5:.*]] = arith.constant 2 : i32
-// CHECK:           %[[VAL_6:.*]] = quake.alloca !quake.veq<2>
-// CHECK:           cc.scope {
-// CHECK:             %[[VAL_7:.*]] = cc.alloca i32
-// CHECK:             cc.store %[[VAL_4]], %[[VAL_7]] : !cc.ptr<i32>
-// CHECK:             %[[VAL_8:.*]] = cc.alloca i32
-// CHECK:             cc.store %[[VAL_3]], %[[VAL_8]] : !cc.ptr<i32>
-// CHECK:             cc.loop while {
-// CHECK:               %[[VAL_9:.*]] = cc.load %[[VAL_7]] : !cc.ptr<i32>
-// CHECK:               %[[VAL_10:.*]] = arith.cmpi slt, %[[VAL_9]], %[[VAL_2]] : i32
-// CHECK:               cc.condition %[[VAL_10]]
-// CHECK:             } do {
-// CHECK:               %[[VAL_11:.*]] = quake.extract_ref %[[VAL_6]][0] : (!quake.veq<2>) -> !quake.ref
-// CHECK:               quake.h %[[VAL_11]] : (!quake.ref) -> ()
-// CHECK:               cc.continue
-// CHECK:             } step {
-// CHECK:               %[[VAL_12:.*]] = cc.load %[[VAL_7]] : !cc.ptr<i32>
-// CHECK:               %[[VAL_13:.*]] = arith.addi %[[VAL_12]], %[[VAL_1]] : i32
-// CHECK:               cc.store %[[VAL_13]], %[[VAL_7]] : !cc.ptr<i32>
-// CHECK:               %[[VAL_14:.*]] = cc.load %[[VAL_8]] : !cc.ptr<i32>
-// CHECK:               %[[VAL_15:.*]] = arith.subi %[[VAL_14]], %[[VAL_1]] : i32
-// CHECK:               cc.store %[[VAL_15]], %[[VAL_8]] : !cc.ptr<i32>
-// CHECK:             }
-// CHECK:           }
-// CHECK:           %[[VAL_16:.*]] = cc.alloca i32
-// CHECK:           cc.store %[[VAL_4]], %[[VAL_16]] : !cc.ptr<i32>
-// CHECK:           %[[VAL_17:.*]] = cc.alloca i32
-// CHECK:           cc.store %[[VAL_4]], %[[VAL_17]] : !cc.ptr<i32>
-// CHECK:           cc.store %[[VAL_5]], %[[VAL_17]] : !cc.ptr<i32>
-// CHECK:           %[[VAL_18:.*]] = cc.load %[[VAL_17]] : !cc.ptr<i32>
-// CHECK:           %[[VAL_19:.*]] = arith.addi %[[VAL_18]], %[[VAL_3]] : i32
-// CHECK:           cc.store %[[VAL_19]], %[[VAL_16]] : !cc.ptr<i32>
-// CHECK:           %[[VAL_20:.*]] = cc.load %[[VAL_16]] : !cc.ptr<i32>
-// CHECK:           %[[VAL_21:.*]] = arith.cmpi eq, %[[VAL_20]], %[[VAL_0]] : i32
-// CHECK:           cc.if(%[[VAL_21]]) {
-// CHECK:             %[[VAL_22:.*]] = quake.extract_ref %[[VAL_6]][1] : (!quake.veq<2>) -> !quake.ref
-// CHECK:             quake.x %[[VAL_22]] : (!quake.ref) -> ()
+// CHECK-LABEL:   func.func @__nvqpp__mlirgen__comma_takes_rhs_value() attributes {"cudaq-entrypoint", "cudaq-kernel"} {
+// CHECK:           %[[VAL_0:.*]] = arith.constant 5 : i32
+// CHECK:           %[[VAL_1:.*]] = arith.constant 3 : i32
+// CHECK:           %[[VAL_2:.*]] = arith.constant 0 : i32
+// CHECK:           %[[VAL_3:.*]] = arith.constant 2 : i32
+// CHECK:           %[[VAL_4:.*]] = quake.alloca !quake.veq<2>
+// CHECK:           %[[VAL_5:.*]] = cc.alloca i32
+// CHECK:           cc.store %[[VAL_2]], %[[VAL_5]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_6:.*]] = cc.alloca i32
+// CHECK:           cc.store %[[VAL_2]], %[[VAL_6]] : !cc.ptr<i32>
+// CHECK:           cc.store %[[VAL_3]], %[[VAL_6]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_7:.*]] = cc.load %[[VAL_6]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_8:.*]] = arith.addi %[[VAL_7]], %[[VAL_1]] : i32
+// CHECK:           cc.store %[[VAL_8]], %[[VAL_5]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_9:.*]] = cc.load %[[VAL_5]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_10:.*]] = arith.cmpi eq, %[[VAL_9]], %[[VAL_0]] : i32
+// CHECK:           cc.if(%[[VAL_10]]) {
+// CHECK:             %[[VAL_11:.*]] = quake.extract_ref %[[VAL_4]][1] : (!quake.veq<2>) -> !quake.ref
+// CHECK:             quake.x %[[VAL_11]] : (!quake.ref) -> ()
 // CHECK:           }
 // CHECK:           return
 // CHECK:         }
+// clang-format on
+
+// An lvalue rhs used as a value the load.
+struct comma_with_lvalue_rhs {
+  void operator()() __qpu__ {
+    cudaq::qvector q(2);
+    int a = 0, b = 0;
+    a = (b = 2, b);
+    if (a == 2)
+      x(q[1]);
+  }
+};
+
+// clang-format off
+// CHECK-LABEL:   func.func @__nvqpp__mlirgen__comma_with_lvalue_rhs() attributes {"cudaq-entrypoint", "cudaq-kernel"} {
+// CHECK:           %[[VAL_0:.*]] = arith.constant 0 : i32
+// CHECK:           %[[VAL_1:.*]] = arith.constant 2 : i32
+// CHECK:           %[[VAL_2:.*]] = quake.alloca !quake.veq<2>
+// CHECK:           %[[VAL_3:.*]] = cc.alloca i32
+// CHECK:           cc.store %[[VAL_0]], %[[VAL_3]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_4:.*]] = cc.alloca i32
+// CHECK:           cc.store %[[VAL_0]], %[[VAL_4]] : !cc.ptr<i32>
+// CHECK:           cc.store %[[VAL_1]], %[[VAL_4]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_5:.*]] = cc.load %[[VAL_4]] : !cc.ptr<i32>
+// CHECK:           cc.store %[[VAL_5]], %[[VAL_3]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_6:.*]] = cc.load %[[VAL_3]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_7:.*]] = arith.cmpi eq, %[[VAL_6]], %[[VAL_1]] : i32
+// CHECK:           cc.if(%[[VAL_7]]) {
+// CHECK:             %[[VAL_8:.*]] = quake.extract_ref %[[VAL_2]][1] : (!quake.veq<2>) -> !quake.ref
+// CHECK:             quake.x %[[VAL_8]] : (!quake.ref) -> ()
+// CHECK:           }
+// CHECK:           return
+// CHECK:         }
+// clang-format on
+
+// An lvalue rhs assigned through the address.
+struct comma_result_is_assignable {
+  void operator()() __qpu__ {
+    cudaq::qvector q(2);
+    int a = 0, b = 0;
+    (a, b) = 5;
+    if (b == 5)
+      x(q[1]);
+  }
+};
+
+// clang-format off
+// CHECK-LABEL:   func.func @__nvqpp__mlirgen__comma_result_is_assignable() attributes {"cudaq-entrypoint", "cudaq-kernel"} {
+// CHECK:           %[[VAL_0:.*]] = arith.constant 5 : i32
+// CHECK:           %[[VAL_1:.*]] = arith.constant 0 : i32
+// CHECK:           %[[VAL_2:.*]] = quake.alloca !quake.veq<2>
+// CHECK:           %[[VAL_3:.*]] = cc.alloca i32
+// CHECK:           cc.store %[[VAL_1]], %[[VAL_3]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_4:.*]] = cc.alloca i32
+// CHECK:           cc.store %[[VAL_1]], %[[VAL_4]] : !cc.ptr<i32>
+// CHECK:           cc.store %[[VAL_0]], %[[VAL_4]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_5:.*]] = cc.load %[[VAL_4]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_6:.*]] = arith.cmpi eq, %[[VAL_5]], %[[VAL_0]] : i32
+// CHECK:           cc.if(%[[VAL_6]]) {
+// CHECK:             %[[VAL_7:.*]] = quake.extract_ref %[[VAL_2]][1] : (!quake.veq<2>) -> !quake.ref
+// CHECK:             quake.x %[[VAL_7]] : (!quake.ref) -> ()
+// CHECK:           }
+// CHECK:           return
+// CHECK:         }
+// clang-format on
+
+// Left associative (a chain yields its last operand).
+struct comma_chain {
+  void operator()() __qpu__ {
+    cudaq::qvector q(2);
+    int a = 0, b = 0, c = 0;
+    a = (b = 1, c = 2, b + c);
+    if (a == 3)
+      x(q[1]);
+  }
+};
+
+// clang-format off
+// CHECK-LABEL:   func.func @__nvqpp__mlirgen__comma_chain() attributes {"cudaq-entrypoint", "cudaq-kernel"} {
+// CHECK:           %[[VAL_0:.*]] = arith.constant 3 : i32
+// CHECK:           %[[VAL_1:.*]] = arith.constant 1 : i32
+// CHECK:           %[[VAL_2:.*]] = arith.constant 0 : i32
+// CHECK:           %[[VAL_3:.*]] = arith.constant 2 : i32
+// CHECK:           %[[VAL_4:.*]] = quake.alloca !quake.veq<2>
+// CHECK:           %[[VAL_5:.*]] = cc.alloca i32
+// CHECK:           cc.store %[[VAL_2]], %[[VAL_5]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_6:.*]] = cc.alloca i32
+// CHECK:           cc.store %[[VAL_2]], %[[VAL_6]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_7:.*]] = cc.alloca i32
+// CHECK:           cc.store %[[VAL_2]], %[[VAL_7]] : !cc.ptr<i32>
+// CHECK:           cc.store %[[VAL_1]], %[[VAL_6]] : !cc.ptr<i32>
+// CHECK:           cc.store %[[VAL_3]], %[[VAL_7]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_8:.*]] = cc.load %[[VAL_6]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_9:.*]] = cc.load %[[VAL_7]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_10:.*]] = arith.addi %[[VAL_8]], %[[VAL_9]] : i32
+// CHECK:           cc.store %[[VAL_10]], %[[VAL_5]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_11:.*]] = cc.load %[[VAL_5]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_12:.*]] = arith.cmpi eq, %[[VAL_11]], %[[VAL_0]] : i32
+// CHECK:           cc.if(%[[VAL_12]]) {
+// CHECK:             %[[VAL_13:.*]] = quake.extract_ref %[[VAL_4]][1] : (!quake.veq<2>) -> !quake.ref
+// CHECK:             quake.x %[[VAL_13]] : (!quake.ref) -> ()
+// CHECK:           }
+// CHECK:           return
+// CHECK:         }
+// clang-format on
+
+// Lowest precedence: `a = 1, a = 2` is `(a = 1), (a = 2)`.
+struct comma_binds_looser_than_assign {
+  void operator()() __qpu__ {
+    cudaq::qvector q(2);
+    int a = 0;
+    a = 1, a = 2;
+    if (a == 2)
+      x(q[1]);
+  }
+};
+
+// clang-format off
+// CHECK-LABEL:   func.func @__nvqpp__mlirgen__comma_binds_looser_than_assign() attributes {"cudaq-entrypoint", "cudaq-kernel"} {
+// CHECK:           %[[VAL_0:.*]] = arith.constant 1 : i32
+// CHECK:           %[[VAL_1:.*]] = arith.constant 0 : i32
+// CHECK:           %[[VAL_2:.*]] = arith.constant 2 : i32
+// CHECK:           %[[VAL_3:.*]] = quake.alloca !quake.veq<2>
+// CHECK:           %[[VAL_4:.*]] = cc.alloca i32
+// CHECK:           cc.store %[[VAL_1]], %[[VAL_4]] : !cc.ptr<i32>
+// CHECK:           cc.store %[[VAL_0]], %[[VAL_4]] : !cc.ptr<i32>
+// CHECK:           cc.store %[[VAL_2]], %[[VAL_4]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_5:.*]] = cc.load %[[VAL_4]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_6:.*]] = arith.cmpi eq, %[[VAL_5]], %[[VAL_2]] : i32
+// CHECK:           cc.if(%[[VAL_6]]) {
+// CHECK:             %[[VAL_7:.*]] = quake.extract_ref %[[VAL_3]][1] : (!quake.veq<2>) -> !quake.ref
+// CHECK:             quake.x %[[VAL_7]] : (!quake.ref) -> ()
+// CHECK:           }
+// CHECK:           return
+// CHECK:         }
+// clang-format on
+
+// Parenthesized, the comma is the assignment's rhs, so `a` never sees 1.
+struct comma_as_assign_operand {
+  void operator()() __qpu__ {
+    cudaq::qvector q(2);
+    int a = 0, b = 0;
+    a = (b = 1, 2);
+    if (a == 2)
+      x(q[1]);
+  }
+};
+
+// clang-format off
+// CHECK-LABEL:   func.func @__nvqpp__mlirgen__comma_as_assign_operand() attributes {"cudaq-entrypoint", "cudaq-kernel"} {
+// CHECK:           %[[VAL_0:.*]] = arith.constant 1 : i32
+// CHECK:           %[[VAL_1:.*]] = arith.constant 0 : i32
+// CHECK:           %[[VAL_2:.*]] = arith.constant 2 : i32
+// CHECK:           %[[VAL_3:.*]] = quake.alloca !quake.veq<2>
+// CHECK:           %[[VAL_4:.*]] = cc.alloca i32
+// CHECK:           cc.store %[[VAL_1]], %[[VAL_4]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_5:.*]] = cc.alloca i32
+// CHECK:           cc.store %[[VAL_1]], %[[VAL_5]] : !cc.ptr<i32>
+// CHECK:           cc.store %[[VAL_0]], %[[VAL_5]] : !cc.ptr<i32>
+// CHECK:           cc.store %[[VAL_2]], %[[VAL_4]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_6:.*]] = cc.load %[[VAL_4]] : !cc.ptr<i32>
+// CHECK:           %[[VAL_7:.*]] = arith.cmpi eq, %[[VAL_6]], %[[VAL_2]] : i32
+// CHECK:           cc.if(%[[VAL_7]]) {
+// CHECK:             %[[VAL_8:.*]] = quake.extract_ref %[[VAL_3]][1] : (!quake.veq<2>) -> !quake.ref
+// CHECK:             quake.x %[[VAL_8]] : (!quake.ref) -> ()
+// CHECK:           }
+// CHECK:           return
+// CHECK:         }
+// clang-format on
